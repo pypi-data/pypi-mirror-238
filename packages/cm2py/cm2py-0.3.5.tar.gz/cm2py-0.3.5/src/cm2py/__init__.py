3dd2df470b06cf1da161e8800f9918d22178ea84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:07:20 +0000

    Add package cm2py/0.3.5/cm2py-0.3.5.tar.gz

---
+++ packages/cm2py/cm2py-0.3.5.tar.gz/cm2py-0.3.5/src/cm2py/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Pre-defined blockId definitions
+NOR = 0
+AND = 1
+OR = 2
+XOR = 3
+BUTTON = 4
+FLIPFLOP = 5
+LED = 6
+SOUND = 7
+CONDUCTOR = 8
+CUSTOM = 9
+NAND = 10
+XNOR = 11
+RANDOM = 12
+TEXT = 13
+TILE = 14
+
+from .cm2py import *
