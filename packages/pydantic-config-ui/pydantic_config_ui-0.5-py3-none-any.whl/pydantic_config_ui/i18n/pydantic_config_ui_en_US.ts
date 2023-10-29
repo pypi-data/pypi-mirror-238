10428286a6d4fd250fdbe95b684682604f603aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:08:34 +0000

    Add package pydantic-config-ui/0.5/pydantic_config_ui-0.5-py3-none-any.whl

---
+++ packages/pydantic-config-ui/pydantic_config_ui-0.5-py3-none-any.whl/pydantic_config_ui/i18n/pydantic_config_ui_en_US.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>ConfigBaseDialog</name>
+    <message>
+        <location filename="../config_dialog.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../config_dialog.ui" line="53"/>
+        <source>Title for the Configuration</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../config_dialog.ui" line="73"/>
+        <source>Optional Logo</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../config_dialog.ui" line="87"/>
+        <source>Arguments</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../config_dialog.ui" line="158"/>
+        <source>Save As</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../config_dialog.ui" line="165"/>
+        <source>Load</source>
+        <translation>Load</translation>
+    </message>
+    <message>
+        <location filename="../config_dialog.ui" line="185"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../config_dialog.ui" line="192"/>
+        <source>Ok</source>
+        <translation>Ready</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigurationEditor</name>
+    <message>
+        <location filename="../configuration_editor.py" line="124"/>
+        <source>configuration file &apos;{0}&apos; not found</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_editor.py" line="135"/>
+        <source>Configuration contains Errors</source>
+        <translation>Configuration contains mistakes</translation>
+    </message>
+    <message>
+        <location filename="../configuration_editor.py" line="136"/>
+        <source>Please correct before loading again.</source>
+        <translation>Please correct errors before loading again.</translation>
+    </message>
+    <message>
+        <location filename="../configuration_editor.py" line="186"/>
+        <source>Save Configuration</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_editor.py" line="197"/>
+        <source>Read Configuration</source>
+        <translation>Load Configuration</translation>
+    </message>
+    <message>
+        <location filename="../configuration_editor.py" line="225"/>
+        <source>Missing Configuration</source>
+        <translation>Missing Configuration</translation>
+    </message>
+    <message>
+        <location filename="../configuration_editor.py" line="226"/>
+        <source>Remove &apos;{0}&apos; from recent list?</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FieldFileSelect</name>
+    <message>
+        <location filename="../field_file_select.py" line="46"/>
+        <source>Select File</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FieldFolderSelect</name>
+    <message>
+        <location filename="../field_folder_select.py" line="17"/>
+        <source>select a folder</source>
+        <translation>select a folder from your system</translation>
+    </message>
+    <message>
+        <location filename="../field_folder_select.py" line="26"/>
+        <source>Select Folder</source>
+        <translation>Select Folder</translation>
+    </message>
+</context>
+<context>
+    <name>MainConfiguration</name>
+    <message>
+        <location filename="../configuration_exec.py" line="83"/>
+        <source>Clean Log</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="84"/>
+        <source>Edit Configuration</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="85"/>
+        <source>Execute</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="86"/>
+        <source>Finish</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="146"/>
+        <source>config edit canceled -- no execution possible</source>
+        <translation>edit canceled -- nothing will be executed</translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="163"/>
+        <source>Process is running.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="164"/>
+        <source>Really stop the process?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="166"/>
+        <source>Yes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="167"/>
+        <source>No</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="172"/>
+        <source>request thread interruption</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MyLongThread</name>
+    <message>
+        <location filename="../configuration_exec.py" line="194"/>
+        <source>Missing configuration</source>
+        <translation>without configuration we do nothing</translation>
+    </message>
+    <message>
+        <location filename="../configuration_exec.py" line="197"/>
+        <source>Ready</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
