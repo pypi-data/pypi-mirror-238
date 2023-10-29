79360405956ad255b6e3ec8261a13bd4535dd4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:06:35 +0000

    Add package mangoplateapi/1.0.13/mangoplateapi-1.0.13-py3-none-any.whl

---
+++ packages/mangoplateapi/mangoplateapi-1.0.13-py3-none-any.whl/mangoplateapi/config.py
@@ -0,0 +1,6 @@
+HEADERS = {
+    'referer' : 'https://www.mangoplate.com',
+    'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+}
+
+MAX_PAGE = 2000
