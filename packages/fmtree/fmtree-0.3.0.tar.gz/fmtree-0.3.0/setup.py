c08e844a3768ce9ada8bb76433977008d52c4697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:08:00 +0000

    Add package fmtree/0.3.0/fmtree-0.3.0.tar.gz

---
+++ packages/fmtree/fmtree-0.3.0.tar.gz/fmtree-0.3.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fmtree",
+    version="0.3.0",
+    author="Huakun Shen",
+    author_email="huakun.shen@huakunshen.com",
+    description="Scrape File System and output in different formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fmtree-dev/fmtree",
+    project_urls={
+        "Bug Tracker": "https://github.com/fmtree-dev/fmtree/issues",
+        "Documentation": "https://fmtree-dev.github.io/fmtree/"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+    package_data={
+        'fmtree': [
+            "visualizer/template/*",
+            "visualizer/template/assets/*",
+        ]
+    }
+)
+
+# python -m build
+# python -m twine upload dist/* --verbose --repository testpypi
