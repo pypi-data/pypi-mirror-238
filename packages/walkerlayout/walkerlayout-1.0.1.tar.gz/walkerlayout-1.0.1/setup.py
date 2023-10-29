f3b37e1a7d43c2d30466b567c0bbf4442f214a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:06:28 +0000

    Add package walkerlayout/1.0.1/walkerlayout-1.0.1.tar.gz

---
+++ packages/walkerlayout/walkerlayout-1.0.1.tar.gz/walkerlayout-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+# Read the content of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='walkerlayout',
+    version='1.0.1',
+    description='A time-linear Python implementation of Walker\'s algorithm for level-based tree layouting/drawing',
+    author='Elias Foramitti',
+    author_email='elias@foramitti.com',
+    packages=['walkerlayout'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    project_urls={
+        'GitHub': 'https://github.com/EliasLF/walkerlayout',
+    },
+)
