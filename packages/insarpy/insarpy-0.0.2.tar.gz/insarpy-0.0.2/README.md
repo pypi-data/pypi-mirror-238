9ac827aa1b424f81b48acdeb6c628d50101962c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:08:06 +0000

    Add package insarpy/0.0.2/insarpy-0.0.2.tar.gz

---
+++ packages/insarpy/insarpy-0.0.2.tar.gz/insarpy-0.0.2/README.md
@@ -0,0 +1,17 @@
+# insarpy
+
+
+[![image](https://img.shields.io/pypi/v/insarpy.svg)](https://pypi.python.org/pypi/insarpy)
+[![image](https://img.shields.io/conda/vn/conda-forge/insarpy.svg)](https://anaconda.org/conda-forge/insarpy)
+
+
+**A python package for interactive mapping.**
+
+
+-   Free software: MIT license
+-   Documentation: https://Abubakr-Abdelrahman.github.io/insarpy
+    
+
+## Features
+
+-   TODO
---
+++ packages/insarpy/insarpy-0.0.2.tar.gz/insarpy-0.0.2/tests/test_insarpy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `insarpy` package."""
+
+
+import unittest
+
+from insarpy import insarpy
+
+
+class TestInsarpy(unittest.TestCase):
+    """Tests for `insarpy` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
