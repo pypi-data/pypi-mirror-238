c2308763640b5927d81f05828b45dba2dd7ff8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 15:04:27 +0000

    Add package djangorestframework-nimbus/4.4.5/djangorestframework_nimbus-4.4.5-py2.py3-none-any.whl

---
+++ packages/djangorestframework-nimbus/djangorestframework_nimbus-4.4.5-py2.py3-none-any.whl/rest_framework_nimbus/renderers/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from .base import JSONRenderer, UTF8JSONRenderer, JSONPRenderer, BaseAPIRenderer
+from .api import APIRenderer
