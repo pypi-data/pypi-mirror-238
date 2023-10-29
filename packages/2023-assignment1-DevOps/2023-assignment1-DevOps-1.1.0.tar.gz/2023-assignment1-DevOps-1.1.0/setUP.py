e889c6bb65880933f6aa5c41b20cddaad5862615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 15:05:02 +0000

    Add package 2023-assignment1-DevOps/1.1.0/2023-assignment1-DevOps-1.1.0.tar.gz

---
+++ packages/2023-assignment1-DevOps/2023-assignment1-DevOps-1.1.0.tar.gz/2023-assignment1-DevOps-1.1.0/setUP.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='2023-assignment1-DevOps',
+    version='1.1.0',
+    description='Assignment1',
+    author='GruppoELA',
+    author_email='l.perfetti2001@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pymongo',
+        'pytest', 
+        'prospector',
+        'bandit',
+        'mkdocs',
+    ],
+)
