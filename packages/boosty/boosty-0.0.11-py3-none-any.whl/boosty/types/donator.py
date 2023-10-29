31502bc04c7e270155ab639e4d8ab86723fa7089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:08:02 +0000

    Add package boosty/0.0.11/boosty-0.0.11-py3-none-any.whl

---
+++ packages/boosty/boosty-0.0.11-py3-none-any.whl/boosty/types/donator.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from .base import BaseObject
+from .users import DonatorUser
+
+
+class Donator(BaseObject):
+    id: int
+    bloggerId: int
+    targetId: int
+    createdAt: datetime
+    user: DonatorUser
+    amount: float
+
+
+class DonatorsResponseExtra(BaseObject):
+    isLast: bool
+    offset: int | None = None
+    """Unknown, example: 10"""
+
+
+class DonatorsResponse(BaseObject):
+    data: list[Donator]
+    extra: DonatorsResponseExtra
