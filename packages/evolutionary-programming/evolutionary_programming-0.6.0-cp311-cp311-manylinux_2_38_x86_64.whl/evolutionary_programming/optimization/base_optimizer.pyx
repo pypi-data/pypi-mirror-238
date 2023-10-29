2ac80155a9c965fa6a45080771e1416202cd2598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:06:44 +0000

    Add package evolutionary-programming/0.6.0/evolutionary_programming-0.6.0-cp311-cp311-manylinux_2_38_x86_64.whl

---
+++ packages/evolutionary-programming/evolutionary_programming-0.6.0-cp311-cp311-manylinux_2_38_x86_64.whl/evolutionary_programming/optimization/base_optimizer.pyx
@@ -0,0 +1,27 @@
+import numpy as np
+cimport numpy as np
+from evolutionary_programming.objective_function.base_function cimport BaseFunction
+
+
+cdef class PopulationBasedOptimizer:
+    def __init__(
+        self,
+        int n_individuals,
+        int n_dims,
+        list min_bounds,
+        list max_bounds,
+    ) -> None:
+        self._n_individuals = n_individuals
+        self._n_dims = n_dims
+        self._min_bounds = min_bounds
+        self._max_bounds = max_bounds
+        self._history = []
+
+    cpdef void _init_individuals(self) except *:
+        raise NotImplementedError
+
+    cpdef void optimize(self, int iterations, BaseFunction function) except *:
+        raise NotImplementedError
+
+    cpdef np.ndarray get_population(self) except *:
+        raise NotImplementedError
