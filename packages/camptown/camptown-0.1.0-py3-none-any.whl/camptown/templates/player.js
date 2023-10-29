5b9a1a878f744f2bba9cc3080ed45e21f670a4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:08:00 +0000

    Add package camptown/0.1.0/camptown-0.1.0-py3-none-any.whl

---
+++ packages/camptown/camptown-0.1.0-py3-none-any.whl/camptown/templates/player.js
@@ -0,0 +1,71 @@
+window.addEventListener("load", () => {
+
+    // Only one disclosure should be open at a time
+    disclosures = document.querySelectorAll('input[type="checkbox"]');
+
+    function closeOthers(disclosure) {
+        console.log("closeOthers", disclosure);
+        if (disclosure.checked) {
+            disclosures.forEach((other) => {
+                if (disclosure != other) {
+                    other.checked = false;
+                }
+            });
+        }
+    }
+    disclosures.forEach((disclosure) => {
+        disclosure.addEventListener("change", () => {
+            closeOthers(disclosure);
+        });
+    });
+
+    document.querySelectorAll('button[data-disclosure]').forEach((button) => {
+        button.addEventListener("click", () => {
+            var check = document.getElementById(button.dataset.disclosure);
+            check.checked = !check.checked;
+            closeOthers(check);
+        });
+    });
+
+    let player = document.querySelector('audio');
+    var playlist = [];
+    var currentTrack = undefined;
+
+    // preserve the default album art
+    var albumArt = document.querySelector('img#coverart');
+    albumArt = {
+        src: albumArt.src,
+        srcset: albumArt.srcset
+    };
+
+    function playTrack(idx) {
+        console.log("Playing track " + idx + ": " + playlist[idx].title);
+    }
+
+    tracks = document.querySelectorAll('#tracklist .file');
+    tracks.forEach((track) => {
+        console.log(track);
+        var link = track.querySelector('a');
+
+        var entry = {
+            url: link?.href,
+            img: track.querySelector('img'),
+            title: track.querySelector('.title').textContent,
+            button: track.querySelector('button')
+        };
+
+
+        if (link) {
+            var idx = playlist.length;
+            playlist.push(entry);
+            track.addEventListener("click", () => {
+                playTrack(idx);
+            });
+            link.addEventListener("click", (e) => {
+                e.preventDefault();
+                playTrack(idx);
+            });
+        }
+    });
+});
+
