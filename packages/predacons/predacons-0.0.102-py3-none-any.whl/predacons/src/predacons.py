262d0f2aef9cd759483101d158e9b1afb70575f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 15:04:15 +0000

    Add package predacons/0.0.102/predacons-0.0.102-py3-none-any.whl

---
+++ packages/predacons/predacons-0.0.102-py3-none-any.whl/predacons/src/predacons.py
@@ -0,0 +1,48 @@
+from .load_data import LoadData
+from .train_predacons import TrainPredacons
+from .generate import Generate
+
+def rollout():
+    print("Predacons rollout !!!")
+    print("\nread_documents_from_directory -- Load data from directory")
+    print("    directory -- Directory path")
+    print("\nclean_text -- Clean text")
+    print("    text -- Text")
+    print("\ntrain -- Train Predacons")
+    print("    train_file_path -- Train file path")
+    print("    model_name -- Model name")
+    print("    output_dir -- Output directory")
+    print("    overwrite_output_dir -- Overwrite output directory")
+    print("    per_device_train_batch_size -- Per device train batch size")
+    print("    num_train_epochs -- Number of train epochs")
+    print("    save_steps -- Save steps")
+    print("\ngenerate_text -- Generate text")
+    print("    model_path -- Model path")
+    print("    sequence -- Sequence")
+    print("    max_length -- Max length")
+    print("\nPredacons rollout !!!")
+
+# Load data
+def read_documents_from_directory(directory,encoding="utf-8"):
+    return LoadData.read_documents_from_directory(directory,encoding)
+def clean_text(text):
+    return LoadData.clean_text(text)
+
+# Train Predacons
+def train(train_file_path,model_name,
+          output_dir,
+          overwrite_output_dir,
+          per_device_train_batch_size,
+          num_train_epochs,
+          save_steps):
+    TrainPredacons.train(train_file_path,model_name,
+          output_dir,
+          overwrite_output_dir,
+          per_device_train_batch_size,
+          num_train_epochs,
+          save_steps)
+    
+# Generate text
+def generate_text(model_path, sequence, max_length):
+    Generate.generate_text(model_path, sequence, max_length)
+
