4e4c0cac833f0ea7c5de327afd71657b8b117389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:08:33 +0000

    Add package qwak-core/0.1.48/qwak_core-0.1.48-py3-none-any.whl

---
+++ packages/qwak-core/qwak_core-0.1.48-py3-none-any.whl/qwak/inner/build_logic/phases/phases_pipeline.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from qwak.inner.build_config.build_config_v1 import BuildConfigV1
+from ..constants.step_description import BuildPhase, PhaseDetails
+from ..interface.context_interface import Context
+
+from qwak.inner.build_logic.interface.step_inteface import Step
+
+
+class PhasesPipeline:
+    def __init__(
+        self,
+        config: BuildConfigV1,
+        context: Context,
+        build_phase: BuildPhase = None,
+    ) -> None:
+        self._phases: List[PhasesPipeline] = []
+        self._steps: List[Step] = []
+        self.context = context
+        self._config = config
+        self.build_phase = build_phase
+
+    @property
+    def phases(self) -> List["PhasesPipeline"]:
+        return self._phases
+
+    @property
+    def steps(self) -> List[Step]:
+        return self._steps
+
+    def add_phase(self, steps: List[Step], build_phase: BuildPhase):
+        phase = PhasesPipeline(
+            config=self._config, context=self.context, build_phase=build_phase
+        )
+
+        for step in steps:
+            step.context = self.context
+            step.config = self._config
+            step.build_phase = build_phase
+            phase._steps.append(step)
+
+        self._phases.append(phase)
+
+    def get_phase_details(self) -> PhaseDetails:
+        return PhaseDetails(self.build_phase)
