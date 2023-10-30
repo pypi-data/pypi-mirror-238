baf1e9ad1fb885f63b60b4632ffe23e26144d19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 04:05:00 +0000

    Add package pyfem/0.1.7/pyfem-0.1.7.tar.gz

---
+++ packages/pyfem/pyfem-0.1.7.tar.gz/pyfem-0.1.7/src/pyfem/utils/colors.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+定义色彩关键字，用于对字符串着色。
+"""
+import os
+import colorlog
+
+IS_COLORED = True
+
+if os.environ.get('TERM', '') != '' or IS_COLORED:
+    CYAN = '\033[36m'
+    MAGENTA = '\033[35m'
+    BLUE = '\033[34m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    RED = '\033[31m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+else:
+    CYAN = ''
+    MAGENTA = ''
+    BLUE = ''
+    GREEN = ''
+    YELLOW = ''
+    RED = ''
+    BOLD = ''
+    UNDERLINE = ''
+    END = ''
+
+
+def error_style(error_msg: str) -> str:
+    return RED + BOLD + error_msg + END
+
+
+def info_style(info_msg: str) -> str:
+    return GREEN + BOLD + info_msg + END
+
+
+def warn_style(info_msg: str) -> str:
+    return YELLOW + BOLD + info_msg + END
