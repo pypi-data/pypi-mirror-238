65eded3048f5a6cacea624e88d9f61781e0bc3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:03 +0000

    Add package example-package-skymap/0.0.4/example_package_skymap-0.0.4.tar.gz

---
+++ packages/example-package-skymap/example_package_skymap-0.0.4.tar.gz/example_package_skymap-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="example_package_skymap",
+    version="0.0.4",
+    author="Nguyen Van Trien",
+    author_email="trien.nv195934@gmail.com",
+    description="A small example package",
+    packages=['dist'],
+    install_requires=[
+        "python-dotenv",
+        # Add any other dependencies here
+    ],
+    keywords=["python", "eofactory", "skymap", "data"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
