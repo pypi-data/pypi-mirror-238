1922a9c1c6e97a442f5e5708a7384ba90b65f755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 16:06:15 +0000

    Add package introsort-multithreaded/0.0.6/introsort_multithreaded-0.0.6.tar.gz

---
+++ packages/introsort-multithreaded/introsort_multithreaded-0.0.6.tar.gz/introsort_multithreaded-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='introsort_multithreaded',
+    packages=['introsort_multithreaded'],
+    package_data = {'introsort_multithreaded': ['sorting_eleven', 'sorting_twelve']},
+    version='0.0.6',
+    description='multithreaded introsort',
+    long_description=open('README.txt').read(),
+    author='Chong Yih Yang',
+    author_email='chongyihyang713@gmail.com',
+    license='MIT', 
+    classifiers=classifiers,
+    keywords='sorting',
+    python_requires='>=3.11.0'
+)
