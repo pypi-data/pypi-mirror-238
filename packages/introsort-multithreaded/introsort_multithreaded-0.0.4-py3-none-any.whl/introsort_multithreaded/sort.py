5e895c2b561cf6133bff52f96351565c7b91cacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:08:18 +0000

    Add package introsort-multithreaded/0.0.4/introsort_multithreaded-0.0.4-py3-none-any.whl

---
+++ packages/introsort-multithreaded/introsort_multithreaded-0.0.4-py3-none-any.whl/introsort_multithreaded/sort.py
@@ -0,0 +1,21 @@
+from introsort_multithreaded import sorting
+
+def sort_unstable(lst):
+    if type(lst[0]) is int: 
+        return sorting.sort_unstable_int(lst)
+    elif type(lst[0]) is float: 
+        return sorting.sort_unstable_frac(lst)
+    elif type(lst[0]) is str: 
+        return sorting.sort_unstable_str(lst)
+    else: 
+        print("Unknown type")
+        return
+
+def sort_stable(lst):
+    if type(lst[0]) is float:
+        return sorting.sort_stable_frac(lst)
+    elif type(lst[0]) is str:
+        return sorting.sort_stable_str(lst)
+    else:
+        print("Unknown type")
+        return
