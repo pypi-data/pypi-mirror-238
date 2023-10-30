3f523e2e97f530051f7053f801b628db8f18988a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 16:05:54 +0000

    Add package blendersynth/0.1.5/blendersynth-0.1.5.tar.gz

---
+++ packages/blendersynth/blendersynth-0.1.5.tar.gz/blendersynth-0.1.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+	name='blendersynth',
+	version='0.1.5',
+	install_requires=["appdirs>=1.4.4", "numpy", "tqdm", "ffmpeg-python"],
+	packages=find_packages(include=['blendersynth', 'blendersynth.*']),
+)
