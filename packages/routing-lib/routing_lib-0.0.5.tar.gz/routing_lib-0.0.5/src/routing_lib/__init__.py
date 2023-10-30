248c0c4ae8231072764f6c0ae66faff2f8b4bff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:04:08 +0000

    Add package routing-lib/0.0.5/routing_lib-0.0.5.tar.gz

---
+++ packages/routing-lib/routing_lib-0.0.5.tar.gz/routing_lib-0.0.5/src/routing_lib/__init__.py
@@ -0,0 +1,7 @@
+"""Package with routing algorithms, measures and utilities. Author: Ludovico Lemma"""
+
+__version__ = '0.0.5'
+
+from .routing_algorithms import *
+from .routing_measures import *
+from .routing_utils import *
