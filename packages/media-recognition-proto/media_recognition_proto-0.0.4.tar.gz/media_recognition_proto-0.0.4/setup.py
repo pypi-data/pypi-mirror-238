682622d0b270eb49ea5856dc093f044d20f04853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:05:57 +0000

    Add package media-recognition-proto/0.0.4/media_recognition_proto-0.0.4.tar.gz

---
+++ packages/media-recognition-proto/media_recognition_proto-0.0.4.tar.gz/media_recognition_proto-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='media_recognition_proto',
+    version='0.0.4',
+    description='GRPC client for media_recognition_proto',
+    author='ci',
+    author_email='p.a.anokhin@gmail.com',
+    packages=['media_recognition_proto'],
+    package_data={
+      'media_recognition_proto': ['*.pyi', 'py.typed'],
+    },
+    include_package_data=True,
+)
