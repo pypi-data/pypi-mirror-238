8720ffb5a86d98034d2ecc06064dd638affac20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 14:05:59 +0000

    Add package web3checksum/0.1.0/web3checksum-0.1.0.tar.gz

---
+++ packages/web3checksum/web3checksum-0.1.0.tar.gz/web3checksum-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='web3checksum',
+    version='0.1.0',
+    author='WebDev',
+    author_email='webdev181011@gmail.com',
+    description='Checksum of web3 address',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
