948142e8ff68390b317e454426d64533dd52f93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 16:06:01 +0000

    Add package vortexasdk/0.60.0/vortexasdk-0.60.0-py3-none-any.whl

---
+++ packages/vortexasdk/vortexasdk-0.60.0-py3-none-any.whl/vortexasdk/endpoints/endpoints.py
@@ -0,0 +1,43 @@
+CARGO_TIMESERIES_RESOURCE = "/time-series"
+
+CARGO_MOVEMENTS_RESOURCE = "/cargo-movements/search"
+CARGO_MOVEMENT_RESOURCE = "/cargo-movements"
+EIA_FORECASTS_RESOURCE = "/search/eia-forecasts"
+
+ORIGIN_BREAKDOWN_RESOURCE = "/breakdown/origin"
+DESTINATION_BREAKDOWN_RESOURCE = "/breakdown/destination"
+
+PRODUCT_BREAKDOWN_RESOURCE = "/breakdown/product"
+PARENT_PRODUCT_BREAKDOWN_RESOURCE = "/breakdown/parent-product"
+
+MOVEMENT_STATUS_BREAKDOWN_RESOURCE = "/breakdown/movement-status"
+
+VESSEL_CLASS_BREAKDOWN_RESOURCE = "/breakdown/vessel-class"
+
+VESSEL_AVAILABILITY_SEARCH_RESOURCE = "/vessel-availability/search"
+VESSEL_AVAILABILITY_TIMERSERIES_RESOURCE = "/vessel-availability/timeseries"
+VESSEL_AVAILABILITY_BREAKDOWN_RESOURCE = "/vessel-availability/breakdown"
+
+CRUDE_ONSHORE_INVENTORIES_SEARCH = "/onshore-inventories/search"
+CRUDE_ONSHORE_INVENTORIES_TIMESERIES = "/onshore-inventories/timeseries"
+
+ASSET_TANKS_REFERENCE = "/reference/asset-tanks"
+ATTRIBUTES_REFERENCE = "/reference/attributes"
+CORPORATIONS_REFERENCE = "/reference/charterers"
+GEOGRAPHIES_REFERENCE = "/reference/geographies"
+PRODUCTS_REFERENCE = "/reference/products"
+STORAGE_TERMINALS_REFERENCE = "/reference/storage-terminals"
+VESSELS_REFERENCE = "/reference/vessels"
+
+FREIGHT_PRICING_SEARCH = "/freight-pricing/rates"
+FREIGHT_PRICING_TIMESERIES = "/freight-pricing/timeseries"
+
+VOYAGES_SEARCH_ENRICHED = "/voyages/search-enriched"
+VOYAGES_TOP_HITS = "/voyages/top-hits"
+VOYAGES_CONGESTION_BREAKDOWN = "/voyages/congestion-breakdown"
+VOYAGES_TIMESERIES = "/voyages/timeseries"
+
+FIXTURES = "/search/fixtures"
+
+API_FQDN = "https://api.vortexa.com"
+API_URL = f"{API_FQDN}/v5"
