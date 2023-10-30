1e0c73ebf832dd37e3b2d3f0d60804607264da69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:00 +0000

    Add package gridengine-framework/0.1.1/gridengine_framework-0.1.1.tar.gz

---
+++ packages/gridengine-framework/gridengine_framework-0.1.1.tar.gz/gridengine_framework-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gridengine_framework',
+    version='0.1.1',
+    description='A framework for generating and manipulating grid-based game worlds',
+    author='James Evans',
+    author_email='joesaysahoy@gmail.com',
+    url='https://github.com/primal-coder/grid-engine',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pillow',
+        'pyglet',
+        'pymunk',
+        'noise'
+    ],
+    keywords='game development 2d grid world generation procedural generation cell numpy pillow pyglet pymunk cli'
+)
