1b61513f53ecf6e93bd8887318a353050dcbfa5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 14:06:43 +0000

    Add package pubsams/0.1.6/Pubsams-0.1.6.tar.gz

---
+++ packages/pubsams/Pubsams-0.1.6.tar.gz/Pubsams-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='Pubsams',
+    version='0.1.6',
+    description='Alterantive Package for PubSam',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Ahmed Alhilal',
+    author_email='Aalhilal@kfu.ed.sa',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Cheminoframtics',
+    packages=find_packages(),
+    install_requires=[
+        'rdkit',
+        'Pillow',
+        'ipython',
+        'mordred',
+        'pandas',
+        'statsmodels',
+        'matplotlib'
+    ]
+
+)
