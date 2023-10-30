81b6d6d99efa6830ce7150b28ac824d526a3078d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:05:29 +0000

    Add package nba-stats-dragon/0.1.1/nba-stats-dragon-0.1.1.tar.gz

---
+++ packages/nba-stats-dragon/nba-stats-dragon-0.1.1.tar.gz/nba-stats-dragon-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = ['click==8.1.3', 'requests==2.31.0', 'tabulate==0.8.9', 
+                    'nba-api==1.3.1', 'pandas==2.0.3', 'prettytable==3.9.0',
+                    'lxml==4.9.3', 'html5lib==1.1', 'bs4==0.0.1', 'scipy==1.10.1']
+
+setup(
+    name='nba-stats-dragon',
+    description='NBA CLI Tool to display Head to Head Stats.',
+    packages=find_packages(),
+    author='Rakeen Rouf (rmr327)',
+    entry_points="""
+    [console_scripts]
+    nbastats=mylib.nba_stats:run_stats
+    """,
+    install_requires=install_requires,
+    version='0.1.1',
+    url='https://github.com/nogibjj/nba-cli-tool',
+)
