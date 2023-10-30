b88ed85b871d8321d36ac8bcfbadaaf43b103d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 04:05:26 +0000

    Add package accrete/0.0.16/accrete-0.0.16.tar.gz

---
+++ packages/accrete/accrete-0.0.16.tar.gz/accrete-0.0.16/accrete/contrib/ui/__init__.py
@@ -0,0 +1,11 @@
+from .filter import Filter
+from .helper import (
+    ClientAction,
+    DetailContext,
+    FormContext,
+    ListContext,
+    BreadCrumb,
+    TableField,
+    build_querystring,
+    queryset_from_querystring
+)
