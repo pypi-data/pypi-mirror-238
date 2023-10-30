ad41d904467c2b812d7020cda293379b18905563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:07:45 +0000

    Add package reyworm/0.8/reyworm-0.8-py3-none-any.whl

---
+++ packages/reyworm/reyworm-0.8-py3-none-any.whl/reyworm/rcommon.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-04-22 22:41:54
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Rey's common methods.
+"""
+
+
+from .rtranslate import translate
