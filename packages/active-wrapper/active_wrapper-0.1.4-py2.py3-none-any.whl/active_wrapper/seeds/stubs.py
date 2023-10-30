9784a55d9a9b49959c014ea80bc01705d7680b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:35 +0000

    Add package active-wrapper/0.1.4/active_wrapper-0.1.4-py2.py3-none-any.whl

---
+++ packages/active-wrapper/active_wrapper-0.1.4-py2.py3-none-any.whl/active_wrapper/seeds/stubs.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+DEFAULT_STUB = """from active_wrapper.seeds import Seeder
+
+
+class DummyClass(Seeder):
+
+    def run(self):
+        \"\"\"
+        Run the database seeds.
+        \"\"\"
+        pass
+
+"""
