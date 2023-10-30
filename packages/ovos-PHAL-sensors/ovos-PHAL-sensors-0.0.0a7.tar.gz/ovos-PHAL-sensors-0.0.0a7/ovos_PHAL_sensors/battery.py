fa4ddd8e1dbff4a89a37963d421571d944a41f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 14:05:56 +0000

    Add package ovos-PHAL-sensors/0.0.0a7/ovos-PHAL-sensors-0.0.0a7.tar.gz

---
+++ packages/ovos-PHAL-sensors/ovos-PHAL-sensors-0.0.0a7.tar.gz/ovos-PHAL-sensors-0.0.0a7/ovos_PHAL_sensors/battery.py
@@ -0,0 +1,27 @@
+import dataclasses
+
+import psutil
+from ovos_utils import classproperty
+
+from ovos_PHAL_sensors.base import PercentageSensor
+
+
+@dataclasses.dataclass
+class BatterySensor(PercentageSensor):
+    unique_id: str = "percent"
+    device_name: str = "battery"
+
+    @classproperty
+    def value(self):
+        return round(psutil.sensors_battery().percent, 3)
+
+    @classproperty
+    def attrs(cls):
+        return {"friendly_name": cls.__name__,
+                "device_class": "battery",
+                "unit_of_measurement": cls.unit}
+
+
+if __name__ == "__main__":
+    print(BatterySensor())
+    # battery_percent(51.12, %)
