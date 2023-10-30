a55b54b102011dac5569e53f38dbfd4518e5018b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:51 +0000

    Add package SilverStreamingApplicationDQ/1.5/SilverStreamingApplicationDQ-1.5.tar.gz

---
+++ packages/SilverStreamingApplicationDQ/SilverStreamingApplicationDQ-1.5.tar.gz/SilverStreamingApplicationDQ-1.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SilverStreamingApplicationDQ',
+    version='1.5',
+    packages=['com.phida.main', 'com.phida.main.bronze', 'com.phida.tests'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='ssa and workflow builder updated to support files without timestamp.'
+)
