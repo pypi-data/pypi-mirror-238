d4d977653290ab54fe7d338bd40edc3af66c1a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 14:06:07 +0000

    Add package bed-reader/0.2.37/bed_reader-0.2.37.tar.gz

---
+++ packages/bed-reader/bed_reader-0.2.37.tar.gz/bed_reader-0.2.37/useful.bat
@@ -0,0 +1,9 @@
+pip# test Rust
+cargo test
+
+# install packages, compile Rust for Python, and test Python
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+maturin develop
+pytest bed_reader
+maturin develop --target-dir c:\deldir\
