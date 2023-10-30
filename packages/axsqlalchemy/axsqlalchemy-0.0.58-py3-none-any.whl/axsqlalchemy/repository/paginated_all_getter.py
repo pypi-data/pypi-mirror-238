4a4b0c3b867c8fcdda1eecdba5b9c4c4ffeef2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:06 +0000

    Add package axsqlalchemy/0.0.58/axsqlalchemy-0.0.58-py3-none-any.whl

---
+++ packages/axsqlalchemy/axsqlalchemy-0.0.58-py3-none-any.whl/axsqlalchemy/repository/paginated_all_getter.py
@@ -0,0 +1,26 @@
+from typing import Generic, Iterable, List, Optional
+from sqlalchemy import select
+from pydantic import parse_obj_as
+
+from .types import TIModel, TOModel, TDBModel
+from .paginated import PaginatedRepo
+
+
+class PaginatedAllGetterRepo(PaginatedRepo[TDBModel, TIModel, TOModel], Generic[TDBModel, TIModel, TOModel]):
+    @property
+    def _base_all_paginated_query(self): 
+        return self._base_all_query
+
+    async def all(self, *ids, filters: Iterable = tuple(), count: Optional[int] = None, page: Optional[int] = None, query = None) -> list[TOModel]:
+        return await super().all(
+            *ids, 
+            filters=filters,
+            query=(
+                self.paginate_query( 
+                    (query or self._base_all_paginated_query),
+                    count=count,
+                    page=page,
+                )
+            ),
+        )
+
