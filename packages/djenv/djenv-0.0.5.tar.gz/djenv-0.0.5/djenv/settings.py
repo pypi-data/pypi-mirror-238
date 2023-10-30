dcf16a0db0e83966d8f786fce751144c611d6513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:06:30 +0000

    Add package djenv/0.0.5/djenv-0.0.5.tar.gz

---
+++ packages/djenv/djenv-0.0.5.tar.gz/djenv-0.0.5/djenv/settings.py
@@ -0,0 +1,25 @@
+import json
+import os
+import re
+import simple_env as se
+
+v = None
+
+for k in os.environ:
+    if k not in ['DJANGO_SETTINGS_MODULE']:
+        if re.match(r'^DJANGO_[A-Z][A-Z_]+[A-Z]$', k):
+            v = se.get(k)
+
+            if isinstance(v, str):
+                v = v.strip() # remove excess white space
+                if ((v.startswith("{") and v.endswith("}")) or (v.startswith("[") and v.endswith("]"))):
+                    v = json.loads(v)
+
+            globals()[k.replace("DJANGO_", "")] = v
+
+del k
+del json
+del os
+del re
+del v
+del se
