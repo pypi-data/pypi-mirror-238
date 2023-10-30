bc26c36e867070061705490fd4b125d83c2fdaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 16:06:21 +0000

    Add package mep-tools/1.1.29/mep_tools-1.1.29-py3-none-any.whl

---
+++ packages/mep-tools/mep_tools-1.1.29-py3-none-any.whl/mep_tools/connectors/__init__.py
@@ -0,0 +1,7 @@
+# GENERATED FILE DO NOT CHANGE !!!
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent))
+from connector_pb2 import *
+from connector_pb2_grpc import *
+from connector_pb2 import *
