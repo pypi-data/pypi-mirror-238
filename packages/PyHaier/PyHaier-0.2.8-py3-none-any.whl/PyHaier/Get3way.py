3ce3421be751263814688eb9d3490c5a40326907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 04:05:29 +0000

    Add package PyHaier/0.2.8/PyHaier-0.2.8-py3-none-any.whl

---
+++ packages/PyHaier/PyHaier-0.2.8-py3-none-any.whl/PyHaier/Get3way.py
@@ -0,0 +1,14 @@
+def Get3way(payload):
+    """
+    Function for displaying if pump is on or off
+    payload - register from 141 to 157
+    :return:
+    """
+    tmp = divmod(int(hex(payload[0]), 16), 256)[0]
+    if tmp == 4:
+        threeway = 'CH'
+    elif tmp == 6 or tmp == 0:
+        threeway = 'DHW'
+    else:
+        threeway = 'ERR'
+    return threeway
---
+++ packages/PyHaier/PyHaier-0.2.8-py3-none-any.whl/PyHaier/GetMode.py
@@ -0,0 +1,15 @@
+def GetMode(payload):
+    """
+    Mode display function
+    :return:
+    """
+    if payload[0] == 0:
+        mode="eco"
+    elif payload[0] == 1:
+        mode="quiet"
+    elif payload[0] == 2:
+        mode="turbo"
+    else:
+        mode="Mode Undefinde"
+
+    return mode
