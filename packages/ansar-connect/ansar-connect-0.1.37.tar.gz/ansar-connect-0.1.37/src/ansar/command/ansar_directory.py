c420cf321a7c19730af4e8157f73ead69f1aaf08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 14:06:36 +0000

    Add package ansar-connect/0.1.37/ansar-connect-0.1.37.tar.gz

---
+++ packages/ansar-connect/ansar-connect-0.1.37.tar.gz/ansar-connect-0.1.37/src/ansar/command/ansar_directory.py
@@ -0,0 +1,45 @@
+# Author: Scott Woods <scott.18.ansar@gmail.com.com>
+# MIT License
+#
+# Copyright (c) 2017-2023 Scott Woods
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+'''.
+
+.
+'''
+import ansar.connect as ar
+
+# Just need an object to stay "on duty" while the 
+# global ServiceDirectory does its work.
+def sentry(self):
+	self.select(ar.Stop)
+	return ar.Aborted()
+
+ar.bind(sentry)
+
+# Entry point for python packaging. The dollar
+# ansar-directory command starts here.
+def main():
+    ar.create_node(sentry)
+
+# The standard entry point. Needed for IDEs
+# and debugger sessions.
+if __name__ == '__main__':
+    main()
