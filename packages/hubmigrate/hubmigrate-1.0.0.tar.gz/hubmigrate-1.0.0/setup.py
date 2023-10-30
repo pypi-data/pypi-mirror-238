084d1c50b300760776a3bf873c7b6e15d200d5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 04:06:05 +0000

    Add package hubmigrate/1.0.0/hubmigrate-1.0.0.tar.gz

---
+++ packages/hubmigrate/hubmigrate-1.0.0.tar.gz/hubmigrate-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hubmigrate",
+    version="1.0.0",
+    description="An open-source Python library is designed to simplify the process of migrating data to HubSpot, a powerful customer relationship management (CRM) platform. Whether you're moving contacts, companies, deals, or any other data to HubSpot, this library provides an easy-to-use interface for managing your migration tasks.",
+    author="bcostaaa01",
+    author_email="bcostaaa01@gmail.com",
+    install_requires=["pytest", "python-decouple"],
+    packages=find_packages(),
+)
