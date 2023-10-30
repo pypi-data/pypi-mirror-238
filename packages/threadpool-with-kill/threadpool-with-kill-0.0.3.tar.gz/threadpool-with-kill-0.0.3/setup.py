3ac4999d9aa76192a5f273fc77d70f4f4f583209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:07:08 +0000

    Add package threadpool-with-kill/0.0.3/threadpool-with-kill-0.0.3.tar.gz

---
+++ packages/threadpool-with-kill/threadpool-with-kill-0.0.3.tar.gz/threadpool-with-kill-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="threadpool-with-kill",
+    version="0.0.3",
+    author="Chengze Zhang",
+    author_email="929160190@qq.com",
+    description="A thread pool for python",
+    url="https://github.com/chasezcz/ThreadPool",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
