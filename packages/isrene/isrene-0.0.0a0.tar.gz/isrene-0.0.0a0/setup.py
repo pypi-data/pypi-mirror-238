10f931da56010a278957e49aff8d79977f045e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:07:48 +0000

    Add package isrene/0.0.0a0/isrene-0.0.0a0.tar.gz

---
+++ packages/isrene/isrene-0.0.0a0.tar.gz/isrene-0.0.0a0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='isrene',
+    version='0.0.0a0',
+    description='',
+    long_description='',
+#    url='https://www.example.com',
+ #   author='Daniel Weindl',
+ #   author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
