73985477d901fc199c9fb8e664dd25a1ffc7a56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:05:54 +0000

    Add package pypsf/1.0.6/pypsf-1.0.6.tar.gz

---
+++ packages/pypsf/pypsf-1.0.6.tar.gz/pypsf-1.0.6/pypsf/clustering.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+def run_clustering(cycles: list[np.array], n_clusters: int) -> KMeans:
+    """
+    Apply K-means clustering to the provided list of cycles.
+    Args:
+        cycles (list[np.array]):
+            The cycles to cluster
+        n_clusters (int):
+            Number of clusters (k) to form.
+
+    Returns:
+        kmeans (KMeans):
+            The fitted K-means clustering object
+    """
+    return KMeans(n_clusters=n_clusters, init='random', n_init="auto", random_state=3683475120).fit(np.array(cycles))
