eefa680c7306ce55024c969b610700d0cc46f281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:06:30 +0000

    Add package zymodbustcpclient/1.1/zymodbustcpclient-1.1.tar.gz

---
+++ packages/zymodbustcpclient/zymodbustcpclient-1.1.tar.gz/zymodbustcpclient-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zymodbustcpclient',
+    version='1.01',
+    author='zhang yue',
+    packages=['zymodbustcpclient'],
+    install_requires=[
+        '',
+    ],
+)
