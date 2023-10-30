bfdde16cee61d4489388bdc745aacea69fbc9cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:07:33 +0000

    Add package vhelpers/0.1.9/vhelpers-0.1.9.tar.gz

---
+++ packages/vhelpers/vhelpers-0.1.9.tar.gz/vhelpers-0.1.9/vhelpers/types_.py
@@ -0,0 +1,44 @@
+"""Typing."""
+from datetime import date, datetime
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Sequence, Set, Tuple, TypeVar, Union
+
+# 1 level
+DAny = Dict[str, Any]
+DInt = Dict[str, int]
+IntStr = Union[int, str]
+LAny = List[Any]
+LStr = List[str]
+Dtime = datetime
+ODtime = Optional[datetime]
+Param = Tuple[str, Any]
+SDate = Set[date]
+SeqStr = Sequence[str]
+T = TypeVar("T")
+T2Int = Tuple[int, int]
+T2Str = Tuple[str, str]
+T3Str = Tuple[str, str, str]
+T4Str = Tuple[str, str, str, str]
+TList = (list, set, tuple)
+UPath = Union[Path, str]
+
+# 2 level
+DLStr = Dict[str, LStr]
+LLAny = List[LAny]
+LParam = List[Param]
+LT = List[T]
+LT3Str = List[T3Str]
+SParam = Set[Param]
+ST = Set[T]
+SeqT = Sequence[T]
+TParam = Tuple[Param, ...]
+TT = Tuple[T]
+UStr = Union[str, SeqStr]
+
+# 3 level
+UParam = Union[LParam, SParam, TParam]
+UT = Union[LT, ST, TT]
+T3Strs = Tuple[str, str, UStr]
+
+# 4 level
+LT3Strs = List[T3Strs]
