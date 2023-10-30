7a37adecd0e2b63f37774c3d92163e2191373a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:05:48 +0000

    Add package avltree/1.1.2/avltree-1.1.2-py3-none-any.whl

---
+++ packages/avltree/avltree-1.1.2-py3-none-any.whl/avltree/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Charles Andrews
+""".. include:: ../../README.md"""  # noqa: D415
+from __future__ import annotations
+
+from typing import Final
+
+from ._avl_tree import AvlTree
+
+__all__: Final[list[str]] = ["AvlTree"]
+__docformat__ = "google"
