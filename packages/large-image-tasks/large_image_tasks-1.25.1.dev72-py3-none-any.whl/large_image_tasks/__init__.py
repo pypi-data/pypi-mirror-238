50ae64c5a825f4c22bac8df4fcb75d752b8a1f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:02 +0000

    Add package large-image-tasks/1.25.1.dev72/large_image_tasks-1.25.1.dev72-py3-none-any.whl

---
+++ packages/large-image-tasks/large_image_tasks-1.25.1.dev72-py3-none-any.whl/large_image_tasks/__init__.py
@@ -0,0 +1,29 @@
+"""Top-level package for Large Image Tasks."""
+
+__author__ = """Kitware Inc"""
+__email__ = 'kitware@kitware.com'
+
+
+from girder_worker import GirderWorkerPluginABC
+
+try:
+    from importlib.metadata import PackageNotFoundError
+    from importlib.metadata import version as _importlib_version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError
+    from importlib_metadata import version as _importlib_version
+try:
+    __version__ = _importlib_version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+
+class LargeImageTasks(GirderWorkerPluginABC):
+    def __init__(self, app, *args, **kwargs):
+        self.app = app
+
+    def task_imports(self):
+        # Return a list of python importable paths to the
+        # plugin's path directory
+        return ['large_image_tasks.tasks']
