799ff8be00d8718f71ef8a923491d1af044c4202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 16:06:57 +0000

    Add package cloudshell-cp-vcenter/7.0.0/cloudshell_cp_vcenter-7.0.0-py3-none-any.whl

---
+++ packages/cloudshell-cp-vcenter/cloudshell_cp_vcenter-7.0.0-py3-none-any.whl/cloudshell/cp/vcenter/constants.py
@@ -0,0 +1,9 @@
+SHELL_NAME = "VMware vCenter Cloud Provider 2G"
+STATIC_SHELL_NAME = "Generic Static vCenter VM 2G"
+
+VM_FROM_VM_DEPLOYMENT_PATH = f"{SHELL_NAME}.vCenter VM From VM 2G"
+VM_FROM_TEMPLATE_DEPLOYMENT_PATH = f"{SHELL_NAME}.vCenter VM From Template 2G"
+VM_FROM_LINKED_CLONE_DEPLOYMENT_PATH = f"{SHELL_NAME}.vCenter VM From Linked Clone 2G"
+VM_FROM_IMAGE_DEPLOYMENT_PATH = f"{SHELL_NAME}.vCenter VM From Image 2G"
+
+DEPLOYED_APPS_FOLDER = "Deployed Apps"
