b0c1bcda29d3f2e82cbc0ede15336945c4e49650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 05:03:57 +0000

    Add package SimpleApplication/1.2.6/SimpleApplication-1.2.6.tar.gz

---
+++ packages/SimpleApplication/SimpleApplication-1.2.6.tar.gz/SimpleApplication-1.2.6/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    include_package_data=True,
+    name='SimpleApplication',
+    version='1.2.6',
+    description="Simple Application in Python",
+    url='https://gitlab.com/carrivalegervasi/provacg.git',
+    author="CarrivaleGervasi",
+    packages=setuptools.find_packages(),
+    install_requires=required,
+    long_description='A Readme is coming soon',
+    long_description_content_type="text/markdown",
+)
