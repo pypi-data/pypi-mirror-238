1116508c1999f74b20199f9856c6e43023370828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:07:39 +0000

    Add package Surfactant/0.0.0rc1/Surfactant-0.0.0rc1-py3-none-any.whl

---
+++ packages/Surfactant/Surfactant-0.0.0rc1-py3-none-any.whl/surfactant/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC
+# See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+try:
+    from ._version import __version__, __version_tuple__
+except ModuleNotFoundError:
+    __version__ = ""
+    __version_tuple__ = ()
