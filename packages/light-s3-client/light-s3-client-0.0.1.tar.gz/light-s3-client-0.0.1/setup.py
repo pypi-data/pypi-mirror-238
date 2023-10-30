c591900fa8888c3b9653aba1856534ebe5285e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 04:05:16 +0000

    Add package light-s3-client/0.0.1/light-s3-client-0.0.1.tar.gz

---
+++ packages/light-s3-client/light-s3-client-0.0.1.tar.gz/light-s3-client-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="light-s3-client",
+    version="0.0.1",
+    author="Douglas Coburn",
+    author_email="douglas@dactbc.com",
+    description="A lightweight S3 client that does not rely on boto3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dacoburn/light-s3",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
