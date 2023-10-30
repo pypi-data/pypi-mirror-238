7eba3805708a84da56770d469b070232bbc89e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:07:29 +0000

    Add package g42cloudsdkcdn/0.0.10b0/g42cloudsdkcdn-0.0.10b0-py2.py3-none-any.whl

---
+++ packages/g42cloudsdkcdn/g42cloudsdkcdn-0.0.10b0-py2.py3-none-any.whl/g42cloudsdkcdn/v2/cdn_async_client.py
@@ -0,0 +1,269 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+import importlib
+
+from g42cloudsdkcore.client import Client, ClientBuilder
+from g42cloudsdkcore.utils import http_utils
+from g42cloudsdkcore.sdk_stream_request import SdkStreamRequest
+
+
+class CdnAsyncClient(Client):
+    def __init__(self):
+        super(CdnAsyncClient, self).__init__()
+        self.model_package = importlib.import_module("g42cloudsdkcdn.v2.model")
+
+    @classmethod
+    def new_builder(cls, clazz=None):
+        if not clazz:
+            client_builder = ClientBuilder(cls, "GlobalCredentials")
+        else:
+            if clazz.__name__ != "CdnAsyncClient":
+                raise TypeError("client type error, support client type is CdnAsyncClient")
+            client_builder = ClientBuilder(clazz, "GlobalCredentials")
+
+        
+
+        return client_builder
+
+    def show_domain_location_stats_async(self, request):
+        """
+
+        :param request: Request instance for ShowDomainLocationStats
+        :type request: :class:`g42cloudsdkcdn.v2.ShowDomainLocationStatsRequest`
+        :rtype: :class:`g42cloudsdkcdn.v2.ShowDomainLocationStatsResponse`
+        """
+        return self._show_domain_location_stats_with_http_info(request)
+
+    def _show_domain_location_stats_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'action' in local_var_params:
+            query_params.append(('action', local_var_params['action']))
+        if 'start_time' in local_var_params:
+            query_params.append(('start_time', local_var_params['start_time']))
+        if 'end_time' in local_var_params:
+            query_params.append(('end_time', local_var_params['end_time']))
+        if 'domain_name' in local_var_params:
+            query_params.append(('domain_name', local_var_params['domain_name']))
+        if 'stat_type' in local_var_params:
+            query_params.append(('stat_type', local_var_params['stat_type']))
+        if 'interval' in local_var_params:
+            query_params.append(('interval', local_var_params['interval']))
+        if 'country' in local_var_params:
+            query_params.append(('country', local_var_params['country']))
+        if 'province' in local_var_params:
+            query_params.append(('province', local_var_params['province']))
+        if 'isp' in local_var_params:
+            query_params.append(('isp', local_var_params['isp']))
+        if 'group_by' in local_var_params:
+            query_params.append(('group_by', local_var_params['group_by']))
+        if 'enterprise_project_id' in local_var_params:
+            query_params.append(('enterprise_project_id', local_var_params['enterprise_project_id']))
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v1.0/cdn/statistics/domain-location-stats',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ShowDomainLocationStatsResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def show_domain_stats_async(self, request):
+        """
+
+        :param request: Request instance for ShowDomainStats
+        :type request: :class:`g42cloudsdkcdn.v2.ShowDomainStatsRequest`
+        :rtype: :class:`g42cloudsdkcdn.v2.ShowDomainStatsResponse`
+        """
+        return self._show_domain_stats_with_http_info(request)
+
+    def _show_domain_stats_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'action' in local_var_params:
+            query_params.append(('action', local_var_params['action']))
+        if 'start_time' in local_var_params:
+            query_params.append(('start_time', local_var_params['start_time']))
+        if 'end_time' in local_var_params:
+            query_params.append(('end_time', local_var_params['end_time']))
+        if 'domain_name' in local_var_params:
+            query_params.append(('domain_name', local_var_params['domain_name']))
+        if 'stat_type' in local_var_params:
+            query_params.append(('stat_type', local_var_params['stat_type']))
+        if 'interval' in local_var_params:
+            query_params.append(('interval', local_var_params['interval']))
+        if 'group_by' in local_var_params:
+            query_params.append(('group_by', local_var_params['group_by']))
+        if 'service_area' in local_var_params:
+            query_params.append(('service_area', local_var_params['service_area']))
+        if 'enterprise_project_id' in local_var_params:
+            query_params.append(('enterprise_project_id', local_var_params['enterprise_project_id']))
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v1.0/cdn/statistics/domain-stats',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ShowDomainStatsResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def show_top_url_async(self, request):
+        """
+
+        :param request: Request instance for ShowTopUrl
+        :type request: :class:`g42cloudsdkcdn.v2.ShowTopUrlRequest`
+        :rtype: :class:`g42cloudsdkcdn.v2.ShowTopUrlResponse`
+        """
+        return self._show_top_url_with_http_info(request)
+
+    def _show_top_url_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'start_time' in local_var_params:
+            query_params.append(('start_time', local_var_params['start_time']))
+        if 'end_time' in local_var_params:
+            query_params.append(('end_time', local_var_params['end_time']))
+        if 'domain_name' in local_var_params:
+            query_params.append(('domain_name', local_var_params['domain_name']))
+        if 'stat_type' in local_var_params:
+            query_params.append(('stat_type', local_var_params['stat_type']))
+        if 'service_area' in local_var_params:
+            query_params.append(('service_area', local_var_params['service_area']))
+        if 'enterprise_project_id' in local_var_params:
+            query_params.append(('enterprise_project_id', local_var_params['enterprise_project_id']))
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v1.0/cdn/statistics/top-url',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ShowTopUrlResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def call_api(self, resource_path, method, path_params=None, query_params=None, header_params=None, body=None,
+                 post_params=None, cname=None, response_type=None, response_headers=None, auth_settings=None,
+                 collection_formats=None, request_type=None):
+        """Makes the HTTP request and returns deserialized data.
+
+        :param resource_path: Path to method endpoint.
+        :param method: Method to call.
+        :param path_params: Path parameters in the url.
+        :param query_params: Query parameters in the url.
+        :param header_params: Header parameters to be
+            placed in the request header.
+        :param body: Request body.
+        :param post_params: Request post form parameters,
+            for `application/x-www-form-urlencoded`, `multipart/form-data`.
+        :param cname: Used for obs endpoint.
+        :param auth_settings: Auth Settings names for the request.
+        :param response_type: Response data type.
+        :param response_headers: Header should be added to response data.
+        :param collection_formats: dict of collection formats for path, query,
+            header, and post parameters.
+        :param request_type: Request data type.
+        :return:
+            Return the response directly.
+        """
+        return self.do_http_request(
+            method=method,
+            resource_path=resource_path,
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body,
+            post_params=post_params,
+            cname=cname,
+            response_type=response_type,
+            response_headers=response_headers,
+            collection_formats=collection_formats,
+            request_type=request_type,
+	    async_request=True)
