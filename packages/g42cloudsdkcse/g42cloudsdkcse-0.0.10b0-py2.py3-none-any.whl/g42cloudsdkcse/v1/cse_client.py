802b229c8d64be81ca01276014b95a380c5f7799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:07:29 +0000

    Add package g42cloudsdkcse/0.0.10b0/g42cloudsdkcse-0.0.10b0-py2.py3-none-any.whl

---
+++ packages/g42cloudsdkcse/g42cloudsdkcse-0.0.10b0-py2.py3-none-any.whl/g42cloudsdkcse/v1/cse_client.py
@@ -0,0 +1,503 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+import importlib
+
+from g42cloudsdkcore.client import Client, ClientBuilder
+from g42cloudsdkcore.utils import http_utils
+from g42cloudsdkcore.sdk_stream_request import SdkStreamRequest
+
+
+class CseClient(Client):
+    def __init__(self):
+        super(CseClient, self).__init__()
+        self.model_package = importlib.import_module("g42cloudsdkcse.v1.model")
+
+    @classmethod
+    def new_builder(cls, clazz=None):
+        if not clazz:
+            client_builder = ClientBuilder(cls)
+        else:
+            if clazz.__name__ != "CseClient":
+                raise TypeError("client type error, support client type is CseClient")
+            client_builder = ClientBuilder(clazz)
+
+        
+
+        return client_builder
+
+    def create_engine(self, request):
+        """
+        :param request: Request instance for CreateEngine
+        :type request: :class:`g42cloudsdkcse.v1.CreateEngineRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.CreateEngineResponse`
+        """
+        return self._create_engine_with_http_info(request)
+
+    def _create_engine_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+        if 'x_enterprise_project_id' in local_var_params:
+            header_params['X-Enterprise-Project-ID'] = local_var_params['x_enterprise_project_id']
+
+        form_params = {}
+
+        body_params = None
+        if 'body' in local_var_params:
+            body_params = local_var_params['body']
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/enginemgr/engines',
+            method='POST',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='CreateEngineResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def delete_engine(self, request):
+        """
+        :param request: Request instance for DeleteEngine
+        :type request: :class:`g42cloudsdkcse.v1.DeleteEngineRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.DeleteEngineResponse`
+        """
+        return self._delete_engine_with_http_info(request)
+
+    def _delete_engine_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'engine_id' in local_var_params:
+            path_params['engine_id'] = local_var_params['engine_id']
+
+        query_params = []
+
+        header_params = {}
+        if 'x_enterprise_project_id' in local_var_params:
+            header_params['X-Enterprise-Project-ID'] = local_var_params['x_enterprise_project_id']
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/enginemgr/engines/{engine_id}',
+            method='DELETE',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='DeleteEngineResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def download_kie(self, request):
+        """
+        :param request: Request instance for DownloadKie
+        :type request: :class:`g42cloudsdkcse.v1.DownloadKieRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.DownloadKieResponse`
+        """
+        return self._download_kie_with_http_info(request)
+
+    def _download_kie_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'label' in local_var_params:
+            query_params.append(('label', local_var_params['label']))
+        if 'match' in local_var_params:
+            query_params.append(('match', local_var_params['match']))
+
+        header_params = {}
+        if 'x_enterprise_project_id' in local_var_params:
+            header_params['X-Enterprise-Project-ID'] = local_var_params['x_enterprise_project_id']
+        if 'x_engine_id' in local_var_params:
+            header_params['x-engine-id'] = local_var_params['x_engine_id']
+
+        form_params = {}
+
+        body_params = None
+        if 'body' in local_var_params:
+            body_params = local_var_params['body']
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v1/{project_id}/kie/download',
+            method='POST',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='DownloadKieResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def list_engines(self, request):
+        """
+        :param request: Request instance for ListEngines
+        :type request: :class:`g42cloudsdkcse.v1.ListEnginesRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.ListEnginesResponse`
+        """
+        return self._list_engines_with_http_info(request)
+
+    def _list_engines_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'offset' in local_var_params:
+            query_params.append(('offset', local_var_params['offset']))
+        if 'limit' in local_var_params:
+            query_params.append(('limit', local_var_params['limit']))
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/enginemgr/engines',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ListEnginesResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def list_flavors(self, request):
+        """
+        :param request: Request instance for ListFlavors
+        :type request: :class:`g42cloudsdkcse.v1.ListFlavorsRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.ListFlavorsResponse`
+        """
+        return self._list_flavors_with_http_info(request)
+
+    def _list_flavors_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'spec_type' in local_var_params:
+            query_params.append(('spec_type', local_var_params['spec_type']))
+
+        header_params = {}
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/enginemgr/flavors',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ListFlavorsResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def show_engine(self, request):
+        """
+        :param request: Request instance for ShowEngine
+        :type request: :class:`g42cloudsdkcse.v1.ShowEngineRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.ShowEngineResponse`
+        """
+        return self._show_engine_with_http_info(request)
+
+    def _show_engine_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'engine_id' in local_var_params:
+            path_params['engine_id'] = local_var_params['engine_id']
+
+        query_params = []
+
+        header_params = {}
+        if 'x_enterprise_project_id' in local_var_params:
+            header_params['X-Enterprise-Project-ID'] = local_var_params['x_enterprise_project_id']
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/enginemgr/engines/{engine_id}',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ShowEngineResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def show_engine_job(self, request):
+        """
+        :param request: Request instance for ShowEngineJob
+        :type request: :class:`g42cloudsdkcse.v1.ShowEngineJobRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.ShowEngineJobResponse`
+        """
+        return self._show_engine_job_with_http_info(request)
+
+    def _show_engine_job_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'engine_id' in local_var_params:
+            path_params['engine_id'] = local_var_params['engine_id']
+        if 'job_id' in local_var_params:
+            path_params['job_id'] = local_var_params['job_id']
+
+        query_params = []
+
+        header_params = {}
+        if 'x_enterprise_project_id' in local_var_params:
+            header_params['X-Enterprise-Project-ID'] = local_var_params['x_enterprise_project_id']
+
+        form_params = {}
+
+        body_params = None
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['application/json'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v2/{project_id}/enginemgr/engines/{engine_id}/jobs/{job_id}',
+            method='GET',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='ShowEngineJobResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def upload_kie(self, request):
+        """
+        :param request: Request instance for UploadKie
+        :type request: :class:`g42cloudsdkcse.v1.UploadKieRequest`
+        :rtype: :class:`g42cloudsdkcse.v1.UploadKieResponse`
+        """
+        return self._upload_kie_with_http_info(request)
+
+    def _upload_kie_with_http_info(self, request):
+        local_var_params = {attr: getattr(request, attr) for attr in request.attribute_map if hasattr(request, attr)}
+
+        cname = None
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'override' in local_var_params:
+            query_params.append(('override', local_var_params['override']))
+        if 'label' in local_var_params:
+            query_params.append(('label', local_var_params['label']))
+
+        header_params = {}
+        if 'x_enterprise_project_id' in local_var_params:
+            header_params['X-Enterprise-Project-ID'] = local_var_params['x_enterprise_project_id']
+        if 'x_engine_id' in local_var_params:
+            header_params['x-engine-id'] = local_var_params['x_engine_id']
+
+        form_params = {}
+        if 'upload_file' in local_var_params:
+            form_params['upload_file'] = local_var_params['upload_file']
+
+        body_params = None
+        if 'body' in local_var_params:
+            body_params = local_var_params['body']
+        if isinstance(request, SdkStreamRequest):
+            body_params = request.get_file_stream()
+
+        response_headers = []
+
+        header_params['Content-Type'] = http_utils.select_header_content_type(
+            ['multipart/form-data'])
+
+        auth_settings = []
+
+        return self.call_api(
+            resource_path='/v1/{project_id}/kie/file',
+            method='POST',
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body_params,
+            post_params=form_params,
+            cname=cname,
+            response_type='UploadKieResponse',
+            response_headers=response_headers,
+            auth_settings=auth_settings,
+            collection_formats=collection_formats,
+            request_type=request.__class__.__name__)
+
+    def call_api(self, resource_path, method, path_params=None, query_params=None, header_params=None, body=None,
+                 post_params=None, cname=None, response_type=None, response_headers=None, auth_settings=None,
+                 collection_formats=None, request_type=None):
+        """Makes the HTTP request and returns deserialized data.
+
+        :param resource_path: Path to method endpoint.
+        :param method: Method to call.
+        :param path_params: Path parameters in the url.
+        :param query_params: Query parameters in the url.
+        :param header_params: Header parameters to be placed in the request header.
+        :param body: Request body.
+        :param post_params: Request post form parameters,
+            for `application/x-www-form-urlencoded`, `multipart/form-data`.
+        :param cname: Used for obs endpoint.
+        :param auth_settings: Auth Settings names for the request.
+        :param response_type: Response data type.
+        :param response_headers: Header should be added to response data.
+        :param collection_formats: dict of collection formats for path, query,
+            header, and post parameters.
+        :param request_type: Request data type.
+        :return:
+            Return the response directly.
+        """
+        return self.do_http_request(
+            method=method,
+            resource_path=resource_path,
+            path_params=path_params,
+            query_params=query_params,
+            header_params=header_params,
+            body=body,
+            post_params=post_params,
+            cname=cname,
+            response_type=response_type,
+            response_headers=response_headers,
+            collection_formats=collection_formats,
+            request_type=request_type)
