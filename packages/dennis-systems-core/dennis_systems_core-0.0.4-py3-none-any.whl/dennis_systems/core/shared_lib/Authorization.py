5c94d5e3646823996eba412cbec5e328a7b0b6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:05:53 +0000

    Add package dennis-systems-core/0.0.4/dennis_systems_core-0.0.4-py3-none-any.whl

---
+++ packages/dennis-systems-core/dennis_systems_core-0.0.4-py3-none-any.whl/dennis_systems/core/shared_lib/Authorization.py
@@ -0,0 +1,26 @@
+import jwt
+from django.core.exceptions import PermissionDenied
+from django.http import HttpRequest
+
+from dennis_systems.core.shared_lib.AuthorizationResponse import AuthorizationResponse
+from utils import file_utils
+
+
+class Authorization:
+    __AUTH = 'Authentication'
+
+    @staticmethod
+    def has_auth_token(request: HttpRequest) -> bool:
+        return request.headers.get(Authorization.__AUTH) is not None
+
+    @staticmethod
+    def parce_token(request: HttpRequest) -> AuthorizationResponse:
+        if not Authorization.has_auth_token(request):
+            raise PermissionDenied("global.auth.no_token")
+
+        token_reader = file_utils.read_from_file("../../../token.jwt")
+        decoded = jwt.decode(request.headers.get(Authorization.__AUTH), token_reader, algorithms=["HS256"])
+
+        print(decoded)
+
+        return AuthorizationResponse()
