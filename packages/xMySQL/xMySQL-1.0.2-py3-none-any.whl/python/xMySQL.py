496d65c66886357f9d920ae98c776f6039365682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:48 +0000

    Add package xMySQL/1.0.2/xMySQL-1.0.2-py3-none-any.whl

---
+++ packages/xMySQL/xMySQL-1.0.2-py3-none-any.whl/python/xMySQL.py
@@ -0,0 +1,81 @@
+import os
+import mysql.connector
+import configparser
+from sqlalchemy import create_engine
+
+class Config:
+    def __init__(self, database,host,user,password):
+        self.connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.connection.cursor()
+
+    def commit(self,query):
+        self.cursor.execute(query)
+        self.connection.commit()
+
+        data = self.cursor.fetchall()
+        
+        if self.connection:
+            self.connection.close()
+            self.cursor.close()
+
+        return data
+
+    def commit_values(self,query,values):
+        self.cursor.execute(query,values)
+        self.connection.commit()
+
+        data = self.cursor.fetchall()
+        
+        if self.connection:
+            self.connection.close()
+            self.cursor.close()
+
+        return data
+
+    def commit_many(self, query, values):
+        self.cursor.executemany(query,values)
+        self.connection.commit()
+
+        data = self.cursor.fetchall()
+        
+        if self.connection:
+            self.connection.close()
+            self.cursor.close()
+
+        return data
+    
+    def fetchall(self, query):
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
+        
+        if self.connection:
+            self.connection.close()
+            self.cursor.close()
+
+        return data
+
+    def fetchone(self, query):
+        self.cursor.execute(query)
+        data = self.cursor.fetchone()
+        
+        if self.connection:
+            self.connection.close()
+            self.cursor.close()
+
+        return data
+    
+    def fetchmany(self, query):
+        self.cursor.execute(query)
+        data = self.cursor.fetchmany()
+        
+        if self.connection:
+            self.connection.close()
+            self.cursor.close()
+
+        return data
+
