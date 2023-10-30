75c0574c6fa0083e192979713dd9b8769073b9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:05:59 +0000

    Add package lst-pressure/0.0.1a14/lst_pressure-0.0.1a14-py3-none-any.whl

---
+++ packages/lst-pressure/lst_pressure-0.0.1a14-py3-none-any.whl/tests/lstcalendar/test_LSTCalendarDate.py
@@ -0,0 +1,9 @@
+from lstpressure.lstcalendar import LSTCalendar
+from typing import List
+from lstpressure.observation import Observation
+
+
+def test_self_eligible_observations(observations: List["Observation"], lst_calendar: "LSTCalendar"):
+    lst_calendar.load_observations(observations)
+    for i, date in enumerate(lst_calendar.dates):
+        eligible_observations = date.eligible_observations()
