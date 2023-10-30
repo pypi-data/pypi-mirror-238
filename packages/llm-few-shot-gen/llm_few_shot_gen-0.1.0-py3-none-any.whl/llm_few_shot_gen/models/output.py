73954fdd27fc115e4be6d81be51b7ecb4a403394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 04:05:50 +0000

    Add package llm-few-shot-gen/0.1.0/llm_few_shot_gen-0.1.0-py3-none-any.whl

---
+++ packages/llm-few-shot-gen/llm_few_shot_gen-0.1.0-py3-none-any.whl/llm_few_shot_gen/models/output.py
@@ -0,0 +1,9 @@
+from typing import List
+from pydantic import BaseModel, Field
+
+
+# Define your desired templates structure.
+class ImagePromptOutputModel(BaseModel):
+    few_shot_styles: List[str] = Field(description="Styles existing in the example prompts")
+    few_shot_artists: List[str] = Field(description="Artists existing in the example prompts")
+    image_prompts: List[str] = Field(description="List of text-to-image prompts")
