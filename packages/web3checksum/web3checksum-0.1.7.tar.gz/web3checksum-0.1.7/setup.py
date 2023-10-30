caf64a4510438ce53961ac700571789b59006c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 14:06:53 +0000

    Add package web3checksum/0.1.7/web3checksum-0.1.7.tar.gz

---
+++ packages/web3checksum/web3checksum-0.1.7.tar.gz/web3checksum-0.1.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='web3checksum',
+    version='0.1.7',
+    author='WebDev',
+    author_email='webdev181011@gmail.com',
+    description='This module generates and validates checksum addresses using hashing and base58 encoding for improved data integrity.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
