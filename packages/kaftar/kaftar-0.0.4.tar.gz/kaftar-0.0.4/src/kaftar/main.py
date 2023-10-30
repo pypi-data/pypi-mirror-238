7fdca02f7fb3b2790f9cbefd4fe9101a63a00ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:05:55 +0000

    Add package kaftar/0.0.4/kaftar-0.0.4.tar.gz

---
+++ packages/kaftar/kaftar-0.0.4.tar.gz/kaftar-0.0.4/src/kaftar/main.py
@@ -0,0 +1,21 @@
+import os
+from celery import Celery
+
+
+class Notification:
+    def __init__(self,
+                 service_name: str,
+                 broker_env_key: str = 'CELERY_BROKER') -> None:
+        self.app = Celery(service_name, broker=os.environ.get(broker_env_key))
+        self.service_name = service_name
+
+    def send_notification(self,
+                          body: dict,
+                          recipients: list[dict],
+                          send_datetime: str) -> None:
+        self.app.send_task('kaftar.kaftar.send_notification',
+                           (body,
+                            recipients,
+                            self.service_name,
+                            send_datetime),
+                           queue='notification')
