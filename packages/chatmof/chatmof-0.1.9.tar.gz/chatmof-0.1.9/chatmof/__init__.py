b70dcd9cba8c60976f377363a1299e0adae25c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:07:47 +0000

    Add package chatmof/0.1.9/chatmof-0.1.9.tar.gz

---
+++ packages/chatmof/chatmof-0.1.9.tar.gz/chatmof-0.1.9/chatmof/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+__version__ = '0.1.9'   
+__root_dir__ = os.path.dirname(__file__)
+
+
+from langchain.chat_models import ChatOpenAI
+from chatmof.agents.agent import ChatMOF
+from chatmof.config import config
+
+
+__all__ = [
+    "ChatMOF",
+    "config",
+    "ChatOpenAI",
+    "__version__",
+]
