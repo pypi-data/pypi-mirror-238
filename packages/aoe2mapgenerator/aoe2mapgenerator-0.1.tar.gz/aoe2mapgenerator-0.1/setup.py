74986351f37bedf8747bc80dd3e9d972a6fec606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 14:06:36 +0000

    Add package aoe2mapgenerator/0.1/aoe2mapgenerator-0.1.tar.gz

---
+++ packages/aoe2mapgenerator/aoe2mapgenerator-0.1.tar.gz/aoe2mapgenerator-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aoe2mapgenerator",
+    version="0.1",
+    packages=find_packages(),
+    author="Joseph Hayden",
+    author_email="joseph6433@gmail.com",
+    description="A short description of your package",
+    install_requires=[],
+)
