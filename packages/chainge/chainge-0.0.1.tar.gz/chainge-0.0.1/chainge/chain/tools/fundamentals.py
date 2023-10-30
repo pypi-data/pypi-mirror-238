64a2071403aef340dc77922273b9bdc6a6efe611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 03:07:19 +0000

    Add package chainge/0.0.1/chainge-0.0.1.tar.gz

---
+++ packages/chainge/chainge-0.0.1.tar.gz/chainge-0.0.1/chainge/chain/tools/fundamentals.py
@@ -0,0 +1,79 @@
+
+from langchain.tools import BaseTool, StructuredTool, Tool, tool
+from pydantic import BaseModel, Field
+from typing import Optional, Type
+
+#Import our Stock Adapter
+from chainge.api.api import stock_api
+from typing import Union, Tuple, Dict
+
+'''
+
+Tool Structure -->
+
+1. Agent first gets a list of all financial attributes in object
+2. Agent passess input to the tool of all the attributes it needs:
+
+'''
+
+class StockProbeInput(BaseModel):
+    anything: str = Field()
+
+class StockFundamentalsProbeTool(BaseTool):
+    name = "stock_fundamentals_probe"
+
+    description = """Use this tool BEFORE the stock fundamentals tool to see what attributes the fundamentals tool offers. Whatever attributes you see here
+    must be used VERBATIM in the stock fundamentals tool. You won't have to pass any inputs here, but keep track of the values from the list that you'll need to answer your question. 
+    """
+
+    args_schema: Type[BaseModel] = StockProbeInput 
+
+    #Ensure no arguments are passed in
+    def _to_args_and_kwargs(self, tool_input: Union[str, Dict]) -> Tuple[Tuple, Dict]:
+        return (), {}
+
+    def _run(
+        self, run_manager = None
+    ) -> str:
+        """Use the tool."""
+        return f'Here is a list of all the fundamental attributes: {",".join(stock_api.fundamentals("AAPL")["metric"].keys())}'
+
+    async def _arun(
+        self, stock_ticker_name: str, run_manager = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        return f'Here is a list of all the fundamental attributes: {",".join(stock_api.fundamentals("AAPL")["metric"].keys())}'
+
+
+
+class StockFundamentalsInput(BaseModel):
+    stock_ticker_name: str = Field()
+    requested_metrics: str = Field()
+
+
+class StockFundamentalsTool(BaseTool):
+    name = "stock_fundamentals_post_probe_data"
+
+    description = """Use this tool after the stock probe to answer basic questions about stocks, including pricing, average trading volume, volatility (beta), cash flow, and 
+    price highs and low
+    
+    input: stock ticker; AAPL, GOOG, and so on. REMEMBER TO USE THE STOCK PROBE TOOL BEFORE THIS!
+    """
+    args_schema: Type[BaseModel] = StockFundamentalsInput
+
+    def _run(
+        self, stock_ticker_name: str, requested_metrics: str, run_manager = None
+    ) -> str:
+        """Use the tool."""
+
+        desired_attributes = [ v.strip() for v in requested_metrics.split(',')]
+        print("Looking for ", desired_attributes)
+
+        attr = stock_api.fundamentals(stock_ticker_name)["metric"]
+        return str({k : attr[k] for k in attr if k in desired_attributes})
+
+    async def _arun(
+        self, stock_ticker_name: str, run_manager = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        return str({k : attr[k] for k in attr if k in desired_attributes})
