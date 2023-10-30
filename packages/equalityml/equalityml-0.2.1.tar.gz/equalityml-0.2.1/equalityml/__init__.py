cdb25854913989edd9766b5444678d73b7795300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 04:06:20 +0000

    Add package equalityml/0.2.1/equalityml-0.2.1.tar.gz

---
+++ packages/equalityml/equalityml-0.2.1.tar.gz/equalityml-0.2.1/equalityml/__init__.py
@@ -0,0 +1,10 @@
+"""Fairness Assessment and Inequality Reduction tools to help deal with sensitive attributes."""
+
+from .fair import FAIR
+from .stats import paired_ttest
+from .threshold import discrimination_threshold, binary_threshold_score
+
+
+__name__ = "equalityml"
+__version__ = '0.2.1'
+__all__ = ["FAIR"]
