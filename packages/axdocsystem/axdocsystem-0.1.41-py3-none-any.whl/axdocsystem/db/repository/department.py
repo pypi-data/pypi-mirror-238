8a9b821072765ccaf4d7a2b4e5029f289b47ea96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-30 15:06:26 +0000

    Add package axdocsystem/0.1.41/axdocsystem-0.1.41-py3-none-any.whl

---
+++ packages/axdocsystem/axdocsystem-0.1.41-py3-none-any.whl/axdocsystem/db/repository/department.py
@@ -0,0 +1,24 @@
+from sqlalchemy import select
+from axdocsystem.db.models import Department as Model
+from axdocsystem.db.models import Organization as OrganizationModel
+from axdocsystem.db.schemas import DepartmentSchema as Schema
+from axdocsystem.db.schemas import DepartmentFullSchema as FullSchema
+from .base import BaseRepository
+
+
+class DepartmentRepository(BaseRepository[Model, Schema, FullSchema]):
+    @property
+    def _base_get_query(self):
+        return (
+            select(self.Model)
+            .join(
+                OrganizationModel, 
+                onclause=OrganizationModel.id==self.Model.organization_id,
+                isouter=True,
+            )
+        )
+
+    @property
+    def _base_all_query(self):
+        return self._base_get_query
+
