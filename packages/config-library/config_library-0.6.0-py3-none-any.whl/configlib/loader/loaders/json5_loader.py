d2d4249a6abfe0c25f60a74e6c731b3c6cac22fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 07:06:19 +0000

    Add package config-library/0.6.0/config_library-0.6.0-py3-none-any.whl

---
+++ packages/config-library/config_library-0.6.0-py3-none-any.whl/configlib/loader/loaders/json5_loader.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import os
+import typing as t
+from configlib.exceptions import NotSupportedError
+from configlib.loader import register_loader
+from .json_loader import ReturnType
+try:
+    import json5
+except ModuleNotFoundError:
+    raise NotSupportedError('please install config-library[json5] for this')
+
+
+@register_loader('json5')
+def load_json5(fp: t.Union[str, os.PathLike]) -> ReturnType:
+    r"""
+    {
+      // comments
+      unquoted: 'and you can quote me on that',
+      singleQuotes: 'I can use "double quotes" here',
+      lineBreaks: "Look, Mom! \
+    No \\n's!",
+      hexadecimal: 0xdecaf,
+      leadingDecimalPoint: .8675309, andTrailing: 8675309.,
+      positiveSign: +1,
+      trailingComma: 'in objects', andIn: ['arrays',],
+      "backwardsCompatible": "with JSON",
+    }
+    """
+    with open(fp, 'r') as file:
+        return json5.load(file)
