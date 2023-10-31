63fdad05f71fe4a62b66e32ac079896e3009faae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:50 +0000

    Add package fgslpyrest/1.0.1/fgslpyrest-1.0.1.tar.gz

---
+++ packages/fgslpyrest/fgslpyrest-1.0.1.tar.gz/fgslpyrest-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fgslpyrest',
+    version='1.0.1',
+    author='Flávio Gomes da Silva Lisboa',
+    author_email='flavio.lisboa@fgsl.eti.br',
+    description='A class to make HTTP REST requests',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
