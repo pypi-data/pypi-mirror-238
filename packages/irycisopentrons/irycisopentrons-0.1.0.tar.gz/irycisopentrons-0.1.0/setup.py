9245becebb9b96974a8cc2ba15ec8247746a1c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:05:28 +0000

    Add package irycisopentrons/0.1.0/irycisopentrons-0.1.0.tar.gz

---
+++ packages/irycisopentrons/irycisopentrons-0.1.0.tar.gz/irycisopentrons-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='irycisopentrons',
+    version='0.1.0',
+    author='Mariana Fuentes-Quintana Miralles',
+    author_email='marianafuentesmail@gmail.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
