349f4fca0067d9e328f97f8e0492b1a499e8e3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:19 +0000

    Add package BetheFluid/0.1/BetheFluid-0.1.tar.gz

---
+++ packages/BetheFluid/BetheFluid-0.1.tar.gz/BetheFluid-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="BetheFluid",
+    version="0.1",
+    description='Python package for solving GHD equations',
+    author='Antoni Lis',
+    packages=['BetheFluid'],
+    zip_safe=False
+)
