6c457840be3ffbb958c82c97e8b4439d7a7d21e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:05:42 +0000

    Add package hkshiteshpkgnew/0.0.1/hkshiteshpkgnew-0.0.1.tar.gz

---
+++ packages/hkshiteshpkgnew/hkshiteshpkgnew-0.0.1.tar.gz/hkshiteshpkgnew-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='hkshiteshpkgnew',
+    version='0.0.1',
+    description='limeguru python module',
+    url='https://github.com/hkshitesh/test',
+    author='hkshitesh',
+    author_email='hkshitesh@gmail.com',
+    packages=setuptools.find_packages(),    
+    long_description='limeguru python module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
