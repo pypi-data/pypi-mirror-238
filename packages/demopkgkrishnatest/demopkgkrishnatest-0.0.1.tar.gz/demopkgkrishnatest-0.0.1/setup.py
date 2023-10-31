5fd045b3875e230b62032e036102575dd3b69405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:09:22 +0000

    Add package demopkgkrishnatest/0.0.1/demopkgkrishnatest-0.0.1.tar.gz

---
+++ packages/demopkgkrishnatest/demopkgkrishnatest-0.0.1.tar.gz/demopkgkrishnatest-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='demopkgkrishnatest',
+    version='0.0.1',
+    description='kr python module',
+    author='bhanushali-adi',
+    author_email='krishna.bhanushali@analog.com',
+    packages=setuptools.find_packages(),    
+    long_description='krishna python module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
