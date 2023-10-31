04844655ccc51e481117977eb34b9f54d31d4390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:08:28 +0000

    Add package asic/0.1.0/asic-0.1.0.tar.gz

---
+++ packages/asic/asic-0.1.0.tar.gz/asic-0.1.0/src/asic/config.py
@@ -0,0 +1,68 @@
+import json as json
+import re
+from enum import Enum
+
+import pkg_resources
+from pydantic import BaseModel, constr
+
+LOCATION_TEMPLATE = "{remote_parent}/{normalized_version}/{remote_name}"
+
+LOCATION_REGEX = re.compile(
+    r"""
+    /                                       # Inner directory start
+    (?P<location_year>[0-9]{4})             # Year of the location directory
+    -                                       # Separator between year and month
+    (?P<location_month>[0-9]{2})            # Month of the location directory
+    /                                       # Directory sepparator
+    (?P<location_normalized_version>[0-9]{3})  # Version of the location directory
+    /                                       # Inner directory end
+    """,
+    re.VERBOSE,
+)
+
+
+class ASICExtesionMap(BaseModel):
+    asic_extension: constr(pattern=r"^\.[a-zA-Z0-9]*$")  # type: ignore # noqa: F722
+    normalized_version: constr(pattern=r"^[0-9]{3}$")  # type: ignore # noqa: F722
+    order: int
+
+
+class ASICFileVisibility(str, Enum):
+    PUBLIC = "public"
+    AGENT = "agent"
+
+
+class ASICFileConfig(BaseModel):
+    code: str
+    visibility: ASICFileVisibility
+    name_pattern: str
+    location_pattern: str
+    description: str | None
+
+
+def load_asic_file_extension_map() -> dict[str, ASICExtesionMap]:
+    """Return a list of ASIC extension maps"""
+    # This is a stream-like object. If you want the actual info, call
+    # stream.read()
+    stream = pkg_resources.resource_stream("asic", "data/ASIC_FILE_EXTENSION_MAP.jsonl")
+    lines = []
+    for line in stream:
+        lines.append(json.loads(line))
+
+    asic_file_extension_mapper = {
+        line["asic_extension"]: ASICExtesionMap.model_validate(line) for line in lines
+    }
+    return asic_file_extension_mapper
+
+
+def load_asic_file_config() -> dict[str, ASICFileConfig]:
+    """Return a list of ASIC file configurations"""
+    # This is a stream-like object. If you want the actual info, call
+    # stream.read()
+    stream = pkg_resources.resource_stream("asic", "data/ASIC_FILE_CONFIG.jsonl")
+    lines = []
+    for line in stream:
+        lines.append(json.loads(line))
+
+    asic_file_config = {line["code"]: ASICFileConfig.model_validate(line) for line in lines}
+    return asic_file_config
