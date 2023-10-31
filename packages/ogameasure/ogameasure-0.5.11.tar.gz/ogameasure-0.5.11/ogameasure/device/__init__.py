06654b0e07a6d7f6526d7971bd6dc3e8504b633e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 17:07:30 +0000

    Add package ogameasure/0.5.11/ogameasure-0.5.11.tar.gz

---
+++ packages/ogameasure/ogameasure-0.5.11.tar.gz/ogameasure-0.5.11/ogameasure/device/__init__.py
@@ -0,0 +1,15 @@
+from .SCPI import scpi_common
+
+from . import Agilent
+from . import Agilent as Keysight
+from . import Anritsu
+#from . import Canon
+from . import Cosmotechs
+from . import ELVA1
+from . import HEIDENHAIN
+from . import KIKUSUI
+from . import Lakeshore
+from . import Pfeiffer
+from . import Phasematrix
+from . import SENA
+from . import TandD
