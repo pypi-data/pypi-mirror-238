555a29038403df32b8e48867659e699f6ef6a8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:06:35 +0000

    Add package dstack/0.12.2rc1/dstack-0.12.2rc1.tar.gz

---
+++ packages/dstack/dstack-0.12.2rc1.tar.gz/dstack-0.12.2rc1/src/dstack/_internal/core/models/backends/base.py
@@ -0,0 +1,28 @@
+import enum
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class BackendType(str, enum.Enum):
+    AWS = "aws"
+    AZURE = "azure"
+    GCP = "gcp"
+    LAMBDA = "lambda"
+    LOCAL = "local"
+    TENSORDOCK = "tensordock"
+
+
+class ConfigElementValue(BaseModel):
+    value: str
+    label: str
+
+
+class ConfigElement(BaseModel):
+    selected: Optional[str] = None
+    values: List[ConfigElementValue] = []
+
+
+class ConfigMultiElement(BaseModel):
+    selected: List[str] = []
+    values: List[ConfigElementValue] = []
