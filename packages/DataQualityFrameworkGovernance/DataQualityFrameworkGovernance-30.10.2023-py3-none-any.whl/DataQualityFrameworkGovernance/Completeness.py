a67adad44249bb7573189997fc5ec6e69c5b9cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:08:25 +0000

    Add package DataQualityFrameworkGovernance/30.10.2023/DataQualityFrameworkGovernance-30.10.2023-py3-none-any.whl

---
+++ packages/DataQualityFrameworkGovernance/DataQualityFrameworkGovernance-30.10.2023-py3-none-any.whl/DataQualityFrameworkGovernance/Completeness.py
@@ -0,0 +1,20 @@
+# Completeness
+
+def missing_values(location):
+    import pandas as pd
+    # Load dataset
+    df = pd.read_csv(location)
+
+    missing_values = df.isnull().sum()
+    print(missing_values)
+
+def completeness_percentage(location):
+    import pandas as pd
+    # Load dataset
+    df = pd.read_csv(location)
+    
+    total_entries = df.shape[0]
+    complete_entries = df.dropna().shape[0]
+    completeness_percentage = (complete_entries / total_entries) * 100
+    completeness_percentage = f"Data completeness: {completeness_percentage:.2f}%"
+    print(completeness_percentage)
