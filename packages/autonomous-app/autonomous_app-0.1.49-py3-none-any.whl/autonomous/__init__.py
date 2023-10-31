233f13d425f137694d028c3973b935c58cb026a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:43 +0000

    Add package autonomous-app/0.1.49/autonomous_app-0.1.49-py3-none-any.whl

---
+++ packages/autonomous-app/autonomous_app-0.1.49-py3-none-any.whl/autonomous/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.49"
+
+from .logger import log
+from .model.automodel import AutoModel
+from .auth.autoauth import AutoAuth
---
+++ packages/autonomous-app/autonomous_app-0.1.49-py3-none-any.whl/autonomous/app_template/app/views/index.py
@@ -0,0 +1,41 @@
+# Built-In Modules
+
+# external Modules
+from flask import Blueprint, render_template, request, session
+
+from autonomous import log
+from autonomous.auth import auth_required
+
+index_page = Blueprint("index", __name__)
+
+
+@index_page.route("/", methods=("GET",))
+def index():
+    return render_template("index.html")
+
+
+@index_page.route(
+    "/protected",
+    methods=(
+        "GET",
+        "POST",
+    ),
+)
+@auth_required
+def protected():
+    return {"result": "success"}
+
+
+@index_page.route("/add", methods=("POST",))
+def add():
+    return {"result": "success"}
+
+
+@index_page.route("/update", methods=("POST",))
+def updates():
+    return "updated"
+
+
+@index_page.route("/delete", methods=("POST",))
+def delete():
+    return "deleted"
