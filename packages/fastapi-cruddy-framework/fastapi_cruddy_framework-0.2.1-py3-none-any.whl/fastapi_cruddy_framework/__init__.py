7c1dbb43442d9cf894ec5348919e14bac12f7441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:29 +0000

    Add package fastapi-cruddy-framework/0.2.1/fastapi_cruddy_framework-0.2.1-py3-none-any.whl

---
+++ packages/fastapi-cruddy-framework/fastapi_cruddy_framework-0.2.1-py3-none-any.whl/fastapi_cruddy_framework/__init__.py
@@ -0,0 +1,40 @@
+# This is a rails-like library. You're welcome Python community.
+# Love,
+# A Sails / Ember lover.
+# -----------------------------------------------------------------
+from .inflector import pluralizer
+from .uuid import UUID, uuid7
+from .schemas import (
+    T,
+    RelationshipConfig,
+    CruddyGenericModel,
+    BulkDTO,
+    MetaObject,
+    PageResponse,
+    ResponseSchema,
+    CruddyModel,
+    CruddyIntIDModel,
+    CruddyUUIDModel,
+    ExampleUpdate,
+    ExampleCreate,
+    ExampleView,
+    Example,
+    UTCDateTime,
+)
+from .controller import Actions, CruddyController, ControllerConfigurator
+from .repository import AbstractRepository
+from .adapters import BaseAdapter, SqliteAdapter, MysqlAdapter, PostgresqlAdapter
+from .resource import Resource, ResourceRegistry, CruddyResourceRegistry
+from .router import getModuleDir, getDirectoryModules, CreateRouterFromResources
+from .util import (
+    get_pk,
+    possible_id_types,
+    lifecycle_types,
+    build_tz_aware_date,
+    coerce_to_utc_datetime,
+)
+from .test_helpers import BrowserTestClient
+from async_asgi_testclient import TestClient
+from async_asgi_testclient.websocket import WebSocketSession
+
+# -----------------------------------------------------------------
