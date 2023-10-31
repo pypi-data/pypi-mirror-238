ad3018dc5165054b967ca151f07310b25bfffd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:06:23 +0000

    Add package letterbomb-web/1.0.1/letterbomb_web-1.0.1-py3-none-any.whl

---
+++ packages/letterbomb-web/letterbomb_web-1.0.1-py3-none-any.whl/letterbomb_web/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# SPDX-License-Identifier: MIT
+"""CLI component."""
+
+import letterbomb_web.app
+
+if __name__ == "__main__":
+    letterbomb_web.app.start()
