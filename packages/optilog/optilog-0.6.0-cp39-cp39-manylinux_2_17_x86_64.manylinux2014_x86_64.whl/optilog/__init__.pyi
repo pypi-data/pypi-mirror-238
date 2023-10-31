2116e88d6c67e0a3f58c30d8afe4204c269b8da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:36 +0000

    Add package optilog/0.6.0/optilog-0.6.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/optilog/optilog-0.6.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/optilog/__init__.pyi
@@ -0,0 +1,10 @@
+from _typeshed import Incomplete
+from typing import NamedTuple
+
+class VersionInfo(NamedTuple):
+    major: Incomplete
+    minor: Incomplete
+    micro: Incomplete
+    release_level: Incomplete
+
+version_info: Incomplete
