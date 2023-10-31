8e64b598ae0e844c64d02e223b137de8195e638f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:44 +0000

    Add package pytest-mysql/2.5.0/pytest-mysql-2.5.0.tar.gz

---
+++ packages/pytest-mysql/pytest-mysql-2.5.0.tar.gz/pytest-mysql-2.5.0/tests/test_mysqlnoproc.py
@@ -0,0 +1,30 @@
+"""MySQL tests that do not start mysql server."""
+from MySQLdb import Connection
+
+from pytest_mysql import factories
+from tests.test_mysql import QUERY
+
+mysql_noproc2 = factories.mysql_noproc()
+mysqlnoproc_client = factories.mysql("mysql_noproc2")
+
+
+def test_mysql_noproc(mysqlnoproc_client: Connection) -> None:
+    """Check if noproc fixture connects to the running mysql instance."""
+    cursor = mysqlnoproc_client.cursor()
+    cursor.execute(QUERY)
+    mysqlnoproc_client.commit()  # type: ignore
+    cursor.close()
+
+
+def test_mysql_noproc_closing_connection_not_throwing_exception(
+    mysqlnoproc_client: Connection,
+) -> None:
+    """Check if closing the connection doesn't throw an exception.
+
+    When cleaning the fixture.
+    """
+    cursor = mysqlnoproc_client.cursor()
+    cursor.execute(QUERY)
+    mysqlnoproc_client.commit()  # type: ignore
+    cursor.close()
+    mysqlnoproc_client.close()  # type: ignore
