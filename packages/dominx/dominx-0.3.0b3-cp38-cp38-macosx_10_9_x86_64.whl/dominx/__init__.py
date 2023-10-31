fbfde958cd1b7fe365da45efe70fc35cf1967592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:06:45 +0000

    Add package dominx/0.3.0b3/dominx-0.3.0b3-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/dominx/dominx-0.3.0b3-cp38-cp38-macosx_10_9_x86_64.whl/dominx/__init__.py
@@ -0,0 +1,12 @@
+"""
+The Python library for game development.
+
+The **dominx** library is a powerful high-level Python library designed specifically
+for game development, leveraging the low-level APIs of operating systems like Win32,
+X11, Cocoa, and more. It offers a seamless interface for game developers to harness
+the full potential of these low-level APIs without getting bogged down in complex
+code.
+"""
+
+__author__ = "Zenthm"
+__version__ = "0.3.0b3"
