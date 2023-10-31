0bb42b083534117d9df7f5627ef6d004ff998f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:10:23 +0000

    Add package types-awscrt/0.19.7/types_awscrt-0.19.7-py3-none-any.whl

---
+++ packages/types-awscrt/types_awscrt-0.19.7-py3-none-any.whl/awscrt-stubs/s3.pyi
@@ -0,0 +1,131 @@
+from concurrent.futures import Future
+from dataclasses import dataclass
+from enum import IntEnum
+from threading import Event
+from typing import Any, Callable, List, Optional, Tuple
+
+from awscrt import NativeResource as NativeResource
+from awscrt.auth import AwsCredentialsProvider as AwsCredentialsProvider
+from awscrt.auth import AwsSigningConfig
+from awscrt.http import HttpRequest as HttpRequest
+from awscrt.io import ClientBootstrap as ClientBootstrap
+from awscrt.io import TlsConnectionOptions as TlsConnectionOptions
+
+class S3RequestType(IntEnum):
+    DEFAULT: int
+    GET_OBJECT: int
+    PUT_OBJECT: int
+
+class S3RequestTlsMode(IntEnum):
+    ENABLED: int
+    DISABLED: int
+
+class S3ChecksumAlgorithm(IntEnum):
+    CRC32C: int
+    CRC32: int
+    SHA1: int
+    SHA256: int
+
+class S3ChecksumLocation(IntEnum):
+    HEADER: int
+    TRAILER: int
+
+@dataclass
+class S3ChecksumConfig:
+    algorithm: Optional[S3ChecksumAlgorithm] = ...
+    location: Optional[S3ChecksumLocation] = ...
+    validate_response: bool = ...
+
+class S3Client(NativeResource):
+    shutdown_event: Event
+    def __init__(
+        self,
+        *,
+        bootstrap: Optional[ClientBootstrap],
+        region: str,
+        tls_mode: Optional[S3RequestTlsMode] = ...,
+        signing_config: Optional[AwsSigningConfig] = ...,
+        credential_provider: Optional[S3RequestTlsMode] = ...,
+        tls_connection_options: Optional[TlsConnectionOptions] = ...,
+        part_size: Optional[int] = ...,
+        throughput_target_gbps: Optional[float] = ...,
+    ) -> None: ...
+    def make_request(
+        self,
+        *,
+        request: HttpRequest,
+        type: S3RequestType,
+        signing_config: Optional[AwsSigningConfig] = ...,
+        credential_provider: Optional[AwsCredentialsProvider] = ...,
+        checksum_config: Optional[S3ChecksumConfig] = ...,
+        recv_filepath: Optional[str] = ...,
+        send_filepath: Optional[str] = ...,
+        on_headers: Optional[Callable[[int, List[Tuple[str, str]]], None]] = ...,
+        on_body: Optional[Callable[[bytes, int], None]] = ...,
+        on_done: Optional[
+            Callable[
+                [Optional[BaseException], Optional[List[Tuple[str, str]]], Optional[bytes]], None
+            ]
+        ] = ...,
+        on_progress: Optional[Callable[[int], None]] = ...,
+    ) -> S3Request: ...
+
+class S3Request(NativeResource):
+    shutdown_event: Event
+    def __init__(
+        self,
+        *,
+        client: S3Client,
+        request: HttpRequest,
+        type: S3RequestType,
+        signing_config: Optional[AwsSigningConfig] = ...,
+        credential_provider: Optional[AwsCredentialsProvider] = ...,
+        checksum_config: Optional[S3ChecksumConfig] = ...,
+        recv_filepath: Optional[str] = ...,
+        send_filepath: Optional[str] = ...,
+        on_headers: Optional[Callable[[int, List[Tuple[str, str]]], None]] = ...,
+        on_body: Optional[Callable[[bytes, int], None]] = ...,
+        on_done: Optional[
+            Callable[
+                [Optional[BaseException], Optional[List[Tuple[str, str]]], Optional[bytes]], None
+            ]
+        ] = ...,
+        on_progress: Optional[Callable[[int], None]] = ...,
+        region: Optional[str] = ...,
+    ) -> None: ...
+    @property
+    def finished_future(self) -> Future[Optional[BaseException]]: ...
+    def cancel(self) -> None: ...
+
+class _S3ClientCore:
+    def __init__(
+        self,
+        bootstrap: ClientBootstrap,
+        credential_provider: Optional[AwsCredentialsProvider] = ...,
+        signing_config: Optional[AwsSigningConfig] = ...,
+        tls_connection_options: Optional[TlsConnectionOptions] = ...,
+    ) -> None: ...
+
+class _S3RequestCore:
+    def __init__(
+        self,
+        request: HttpRequest,
+        finish_future: Future[Optional[BaseException]],
+        shutdown_event: Event,
+        signing_config: Optional[AwsSigningConfig] = ...,
+        credential_provider: Optional[AwsCredentialsProvider] = ...,
+        on_headers: Optional[Callable[[int, List[Tuple[str, str]]], None]] = ...,
+        on_body: Optional[Callable[[bytes, int], None]] = ...,
+        on_done: Optional[
+            Callable[
+                [Optional[BaseException], Optional[List[Tuple[str, str]]], Optional[bytes]], None
+            ]
+        ] = ...,
+        on_progress: Optional[Callable[[int], None]] = ...,
+    ) -> None: ...
+
+def create_default_s3_signing_config(
+    *, region: str, credential_provider: AwsCredentialsProvider, **kwargs: Any
+) -> AwsSigningConfig: ...
+def get_ec2_instance_type() -> str: ...
+def is_optimized_for_system() -> bool: ...
