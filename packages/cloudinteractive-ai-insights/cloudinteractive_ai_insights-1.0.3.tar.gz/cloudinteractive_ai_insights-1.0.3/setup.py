3f125f3809241b8757894b623ee218e60f535846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:09:50 +0000

    Add package cloudinteractive-ai-insights/1.0.3/cloudinteractive_ai_insights-1.0.3.tar.gz

---
+++ packages/cloudinteractive-ai-insights/cloudinteractive_ai_insights-1.0.3.tar.gz/cloudinteractive_ai_insights-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudinteractive_ai_insights",
+    version="1.0.3",
+    description="Collection of AI tools designed to assist with your assignments and projects.",
+    author="CoppermineSP",
+    author_email="copperminesp@cloudinteractive.net",
+    url="https://github.com/Coppermine-SP/cloudinteractive-ai-insights",
+    install_requires=["azure-cognitiveservices-vision-computervision","openai","pdf2image","tiktoken"],
+    python_requires=">=3.9",
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ai-insights=cloudinteractive_ai_insights.main:main',
+        ]
+    }
+)
