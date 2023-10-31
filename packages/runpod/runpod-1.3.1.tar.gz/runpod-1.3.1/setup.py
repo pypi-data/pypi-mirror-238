d6f3c13c9ead78aa354c9895c9687978a40f4c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:05:24 +0000

    Add package runpod/1.3.1/runpod-1.3.1.tar.gz

---
+++ packages/runpod/runpod-1.3.1.tar.gz/runpod-1.3.1/setup.py
@@ -0,0 +1,23 @@
+'''
+runpod-python | setup.py
+Called to setup the runpod-python package.
+'''
+
+from setuptools import setup
+
+with open('requirements.txt', encoding="UTF-8") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+if __name__ == "__main__":
+    setup(
+        name='runpod',
+        use_scm_version=True,
+        setup_requires=['setuptools_scm'],
+        install_requires=requirements,
+
+        entry_points={
+            'console_scripts': [
+                'runpod = runpod.cli.entry:runpod_cli'
+            ]
+        }
+    )
