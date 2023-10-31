4023f81214109a16c8b58ce0a6017454356f4902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:06:35 +0000

    Add package sagemaker-jupyterlab-extension/0.1.3/sagemaker-jupyterlab-extension-0.1.3.tar.gz

---
+++ packages/sagemaker-jupyterlab-extension/sagemaker-jupyterlab-extension-0.1.3.tar.gz/sagemaker-jupyterlab-extension-0.1.3/sagemaker_jupyterlab_extension/tests/helper.py
@@ -0,0 +1,36 @@
+import os
+import json
+from pathlib import Path
+
+""""
+This creates a temporary log file in the test folder, 
+make sure to cleanup later by invoking the cleanup function and pass right path
+"""
+
+# Don not touch this line until logging path FileHandler is changed in SageMakerJupterLabExtensionCommon.
+JUPTER_LOG_FILES_PATH = "jupterlab/"
+
+
+def set_log_file_directory(env_var):
+    log_path = Path(__file__).parents[1]
+    os.environ[env_var] = str(log_path)
+    log_path = os.path.join(log_path, JUPTER_LOG_FILES_PATH)
+    return log_path
+
+
+"""Read the last entry from the temporary logfile"""
+
+
+def get_last_entry(file_name):
+    with open(file_name) as fid:
+        lines = fid.readlines()
+    return json.loads(lines[-1])
+
+
+"""Remove the temporary logfile"""
+
+
+def remove_temp_file_and_env(logile, env_var, filepath):
+    os.remove(logile)
+    os.rmdir(filepath)
+    del os.environ[env_var]
