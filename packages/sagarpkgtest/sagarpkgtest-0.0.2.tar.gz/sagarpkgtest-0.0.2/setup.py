707dbf542cec65322da835eeed6335047953703a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:09:06 +0000

    Add package sagarpkgtest/0.0.2/sagarpkgtest-0.0.2.tar.gz

---
+++ packages/sagarpkgtest/sagarpkgtest-0.0.2.tar.gz/sagarpkgtest-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='sagarpkgtest',
+    version='0.0.2',
+    description='ADI python Package module',
+    url='https://github.com/Sagya7/python_day2_capstone.git',
+    author='sagar',
+    author_email='sagarwalishettiiitm@gmail.com',
+    packages=setuptools.find_packages(),
+    long_description='sagar python module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+
+)
