0010af37626ca6caeaf5a279129335b4675d7c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:10:02 +0000

    Add package ibims/0.1.6/ibims-0.1.6.tar.gz

---
+++ packages/ibims/ibims-0.1.6.tar.gz/ibims-0.1.6/src/ibims/bo/rechnung.py
@@ -0,0 +1,22 @@
+"""
+Extend BO rechnung to hold more possible invoice types
+"""
+from typing import Optional
+
+from bo4e.bo.rechnung import Rechnung
+
+from ibims.com import RechnungspositionErweitert
+from ibims.enum import RechnungstypErweitert
+
+
+class RechnungErweitert(Rechnung):
+    """
+    replace enum Rechnungstyp mit RechnungstypErweitert and add attributes, which can be found in the
+    go implementation
+    https://github.com/Hochfrequenz/go-bo4e/blob/3414a1eac741542628df796d6beb43eaa27b0b3e/bo/rechnung.go
+    """
+
+    rechnungspositionen: Optional[list[RechnungspositionErweitert]] = None  # type: ignore[assignment]
+    rechnungstyp: RechnungstypErweitert  # type: ignore[assignment]
+    ist_selbstausgestellt: Optional[bool] = None
+    ist_reverse_charge: Optional[bool] = None
