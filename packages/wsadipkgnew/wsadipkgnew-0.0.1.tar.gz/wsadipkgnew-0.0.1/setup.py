dee9b4076e659240819d0a3a256c9f91a3889d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:07:53 +0000

    Add package wsadipkgnew/0.0.1/wsadipkgnew-0.0.1.tar.gz

---
+++ packages/wsadipkgnew/wsadipkgnew-0.0.1.tar.gz/wsadipkgnew-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='wsadipkgnew',
+    version='0.0.1',
+    description='ADI python Package module',
+    url='https://github.com/waseema',
+    author='waseema',
+    author_email='vaseemamit@gmail.com',
+    packages=setuptools.find_packages(),
+    long_description='ADI python Package module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
