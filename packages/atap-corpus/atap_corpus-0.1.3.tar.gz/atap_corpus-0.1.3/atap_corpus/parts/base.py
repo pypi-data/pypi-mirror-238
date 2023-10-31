07e08708acef5a6e916e34d716bdcd08d3b4cfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:05:54 +0000

    Add package atap-corpus/0.1.3/atap_corpus-0.1.3.tar.gz

---
+++ packages/atap-corpus/atap_corpus-0.1.3.tar.gz/atap_corpus-0.1.3/atap_corpus/parts/base.py
@@ -0,0 +1,21 @@
+from abc import ABCMeta, abstractmethod
+from typing import Callable
+
+from atap_corpus.interfaces import Clonable, Serialisable
+from atap_corpus._types import Docs, Doc, TFreqTable
+
+
+class BaseFreqTable(metaclass=ABCMeta):
+    pass
+
+
+class BaseDTM(Clonable, Serialisable, metaclass=ABCMeta):
+    # todo: core interface functions need to be defined here.
+    @abstractmethod
+    def to_freqtable(self) -> TFreqTable:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def from_docs(cls, docs: Docs, tokeniser_func: Callable[[Doc], list[str]], *args, **kwargs):
+        raise NotImplementedError()
