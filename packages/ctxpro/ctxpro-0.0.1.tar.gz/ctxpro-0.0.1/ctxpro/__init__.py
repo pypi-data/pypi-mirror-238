3093e5eef5a9a12b83a4121a18fcf5c8593fb13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:05:55 +0000

    Add package ctxpro/0.0.1/ctxpro-0.0.1.tar.gz

---
+++ packages/ctxpro/ctxpro-0.0.1.tar.gz/ctxpro-0.0.1/ctxpro/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__version__ = '0.0.1'
+__description__ = 'Simple toolkit that extracts ambiguities in documents that require context to resolve.'
+
+def annotate():
+    raise NotImplementedError
