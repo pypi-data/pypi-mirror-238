03bef359b10caf21f17a2f966ea120898786a27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:05:38 +0000

    Add package netunicorn-base/0.3.8/netunicorn-base-0.3.8.tar.gz

---
+++ packages/netunicorn-base/netunicorn-base-0.3.8.tar.gz/netunicorn-base-0.3.8/src/netunicorn/base/__init__.py
@@ -0,0 +1,32 @@
+"""
+A base module with all the core classes and functions.
+"""
+
+from returns.pipeline import is_successful
+from returns.result import Failure, Result, Success
+
+from .architecture import Architecture
+from .environment_definitions import DockerImage, ShellExecution
+from .experiment import Experiment, ExperimentExecutionInformation, ExperimentStatus
+from .nodes import Node
+from .pipeline import Pipeline
+from .task import Task, TaskDispatcher
+from .types import FlagValues
+
+__all__ = [
+    "Experiment",
+    "ExperimentStatus",
+    "ExperimentExecutionInformation",
+    "Pipeline",
+    "Task",
+    "TaskDispatcher",
+    "Result",
+    "Success",
+    "Failure",
+    "is_successful",
+    "FlagValues",
+    "ShellExecution",
+    "DockerImage",
+    "Node",
+    "Architecture",
+]
