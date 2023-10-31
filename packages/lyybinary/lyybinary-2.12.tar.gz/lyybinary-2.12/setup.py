a4080867ef982ea3fe3cd0a6e65558e58c28827a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:09:10 +0000

    Add package lyybinary/2.12/lyybinary-2.12.tar.gz

---
+++ packages/lyybinary/lyybinary-2.12.tar.gz/lyybinary-2.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyybinary',
+    version='2.12',
+    author='lyy',
+    author_email='',
+    description='calendar for  lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
