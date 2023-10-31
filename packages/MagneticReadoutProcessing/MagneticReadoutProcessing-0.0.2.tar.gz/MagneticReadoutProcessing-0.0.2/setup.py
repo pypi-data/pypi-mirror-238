40b1c35c7555f05aabb3a2616cab415917bac978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 17:07:29 +0000

    Add package MagneticReadoutProcessing/0.0.2/MagneticReadoutProcessing-0.0.2.tar.gz

---
+++ packages/MagneticReadoutProcessing/MagneticReadoutProcessing-0.0.2.tar.gz/MagneticReadoutProcessing-0.0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import pathlib
+import pkg_resources
+import setuptools
+import os
+
+req_path = 'requirements.txt' # os.path.join(os.path.dirname(__file__), 'requirements.txt')
+with pathlib.Path(req_path).open() as requirements_txt:
+    install_requires = [str(requirement) for requirement in pkg_resources.parse_requirements(requirements_txt)]
+
+setup(name='MagneticReadoutProcessing',
+      version='0.0.2',
+      description='Process raw data from magnetometers',
+      author='Marcel Ochsendorf',
+      author_email='info@marcelochsendorf.com',
+      url='https://github.com/LFB-MRI/MagnetCharacterization/',
+      packages= ['MRP', 'cli', 'tests'],#setuptools.find_packages('src', exclude=['test'], include=['cli']),
+      install_requires=install_requires
+     )
---
+++ packages/MagneticReadoutProcessing/MagneticReadoutProcessing-0.0.2.tar.gz/MagneticReadoutProcessing-0.0.2/tests/fix_import.py
@@ -0,0 +1,7 @@
+#import os.path, sys
+#sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
+def __fix_import__fix_import():
+    pass
+    #from pathlib import Path
+    #print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
