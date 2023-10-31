0c210a497aa0ab3a950ddb248cc305a4abbef482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 03:06:09 +0000

    Add package pomprix/0.0.1/pomprix-0.0.1.tar.gz

---
+++ packages/pomprix/pomprix-0.0.1.tar.gz/pomprix-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pomprix',
+    version='0.0.1',
+    description='pomprix python SDK',
+    long_description='pre-release version of pomprix python SDK',
+    url='https://www.pomprix.com',
+    author='frouhi',
+    author_email='frouhi@frouhi.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
