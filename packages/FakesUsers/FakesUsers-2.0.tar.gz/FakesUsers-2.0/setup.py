c0cad6b14cc763dd90ca6aceb6d922ca9c25fdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:05:28 +0000

    Add package FakesUsers/2.0/FakesUsers-2.0.tar.gz

---
+++ packages/FakesUsers/FakesUsers-2.0.tar.gz/FakesUsers-2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='FakesUsers',
+  version='2.0',
+  author='filcher2011',
+  author_email='filcher2011@mail.ru',
+  description='FakesUsers - Python library that regenerates random names, phone numbers and addresses. For now, only the Russian generation is available, but over time we will add other generations.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=['FakesUsers'],
+  install_requires=['requests>=2.25.1'],
+  keywords='FakesUsers',
+  python_requires='>=3.7'
+)
