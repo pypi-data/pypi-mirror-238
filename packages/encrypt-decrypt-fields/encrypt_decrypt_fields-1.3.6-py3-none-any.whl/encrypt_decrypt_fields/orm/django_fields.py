ef61bcac40b9569d0b277463875a4b7262b6823d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:10:00 +0000

    Add package encrypt-decrypt-fields/1.3.6/encrypt_decrypt_fields-1.3.6-py3-none-any.whl

---
+++ packages/encrypt-decrypt-fields/encrypt_decrypt_fields-1.3.6-py3-none-any.whl/encrypt_decrypt_fields/orm/django_fields.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.db import models
+
+from encrypt_decrypt_fields.services import Crypto
+
+
+class EncryptedBinaryField(models.BinaryField):
+    """Store encrypted value as binary"""
+
+    def __init__(self, *args, **kwargs):
+        self.key = kwargs.pop("key", None)
+        if self.key is None:
+            self.key = settings.SECRET_KEY
+
+        self.crypto = Crypto(self.key)
+        super().__init__(*args, **kwargs)
+
+    def get_db_prep_value(self, value, connection, prepared=False):
+        if not value or isinstance(value, (bytes, memoryview)):
+            return value
+
+        encrypted_text = self.crypto.encrypt(str(value))
+        return super().get_db_prep_value(encrypted_text, connection, prepared)
