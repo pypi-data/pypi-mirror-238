e56c8426825db5e50b5e8f44f300226fec02467d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 03:07:27 +0000

    Add package ddtrace/1.16.4/ddtrace-1.16.4-cp35-cp35m-manylinux2014_aarch64.whl

---
+++ packages/ddtrace/ddtrace-1.16.4-cp35-cp35m-manylinux2014_aarch64.whl/ddtrace/ext/__init__.py
@@ -0,0 +1,22 @@
+class SpanTypes(object):
+    CACHE = "cache"
+    CASSANDRA = "cassandra"
+    ELASTICSEARCH = "elasticsearch"
+    GRPC = "grpc"
+    GRAPHQL = "graphql"
+    HTTP = "http"
+    MONGODB = "mongodb"
+    REDIS = "redis"
+    SQL = "sql"
+    TEMPLATE = "template"
+    TEST = "test"
+    WEB = "web"
+    WORKER = "worker"
+    SYSTEM = "system"
+
+
+class SpanKind(object):
+    CLIENT = "client"
+    SERVER = "server"
+    PRODUCER = "producer"
+    CONSUMER = "consumer"
