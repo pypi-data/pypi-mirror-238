4828a2e19fe378d80e1ec821ea440e925b2311fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:04:49 +0000

    Add package ArtSciColor/0.2.2.1/ArtSciColor-0.2.2.1-py3-none-any.whl

---
+++ packages/ArtSciColor/ArtSciColor-0.2.2.1-py3-none-any.whl/ArtSciColor/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .paths import *
+from .image import *
+from .color import *
+from .swatches import *
+from .auxiliary import *
+from .constants import *
+from .fingerprint import *
