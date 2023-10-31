c42297bf89e7b99b30b76522eb3b3f5d555a062c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:06:23 +0000

    Add package odoo14-addon-l10n-nl-xaf-auditfile-export/14.0.3.2.2.dev4/odoo14_addon_l10n_nl_xaf_auditfile_export-14.0.3.2.2.dev4-py3-none-any.whl

---
+++ packages/odoo14-addon-l10n-nl-xaf-auditfile-export/odoo14_addon_l10n_nl_xaf_auditfile_export-14.0.3.2.2.dev4-py3-none-any.whl/odoo/addons/l10n_nl_xaf_auditfile_export/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2015 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "XAF auditfile export",
+    "version": "14.0.3.2.1",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-netherlands",
+    "license": "AGPL-3",
+    "category": "Localization/Netherlands",
+    "summary": "Export XAF auditfiles for Dutch tax authorities",
+    "depends": ["account"],
+    "data": [
+        "security/ir_rule.xml",
+        "security/ir.model.access.csv",
+        "views/xaf_auditfile_export.xml",
+        "views/templates.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+}
