5796a37776ff6030c2d1286e4ded2bde35bdbb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:08:00 +0000

    Add package ChatRoom-jianjun/3.1.2/ChatRoom_jianjun-3.1.2-cp39-cp39-win_amd64.whl

---
+++ packages/ChatRoom-jianjun/ChatRoom_jianjun-3.1.2-cp39-cp39-win_amd64.whl/ChatRoom/config/DEFAULT_LOG_CONFIG.py
@@ -0,0 +1,11 @@
+# Create Time: 2021-09-02 14:23:52
+# AucoCython No Compile
+# -*- coding: utf-8 -*-
+# config info write here
+# 把日志信息写入下面的字典中
+# id : [类型, 内容]
+LOG_ID_DICT = {
+    "00001" : ["INFO", "test_00001_info"],
+    "00002" : ["INFO", "test_00002_info"],
+    "40001" : [ "ERR",  "Err_40001_info"],
+}
