c3c24ea48b054112e0d5a4fc3afa50eae22b5a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:08:30 +0000

    Add package myadipkg/0.0.1/myadipkg-0.0.1.tar.gz

---
+++ packages/myadipkg/myadipkg-0.0.1.tar.gz/myadipkg-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='myadipkg',
+    version='0.0.1',
+    description='pooja"s ADI python package module',
+    url='https://github.com/hkshitesh/mydemopackage',
+    author='pagarwa3',
+    author_email='poojaag1606@gmail.com',
+    packages=setuptools.find_packages(),    
+    long_description='pooja ADI python package module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
+#https://github.com/hkshitesh/hkshiteshpkg
