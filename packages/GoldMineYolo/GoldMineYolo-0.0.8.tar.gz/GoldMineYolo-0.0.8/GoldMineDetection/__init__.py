dfd44c09aa9f3f355a0149397b6920fc8f2380e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:04 +0000

    Add package GoldMineYolo/0.0.8/GoldMineYolo-0.0.8.tar.gz

---
+++ packages/GoldMineYolo/GoldMineYolo-0.0.8.tar.gz/GoldMineYolo-0.0.8/GoldMineDetection/__init__.py
@@ -0,0 +1,8 @@
+import ultralytics
+from ultralytics import YOLO
+def detect_mine(imagePath):
+    model = YOLO('currentModel.pt')  # takes current model
+    print("detecting a mine!")
+    print(imagePath)
+    return model.predict(source=imagePath, save=True, conf=.45)
+
