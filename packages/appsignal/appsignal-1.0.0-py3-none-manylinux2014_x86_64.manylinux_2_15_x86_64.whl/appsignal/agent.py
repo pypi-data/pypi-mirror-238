09d78f0e8900550c772e98321491d0570f7baf0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:05:35 +0000

    Add package appsignal/1.0.0/appsignal-1.0.0-py3-none-manylinux2014_x86_64.manylinux_2_15_x86_64.whl

---
+++ packages/appsignal/appsignal-1.0.0-py3-none-manylinux2014_x86_64.manylinux_2_15_x86_64.whl/appsignal/agent.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import subprocess
+from dataclasses import dataclass
+from pathlib import Path
+
+from .config import Config
+
+
+@dataclass
+class Agent:
+    path: Path = Path(__file__).parent / "appsignal-agent"
+    active: bool = False
+
+    def start(self, config: Config) -> None:
+        config.set_private_environ()
+        p = subprocess.Popen(
+            [self.path, "start", "--private"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        p.wait(timeout=1)
+        returncode = p.returncode
+        if returncode == 0:
+            self.active = True
+        else:
+            output, _ = p.communicate()
+            out = output.decode("utf-8")
+            print(f"AppSignal agent is unable to start ({returncode}): ", out)
+
+    def diagnose(self, config: Config) -> bytes:
+        config.set_private_environ()
+        return subprocess.run(
+            [self.path, "diagnose", "--private"], capture_output=True
+        ).stdout
+
+    def version(self) -> bytes:
+        return subprocess.run(
+            [self.path, "--version"], capture_output=True
+        ).stdout.split()[-1]
+
+
+agent = Agent()
