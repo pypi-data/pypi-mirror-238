17a3f9f9ea9dcce84d42e07843a3a4e596f90715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:06:18 +0000

    Add package easy-pack/1.0.28/easy-pack-1.0.28.tar.gz

---
+++ packages/easy-pack/easy-pack-1.0.28.tar.gz/easy-pack-1.0.28/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+import os
+os.system('ls')
+readme_file = 'readme.md' if os.path.isfile('readme.md') else 'resources/readme.md' if os.path.isfile('resources/readme.md') else './easy_pack/readme.md'
+with open(readme_file) as f:
+	long_description = f.read()
+
+setup(name='easy-pack',description='easy packing!',author='german espinosa',author_email='germanespinosa@gmail.com',long_description=long_description,long_description_content_type='text/markdown',packages=['easy_pack'],install_requires=['twine'],license='MIT',version='1.0.28',zip_safe=False)
