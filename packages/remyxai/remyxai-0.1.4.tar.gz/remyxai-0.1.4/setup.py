dd64907f4bdaab9e35bd9cfa4a270d63e7a6ccf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:38 +0000

    Add package remyxai/0.1.4/remyxai-0.1.4.tar.gz

---
+++ packages/remyxai/remyxai-0.1.4.tar.gz/remyxai-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="remyxai",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "onnx", 
+        "onnxruntime",
+        "pillow",
+        "requests",
+        "tqdm",
+        ],
+    entry_points={
+        "console_scripts": [
+            "remyxai=remyxai.cli:main",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
