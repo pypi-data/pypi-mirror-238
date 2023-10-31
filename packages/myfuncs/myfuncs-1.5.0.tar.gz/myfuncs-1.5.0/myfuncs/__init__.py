28d4ffa5765333a7ede17d3582486f746bed5c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:06:31 +0000

    Add package myfuncs/1.5.0/myfuncs-1.5.0.tar.gz

---
+++ packages/myfuncs/myfuncs-1.5.0.tar.gz/myfuncs-1.5.0/myfuncs/__init__.py
@@ -0,0 +1,11 @@
+from .main import (
+    ALPHANUMERIC_CHARS,
+    USERNAME_ASCII_CHARS,
+    nlprint,
+    ranstr,
+    print_middle,
+    get_terminal_width,
+    print_columns,
+    objinfo,
+    default_repr,
+)
