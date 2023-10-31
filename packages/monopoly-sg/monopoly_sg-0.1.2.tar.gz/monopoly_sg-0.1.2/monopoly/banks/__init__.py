2ad4abdc14ae6bfb9abf93d9727e9b98d14af65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:07:22 +0000

    Add package monopoly-sg/0.1.2/monopoly_sg-0.1.2.tar.gz

---
+++ packages/monopoly-sg/monopoly_sg-0.1.2.tar.gz/monopoly_sg-0.1.2/monopoly/banks/__init__.py
@@ -0,0 +1,7 @@
+from .citibank import Citibank
+from .dbs import Dbs
+from .hsbc import Hsbc
+from .ocbc import Ocbc
+from .standard_chartered import StandardChartered
+
+__all__ = ["Citibank", "Dbs", "Hsbc", "Ocbc", "StandardChartered"]
