3a1cecd7f9cda8719fbf4e44cee814844208bd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:05:39 +0000

    Add package awsx/1.2/awsx-1.2.tar.gz

---
+++ packages/awsx/awsx-1.2.tar.gz/awsx-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='awsx',
+    version='1.2',
+    py_modules=['main'],
+    install_requires=[
+        'Click',
+        'boto3',
+        'configparser',
+        'pyperclip',
+    ],
+    entry_points='''
+        [console_scripts]
+        awsx=main:role_token
+    ''',
+)
