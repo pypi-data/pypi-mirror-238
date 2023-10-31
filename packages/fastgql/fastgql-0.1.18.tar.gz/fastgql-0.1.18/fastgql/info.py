7abb4c6d81ec925212f18a9046f5f4e796d96482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:07:12 +0000

    Add package fastgql/0.1.18/fastgql-0.1.18.tar.gz

---
+++ packages/fastgql/fastgql-0.1.18.tar.gz/fastgql-0.1.18/fastgql/info.py
@@ -0,0 +1,23 @@
+import typing as T
+import dataclasses
+from fastapi import Request, Response, BackgroundTasks
+from fastgql.gql_ast import models as M
+
+
+@dataclasses.dataclass
+class Info:
+    """needed to make this a raw dataclass because context needs to be kept as a reference... pydantic copies dicts"""
+
+    variables: dict[str, T.Any] | None
+
+    node: M.FieldNode | M.InlineFragmentNode
+    parent_node: M.FieldNode | M.InlineFragmentNode | M.OperationNode
+    context: dict[str, T.Any]
+
+    request: Request
+    response: Response
+    background_tasks: BackgroundTasks
+
+    errors: list[Exception]
+
+    path: tuple[str, ...]
