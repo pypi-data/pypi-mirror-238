5270cdd46fb6c18384bc7e15a8f6b02137437677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:22 +0000

    Add package polymath-distribution/0.1/polymath_distribution-0.1.tar.gz

---
+++ packages/polymath-distribution/polymath_distribution-0.1.tar.gz/polymath_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='polymath_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['polymath_distribution'],
+      author='Olarinde Alli',
+      author_email='polymathalli@gmail.com',
+      zip_safe=False)
