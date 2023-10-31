e92ee48311d76ba8efee4373fa017496c96a60a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:04:54 +0000

    Add package holidays-es/2.0.0/holidays_es-2.0.0-py3-none-any.whl

---
+++ packages/holidays-es/holidays_es-2.0.0-py3-none-any.whl/holidays_es/__init__.py
@@ -0,0 +1,7 @@
+from typing import Sequence
+
+from holidays_es.enums import Province, Scope
+from holidays_es.holidays import HolidaySpain
+
+
+__all__: Sequence[str] = ["HolidaySpain", "Province", "Scope"]
