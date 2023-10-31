1f6d0304b3e19faeadcd56db4ee18bc7743abaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:04 +0000

    Add package gdsfactory/7.8.13/gdsfactory-7.8.13.tar.gz

---
+++ packages/gdsfactory/gdsfactory-7.8.13.tar.gz/gdsfactory-7.8.13/gdsfactory/labels/add_label_yaml.py
@@ -0,0 +1,132 @@
+"""Add label YAML."""
+from __future__ import annotations
+
+import json
+from functools import partial
+from typing import Any
+
+import pydantic
+from omegaconf import OmegaConf
+
+import gdsfactory as gf
+from gdsfactory.read.from_yaml import valid_anchor_point_keywords
+from gdsfactory.typings import LayerSpec
+
+
+@pydantic.validate_call
+def add_label_yaml(
+    component: gf.Component,
+    layer: LayerSpec = "TEXT",
+    measurement: str | None = None,
+    measurement_settings: dict[str, Any] | None = None,
+    analysis: str | None = None,
+    analysis_settings: dict[str, Any] | None = None,
+    doe: str | None = None,
+    with_yaml_format: bool = True,
+    anchor: str = "sw",
+) -> gf.Component:
+    """Returns Component with measurement label.
+
+    Args:
+        component: to add labels to.
+        layer: text label layer.
+        measurement: measurement config name. Defaults to component['info']['measurement'].
+        measurement_settings: measurement settings. Defaults to component['info']['measurement_settings'].
+        analysis: analysis name. Defaults to component['info']['analysis'].
+        analysis_settings: Extra analysis settings. Defaults to component settings.
+        doe: Design of Experiment name. Defaults to component['info']['doe'].
+        with_yaml_format: whether to use yaml or json format.
+        anchor: anchor point for the label. Defaults to south-west "sw". \
+            Valid options are: "n", "s", "e", "w", "ne", "nw", "se", "sw", "c".
+    """
+    from gdsfactory.pdk import get_layer
+
+    measurement = measurement or component.info.get("measurement")
+    measurement_settings = measurement_settings or component.info.get(
+        "measurement_settings"
+    )
+    analysis = analysis or component.info.get("analysis")
+    analysis_settings = analysis_settings or component.info.get("analysis_settings")
+    doe = doe or component.info.get("doe")
+
+    layer = get_layer(layer)
+    analysis_settings = analysis_settings or {}
+    measurement_settings = measurement_settings or {}
+    cell_settings = component.metadata.get("full", {})
+
+    optical_ports = component.get_ports_list(port_type="optical")
+    electrical_ports = component.get_ports_list(port_type="electrical")
+
+    if anchor not in valid_anchor_point_keywords:
+        raise ValueError(f"anchor {anchor} not in {valid_anchor_point_keywords}. ")
+
+    xc, yc = getattr(component.size_info, anchor)
+
+    d = dict(
+        name=component.name,
+        doe=doe,
+        measurement=measurement,
+        cell_settings=cell_settings,
+        analysis=analysis,
+        measurement_settings=measurement_settings,
+        analysis_settings=analysis_settings,
+        xopt=[int(optical_ports[0].x - xc)] if optical_ports else [],
+        yopt=[int(optical_ports[0].y - yc)] if optical_ports else [],
+        xelec=[int(electrical_ports[0].x - xc)] if electrical_ports else [],
+        yelec=[int(electrical_ports[0].y - yc)] if electrical_ports else [],
+    )
+    text = OmegaConf.to_yaml(d) if with_yaml_format else json.dumps(d)
+    label = gf.Label(
+        text=text,
+        origin=(xc, yc),
+        anchor="o",
+        layer=layer[0],
+        texttype=layer[1],
+    )
+    component.add(label)
+    return component
+
+
+add_label_json = partial(add_label_yaml, with_yaml_format=False)
+
+
+if __name__ == "__main__":
+    import yaml
+
+    measurement_settings = dict(
+        wavelenth_min=1550, wavelenth_max=1570, wavelength_steps=10
+    )
+    with_yaml_format = False
+    with_yaml_format = True
+
+    decorator = add_label_yaml if with_yaml_format else add_label_json
+
+    info = dict(
+        measurement_settings=measurement_settings,
+        with_yaml_format=with_yaml_format,
+    )
+
+    c = gf.c.straight(length=11)
+    c = gf.c.mmi2x2(length_mmi=2.2)
+    c = gf.routing.add_fiber_array(
+        c,
+        get_input_labels_function=None,
+        grating_coupler=gf.components.grating_coupler_te,
+        decorator=decorator,
+        info=info,
+    )
+
+    c = gf.components.spiral_inner_io_fiber_array(
+        length=20e3,
+        decorator=decorator,
+        info=dict(
+            measurement="optical_loopback2",
+            doe="spiral_sc",
+            measurement_settings=dict(wavelength_alignment=1560),
+        ),
+    )
+    print(len(c.labels[0].text))
+    print(c.labels[0].text)
+    d = yaml.safe_load(c.labels[0].text) if yaml else json.loads(c.labels[0].text)
+    print(d)
+    c.show(show_ports=False)
---
+++ packages/gdsfactory/gdsfactory-7.8.13.tar.gz/gdsfactory-7.8.13/gdsfactory/labels/write_test_manifest.py
@@ -0,0 +1,70 @@
+"""Converts CSV of test site labels into a CSV test manifest."""
+
+import json
+import pathlib
+
+import numpy as np
+import pandas as pd
+
+import gdsfactory as gf
+from gdsfactory.samples.sample_reticle import sample_reticle
+
+
+def write_test_manifest(csvpath: str | pathlib.Path) -> pd.DataFrame:
+    """Converts CSV of test site labels into a CSV test manifest."""
+    df_in = pd.read_csv(csvpath)
+
+    # Initialize an empty list to collect the rows
+    rows = []
+    columns = [
+        "cell",
+        "device",
+        "xopt",
+        "yopt",
+        "xelec",
+        "yelec",
+        "measurement",
+        "measurement_settings",
+        "doe",
+        "analysis",
+        "analysis_settings",
+        "cell_settings",
+    ]
+
+    for _, label in df_in.iterrows():
+        x, y, _orientation = label.x, label.y, label.rotation
+        text = label.text
+        d = json.loads(text)
+
+        row = [
+            d["name"],
+            d["name"] + f"_{int(x)}_{int(y)}",
+            [np.round(i + x, 3) for i in d["xopt"]],
+            [np.round(i + y, 3) for i in d["yopt"]],
+            [np.round(i + x, 3) for i in d["xelec"]],
+            [np.round(i + y, 3) for i in d["yelec"]],
+            d["measurement"],
+            d["measurement_settings"],
+            d["doe"],
+            d["analysis"],
+            d["analysis_settings"],
+            d["cell_settings"],
+        ]
+        rows.append(row)
+
+    return pd.DataFrame(
+        rows,
+        columns=columns,
+    )
+
+
+if __name__ == "__main__":
+    c = sample_reticle(grid=False)
+    c.show(show_ports=True)
+    gdspath = c.write_gds("sample_reticle.gds")
+    csvpath = gf.labels.write_labels.write_labels_gdstk(
+        gdspath, prefixes=("{",), layer_label="TEXT"
+    )
+    df = write_test_manifest(csvpath)
+    df.to_csv("test_manifest.csv")
+    print(df)
