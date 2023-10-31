8cd973967a696f6586b2683891c369c5298b8898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:08:21 +0000

    Add package basicco/10.3.1/basicco-10.3.1.tar.gz

---
+++ packages/basicco/basicco-10.3.1.tar.gz/basicco-10.3.1/tests/test_get_mro.py
@@ -0,0 +1,58 @@
+# type: ignore
+
+import pytest
+import six
+from tippo import Generic, TypeVar
+
+from basicco.get_mro import get_mro, preview_mro, resolve_origin
+
+
+def test_resolve_origin():
+    T = TypeVar("T")  # noqa
+
+    class Base(Generic[T]):
+        pass
+
+    assert resolve_origin(Base) is Base
+    assert resolve_origin(Base[int]) is Base
+    assert resolve_origin(Base[T]) is Base
+
+
+def test_get_mro():
+    T = TypeVar("T")  # noqa
+
+    class Base(Generic[T]):
+        pass
+
+    class SubBase(Base[T]):
+        pass
+
+    class SubSubBase(SubBase[T], Base[T]):
+        pass
+
+    assert get_mro(SubSubBase) == (SubSubBase, SubBase, Base, Generic, object)
+    assert get_mro(SubSubBase[T]) == (SubSubBase, SubBase, Base, Generic, object)
+    assert get_mro(SubSubBase[int]) == (SubSubBase, SubBase, Base, Generic, object)
+
+
+def test_preview_mro():
+    class M(type):
+        pass
+
+    class A(six.with_metaclass(M, object)):
+        pass
+
+    class B(A):
+        pass
+
+    class C(A):
+        pass
+
+    class D(B, C):
+        pass
+
+    assert preview_mro(B, C) == get_mro(D)[1:]
+
+
+if __name__ == "__main__":
+    pytest.main()
---
+++ packages/basicco/basicco-10.3.1.tar.gz/basicco-10.3.1/tests/test_init_subclass.py
@@ -0,0 +1,129 @@
+# type: ignore
+
+import sys
+
+import pytest
+import six
+from tippo import Generic, GenericMeta, TypeVar
+
+from basicco.init_subclass import InitSubclass, InitSubclassMeta
+
+T_co = TypeVar("T_co", covariant=True)
+
+
+def test_class():
+    assert isinstance(InitSubclass, InitSubclassMeta)
+
+
+def test_init_subclass():
+    class A(InitSubclass):
+        __kwargs__ = {}
+
+        def __init_subclass__(cls, foo=None, bar=None, **kwargs):
+            assert cls is not A
+            assert foo == "{}1".format(cls.__name__)
+            assert bar == "{}2".format(cls.__name__)
+            super(A, cls).__init_subclass__(**kwargs)  # noqa
+
+    assert A
+    assert not hasattr(A, "__kwargs__")
+
+    class B(A):
+        __kwargs__ = {"foo": "B1", "bar": "B2"}
+
+    assert B
+    assert not hasattr(B, "__kwargs__")
+
+    class C(B):
+        __kwargs__ = {"foo": "C1", "bar": "C2"}
+
+    assert C
+    assert not hasattr(C, "__kwargs__")
+
+
+def test_bad_base_kwargs():
+    class A(object):
+        __kwargs__ = {}
+
+    with pytest.raises(TypeError):
+
+        class B(InitSubclass, A):
+            pass
+
+        assert not B
+
+
+def test_bad_base_init_subclass():
+    if sys.version_info[:3] < (3, 6):
+
+        class A(object):
+            def __init_subclass__(cls, **kwargs):
+                pass
+
+        with pytest.raises(TypeError):
+
+            class B(A, InitSubclass):
+                pass
+
+            assert not B
+    else:
+
+        class A(object):
+            def __init_subclass__(cls, **kwargs):
+                pass
+
+        class B(A, InitSubclass):
+            pass
+
+        assert B
+
+
+def test_newer_python():
+    if sys.version_info[:3] >= (3, 6):
+        code = """
+class A(InitSubclass):
+
+    def __init_subclass__(cls, foo=None, bar=None, **kwargs):
+        assert cls is not A
+        assert foo == "{}1".format(cls.__name__)
+        assert bar == "{}2".format(cls.__name__)
+        super(A, cls).__init_subclass__(**kwargs)
+
+assert A
+assert not hasattr(A, "__kwargs__")
+
+class B(A, foo="B1"):
+    __kwargs__ = {"bar": "B2"}
+
+assert B
+assert not hasattr(B, "__kwargs__")
+
+class C(B, bar="C2"):
+    __kwargs__ = {"foo": "C1"}
+
+assert C
+assert not hasattr(C, "__kwargs__")
+"""
+        exec(code, globals(), globals())
+
+
+def test_generics():
+    class MyMeta(InitSubclassMeta, GenericMeta):
+        pass
+
+    class Base(six.with_metaclass(MyMeta, InitSubclass)):
+        pass
+
+    class MyClass(Generic[T_co], Base):
+        pass
+
+    assert MyClass[int]
+
+    class MySubClass(MyClass[T_co]):
+        pass
+
+    assert MySubClass[int]
+
+
+if __name__ == "__main__":
+    pytest.main()
