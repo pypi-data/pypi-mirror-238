2569332722bc90c9cf441f90bd5835c050ad8c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:05:24 +0000

    Add package doppy/0.0.0/doppy-0.0.0-py3-none-any.whl

---
+++ packages/doppy/doppy-0.0.0-py3-none-any.whl/doppy/main.py
@@ -0,0 +1,16 @@
+import random
+
+
+def main():
+    print(
+        random.choice(
+            [
+                "Doppy is free.",
+                "Such a beautiful place to be under clouds.",
+                "Doppy is happy to be under a gentle breeze.",
+                "Wind did not mean to storm. Wind only meant to breeze, or lightly gust.",
+                "Wind must not go back to the sky this year.",
+                "The wind has gifted Doppy a sock. Doppy is as free as the wind now.",
+            ]
+        )
+    )
