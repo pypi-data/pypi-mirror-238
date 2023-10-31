de9e6b47d864b1de383c76a39323703109cce6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:09:23 +0000

    Add package CommonTelegramUsers/0.2/CommonTelegramUsers-0.2.tar.gz

---
+++ packages/CommonTelegramUsers/CommonTelegramUsers-0.2.tar.gz/CommonTelegramUsers-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CommonTelegramUsers",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        "pyrogram",
+    ],
+    extras_require={
+        'tgcrypto':  ["tgcrypto"],
+    },
+)
