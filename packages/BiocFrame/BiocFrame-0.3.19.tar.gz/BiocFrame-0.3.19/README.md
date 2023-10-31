72642cd66c10572fd0841ccd304835e554ce5cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:08:15 +0000

    Add package BiocFrame/0.3.19/BiocFrame-0.3.19.tar.gz

---
+++ packages/BiocFrame/BiocFrame-0.3.19.tar.gz/BiocFrame-0.3.19/README.md
@@ -0,0 +1,205 @@
+<!-- These are examples of badges you might want to add to your README:
+     please update the URLs accordingly
+
+[![Built Status](https://api.cirrus-ci.com/github/<USER>/BiocFrame.svg?branch=main)](https://cirrus-ci.com/github/<USER>/BiocFrame)
+[![ReadTheDocs](https://readthedocs.org/projects/BiocFrame/badge/?version=latest)](https://BiocFrame.readthedocs.io/en/stable/)
+[![Coveralls](https://img.shields.io/coveralls/github/<USER>/BiocFrame/main.svg)](https://coveralls.io/r/<USER>/BiocFrame)
+[![PyPI-Server](https://img.shields.io/pypi/v/BiocFrame.svg)](https://pypi.org/project/BiocFrame/)
+[![Conda-Forge](https://img.shields.io/conda/vn/conda-forge/BiocFrame.svg)](https://anaconda.org/conda-forge/BiocFrame)
+[![Monthly Downloads](https://pepy.tech/badge/BiocFrame/month)](https://pepy.tech/project/BiocFrame)
+[![Twitter](https://img.shields.io/twitter/url/http/shields.io.svg?style=social&label=Twitter)](https://twitter.com/BiocFrame)
+-->
+
+[![Project generated with PyScaffold](https://img.shields.io/badge/-PyScaffold-005CA0?logo=pyscaffold)](https://pyscaffold.org/)
+[![PyPI-Server](https://img.shields.io/pypi/v/BiocFrame.svg)](https://pypi.org/project/BiocFrame/)
+![Unit tests](https://github.com/BiocPy/BiocFrame/actions/workflows/pypi-test.yml/badge.svg)
+
+# BiocFrame
+
+This package provides `BiocFrame` class, an alternative to Pandas DataFrame's.
+
+`BiocFrame` makes no assumption on the types of the columns, the minimum requirement is each column implements length: `__len__` and slice: `__getitem__` dunder methods. This allows `BiocFrame` to accept nested representations or any supported class as columns.
+
+
+To get started, install the package from [PyPI](https://pypi.org/project/biocframe/)
+
+```shell
+pip install biocframe
+```
+
+## Usage
+
+To construct a `BiocFrame` object, simply provide the data as a dictionary.
+
+```python
+from random import random
+from biocframe import BiocFrame
+
+obj = {
+    "ensembl": ["ENS00001", "ENS00002", "ENS00003"],
+    "symbol": ["MAP1A", "BIN1", "ESR1"],
+}
+bframe = BiocFrame(obj)
+print(bframe)
+```
+
+    ## output
+    BiocFrame with 3 rows & 2 columns
+    ┏━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━┓
+    ┃ ensembl <list> ┃ symbol <list> ┃
+    ┡━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━┩
+    │ ENS00001       │ MAP1A         │
+    │ ENS00002       │ BIN1          │
+    │ ENS00003       │ ESR1          │
+    └────────────────┴───────────────┘
+
+You can specify complex representations as columns, for example
+
+```python
+obj = {
+    "ensembl": ["ENS00001", "ENS00002", "ENS00002"],
+    "symbol": ["MAP1A", "BIN1", "ESR1"],
+    "ranges": BiocFrame({
+        "chr": ["chr1", "chr2", "chr3"],
+        "start": [1000, 1100, 5000],
+        "end": [1100, 4000, 5500]
+    }),
+}
+
+bframe2 = BiocFrame(obj, row_names=["row1", "row2", "row3"])
+print(bframe2)
+```
+
+    ## output
+    BiocFrame with 3 rows & 3 columns
+    ┏━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+    ┃ row_names ┃ ensembl <list> ┃ symbol <list> ┃ ranges <BiocFrame>                          ┃
+    ┡━━━━━━━━━━━╇━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
+    │ row1      │ ENS00001       │ MAP1A         │ {'chr': 'chr1', 'start': 1000, 'end': 1100} │
+    │ row2      │ ENS00002       │ BIN1          │ {'chr': 'chr2', 'start': 1100, 'end': 4000} │
+    │ row3      │ ENS00002       │ ESR1          │ {'chr': 'chr3', 'start': 5000, 'end': 5500} │
+    └───────────┴────────────────┴───────────────┴─────────────────────────────────────────────┘
+
+### Properties
+
+Properties can be accessed directly from the object, for e.g. column names, row names and/or dimensions of the `BiocFrame`.
+
+```python
+# Dimensionality or shape
+print(bframe.dims)
+
+## output
+## (3, 2)
+
+# get the column names
+print(bframe.column_names)
+
+## output
+## ['ensembl', 'symbol']
+```
+
+#### Setters
+
+To set various properties
+
+```python
+# set new column names
+bframe.column_names = ["column1", "column2"]
+print(bframe)
+```
+
+    ## output
+    BiocFrame with 3 rows & 2 columns
+    ┏━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓
+    ┃ column1 <list> ┃ column2 <list> ┃
+    ┡━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━┩
+    │ ENS00001       │ MAP1A          │
+    │ ENS00002       │ BIN1           │
+    │ ENS00003       │ ESR1           │
+    └────────────────┴────────────────┘
+
+To add new columns,
+
+```python
+bframe["score"] = range(2, 5)
+print(bframe)
+```
+
+    ## output
+    BiocFrame with 3 rows & 3 columns
+    ┏━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━┓
+    ┃ column1 <list> ┃ column2 <list> ┃ score <range> ┃
+    ┡━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━┩
+    │ ENS00001       │ MAP1A          │ 2             │
+    │ ENS00002       │ BIN1           │ 3             │
+    │ ENS00003       │ ESR1           │ 4             │
+    └────────────────┴────────────────┴───────────────┘
+
+### Subset `BiocFrame`
+
+Use the subset (`[]`) operator to **slice** the object,
+
+```python
+sliced = bframe[1:2, [True, False, False]]
+print(sliced)
+```
+
+    ## output
+    BiocFrame with 1 row & 1 column
+    ┏━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓
+    ┃ row_names ┃ column1 <list> ┃
+    ┡━━━━━━━━━━━╇━━━━━━━━━━━━━━━━┩
+    │ 1         │ ENS00002       │
+    └───────────┴────────────────┘
+
+This operation accepts different slice input types, you can either specify a boolean vector, a `slice` object, a list of indices, or row/column names to subset.
+
+
+### Combine
+
+`BiocFrame` implements the combine generic from [biocgenerics](https://github.com/BiocPy/generics). To combine multiple objects,
+
+```python
+bframe1 = BiocFrame(
+    {
+        "odd": [1, 3, 5, 7, 9],
+        "even": [0, 2, 4, 6, 8],
+    }
+)
+
+bframe2 = BiocFrame(
+    {
+        "odd": [11, 33, 55, 77, 99],
+        "even": [0, 22, 44, 66, 88],
+    }
+)
+
+from biocgenerics.combine import combine
+combined = combine(bframe1, bframe2)
+
+# OR an object oriented approach
+
+combined = bframe.combine(bframe2)
+```
+
+    ## output
+    BiocFrame with 10 rows & 2
+            columns
+    ┏━━━━━━━━━━━━┳━━━━━━━━━━━━━┓
+    ┃ odd <list> ┃ even <list> ┃
+    ┡━━━━━━━━━━━━╇━━━━━━━━━━━━━┩
+    │ 1          │ 0           │
+    │ 3          │ 2           │
+    │ ...        │ ...         │
+    │ 99         │ 88          │
+    └────────────┴─────────────┘
+
+For more details, check out the BiocFrame class [reference](https://biocpy.github.io/BiocFrame/api/biocframe.html#biocframe.BiocFrame.BiocFrame).
+
+
+<!-- pyscaffold-notes -->
+
+## Note
+
+This project has been set up using PyScaffold 4.5. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
---
+++ packages/BiocFrame/BiocFrame-0.3.19.tar.gz/BiocFrame-0.3.19/tests/test_methods.py
@@ -0,0 +1,489 @@
+import numpy as np
+import pytest
+import pandas as pd
+from biocframe.BiocFrame import BiocFrame
+from biocframe.Factor import Factor
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+
+def test_bframe_basic_ops():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+
+    assert bframe is not None
+    assert len(bframe.column_names) == 3
+    assert (
+        len(
+            list(
+                set(bframe.column_names).difference(
+                    [
+                        "column1",
+                        "nested",
+                        "column2",
+                    ]
+                )
+            )
+        )
+        == 0
+    )
+
+    assert len(bframe.dims) == 2
+    assert bframe.dims == (3, 3)
+
+    assert bframe.row_names is None
+
+    assert bframe.column_names is not None
+    assert len(bframe.column_names) == 3
+
+    assert bframe.metadata == {}
+
+    assert len(bframe.dims) == 2
+    assert bframe.dims == (3, 3)
+
+
+def test_bframe_setters():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+
+    assert bframe is not None
+
+    assert bframe.row_names is None
+
+    bframe.row_names = ["row1", "row2", "row3"]
+    assert bframe.row_names is not None
+    assert len(bframe.row_names) == 3
+
+    assert bframe.column_names is not None
+    assert len(bframe.column_names) == 3
+
+    bframe.column_names = ["col1", "col2", "col3"]
+
+    assert bframe.column_names is not None
+    assert len(bframe.column_names) == 3
+
+    assert bframe.metadata == {}
+
+    bframe.metadata = {"a": "b"}
+    assert bframe.metadata is not None
+
+    bframe["new_col"] = [1, 2, 3]
+
+    assert bframe is not None
+
+    assert len(bframe.dims) == 2
+    assert bframe.dims == (3, 4)
+
+    bframe["col2"] = [1, 2, 3]
+
+    assert bframe is not None
+
+    assert len(bframe.dims) == 2
+    assert bframe.dims == (3, 4)
+
+
+def test_bframe_setters_with_rows():
+    obj = {
+        "column1": [1, 2, 3, 4, 5],
+        "column2": ["b", "n", "m", "a", "c"],
+    }
+
+    bframe = BiocFrame(obj)
+    bframe[1:3, "column1"] = [20, 30]
+    assert bframe.column("column1") == [1, 20, 30, 4, 5]
+
+    bframe = BiocFrame(obj)
+    bframe[1:3, ["column1", "column2"]] = BiocFrame(
+        {"column1": [20, 30], "column2": ["E", "F"]}
+    )
+    assert bframe.column("column1") == [1, 20, 30, 4, 5]
+    assert bframe.column("column2") == ["b", "E", "F", "a", "c"]
+
+    # Works even when columns are out of order.
+    bframe = BiocFrame(obj)
+    bframe[1:3, ["column2", "column1"]] = BiocFrame(
+        {"column1": [20, 30], "column2": ["E", "F"]}
+    )
+    assert bframe.column("column1") == [1, 20, 30, 4, 5]
+    assert bframe.column("column2") == ["b", "E", "F", "a", "c"]
+
+
+def test_bframe_setters_should_fail():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+
+    assert bframe is not None
+
+    assert bframe.row_names is None
+
+    with pytest.raises(Exception):
+        bframe.row_names = ["row1", "row2"]
+
+    with pytest.raises(Exception):
+        bframe.column_names = ["col2", "col3"]
+
+    assert bframe.column_names is not None
+    assert len(bframe.column_names) == 3
+
+    with pytest.raises(Exception):
+        bframe["new_col"] = [2, 3]
+
+    assert bframe is not None
+
+    assert len(bframe.dims) == 2
+    assert bframe.dims == (3, 3)
+
+    with pytest.raises(Exception):
+        bframe["col2"] = [1, 2]
+
+    assert bframe is not None
+
+    assert len(bframe.dims) == 2
+    assert bframe.dims == (3, 3)
+
+
+def test_bframe_slice():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+    slice = bframe[0:2, 0:2]
+
+    assert slice is not None
+    assert len(slice.column_names) == 2
+    assert len(list(set(slice.column_names).difference(["column1", "nested"]))) == 0
+
+    assert len(slice.dims) == 2
+    assert slice.dims == (2, 2)
+
+    sliced_list = bframe[[0, 2], 0:2]
+
+    assert sliced_list is not None
+    assert len(sliced_list.column_names) == 2
+    assert (
+        len(list(set(sliced_list.column_names).difference(["column1", "nested"]))) == 0
+    )
+
+    assert len(sliced_list.dims) == 2
+    assert sliced_list.dims == (2, 2)
+
+
+def test_bframe_slice_with_extras():
+    bframe = BiocFrame(
+        {
+            "column1": [1, 2, 3],
+            "column2": [4, 5, 6],
+        },
+        mcols=BiocFrame({"foo": [-1, -2], "bar": ["A", "B"]}),
+        metadata={"YAY": 2},
+    )
+
+    subframe = bframe[0:2, :]
+    assert subframe.mcols.shape[0] == bframe.mcols.shape[1]
+    assert subframe.metadata == bframe.metadata
+
+    subframe = bframe[:, [1]]
+    assert subframe.mcols.shape[0] == 1
+    assert subframe.mcols.column("foo") == [-2]
+    assert subframe.metadata == bframe.metadata
+
+
+def test_bframe_unary_slice():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+
+    unary_col = bframe[[True, False, True], [True, False, False]]
+    assert unary_col is not None
+    assert isinstance(unary_col, BiocFrame)
+    assert len(unary_col) == 2
+
+    unary_row = bframe[[1], :]
+    assert unary_row is not None
+    assert isinstance(unary_row, BiocFrame)
+    assert len(unary_row) == 1
+
+    unary_row = bframe[1, :]
+    assert unary_row is not None
+    assert isinstance(unary_row, dict)
+    assert len(unary_row.keys()) == 3
+
+    unary_col = bframe[[True, False, True], 2]
+    assert unary_col is not None
+    assert isinstance(unary_col, list)
+    assert len(unary_col) == 2
+
+
+def test_bframe_delete():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+    del bframe["nested"]
+
+    assert bframe is not None
+    assert bframe.dims == (3, 2)
+
+
+def test_bframe_ufuncs():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+
+    new_bframe = np.sqrt(bframe)
+    assert new_bframe is not None
+
+
+def test_nested_biocFrame_slice():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": BiocFrame(
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": ["j", "k", "l"],
+            }
+        ),
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+    assert bframe is not None
+
+    slice = bframe[0:2, 0:2]
+
+    assert slice is not None
+    assert len(slice.column_names) == 2
+    assert len(list(set(slice.column_names).difference(["column1", "nested"]))) == 0
+
+    assert len(slice.dims) == 2
+    assert slice.dims == (2, 2)
+
+    slice_nbframe = slice.column("nested")
+    assert isinstance(slice_nbframe, BiocFrame)
+    assert len(slice_nbframe.dims) == 2
+    assert slice_nbframe.dims == (2, 3)
+
+
+def test_bframe_iter():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": [
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": {"dcol1": ["j", "k", "l"], "dcol2": ["a", "s", "l"]},
+            },
+            {
+                "ncol2": ["a"],
+                "deep": {"dcol1": ["j"], "dcol2": ["a"]},
+            },
+            {
+                "ncol1": [5, 6],
+                "ncol2": ["b", "c"],
+            },
+        ],
+        "column2": ["b", "n", "m"],
+    }
+
+    bframe = BiocFrame(obj)
+    assert bframe is not None
+
+    iterCount = 0
+    for k, v in bframe:
+        iterCount += 1
+
+    assert iterCount == bframe.dims[0]
+
+
+def test_slice_empty_obj():
+    bframe = BiocFrame({}, number_of_rows=100)
+    assert bframe is not None
+
+    sliced_bframe = bframe[10:30, :]
+    assert sliced_bframe is not None
+
+    assert sliced_bframe.shape == (20, 0)
+
+
+def test_nested_biocFrame_preserve_types():
+    obj = {
+        "column1": [1, 2, 3],
+        "nested": BiocFrame(
+            {
+                "ncol1": [4, 5, 6],
+                "ncol2": ["a", "b", "c"],
+                "deep": ["j", "k", "l"],
+            }
+        ),
+        "column2": np.array([1, 2, 3]),
+    }
+
+    bframe = BiocFrame(obj)
+    assert bframe is not None
+
+    sliced = bframe[0:2, :]
+
+    assert isinstance(sliced, BiocFrame)
+    assert isinstance(sliced.column("nested"), BiocFrame)
+    assert isinstance(sliced.row(0), dict)
+    assert isinstance(sliced.column("column2"), np.ndarray)
+
+
+def test_export_pandas():
+    obj = BiocFrame(
+        {
+            "column1": [1, 2, 3],
+            "nested": BiocFrame(
+                {
+                    "ncol1": [4, 5, 6],
+                    "ncol2": ["a", "b", "c"],
+                    "deep": ["j", "k", "l"],
+                }
+            ),
+            "column2": np.array([1, 2, 3]),
+        }
+    )
+
+    pdf = obj.to_pandas()
+    assert pdf is not None
+    assert isinstance(pdf, pd.DataFrame)
+    assert len(pdf) == len(obj)
+    assert len(set(pdf.columns).difference(obj.colnames)) == 0
+
+    obj["factor"] = Factor([0, 2, 1], levels=["A", "B", "C"])
+    pdf = obj.to_pandas()
+    assert pdf is not None
+    assert isinstance(pdf, pd.DataFrame)
+    assert len(pdf) == len(obj)
+    assert len(set(pdf.columns).difference(obj.colnames)) == 0
+    assert pdf["factor"] is not None
