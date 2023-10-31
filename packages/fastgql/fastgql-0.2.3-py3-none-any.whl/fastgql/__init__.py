ba0e01de9e8693a21ab855879a7b39d0724edea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:23 +0000

    Add package fastgql/0.2.3/fastgql-0.2.3-py3-none-any.whl

---
+++ packages/fastgql/fastgql-0.2.3-py3-none-any.whl/fastgql/__init__.py
@@ -0,0 +1,43 @@
+from .gql_models import GQL, GQLInput, GQLError, GQLConfigDict, GQLInterface
+from .schema_builder import SchemaBuilder
+from .info import Info
+from .context import BaseContext
+from .depends import Depends
+from .query_builders.edgedb.logic import get_qb
+from .query_builders.edgedb.query_builder import QueryBuilder, ChildEdge
+from .query_builders.edgedb.config import Link, Property, QueryBuilderConfig
+from .gql_ast.models import (
+    Node,
+    FieldNode,
+    FieldNodeModel,
+    FieldNodeField,
+    FieldNodeMethod,
+    OperationNode,
+)
+
+build_router = SchemaBuilder.build_router
+
+__all__ = [
+    "SchemaBuilder",
+    "build_router",
+    "GQL",
+    "GQLInterface",
+    "GQLInput",
+    "GQLError",
+    "GQLConfigDict",
+    "Info",
+    "BaseContext",
+    "Depends",
+    "get_qb",
+    "QueryBuilder",
+    "ChildEdge",
+    "Link",
+    "Property",
+    "QueryBuilderConfig",
+    "Node",
+    "FieldNode",
+    "FieldNodeModel",
+    "FieldNodeField",
+    "FieldNodeMethod",
+    "OperationNode",
+]
