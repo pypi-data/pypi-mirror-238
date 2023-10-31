794595710e50543291d8ef67a882152f344e51b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:05:42 +0000

    Add package clickzetta-connector/0.8.51.2/clickzetta_connector-0.8.51.2-py2.py3-none-any.whl

---
+++ packages/clickzetta-connector/clickzetta_connector-0.8.51.2-py2.py3-none-any.whl/clickzetta/__init__.py
@@ -0,0 +1,6 @@
+from clickzetta.client import Client
+from clickzetta.enums import LoginParams
+from clickzetta.table import Table
+from clickzetta.dbapi.connection import connect
+
+__all__ = ["Client", "LoginParams", "Table", "connect"]
