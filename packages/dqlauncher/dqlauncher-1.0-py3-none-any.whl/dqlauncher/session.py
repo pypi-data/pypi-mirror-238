3daefc6a5d9de0416d8aa82a05af108fbf6c230c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:08:50 +0000

    Add package dqlauncher/1.0/dqlauncher-1.0-py3-none-any.whl

---
+++ packages/dqlauncher/dqlauncher-1.0-py3-none-any.whl/dqlauncher/session.py
@@ -0,0 +1,31 @@
+"""
+DQLauncher version 1.0
+
+Session
+
+Author: Pablo Sierra Lorente
+Year: 2023
+"""
+
+from pyspark.sql import SparkSession
+from dqlauncher.validator import Validator
+
+
+class DQLauncherSession:
+
+    builder = SparkSession.builder
+
+    def __init__(self, appName="DQ Launcher App", master="local[*]", spark_config=None):
+        self.builder = SparkSession.builder
+        if appName:
+            self.builder.appName(appName)
+        if master:
+            self.builder.master(master)
+        if spark_config:
+            for key, value in spark_config.items():
+                self.builder.config(key, value)
+        self.spark = self.builder.getOrCreate()
+
+    def createValidator(self, data, schema):
+        spark_df = self.spark.createDataFrame(data, schema=schema)
+        return Validator(spark_df)
