a0af2f433f72a79e0f5e377c3da212321f74e337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:07:04 +0000

    Add package satori-ci/1.11.14/satori_ci-1.11.14-py3-none-any.whl

---
+++ packages/satori-ci/satori_ci-1.11.14-py3-none-any.whl/satoricli/api.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+import httpx
+
+HOST = "https://api.satori-ci.com"
+WS_HOST = "wss://api.satori-ci.com"
+
+client = httpx.Client(
+    base_url=HOST,
+    follow_redirects=True,
+    event_hooks={"response": [lambda r: r.raise_for_status()]},
+    timeout=60,
+)
+
+
+def configure_client(
+    token: str, timeout: Optional[int] = None, host: Optional[str] = None
+):
+    client.headers.update({"Authorization": f"Bearer {token}"})
+
+    if host:
+        client.base_url = host
+
+    if timeout:
+        client.timeout = timeout
