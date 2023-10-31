24c1a105693945cf8f5227aaaa5b83172424d7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:07:01 +0000

    Add package webtranspose/0.1.0/webtranspose-0.1.0.tar.gz

---
+++ packages/webtranspose/webtranspose-0.1.0.tar.gz/webtranspose-0.1.0/src/webtranspose/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for webtranspose."""
+
+__author__ = """Mike Gee"""
+__email__ = "mike@webtranspose.com"
+__version__ = "0.1.0"
+
+from .crawl import *
+from .openai import *
+from .scrape import *
