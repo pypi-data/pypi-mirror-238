983593e696a4aa03dc96c93d1d78f42aa6d16948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:00 +0000

    Add package giskard/2.0.0b29/giskard-2.0.0b29-py3-none-any.whl

---
+++ packages/giskard/giskard-2.0.0b29-py3-none-any.whl/giskard/client/dtos.py
@@ -0,0 +1,24 @@
+from typing import List, Dict, Optional
+
+from pydantic import BaseModel
+
+
+class TestInputDTO(BaseModel):
+    name: str
+    value: str
+    type: str
+    params: List["TestInputDTO"] = list()
+    is_alias: bool = False
+
+
+class SuiteTestDTO(BaseModel):
+    testUuid: str
+    functionInputs: Dict[str, TestInputDTO]
+    displayName: Optional[str] = None
+
+
+class TestSuiteDTO(BaseModel):
+    name: Optional[str]
+    project_key: str
+    tests: List[SuiteTestDTO]
+    function_inputs: List[TestInputDTO]
