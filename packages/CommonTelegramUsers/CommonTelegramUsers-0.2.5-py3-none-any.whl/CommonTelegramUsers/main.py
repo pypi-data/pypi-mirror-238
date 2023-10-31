eaab74729b43d38c2d2cf4917fdca89209925965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:07:59 +0000

    Add package CommonTelegramUsers/0.2.5/CommonTelegramUsers-0.2.5-py3-none-any.whl

---
+++ packages/CommonTelegramUsers/CommonTelegramUsers-0.2.5-py3-none-any.whl/CommonTelegramUsers/main.py
@@ -0,0 +1,44 @@
+from pyrogram import Client
+from typing import Set, Tuple, List
+
+def get_all_participants(app: Client, chat: int, verbose: bool = False) -> Set[int]:
+    all_participants: Set = set()
+    
+    if verbose:
+        n: int = 0
+        for member in app.get_chat_members(chat_id=chat):
+            all_participants.add(member.user.id)
+            n += 1
+            print(f"{n} users processed", end="\r")
+        print("\n")
+    else:
+        for member in app.get_chat_members(chat_id=chat):
+            all_participants.add(member.user.id)
+
+    return all_participants
+
+def process_chat(chat) -> int|str:
+    if chat.isdigit(): return int(chat)
+    else: return chat.replace("@", "").replace("/", "").replace("https:t.me", "")
+
+def find(args: List[str|int], api_id: int, api_hash: str, name: str = "CTU", blacklist: Set[int] = set(), verbose: bool = False) -> Set[int]:
+    app: Client = Client(name=name, api_id=api_id, api_hash=api_hash)
+
+    with app:
+        init: Set[int] = get_all_participants(app=app, chat=process_chat(args[0]), verbose=verbose) - blacklist
+        
+        for chat in args[1:]:
+            init: Set[int] = init & get_all_participants(app=app, chat=process_chat(chat))
+
+        return init
+
+def finds(api_id: int, api_hash: str, name: str = "CTU", blacklist: Set[int] = set(), verbose: bool = False, *args: Tuple[str|int]) -> Set[int]:
+    app: Client = Client(name=name, api_id=api_id, api_hash=api_hash)
+
+    with app:
+        init: Set[int] = get_all_participants(app=app, chat=process_chat(args[0]), verbose=verbose) - blacklist
+        
+        for chat in args[1:]:
+            init: Set[int] = init & get_all_participants(app=app, chat=process_chat(chat))
+
+        return init
