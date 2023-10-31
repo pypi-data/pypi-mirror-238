98d40768ee5311c07d538f9acb7c1e8d4ac930d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:28 +0000

    Add package insideopt-seeker/0.3/insideopt_seeker-0.3.tar.gz

---
+++ packages/insideopt-seeker/insideopt_seeker-0.3.tar.gz/insideopt_seeker-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='insideopt_seeker',
+    version='0.3',
+    description='A module for stochastic non-linear optimization',
+    author='Meinolf Sellmann',
+    author_email='meinolf@gmail.com',
+    packages=['.'],
+    zip_safe=False
+)
