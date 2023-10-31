56e09fc557d5c7e007508593950e6ac2afe20f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 07:06:12 +0000

    Add package TMTool/0.0.50/TMTool-0.0.50-py3-none-any.whl

---
+++ packages/TMTool/TMTool-0.0.50-py3-none-any.whl/Scripts/Model/asset_form.py
@@ -0,0 +1,168 @@
+# this form helps modelers describe data flows within the model
+# this script takes all model's "flows" as input, dynamically creates an input form
+# based on the number of flows, & then the modeler sets CIA + severity metrics for each flow
+# MODELING TIP: a model should always contain flows describing how data-at-rest gets stored/extracted 
+#  from a system (don't just capture data-in-motion). Since threats are generated based on flows,
+#  CIA is also infered from flows
+# TODO: also have notes and justification as text input
+# TODO: since severity is a threat prop, for (some) threats we could store it in the template and chose worst case
+#        PyTM uses severity as a threat property: https://github.com/izar/pytm/blob/master/docs/threats.md
+
+import tkinter as tk
+from tkinter import ttk
+# override the basic Tk widgets
+from tkinter import *
+from ttkthemes import ThemedStyle
+from .. import tooltip
+
+class AssetsWindow(ttk.Frame):
+    def __init__(self, master, flows, *args, **kwargs):
+        self.master = master
+        self.flows = flows
+        self.Assets = dict()
+
+        self.master.configure(background='#404040')
+        self.master.title('ASSET FORM')
+        style = ThemedStyle(self.master)
+        style.set_theme("equilux")
+        ttk.Frame.__init__(self, master, *args, **kwargs)
+        self.label_frame = ttk.Frame(self.master)
+        self.label_frame.grid()
+        self.label_list = []
+
+        self.label_list.append(ttk.Label(self.label_frame, text="List of Assets"))
+        self.label_list.append(ttk.Button(self.label_frame, text="Add new Asset", command=self.add_new_data))
+        for ndex, i in enumerate(self.label_list):
+            i.grid(row=ndex)
+
+    # open a user input form for adding an asset that will be shown on the root screen
+    def add_new_data(self):
+        self.new_f = tk.Tk()
+        self.new_f.configure(background='#404040')
+        self.new_f.title('NEW ASSET')
+        style = ThemedStyle(self.new_f)
+        style.set_theme("equilux")
+
+        a_B1 = ttk.Button(self.new_f, text="Add Asset", command=self.quit_new_asset)
+        _lb1 = Listbox(self.new_f, selectmode='multiple', background='#404040', foreground='#FFFFFF')
+        _lb2 = Listbox(self.new_f, selectmode='multiple', background='#404040', foreground='#FFFFFF')
+        _t1 = ttk.Label(self.new_f, text="Severity Impact:", font=(None, 12, 'bold'))
+        _t2 = ttk.Label(self.new_f, text="Flows Impacted:",  font=(None, 12, 'bold'))
+
+        safe_data=("No Injuries", "Light Injuries", "Severe Injuries", "Life-threatening")
+        fin_data=("No Financial Impact", "Low Impact", "Substantial Loss", "Devastating")
+        op_data=("No Effect", "Minor Disruption", "Moderate Disruption", "Major Disruption", "Fails Saftey Reqs")
+        priv_data=("No Effect", "Low", "Medium", "High")
+        asset_knowledge=("Public","Restricted","Sensitive","Critical")
+        asset_weights=("Low","Medium","High")
+        asset_loss_factors = ("IP", "Personal Identifiers", "Health Data (PHI)", "Strategic Info", "Reputation", "Monetary", "Regulations", "Forensic/Logging" )
+
+        _t3 = ttk.Label(self.new_f, text="Asset Safty Impact")
+        _t4 = ttk.Label(self.new_f, text="Asset Financial Impact")
+        _t5 = ttk.Label(self.new_f, text="Asset Operational Impact")
+        _t6 = ttk.Label(self.new_f, text="Asset Privacy Impact")
+
+        _t7 = ttk.Label(self.new_f, text="Short Title:", font=(None, 12, 'bold'))
+        _e1 = ttk.Entry(self.new_f)
+        _t8 = ttk.Label(self.new_f, text="Description:")
+        _e2 = ttk.Entry(self.new_f)
+
+        _t9 = ttk.Label(self.new_f, text="Required Knowledge:",  font=(None, 12, 'bold'))
+        _t10 = ttk.Label(self.new_f, text="Asset Valued Weight:",  font=(None, 12, 'bold'))
+        _t11 = ttk.Label(self.new_f, text="Cost/Loss Item:",  font=(None, 12, 'bold'))
+
+        _cb1 = ttk.Combobox(self.new_f, values=safe_data)
+        _cb2 = ttk.Combobox(self.new_f, values=fin_data)
+        _cb3 = ttk.Combobox(self.new_f, values=op_data)
+        _cb4 = ttk.Combobox(self.new_f, values=priv_data)
+        _cb5 = ttk.Combobox(self.new_f, values=asset_knowledge)
+        _cb6 = ttk.Combobox(self.new_f, values=asset_weights)
+        _cb1.current(0)
+        _cb2.current(0)
+        _cb3.current(0)
+        _cb4.current(0)
+        _cb5.current(0)
+        _cb6.current(0)
+        # get list of model flow names
+        flo_names = list()
+        for i in range(0, len(self.flows)):
+            flo_names.append(str(dict(list(self.flows.values())[i]).get('Name')))
+        # fill Listboxes    
+        for item in flo_names:
+            _lb1.insert('end', item)
+        for item in asset_loss_factors:
+            _lb2.insert('end', item)
+        # keep LB selection when clicking elsewhere
+        _lb1.configure(exportselection=False)
+        _lb2.configure(exportselection=False)
+        
+        self.new_f.columnconfigure(0, weight=1)
+        self.new_f.columnconfigure(1, weight=1)
+
+        _t7.grid(row=0, column= 0, sticky='nsew')
+        _e1.grid(row=1, column= 0, sticky='nsew')
+        _t8.grid(row=2, column= 0, sticky='nsew')
+        _e2.grid(row=3, column= 0, sticky='nsew')
+        # Listboxes
+        _t2.grid(row=4, column= 0, sticky='nsew')
+        _lb1.grid(row=5, rowspan=5, column= 0, sticky='nsew')
+        _t11.grid(row=10, column= 0, sticky='nsew')
+        _lb2.grid(row=11, rowspan=2, column= 0, sticky='nsew')
+        # comboboxes
+        _t1.grid(row=0, column= 1, sticky='nsew')
+        _t3.grid(row=1, column= 1, sticky='nsew')
+        _cb1.grid(row=2, column= 1, sticky='nsew')
+        _t4.grid(row=3, column= 1, sticky='nsew')
+        _cb2.grid(row=4, column= 1, sticky='nsew')
+        _t5.grid(row=5, column= 1, sticky='nsew')
+        _cb3.grid(row=6, column= 1, sticky='nsew')
+        _t6.grid(row=7, column= 1, sticky='nsew')
+        _cb4.grid(row=8, column= 1, sticky='nsew')
+        _t9.grid(row=9, column= 1, sticky='nsew')
+        _cb5.grid(row=10, column= 1, sticky='nsew')
+        _t10.grid(row=11, column= 1, sticky='sew')
+        _cb6.grid(row=12, column= 1, rowspan=1, sticky='new')
+
+        a_B1.grid(row=13, column= 0, columnspan=2, sticky='nsew')
+        # hover-over tips
+        tooltip.CreateToolTip(_t1, \
+            "Select all severity Impact Levels which apply")
+        tooltip.CreateToolTip(_t2, \
+            "Select all model flows which interact with the asset")
+        tooltip.CreateToolTip(_t9, \
+            "Availability of the asset's information")
+        tooltip.CreateToolTip(_t10, \
+            "Importance of this asset compared to other assets")
+        tooltip.CreateToolTip(_t11, \
+            "Consequences of a compromised asset")
+        self.new_f.mainloop()
+
+    ## quit the "new asset" window
+    def quit_new_asset(self):
+        if not len(self.Assets):
+            a_len = 1
+        else:
+            a_len = len(self.Assets)+1
+        # add asset to dict
+        self.Assets[a_len]='new'+str(a_len)
+        # add label
+        self.label_list.insert(1, ttk.Label(self.label_frame, text=self.Assets.get(a_len)))
+        for widget in self.label_frame.children.values():
+            widget.grid_forget() 
+        for ndex, i in enumerate(self.label_list):
+            i.grid(row=ndex)
+        # resize root
+        x = 200
+        y = (a_len*35+25)
+        self.master.geometry(str(x) + "x" + str(y))
+
+        self.new_f.destroy()
+
+
+def main(flows):
+    root = tk.Tk()
+    app = AssetsWindow(root, flows)
+    app.mainloop()
+
+if __name__ == '__main__':
+   main()
---
+++ packages/TMTool/TMTool-0.0.50-py3-none-any.whl/Scripts/Model/set_assets.py
@@ -0,0 +1,278 @@
+## Steps: load the model file, parse flow elements find elemnt properties,
+## find the "interactor" flow for each threat, fill in AV and Auth as threat properties (from element properites),
+## load metadata (notes) and threat props into cvss dicts
+# TODO: score dicts w/ cvss.py
+# TODO: get flow CIA + severity from CIA_form.py and set threat properties within this script
+
+from lxml import etree
+import tkinter as tk
+from tkinter import filedialog
+import json
+from . import asset_form
+from ..Scoring import cvss
+
+
+# checks element props for anything containing <_name>
+# returns props selected index value
+def get_metric(_props, _name):
+    for prop in _props:
+        for key,value in dict(prop).items():
+            if key is 'PropName' and _name in value:
+                index = int(prop.get('SelectedIndex'))
+                return prop.get('PropValues')[index]
+    return None
+
+# returns element as dictionary
+def get_element(_ele):
+    # set up dictionaries
+    # single element dict
+    element = dict.fromkeys(['GenericTypeId','GUID','Name','SourceGuid','TargetGuid', 'AV', 'Auth'])
+    # namespace for prop elements
+    ele_namespace = {'b': 'http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase'}
+    any_namespace = {'a': 'http://schemas.microsoft.com/2003/10/Serialization/Arrays'}
+    # create a custom element properties dict
+    ele_prop = dict.fromkeys(['PropName', 'PropGUID', 'PropValues', 'SelectedIndex'])
+    ele_props = []
+    # temp list of property values
+    _values = []
+                    # GUID also at this level
+    for ele4 in _ele.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+        # get GUID
+        for guid in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Guid'):
+            element['GUID'] = guid.text
+        for gen_type in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}GenericTypeId'):
+            element['GenericTypeId'] = gen_type.text
+        for source in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}SourceGuid'):
+            element['SourceGuid'] = source.text
+        for target in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}TargetGuid'):
+            element['TargetGuid'] = target.text
+        for props in ele4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model.Abstracts}Properties'):
+            for types in props.findall('.//a:anyType', any_namespace):
+            # get all child elements of anyType element, all properties located here
+                for dis_name in types.findall('.//b:DisplayName', ele_namespace):   
+                    ele_prop['PropName'] = dis_name.text
+                for prop_guid in types.findall('.//b:Name', ele_namespace):   
+                    if prop_guid.text:
+                        ele_prop['PropGUID'] = prop_guid.text
+                    else:
+                        ele_prop['PropGUID'] = ''
+                selection = types.find('.//b:SelectedIndex', ele_namespace)   
+                if selection is None:
+                    ele_prop['SelectedIndex'] = ''
+                    # get all prop values
+                    value = types.find('.//b:Value', ele_namespace)
+                    # set values
+                    if value.text is None:
+                        _values.append('')
+                    else:
+                        _values.append(value.text)
+                    # set custom element name 
+                    if ele_prop['PropName'] == 'Name':
+                        element['Name'] = value.text
+                        ele_prop['PropValues'] = _values.copy()
+                else:
+                    # get prop selection
+                    ele_prop['SelectedIndex'] = selection.text
+                    # get value list for selection
+                    for values in types.findall('.//b:Value/*', ele_namespace):
+                        _values.append(values.text)
+                    ele_prop['PropValues'] = _values.copy()
+                    # add prop to prop list
+                _values.clear()
+                ele_props.append(ele_prop.copy())
+                ele_prop.clear()
+        # save prop list to element dict
+        # TODO: search metrics should be a list
+        element['AV'] = get_metric(ele_props, 'access vector')
+        element['Auth'] = get_metric(ele_props, 'has authentication')
+        return element
+
+def get_meta(root):
+    # find all note elements
+    _id = ''
+    msgs = dict()
+    for notes in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Notes'):
+        for note in notes.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Note'):
+            for id in note.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Id'):
+                _id = id.text
+            for message in note.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Message'):
+                _message = message.text
+            msgs[_id] = _message
+    for key,value in msgs.items():
+        if "METADATA:" in value:
+            _meta = str(value).replace("METADATA:","").replace("\'", "\"")
+            return dict(json.loads(_meta))
+    return None
+
+
+def get_flows(root):
+    flows = dict()
+    for child in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceList'):
+        for drawing in child.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}DrawingSurfaceModel'):
+            # dont need borders currently
+            # for borders in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Borders'):
+            #     _id, ele = get_element(borders)
+            #     elements[_id] = ele
+            for lines in drawing.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Lines'):
+                # this level enumerates a model's elements
+                for line in lines.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfguidanyType'):
+                # unlike stencils, flows have a source and target guids
+                    ele = get_element(line)
+                    #print(ele['properties'])
+                    flows[ele.get('GUID')] = ele
+            return flows
+    return None
+
+# returns threat props as dict
+def get_TPs(root):
+    TPs = dict()
+    for ele in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}KnowledgeBase'):
+        for e2 in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase}ThreatMetaData'):
+            for e3 in e2.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}PropertiesMetaData'):
+                for e4 in e3.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}ThreatMetaDatum'):
+                    _id = ''
+                    _label = ''
+                    for e5 in e4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Id'):
+                        _id = e5.text
+                    for e5 in e4.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}Label'):
+                        _label = e5.text
+                    TPs[_id] = _label
+    return TPs
+
+# finds threat ineractor (flow) and fills in flow values (AV and auth)
+def set_TPs(root, flows):
+    TPs = get_TPs(root)
+    TP_AV_id = ''
+    TP_Auth_id = ''
+    for key,value in TPs.items():
+        if value == 'Access Vector':
+            TP_AV_id = key 
+        if value == 'Authentication':
+            TP_Auth_id = key
+
+    for ele in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}ThreatInstances//*//{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+        _interaction = ''
+        threat_id = ''
+        for id in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase}Id'):
+            threat_id = id.text
+        for e2 in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase}Properties'):
+            for ele2 in e2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfstringstring'):
+                for ele3 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Key'):
+                    if ele3.text == 'InteractionString':
+                        for e in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+                            # get interactor
+                            _interaction = e.text
+                            set_TP(root, flow_val(flows, 'AV', _interaction), threat_id, TP_AV_id)
+                            set_TP(root, flow_val(flows, 'Auth', _interaction), threat_id, TP_Auth_id)
+    return
+
+# set a single threat prop <TP_id> to <_val>
+def set_TP(root, _val, _id, TP_id):
+    for ele in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}ThreatInstances//*//{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+        for id in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase}Id'):
+            if _id == id.text:
+                for e2 in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase}Properties'):
+                    for ele2 in e2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfstringstring'):
+                        for ele3 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Key'):
+                            if ele3.text == TP_id:
+                                for ele4 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+                                    # set element TP here
+                                    ele4.text = _val
+                                    print('set threat ID: ' + _id +  ' threat prop id: ' + TP_id +' to val: ' + _val) 
+    return
+
+# check flows dictionary for <_flow_name>
+# return the value of the flow's dictionary key <_key>
+def flow_val(_flows, _key, _flow_name):
+    for item,value in _flows.items():
+        if value.get('Name') == _flow_name:
+            return value.get(_key)
+    print(_key +' Not found for ' + _flow_name)
+    return None
+
+# sees if current TP is a CVSS metric, return CVSS key or None
+def check_TP(_TPs, _guid):
+    my_list = ['Confidentiality','Integrity','Availability','Access Complexity','Access Vector','Authentication','Severity']
+    for key,val in _TPs.items():
+        for i, j in enumerate(my_list):
+            if j == val:
+                m = my_list[i]
+                if m is 'Confidentiality':
+                    return 'C'
+                elif m is 'Integrity':
+                    return 'I'
+                elif m is 'Availability':
+                    return 'A'
+                elif m is 'Access Complexity':
+                    return 'AC'
+                elif m is 'Access Vector':
+                    return 'AV'
+                elif m is 'Authentication':
+                    return 'Au'
+                elif m is 'Severity':
+                    return 'CDP'
+                else:
+                    print('error getting ' + val)
+                    return None
+            else:
+                continue
+    return None
+
+# goes through threat ids, collects cvss data
+def score_threats(root, meta):
+    TPs = get_TPs(root)
+    for ele in root.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.Model}ThreatInstances//*//{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+        cvss_dict = dict.fromkeys('ID','AV','AC','Au','C','I','A','CDP','TD','CR','IR','AR')
+        # set metrics from notes
+        cvss_dict['TD'] = meta.get('TD')
+        cvss_dict['CR'] = meta.get('CR')
+        cvss_dict['IR'] = meta.get('IR')
+        cvss_dict['AR'] = meta.get('AR')
+        for id in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase}Id'):
+            cvss_dict['ID'] = id.text
+        for e2 in ele.findall('{http://schemas.datacontract.org/2004/07/ThreatModeling.KnowledgeBase}Properties'):
+            for ele2 in e2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}KeyValueOfstringstring'):
+                for ele3 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Key'):
+                    _key = check_TP(TPs, ele3.text)
+                    # not a CVSS metric
+                    if not _key:
+                        continue
+                    else:
+                        for ele4 in ele2.findall('{http://schemas.microsoft.com/2003/10/Serialization/Arrays}Value'):
+                            cvss_dict[_key] = ele4.text
+        score = cvss.CVSS(cvss_dict)
+        print('scored threat ID ' + cvss_dict['ID'] + ' with a score of ' + score.overall_score)
+        # TODO: set CVSS score string
+
+def main():
+    root = tk.Tk()
+    root.withdraw()
+    # get model file
+    try:
+        file_path = filedialog.askopenfilename(parent=root, filetypes=[("MS threat model files", "*.tm7")])
+    except FileNotFoundError:
+        print('Must choose file path, quitting... ')
+        quit()
+    if not file_path:
+        print('Must choose file path, quitting... ')
+        quit()
+
+    root.destroy()
+    tree = etree.parse(file_path)
+    root = tree.getroot()
+
+    meta = get_meta(root)
+    if not meta:
+        print('No meta found, choosing defualts')
+
+    flows = asset_form.main(get_flows(root))
+    print(flows)
+    # set_TPs(root, flows)
+    # score_threats(root, meta)
+
+    # print('Finished!')
+    # tree.write(file_path)
+
+if __name__ == '__main__':
+   main()
+
