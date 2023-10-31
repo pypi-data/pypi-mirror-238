f4878a361ca9c98a8e5393f974117bf40ee9c740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:04:49 +0000

    Add package dcentralab-qa-infra-automation/1.9.2/dcentralab_qa_infra_automation-1.9.2.tar.gz

---
+++ packages/dcentralab-qa-infra-automation/dcentralab_qa_infra_automation-1.9.2.tar.gz/dcentralab_qa_infra_automation-1.9.2/src/dcentralab_qa_infra_automation/drivers/HelperFunctions.py
@@ -0,0 +1,27 @@
+import pytest
+
+from dcentralab_qa_infra_automation.utils.HTTPMethods import get
+
+"""
+helper functions to add options to driver
+
+@Author: Efrat Cohen
+@Date: 04.2023
+"""
+
+
+def addExtensionToChrome():
+    """
+    add CRX extension to chrome
+    :return: add_extension - current extension crx file
+    """
+    add_extension = None
+    # In metamask wallet type
+    if pytest.data_driven.get("wallet_type") == 'MetaMask':
+        add_extension = pytest.user_dir + pytest.properties.get("metamask.extension.crx")
+    elif pytest.data_driven.get("wallet_type") == 'Coinbase':
+        add_extension = pytest.user_dir + pytest.properties.get("coinbase.extension.crx")
+    elif pytest.data_driven.get("wallet_type") == 'Nami':
+        add_extension = pytest.user_dir + pytest.properties.get("nami.extension.crx")
+
+    return add_extension
