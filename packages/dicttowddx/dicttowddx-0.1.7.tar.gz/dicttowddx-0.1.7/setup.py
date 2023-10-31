f87c4092aa37adb3246e65500a4b61db7d716351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:05:29 +0000

    Add package dicttowddx/0.1.7/dicttowddx-0.1.7.tar.gz

---
+++ packages/dicttowddx/dicttowddx-0.1.7.tar.gz/dicttowddx-0.1.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="dicttowddx",
+    packages=find_packages(include=["dicttowddx"]),
+    version="0.1.7",
+    description="Utility lib to convert python dictionaries to valid WDDX data exchange "
+    "format",
+    author="Oyedotun Oyesanmi",
+    long_description_content_type="text/markdown",
+    author_email="dotunoyesanmi@gmail.com",
+    url="https://github.com/dotman14/dicttowddx",
+    long_description=long_description,
+    install_requires=["yattag"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest==7.4.3"],
+    test_suite="tests",
+    python_requires=">=3.8.0",
+    keywords=['python', 'wddx', 'data exchange'],
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
