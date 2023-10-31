9c70c795ee9ef14cff2d912e2f71b07dc947f469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:08:43 +0000

    Add package more-captcha/0.5.0/more_captcha-0.5.0-py3-none-any.whl

---
+++ packages/more-captcha/more_captcha-0.5.0-py3-none-any.whl/captcha/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+"""
+    Captcha
+    ~~~~~~~
+
+    A captcha library that generates audio and image CAPTCHAs.
+
+    :copyright: (c) 2014 by Hsiaoming Yang.
+    :license: BSD, see LICENSE for more details.
+"""
+
+__version__ = '0.5.0'
+__author__ = 'Hsiaoming Yang <me@lepture.com>'
+__co_author__ = 'Ari Archer <ari.web.xyz@gmail.com>'
+__homepage__ = 'https://ari-web.xyz/gh/captcha'
