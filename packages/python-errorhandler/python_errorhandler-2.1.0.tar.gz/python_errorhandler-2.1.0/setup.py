081aa2f7cb489d3aa4897aa4b7efb65517b9ea19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:09:08 +0000

    Add package python-errorhandler/2.1.0/python_errorhandler-2.1.0.tar.gz

---
+++ packages/python-errorhandler/python_errorhandler-2.1.0.tar.gz/python_errorhandler-2.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.1.0' 
+DESCRIPTION = "This Python package, 'python_errorhandler', simplifies error handling in your code. You no longer need to write try-catch blocks for every function. Instead, you can import this package and use the included decorator with your functions.\
+    The decorator allows you to specify a custom error message for better error handling and debugging."
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+        name="python_errorhandler", 
+        version=VERSION,
+        author="Nimesh Prajapati",
+        author_email="prajapatin953@gmail.com",
+        description=DESCRIPTION,
+        url="https://github.com/nimesh-p/python_error_handler",
+        license="MIT License",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        packages=['python_errorhandler'],
+        install_requires=[
+        "Django",  
+        "djangorestframework",
+    ],
+)
