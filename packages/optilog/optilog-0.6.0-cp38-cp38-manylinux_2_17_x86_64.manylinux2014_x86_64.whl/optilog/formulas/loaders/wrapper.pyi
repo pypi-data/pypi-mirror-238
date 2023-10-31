468de4853bb646c3f8f1f68545fbf431965a1797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:36 +0000

    Add package optilog/0.6.0/optilog-0.6.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/optilog/optilog-0.6.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/optilog/formulas/loaders/wrapper.pyi
@@ -0,0 +1,31 @@
+from optilog.formulas import CNF as CNF, WCNF as WCNF
+from pathlib import Path
+from typing import Union
+
+def load_cnf(file_path: Union[str, Path], *args, **kwargs) -> CNF:
+    """
+    Loads a CNF DIMACS [#cnf]_ into a :py:class:`optilog.formulas.CNF` object and returns it.
+    This method is particularly useful when working with big formulas
+    because it is very performant.
+
+    :param file_path: The path of a CNF in DIMACS format.
+
+    .. code-block:: python
+
+        >>> from optilog.formulas.loaders import load_cnf
+        >>> cnf = load_cnf('example.cnf')
+    """
+def load_wcnf(file_path: Union[str, Path], *args, **kwargs) -> WCNF:
+    """
+    Loads a WCNF DIMACS [#wcnf]_ into a :py:class:`optilog.formulas.WCNF` object and returns it.
+    This method is particularly useful when working with big formulas
+    because it is very performant.
+
+    :param file_path: The path of a CNF in DIMACS format.
+
+    .. code-block:: python
+
+        >>> from optilog.formulas.loaders import load_wcnf
+        >>> wcnf = load_wcnf('example.wcnf')
+
+    """
