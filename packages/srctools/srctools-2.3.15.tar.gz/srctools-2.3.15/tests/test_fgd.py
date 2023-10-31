ffa04e2264efbe2a1206faf459b62b9f4b35e014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:06:17 +0000

    Add package srctools/2.3.15/srctools-2.3.15.tar.gz

---
+++ packages/srctools/srctools-2.3.15.tar.gz/srctools-2.3.15/tests/test_fgd.py
@@ -0,0 +1,344 @@
+"""Test the FGD module."""
+from typing import Callable
+import copy
+import io
+
+import pytest
+
+from srctools import Vec
+from srctools.filesys import VirtualFileSystem
+
+
+with pytest.deprecated_call(match=r'srctools\.fgd\.KeyValues is renamed to srctools\.fgd\.KVDef'):
+    from srctools.fgd import *
+
+@pytest.mark.parametrize('name1', ['alpha', 'beta', 'gamma'])
+@pytest.mark.parametrize('name2', ['alpha', 'beta', 'gamma'])
+def test_autovisgroup_cmp(name1: str, name2: str) -> None:
+    """Test the AutoVisgroup class is comparable based on name."""
+    vis1 = AutoVisgroup(name1, 'SomeParent')
+    vis2 = AutoVisgroup(name2, 'SomeParent')
+
+    assert (vis1 == vis2) == (name1 == name2), 'Mismatch'
+    assert (vis1 != vis2) == (name1 != name2), 'Mismatch'
+    assert (vis1 < vis2) == (name1 < name2), 'Mismatch'
+    assert (vis1 > vis2) == (name1 > name2), 'Mismatch'
+    assert (vis1 <= vis2) == (name1 <= name2), 'Mismatch'
+    assert (vis1 >= vis2) == (name1 >= name2), 'Mismatch'
+
+
+def test_entity_parse() -> None:
+    """Verify parsing an entity produces the correct results."""
+    fsys = VirtualFileSystem({'test.fgd': """
+
+@PointClass base(Base1, Base2, Base3) 
+base(Base4, base_5)
+sphere(radii) 
+unknown(a, b, c)
+line(240 180 50, targetname, target)
+autovis(Auto, some, group)
+halfgridsnap // Special, no args
+appliesto(tag1, tag2, !tag3)
+= some_entity: "The description for this prop, which is spread over " 
++ "multiple lines."
+    [
+    keyvalue1(string): "Name" : "default": "documentation"
+    keyvalue2(int): "Hi": 4
+    keyvalue1[tag](boolean): "Tagged Name": 0
+    target(target_destination): "An ent"
+    
+    spawnflags(flags) : "Flags" = [
+        1 : "A" : 0
+        2 : "B" : 1
+        4 : "C" : 0
+        8 : "D value" : 0 [old, !good]
+        8 : "E" : 1 [new]
+    ]
+    
+    choicelist(choices) : "A Choice" : 0 : "Blahdy blah" = 
+        [
+        0: "First"
+        1: "Second" [new]
+        1: "Old second" [-oLd]
+        2: "Third"
+        "four": "Fourth"
+        ]
+"""})
+    fgd = FGD()
+    fgd.parse_file(fsys, fsys['test.fgd'], eval_bases=False)
+    ent = fgd['Some_ENtity']
+    assert ent.type is EntityTypes.POINT
+    assert ent.classname == 'some_entity'
+    assert ent.desc == 'The description for this prop, which is spread over multiple lines.'
+    assert ent.bases == ['Base1', 'Base2', 'Base3', 'Base4', 'base_5']
+
+    assert ent.helpers == [
+        HelperSphere(255, 255, 255, 'radii'),
+        UnknownHelper('unknown', ['a', 'b', 'c']),
+        HelperLine(240, 180, 50, 'targetname', 'target'),
+        HelperHalfGridSnap(),
+        HelperExtAppliesTo(['tag1', 'tag2', '!tag3'])
+    ]
+
+    assert ent.kv['keyvalue1', set()] == KVDef(
+        'keyvalue1',
+        ValueTypes.STRING,
+        'Name',
+        'default',
+        'documentation',
+    )
+    assert ent.kv['keyvalue1', {'tag'}] == KVDef(
+        'keyvalue1',
+        ValueTypes.BOOL,
+        'Tagged Name',
+        '0',
+        '',
+    )
+    assert ent.kv['keyvalue2'] == KVDef(
+        'keyvalue2',
+        ValueTypes.INT,
+        'Hi',
+        '4',
+        '',
+    )
+
+    assert ent.kv['spawnflags'] == KVDef(
+        'spawnflags',
+        ValueTypes.SPAWNFLAGS,
+        'Flags',
+        val_list=[
+            (1, 'A', False, frozenset()),
+            (2, 'B', True, frozenset()),
+            (4, 'C', False, frozenset()),
+            (8, 'D value', False, frozenset({'OLD', '!GOOD'})),
+            (8, 'E', True, frozenset({'NEW'})),
+        ],
+    )
+
+    assert ent.kv['choicelist'] == KVDef(
+        'choicelist',
+        ValueTypes.CHOICES,
+        'A Choice',
+        '0',
+        'Blahdy blah',
+        val_list=[
+            ('0', 'First', frozenset()),
+            ('1', 'Second', frozenset({'NEW'})),
+            ('1', 'Old second', frozenset({'-OLD'})),
+            ('2', 'Third', frozenset()),
+            ('four', 'Fourth', frozenset()),
+        ],
+    )
+
+
+@pytest.mark.parametrize('code, is_readonly, is_report', [
+    ('(int): "None"', False, False),
+    ('(int) readonly: "Readonly"', True, False),
+    ('(*int): "Old Report"', False, True),
+    ('(int) report: "New Report"', False, True),
+    ('(*int) readonly: "Both old-style"', True, True),
+    ('(int) readonly report: "Both new-style"', True, True),
+    # 'report readonly' is not allowed.
+    ('(*int) report: "Redundant"', False, True),
+    ('(*int) readonly report: "Redundant + readonly"', True, True),
+])
+def test_parse_kv_flags(code, is_readonly, is_report) -> None:
+    """Test the readonly and reportable flags can be set."""
+    fsys = VirtualFileSystem({'test.fgd': f"""
+    @PointClass = ent
+        [
+        keyvalue{code}: 1
+        ]
+    """})
+    fgd = FGD()
+    fgd.parse_file(fsys, fsys['test.fgd'], eval_bases=False)
+    kv = fgd['ent'].kv['keyvalue']
+
+    assert kv.readonly is is_readonly, kv
+    assert kv.reportable is is_report, kv
+
+
+def test_export_regressions(file_regression) -> None:
+    """Generate a FGD file to ensure code doesn't unintentionally alter output."""
+    fgd = FGD()
+    base_origin = EntityDef(EntityTypes.BASE, 'Origin')
+    base_angles = EntityDef(EntityTypes.BASE, 'Angles')
+
+    ent = EntityDef(EntityTypes.NPC, 'npc_test')
+    ent.bases = [base_origin, base_angles]
+
+    fgd.entities = {
+        # 'origin': base_origin,
+        'angles': base_angles,
+        'npc_test': ent,
+    }
+    base_origin.keyvalues['origin'] = {frozenset(): KVDef(
+        'origin',
+        ValueTypes.VEC_ORIGIN,
+        'Origin',
+        '0 0 0',
+    )}
+    base_angles.keyvalues['angles'] = {frozenset(): KVDef(
+        'angles',
+        ValueTypes.ANGLES,
+        'Angles (Pitch Yaw Roll)',
+        '0 0 0',
+    )}
+
+    ent.helpers = [
+        HelperSphere(255.0, 128.0, 64.0, 'radius'),
+        HelperModel('models/editor/a_prop.mdl'),
+        UnknownHelper('extrahelper', ['1', '15', 'thirtytwo']),
+        HelperSize(Vec(-16, -16, -16), Vec(16, 16, 16)),
+    ]
+    ent.desc = 'Entity description, extending beyond 1000 characters: ' + ', '.join(map(str, range(500))) + '. Done!'
+    ent.keyvalues['test_kv'] = {frozenset(): KVDef(
+        'test_kv',
+        ValueTypes.COLOR_255,
+        'A test keyvalue',
+        '255 255 128',
+        'Help text for a keyvalue',
+    )}
+
+    # The two special types with value lists.
+    ent.keyvalues['spawnflags'] = {frozenset(): KVDef(
+        'spawnflags',
+        ValueTypes.SPAWNFLAGS,
+        'Flags',
+        val_list=[
+            (1, 'A', False, frozenset()),
+            (2, 'B', True, frozenset()),
+            (4, 'C', False, frozenset()),
+            (8, 'D value', False, frozenset({'OLD', '!GOOD'})),
+            (8, 'E', True, frozenset({'NEW'})),
+        ],
+    )}
+
+    ent.keyvalues['multichoice'] = {frozenset(): KVDef(
+        'multichoice',
+        ValueTypes.CHOICES,
+        'Multiple Choice',
+        val_list=[
+            ('-1', 'Loss', frozenset()),
+            ('0', 'Draw', frozenset()),
+            ('1', 'Win', frozenset()),
+            ('bad', 'Very Bad', frozenset({'NEW'})),
+        ],
+    )}
+
+    # Test exporting with blank defaults and description.
+    ent.keyvalues['test_both'] = {frozenset(): KVDef(
+        'test_both',
+        ValueTypes.STRING,
+        'Both default and desc',
+        'defaulted',
+        'A description',
+    )}
+    ent.keyvalues['test_desc'] = {frozenset(): KVDef(
+        'test_desc',
+        ValueTypes.STRING,
+        'just desc',
+        desc='A description',
+    )}
+    ent.keyvalues['test_def'] = {frozenset(): KVDef(
+        'test_def',
+        ValueTypes.STRING,
+        'Just default',
+        default='defaulted',
+    )}
+    ent.keyvalues['test_neither'] = {frozenset(): KVDef(
+        'test_neither',
+        ValueTypes.STRING,
+        'Neither',
+    )}
+    # Special case, boolean must supply default.
+    ent.keyvalues['test_bool_neither'] = {frozenset(): KVDef(
+        'test_bool_neither',
+        ValueTypes.BOOL,
+        'Neither',
+    )}
+    ent.keyvalues['test_bool_desc'] = {frozenset(): KVDef(
+        'test_bool_desc',
+        ValueTypes.BOOL,
+        'Neither',
+        desc='A description',
+    )}
+
+    ent.inputs['Enable'] = {frozenset(): IODef('Enable')}
+    ent.inputs['SetSkin'] = {frozenset({'since_L4D'}): IODef('SetSkin', ValueTypes.INT, 'Set the skin.')}
+
+    ent.outputs['OnNoDesc'] = {frozenset(): IODef('OnNoDesc', ValueTypes.VOID)}
+    ent.outputs['OnSomething'] = {frozenset({'alpha'}): IODef('OnSomething', ValueTypes.VOID)}
+
+    # IO special case, boolean value type is named differently.
+    ent.outputs['OnGetValue'] = {frozenset(): IODef(
+        'OnGetValue',
+        ValueTypes.BOOL,
+        'Get some value',
+    )}
+
+    buf = io.StringIO()
+    fgd.export(buf)
+    file_regression.check(buf.getvalue(), extension='.fgd')
+
+
+@pytest.mark.parametrize('func', [
+    KVDef.copy, copy.copy, copy.deepcopy,
+], ids=['method', 'copy', 'deepcopy'])
+def test_kv_copy(func: Callable[[KVDef], KVDef]) -> None:
+    """Test copying of keyvalues objects."""
+    test_kv = KVDef(
+        name='some_key',
+        type=ValueTypes.TARG_DEST,
+        disp_name='Some Key',
+        default='!player',
+        desc='Does something',
+        readonly=False,
+        reportable=True,
+    )
+    duplicate = func(test_kv)
+    assert duplicate.name == test_kv.name
+    assert duplicate.type is test_kv.type
+    assert duplicate.disp_name == test_kv.disp_name
+    assert duplicate.default == test_kv.default
+    assert duplicate.desc == test_kv.desc
+    assert duplicate.val_list is None
+    assert not duplicate.readonly
+    assert duplicate.reportable
+
+    test_kv = KVDef(
+        name='another_key',
+        type=ValueTypes.CHOICES,
+        disp_name='Another Key',
+        default='45',
+        desc='Does something else',
+        readonly=True,
+        reportable=False,
+        val_list=[
+            ('43', 'Fourty-Three', frozenset()),
+            ('44', 'Fourty-Four', frozenset(['a', 'b'])),
+            ('45', 'Fourty-Five', frozenset('-engine')),
+        ]
+    )
+    duplicate = func(test_kv)
+    assert duplicate.type is test_kv.type
+    assert duplicate.val_list is not test_kv.val_list
+    assert duplicate.val_list == test_kv.val_list
+    assert duplicate.readonly
+    assert not duplicate.reportable
+
+
+@pytest.mark.parametrize('func', [
+    IODef.copy, copy.copy, copy.deepcopy,
+], ids=['method', 'copy', 'deepcopy'])
+def test_io_copy(func: Callable[[IODef], IODef]) -> None:
+    """Test copying of IODef objects."""
+    test_io = IODef(
+        name='OnWhatever',
+        type=ValueTypes.VOID,
+        desc='Does something',
+    )
+    duplicate = func(test_io)
+    assert duplicate.name == 'OnWhatever'
+    assert duplicate.type is ValueTypes.VOID
+    assert duplicate.desc == 'Does something'
---
+++ packages/srctools/srctools-2.3.15.tar.gz/srctools-2.3.15/tests/test_matrix.py
@@ -0,0 +1,255 @@
+"""Tests the Matrix/FrozenMatrix class in srctools.math."""
+import copy
+import fractions
+import itertools
+import pickle
+
+from dirty_equals import IsFloat
+import pytest
+
+from helpers import *
+
+
+def test_matrix_constructor(py_c_vec: PyCVec, frozen_thawed_matrix: MatrixClass) -> None:
+    """Test constructing matrices."""
+    Matrix = frozen_thawed_matrix
+    mat = Matrix()
+    assert type(mat) is Matrix
+    for x, y in itertools.product(range(3), range(3)):
+        assert mat[x, y] == (1.0 if x == y else 0.0)
+
+    mat_mutable = vec_mod.Matrix.from_angle(34.0, 29.0, 86.0)
+    mat_frozen = vec_mod.FrozenMatrix.from_angle(34.0, 29.0, 86.0)
+    copy_mutable = Matrix(mat_mutable)
+    copy_frozen = Matrix(mat_frozen)
+
+    assert type(copy_mutable) is Matrix
+    assert type(copy_frozen) is Matrix
+
+    assert copy_mutable is not mat_mutable
+
+    for x, y in itertools.product(range(3), range(3)):
+        assert copy_mutable[x, y] == mat_mutable[x, y]
+        assert copy_frozen[x, y] == mat_frozen[x, y]
+
+    # FrozenMatrix should not make a copy.
+    # TODO: Cython doesn't let you override tp_new for this yet.
+    if Matrix is vec_mod.Py_FrozenMatrix:
+        assert mat_frozen is copy_frozen
+
+
+def test_as_matrix(py_c_vec: PyCVec) -> None:
+    """Test the as_matrix() method. """
+    Matrix = vec_mod.Matrix
+    FrozenMatrix = vec_mod.FrozenMatrix
+    Angle = vec_mod.Angle
+    FrozenAngle = vec_mod.FrozenAngle
+
+    assert_rot(vec_mod.to_matrix(None), Matrix())
+    ang = Angle(12, 34, -15)
+    rot = Matrix.from_angle(ang)
+    assert_rot(vec_mod.to_matrix(Matrix.from_angle(ang)), rot)
+    assert_rot(vec_mod.to_matrix(FrozenMatrix.from_angle(ang)), rot)
+    assert_rot(vec_mod.to_matrix(Angle(ang)), rot)
+    assert_rot(vec_mod.to_matrix(FrozenAngle(ang)), rot)
+    assert_rot(vec_mod.to_matrix((ang.pitch, ang.yaw, ang.roll)), rot)
+
+
+# noinspection PyArgumentList
+def test_bad_from_basis(
+    py_c_vec: PyCVec,
+    frozen_thawed_vec: VecClass,
+    frozen_thawed_matrix: MatrixClass,
+) -> None:
+    """Test invalid arguments to Matrix.from_basis()"""
+    Vec = frozen_thawed_vec
+    Matrix = frozen_thawed_matrix
+
+    v = Vec(0, 1, 0)
+    with pytest.raises(TypeError):
+        Matrix.from_basis()
+    with pytest.raises(TypeError):
+        Matrix.from_basis(x=v)
+    with pytest.raises(TypeError):
+        Matrix.from_basis(y=v)
+    with pytest.raises(TypeError):
+        Matrix.from_basis(z=v)
+
+
+def test_matrix_no_iteration(py_c_vec: PyCVec) -> None:
+    """Test that Matrix cannot be iterated, as this is rather useless."""
+    Matrix = vec_mod.Matrix
+    with pytest.raises(TypeError):
+        iter(Matrix())
+    with pytest.raises(TypeError):
+        list(Matrix())
+
+
+def text_invalid_from_angle(py_c_vec: PyCVec, frozen_thawed_matrix: MatrixClass) -> None:
+    """Test invalid parameters passed to Matrix.from_angle()."""
+    Matrix = frozen_thawed_matrix
+    with pytest.raises(TypeError):
+        Matrix.from_angle()
+    with pytest.raises(TypeError):
+        Matrix.from_angle(45.0)
+    with pytest.raises(TypeError):
+        Matrix.from_angle(45.0, 30.0)
+    with pytest.raises(TypeError):
+        Matrix.from_angle(roll=30.0)
+    with pytest.raises(TypeError):
+        Matrix.from_angle(yaw=30.0)
+    with pytest.raises(TypeError):
+        Matrix.from_angle(pitch=30.0, roll=12.5)
+
+
+def test_copy(py_c_vec: PyCVec) -> None:
+    """Test copying Matrixes."""
+    Matrix = vec_mod.Matrix
+    FrozenMatrix = vec_mod.FrozenMatrix
+
+    # Some random rotation, so all points are different.
+    test_data = (38, 42, 63)
+
+    orig = Matrix.from_angle(*test_data)
+
+    cpy_meth = orig.copy()
+
+    assert orig is not cpy_meth  # Must be a new object.
+    assert cpy_meth is not orig.copy()  # Cannot be cached
+    assert type(orig) is type(cpy_meth)
+    assert orig == cpy_meth  # Numbers must be exactly identical!
+
+    cpy = copy.copy(orig)
+
+    assert orig is not cpy
+    assert cpy_meth is not copy.copy(orig)
+    assert orig == cpy
+
+    dcpy = copy.deepcopy(orig)
+
+    assert orig is not dcpy
+    assert orig == dcpy
+
+    frozen = FrozenMatrix.from_angle(*test_data)
+    # Copying FrozenMatrix does nothing.
+    assert frozen is frozen.copy()
+    assert frozen is copy.copy(frozen)
+    assert frozen is copy.deepcopy(frozen)
+
+
+def test_from_str(py_c_vec: PyCVec, frozen_thawed_matrix: MatrixClass) -> None:
+    """Test the functionality of Matrix.from_angstr()."""
+    Matrix = frozen_thawed_matrix
+    for pit, yaw, rol in iter_vec(VALID_ZERONUMS):
+        rot = Matrix.from_angle(pit, yaw, rol)
+        assert_rot(Matrix.from_angstr(f'{pit} {yaw} {rol}'), rot)
+        assert_rot(Matrix.from_angstr(f'<{pit} {yaw} {rol}>'), rot)
+        # {x y z}
+        assert_rot(Matrix.from_angstr(f'{{{pit} {yaw} {rol}}}'), rot)
+        assert_rot(Matrix.from_angstr(f'({pit} {yaw} {rol})'), rot)
+        assert_rot(Matrix.from_angstr(f'[{pit} {yaw} {rol}]'), rot)
+
+
+def test_pickle(py_c_vec: PyCVec, frozen_thawed_matrix: MatrixClass) -> None:
+    """Test pickling and unpickling matrices."""
+    Matrix = frozen_thawed_matrix
+    test_data = (38, 42, 63)
+
+    orig = Matrix.from_angle(*test_data)
+
+    pick = pickle.dumps(orig)
+    thaw = pickle.loads(pick)
+
+    assert orig is not thaw
+    assert orig == thaw
+
+    # Ensure we test the right frozen vs mutable class.
+    CyMatrix = getattr(vec_mod, 'Cy_' + Matrix.__name__)
+    PyMatrix = getattr(vec_mod, 'Py_' + Matrix.__name__)
+
+    # Ensure both produce the same pickle - so they can be interchanged.
+    # Copy over the floats, since calculations are going to be slightly different
+    # due to optimisation etc. That's tested elsewhere to ensure accuracy, but
+    # we need exact binary identity.
+    data = [
+        orig[x, y]
+        for x in (0, 1, 2)
+        for y in (0, 1, 2)
+    ]
+    cy_mat = CyMatrix._from_raw(*data)
+    py_mat = PyMatrix._from_raw(*data)
+
+    assert pickle.dumps(cy_mat) == pickle.dumps(py_mat)
+
+
+def test_thaw_freezing(py_c_vec: PyCVec) -> None:
+    """Test methods to convert between frozen <> mutable."""
+    Matrix = vec_mod.Matrix
+    FrozenMatrix = vec_mod.FrozenMatrix
+    Angle = vec_mod.Angle
+    # Other way around is not provided.
+    with pytest.raises(AttributeError):
+        Matrix.thaw()  # noqa
+    with pytest.raises(AttributeError):
+        FrozenMatrix.freeze()  # noqa
+
+    ang = Angle(12.5, 34.3, -15.8)
+    mut = Matrix.from_angle(ang)
+    froze = mut.freeze()
+    thaw = froze.thaw()
+
+    assert_rot(mut, Matrix.from_angle(ang), type=Matrix)
+    assert_rot(froze, Matrix.from_angle(ang), type=FrozenMatrix)
+    assert_rot(thaw, Matrix.from_angle(ang), type=Matrix)
+    # Test calling it on a temporary, in case this is optimised.
+    assert_rot(Matrix.from_angle(ang).freeze(), Matrix.from_angle(ang), type=FrozenMatrix)
+    assert_rot(FrozenMatrix.from_angle(ang).thaw(), Matrix.from_angle(ang), type=Matrix)
+
+
+def test_invalid_setitem(py_c_vec: PyCVec) -> None:
+    """Test trying to store various invalid values into a matrix."""
+    Matrix = vec_mod.Matrix
+    mat = Matrix()
+    mat[1, 1] = 3
+    assert mat[1, 1] == IsFloat(exactly=3.0)
+    mat[0, 2] = fractions.Fraction(1, 2)
+    assert mat[0, 2] == IsFloat(exactly=0.5)
+    with pytest.raises(TypeError):
+        mat[2, 0] = '1'
+    with pytest.raises(TypeError):
+        mat[2, 0] = [1, 2, 3]
+
+
+def test_inverse_known(frozen_thawed_matrix: MatrixClass) -> None:
+    """Test the matrix inverse() method with a known inverse. """
+    Matrix = vec_mod.Matrix
+
+    # Test for matrix with known inverse
+    mat = Matrix()
+    mat[0, 0], mat[0, 1], mat[0, 2] = (+1.0, -3.0, +7.0)
+    mat[1, 0], mat[1, 1], mat[1, 2] = (-1.0, +4.0, -7.0)
+    mat[2, 0], mat[2, 1], mat[2, 2] = (-1.0, +3.0, -6.0)
+
+    correct = Matrix()
+    correct[0, 0], correct[0, 1], correct[0, 2] = (-3.0, +3.0, -7.0)
+    correct[1, 0], correct[1, 1], correct[1, 2] = (+1.0, +1.0, +0.0)
+    correct[2, 0], correct[2, 1], correct[2, 2] = (+1.0, +0.0, +1.0)
+
+    to_invert = frozen_thawed_matrix(mat)
+    invert = to_invert.inverse()
+
+    assert_rot(invert, correct, type=frozen_thawed_matrix)
+
+
+def test_inverse_fail(frozen_thawed_matrix: MatrixClass) -> None:
+    """Test the matrix inverse() method for known failure. """
+    # Test for expected failure
+    mat = vec_mod.Matrix()
+    mat[0, 0], mat[0, 1], mat[0, 2] = (1.0, 2.0, 3.0)
+    mat[1, 0], mat[1, 1], mat[1, 2] = (0.0, 0.0, 0.0)
+    mat[2, 0], mat[2, 1], mat[2, 2] = (2.0, 4.0, 6.0)
+
+    to_invert = frozen_thawed_matrix(mat)
+
+    with pytest.raises(ArithmeticError):
+        to_invert.inverse()
