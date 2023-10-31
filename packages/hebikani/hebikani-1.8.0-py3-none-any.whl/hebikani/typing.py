59b294d77b7708e0216c4d449d980e08da354e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 15:05:42 +0000

    Add package hebikani/1.8.0/hebikani-1.8.0-py3-none-any.whl

---
+++ packages/hebikani/hebikani-1.8.0-py3-none-any.whl/hebikani/typing.py
@@ -0,0 +1,50 @@
+"""Define all the enums used in the CLI."""
+
+
+class HTTPMethod(object):
+    """Define the HTTP methods."""
+
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+
+
+class AnswerType(enumerate):
+    """Enum for the answer type."""
+
+    CORRECT = "correct"
+    INCORRECT = "incorrect"
+    INEXACT = "inexact"  # when the answer is not correct but is close
+    A_BIT_OFF = "a_bit_off"  # when the answer is not correct but is close
+
+
+class QuestionType(enumerate):
+    """Define the question types."""
+
+    MEANING = "meaning"
+    READING = "reading"
+
+
+class SubjectObject(enumerate):
+    """Subject objects."""
+
+    RADICAL = "radical"
+    KANJI = "kanji"
+    VOCABULARY = "vocabulary"
+    KANA_VOCABULARY = "kana_vocabulary"
+
+
+class Gender(enumerate):
+    """Gender"""
+
+    FEMALE = "female"
+    MALE = "male"
+
+
+class VoiceMode(enumerate):
+    """Voice Mode"""
+
+    ALTERNATE = "alternate"
+    RANDOM = "random"
+    FEMALE = "female"
+    MALE = "male"
