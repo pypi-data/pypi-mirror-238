20905d1fa80e363b215fe55f039cd0613224ace7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:36 +0000

    Add package dazer/0.1.12/dazer-0.1.12.tar.gz

---
+++ packages/dazer/dazer-0.1.12.tar.gz/dazer-0.1.12/dazer/__init__.py
@@ -0,0 +1,10 @@
+"""
+dazer, dataset size effect estimator
+"""
+
+__version__ = "0.1.12"
+__author__ = 'Michael Hartung'
+__credits__ = """Michael Hartung, Institute for Computational Systems Biology (https://www.cosy.bio/), University of Hamburg"""
+
+from .Subsampler import Subsampler
+from .Classifier import Classifier
