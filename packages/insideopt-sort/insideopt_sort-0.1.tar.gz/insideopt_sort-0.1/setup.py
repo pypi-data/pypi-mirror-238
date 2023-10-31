346678186ca920e7e0d35b83a472d95e99de1aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:04:41 +0000

    Add package insideopt-sort/0.1/insideopt_sort-0.1.tar.gz

---
+++ packages/insideopt-sort/insideopt_sort-0.1.tar.gz/insideopt_sort-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='insideopt_sort',
+    version='0.1',
+    description='A module that sorts numbers',
+    author='Meinolf Sellmann',
+    author_email='meinolf@gmail.com',
+    packages=['.'],
+    zip_safe=False
+)
