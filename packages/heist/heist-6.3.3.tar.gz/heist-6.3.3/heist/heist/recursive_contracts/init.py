d710fc128f28fdad83402502c7441e210fa1fa34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:06:37 +0000

    Add package heist/6.3.3/heist-6.3.3.tar.gz

---
+++ packages/heist/heist-6.3.3.tar.gz/heist-6.3.3/heist/heist/recursive_contracts/init.py
@@ -0,0 +1,43 @@
+import getpass
+from typing import Dict
+
+DEFAULT_TUNNEL = "asyncssh"
+
+
+async def sig_run(
+    hub,
+    remotes: Dict[str, Dict[str, str]],
+    *,
+    artifact_version,
+    manage_service,
+    roster_file: str,
+    roster: str,
+    **kwargs
+):
+    ...
+
+
+def _validate_remote(remote: Dict[str, str]):
+    if not remote.get("tunnel"):
+        remote["tunnel"] = DEFAULT_TUNNEL
+    return remote
+
+
+async def call_run(hub, ctx):
+    kwargs = ctx.get_arguments()
+    remotes = kwargs.pop("remotes")
+
+    validate_remotes = {}
+    for id_, remote in remotes.items():
+        validate_remotes[id_] = _validate_remote(remote)
+
+    kwargs.update(**kwargs.pop("kwargs", {}))
+    manage_service = kwargs.pop("manage_service", None)
+    artifact_version = kwargs.pop("artifact_version", None)
+    return await ctx.func(
+        kwargs.pop("hub"),
+        remotes=validate_remotes,
+        artifact_version=artifact_version,
+        manage_service=manage_service,
+        **kwargs
+    )
