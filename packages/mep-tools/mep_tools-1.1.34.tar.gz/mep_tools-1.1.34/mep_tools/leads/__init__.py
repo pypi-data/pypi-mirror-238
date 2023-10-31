9bdfab021c7c4c9b0327323d8533285396c128a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 04:06:19 +0000

    Add package mep-tools/1.1.34/mep_tools-1.1.34.tar.gz

---
+++ packages/mep-tools/mep_tools-1.1.34.tar.gz/mep_tools-1.1.34/mep_tools/leads/__init__.py
@@ -0,0 +1,7 @@
+# GENERATED FILE DO NOT CHANGE !!!
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent))
+from lead_tracker_pb2_grpc import *
+from lead_tracker_pb2 import *
+from lead_tracker_pb2 import *
