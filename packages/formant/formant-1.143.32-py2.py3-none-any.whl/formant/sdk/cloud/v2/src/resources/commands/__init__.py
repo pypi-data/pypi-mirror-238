d57d9316232481999f2e55083bc27bfd82e3ac4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:06:32 +0000

    Add package formant/1.143.32/formant-1.143.32-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.143.32-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandListResponse, CommandDeliveryEventStreamType, CommandStreamType, CommandResponseTags, ScopeFilterTypesItem, CommandProgress, CommandDeliveryEventType, CommandTags, Command, CommandTemplateTags, CommandTemplate, CommandRequestListResponse, CommandTemplateParameterMeta, FileInfo, CommandTemplateListResponse, CommandResponseStreamType, ScopeFilter, CommandRequest, CommandDeliveryEvent, CommandParameterMeta, CommandDeliveryEventTags, CommandParameter, CommandQuery, CommandResponse, CommandDeliveryEventMetadata
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.143.32-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/queries/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_query_api_client.models import HealthStatus, StreamDataPointsItem, PointCloud, StreamDataType, StreamData, Vector3, Battery, Odometry, Image, VideoMimeType, Bitset, Location, QueryAggregate, StreamDataTags, Quaternion, StreamAggregateDataType, StreamDataListResponse, StreamAggregateData, NumericSetEntry, Localization, Transform, Twist, Map, Path, File, TransformNode, StreamAggregateDataTags, Video, Query, Goal, Video,VideoMimeType, ImageAnnotation, QueryTypesItem, Health, BoundingBox, Health,HealthStatus
+from formant.sdk.cloud.v2.formant_query_api_client.api.query import query_controller_query
+from formant.sdk.cloud.v2.formant_query_api_client.types import *
