030d1f269b30a36bbbd8daf9d95f2ec0c6e3c321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:07:52 +0000

    Add package JnaneswarPackagetest/0.0.1/JnaneswarPackagetest-0.0.1.tar.gz

---
+++ packages/JnaneswarPackagetest/JnaneswarPackagetest-0.0.1.tar.gz/JnaneswarPackagetest-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='JnaneswarPackagetest',
+    version='0.0.1',
+    description='ADI python Package module',
+    url='https://github.com/Sagya7/python_day2_capstone.git',
+    author='Jnaneswar',
+    author_email='saijnaneswar.juvvisetty@analog.com',
+    packages=setuptools.find_packages(),
+    long_description='Jnaneswar python module',
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
