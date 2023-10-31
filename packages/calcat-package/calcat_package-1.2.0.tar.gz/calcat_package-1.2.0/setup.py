a45eae0a81a59839161de6dc4b69332fd5f1e946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 05:05:28 +0000

    Add package calcat-package/1.2.0/calcat_package-1.2.0.tar.gz

---
+++ packages/calcat-package/calcat_package-1.2.0.tar.gz/calcat_package-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calcat-package',
+    version='1.2.0',
+    packages=find_packages(),
+    author='Agnieszka Thiel',
+    description='A Python package for basic arithmetic operations.',
+    url='https://github.com/TuringCollegeSubmissions/athiel-DWWP.1.5',
+    license='MIT',
+)
