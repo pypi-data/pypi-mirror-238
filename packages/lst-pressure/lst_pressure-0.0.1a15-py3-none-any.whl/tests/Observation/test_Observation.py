1222f3c7043fb028a157030456bb11bf9e5006f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 03:05:43 +0000

    Add package lst-pressure/0.0.1a15/lst_pressure-0.0.1a15-py3-none-any.whl

---
+++ packages/lst-pressure/lst_pressure-0.0.1a15-py3-none-any.whl/tests/Observation/test_Observation.py
@@ -0,0 +1,12 @@
+from typing import List
+from lstpressure.observation import Observation
+from lstpressure.lstcalendar import LSTCalendar
+from ..conftest import OBSERVATIONS
+
+
+def test_self_observable(observations: List["Observation"], lst_calendar: "LSTCalendar"):
+    for i, obs in enumerate(observations):
+        _expected = OBSERVATIONS[i].get("_expected", {})
+        _expected_count = _expected.get("result_count", None)
+        if _expected and _expected_count:
+            assert eval(f"len(obs.observable(lst_calendar)) {_expected_count}")
