1b21249646375b8b71d8df949c3aef06c05a97bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 14:06:31 +0000

    Add package todolist-Telecom/1.0.33/todolist_Telecom-1.0.33.tar.gz

---
+++ packages/todolist-Telecom/todolist_Telecom-1.0.33.tar.gz/todolist_Telecom-1.0.33/bin/list_task.py
@@ -0,0 +1,18 @@
+"""This script list all the of a user in the database."""
+
+from models.task_list import TaskList
+from models.db import execute_query
+
+
+def list_tasks(user_id: int):
+    """List all the of a user in the database."""
+    task_list = TaskList()
+
+    query = """SELECT * FROM tasks INNER JOIN
+            assigned_to ON id = id_task where u.id = ?"""
+    values = (user_id,)
+
+    results = execute_query(query, values)
+    print(results)
+    for result in results:
+        task_list.add_task(result[1], result[2])
