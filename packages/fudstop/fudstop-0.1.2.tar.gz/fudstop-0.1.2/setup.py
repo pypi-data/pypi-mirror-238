0d6869b3537016aff6972965866e864e7370f7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:20 +0000

    Add package fudstop/0.1.2/fudstop-0.1.2.tar.gz

---
+++ packages/fudstop/fudstop-0.1.2.tar.gz/fudstop-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fudstop',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        # list your project dependencies here
+    ],
+)
