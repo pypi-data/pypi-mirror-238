a12f0b3c078652427f91ee655f836de70ac3ee20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:09:05 +0000

    Add package MYBANKPACKAGE/1.0.0/MYBANKPACKAGE-1.0.0.tar.gz

---
+++ packages/MYBANKPACKAGE/MYBANKPACKAGE-1.0.0.tar.gz/MYBANKPACKAGE-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MYBANKPACKAGE',
+    version='1.0.0',
+    description='MyBank Inc. Banking System',
+    author='Sneha Iyer',
+    author_email='iyersneha243@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
