ba56d94d8847b68e632e563a0b52bc1b9e849d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 07:06:21 +0000

    Add package config-library/0.6.0/config-library-0.6.0.tar.gz

---
+++ packages/config-library/config-library-0.6.0.tar.gz/config-library-0.6.0/src/configlib/loader/loaders/json_loader.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import os
+import json
+import typing as t
+from configlib.loader import register_loader
+
+
+ReturnType: t.TypeAlias = t.Union[t.Dict[str, t.Any], t.List[t.Any]]
+
+
+@register_loader('json')
+def load_json(fp: t.Union[str, os.PathLike]) -> ReturnType:
+    r"""
+    {
+      "first_name": "John",
+      "last_name": "Smith",
+      "is_alive": true,
+      "age": 27,
+      "address": {
+        "street_address": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postal_code": "10021-3100"
+      },
+      "phone_numbers": [
+        {
+          "type": "home",
+          "number": "212 555-1234"
+        },
+        {
+          "type": "office",
+          "number": "646 555-4567"
+        }
+      ],
+      "children": [
+        "Catherine",
+        "Thomas",
+        "Trevor"
+      ],
+      "spouse": null
+    }
+    """
+    with open(fp, 'r') as file:
+        return json.load(file)
---
+++ packages/config-library/config-library-0.6.0.tar.gz/config-library-0.6.0/src/configlib/loader/loaders/xml_loader.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import os
+import typing as t
+import xml.etree.ElementTree
+import xml.dom.minidom
+from configlib.loader import register_loader
+
+
+ReturnType: t.TypeAlias = xml.dom.minidom.Element
+
+
+@register_loader('xml')
+def load_xml(fp: t.Union[str, os.PathLike]) -> ReturnType:
+    r"""
+    <?xml version="1.0" encoding="UTF-8"?>
+    <message>
+        <warning>
+             Hello World
+        </warning>
+    </message>
+    """
+    with open(fp, 'r') as file:
+        document: xml.dom.minidom.Document = xml.dom.minidom.parse(file)
+        # document.version, document.encoding, document.standalone
+        root: xml.dom.minidom.Element = document.documentElement
+    return root
