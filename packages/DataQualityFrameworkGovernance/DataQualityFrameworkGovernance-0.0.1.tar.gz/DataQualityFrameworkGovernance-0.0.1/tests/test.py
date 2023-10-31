33a9fc3220925f6e8c12bfc860f80f8f2ab43007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 06:07:36 +0000

    Add package DataQualityFrameworkGovernance/0.0.1/DataQualityFrameworkGovernance-0.0.1.tar.gz

---
+++ packages/DataQualityFrameworkGovernance/DataQualityFrameworkGovernance-0.0.1.tar.gz/DataQualityFrameworkGovernance-0.0.1/tests/test.py
@@ -0,0 +1,7 @@
+print('hello')
+
+dfLocation = "https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/customers/customers-100.csv"
+
+
+from Completeness import *
+missing_values(dfLocation)
