62c1ddc6ef57b6b792a8863090f0489d1c81c005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 03:09:35 +0000

    Add package colabseg/0.0.1/colabseg-0.0.1-py3-none-any.whl

---
+++ packages/colabseg/colabseg-0.0.1-py3-none-any.whl/colabseg/__init__.py
@@ -0,0 +1,10 @@
+#
+# ColabSeg - Interactive segmentation GUI
+#
+# Marc Siggel, December 2021
+
+# from tensorvoting_wrapper import *
+# from segmentation_gui import *
+
+from .new_gui_functions import ColabSegData
+from .image_io import ImageIO
