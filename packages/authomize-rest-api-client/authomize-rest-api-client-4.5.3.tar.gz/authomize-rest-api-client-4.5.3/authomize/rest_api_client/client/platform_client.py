8d842d621b1fa5e823677d526c0066e5ba2dbe77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-31 16:08:04 +0000

    Add package authomize-rest-api-client/4.5.3/authomize-rest-api-client-4.5.3.tar.gz

---
+++ packages/authomize-rest-api-client/authomize-rest-api-client-4.5.3.tar.gz/authomize-rest-api-client-4.5.3/authomize/rest_api_client/client/platform_client.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+from authomize.rest_api_client.client.base_client import BaseClient
+from authomize.rest_api_client.generated.external_rest_api.schemas import (
+    CreateCampaignRequestSchema,
+    CreateCampaignResponseSchema,
+    IncidentExpansion,
+    IsAliveResponse,
+    MeResponse,
+    NonPaginatedResponseSchemaIncidentSchema,
+)
+
+
+class PlatformClient(BaseClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def authorization_header(self) -> str:
+        return f'Bearer {self.auth_token}'
+
+    def is_alive(self) -> IsAliveResponse:
+        return self.http_get('/is_alive')
+
+    def me(self) -> MeResponse:
+        return self.http_get('/me')
+
+    def retrieve_incident(
+        self,
+        incident_id: str,
+        expand: Optional[list[IncidentExpansion]] = None,
+    ) -> NonPaginatedResponseSchemaIncidentSchema:
+        if not incident_id:
+            raise ValueError('Missing incident_id')
+        params = None
+        if expand:
+            params = dict(
+                expand=expand,
+            )
+        return self.http_get(f'/v2/incidents/{incident_id}', params=params)
+
+    def create_campaign(
+        self,
+        body: CreateCampaignRequestSchema,
+    ) -> CreateCampaignResponseSchema:
+        return self.http_post(
+            '/v2/campaigns',
+            body=body.json(),
+        )
