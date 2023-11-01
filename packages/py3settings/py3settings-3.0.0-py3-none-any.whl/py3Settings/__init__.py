ab9221b8a9370c73a18a594476d90388a7de06bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:06:45 +0000

    Add package py3settings/3.0.0/py3settings-3.0.0-py3-none-any.whl

---
+++ packages/py3settings/py3settings-3.0.0-py3-none-any.whl/py3Settings/__init__.py
@@ -0,0 +1,7 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Albert Calasanz Sallen. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+from .main import InAttribute, AppSettings, Option, Attribute, getWithAttr, addFormatSupport, showFileDefs, printObjProps
+# No hay recursividad
+__version__ = "3.0.0"
