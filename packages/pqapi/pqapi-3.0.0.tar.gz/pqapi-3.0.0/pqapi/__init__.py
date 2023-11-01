acc73898e6dc25603f952538e99c38efc4ab037e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 07:07:48 +0000

    Add package pqapi/3.0.0/pqapi-3.0.0.tar.gz

---
+++ packages/pqapi/pqapi-3.0.0.tar.gz/pqapi-3.0.0/pqapi/__init__.py
@@ -0,0 +1,28 @@
+from .version import __version__  # noqa
+from .api import (
+    upload_file,
+    upload_paper,
+    agent_query,
+    get_bibliography,
+    delete_bibliography,
+    async_delete_bibliography,
+    async_get_bibliography,
+    async_agent_query,
+    async_query,
+)  # noqa
+from .models import QueryRequest, UploadMetadata, AnswerResponse  # noqa
+
+__all__ = [
+    "upload_file",
+    "upload_paper",
+    "agent_query",
+    "get_bibliography",
+    "delete_bibliography",
+    "QueryRequest",
+    "UploadMetadata",
+    "AnswerResponse",
+    "async_delete_bibliography",
+    "async_get_bibliography",
+    "async_agent_query",
+    "async_query",
+]
---
+++ packages/pqapi/pqapi-3.0.0.tar.gz/pqapi-3.0.0/pqapi/auth.py
@@ -0,0 +1,9 @@
+import os
+
+
+def get_pqa_key():
+    if "PQA_API_KEY" in os.environ:
+        return os.environ["PQA_API_KEY"]
+    elif "PQA_API_TOKEN" in os.environ:
+        return os.environ["PQA_API_TOKEN"]
+    raise Exception("PQA_API_KEY environment variable not set")
