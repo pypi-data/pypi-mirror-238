db916eb3391d05f54abf75ae718f7f49916851c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:08:17 +0000

    Add package stockdata77/0.0.2/stockdata77-0.0.2.tar.gz

---
+++ packages/stockdata77/stockdata77-0.0.2.tar.gz/stockdata77-0.0.2/sample_cli.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# This is a demo code for Stocks class implemented
+# in stockquotes module
+#
+# Copyright 2022 Dandelion Systems <dandelion.systems@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from time import sleep
+from stockdata77 import Stocks
+
+# Create a database of stock quotes and fill it up
+stocks = Stocks()
+stocks.append("SQQQ", api="FMP", api_key="YOUR_API_KEY_HERE")
+stocks.append("AMZN", api="AV", api_key="YOUR_API_KEY_HERE")
+stocks.append("GMKN", "MOEX")
+
+# Start updating the quotes at 2 second intervals
+stocks.maintain(2)
+
+for i in range(2):
+	sleep(2)
+
+# Stop updating the quotes
+stocks.desist()
+
+print(stocks)
