c754a62af2e0318500ea45968ef3ce8244d21b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:08:18 +0000

    Add package WrenchCL/1.3.27/WrenchCL-1.3.27.tar.gz

---
+++ packages/WrenchCL/WrenchCL-1.3.27.tar.gz/WrenchCL-1.3.27/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# Read the long description from README.md
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# Read the dependencies from requirements.txt
+with open("requirements.txt", "r") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='WrenchCL',
+    version='v1.3.27',
+    author='willem@wrench.ai',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=required,
+    extras_require={
+        'optional': ['colorama>=0.4.6']
+    }
+)
