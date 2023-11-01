5f97533da17b71e39170b589c64a06a253dba6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:06:26 +0000

    Add package gh-reader/0.0.3/gh-reader-0.0.3.tar.gz

---
+++ packages/gh-reader/gh-reader-0.0.3.tar.gz/gh-reader-0.0.3/gh_reader/extractors/__init__.py
@@ -0,0 +1,13 @@
+from . import (
+    commits,
+    issue_comments,
+    issue_events,
+    issue_labels,
+    issues,
+    issues_pr,
+    labels,
+    pull_requests,
+    repository,
+    stargazers,
+    users
+)
