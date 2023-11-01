40673498280fadceced99ead6a002fe0fa791d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:07:20 +0000

    Add package feature-store-refract/1.0.21/feature_store_refract-1.0.21-py3-none-any.whl

---
+++ packages/feature-store-refract/feature_store_refract-1.0.21-py3-none-any.whl/feature_store/init_script.sh
@@ -0,0 +1,7 @@
+echo "Installing packages from INIT"
+#pip uninstall --yes refractio
+#pip uninstall --yes pandas
+#pip install --upgrade pip
+pip install feast==0.34.1
+pip install snowflake-snowpark-python==1.6.1
+echo "Done with dependency installation!";
