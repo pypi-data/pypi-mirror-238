8857b6b44929109899c8b7c7a9dba154eac00ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:06:51 +0000

    Add package urluckynum/9.33/urluckynum-9.33.tar.gz

---
+++ packages/urluckynum/urluckynum-9.33.tar.gz/urluckynum-9.33/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+# The path to the requirements.txt file.
+requirements_path = os.path.join("urluckynum", "requirements.txt")
+
+# Read the dependencies from the requirements.txt file.
+with open(requirements_path, "r") as f:
+    dependencies = f.read().splitlines()
+
+# Specify the package structure.
+setup(
+    name='urluckynum', # The name of the package.
+    version='9.33', # The version of the package.
+    description='My Application with a DB', # A short description of the package.
+    author='LuckyWave Group', # The author of the package.
+    packages=[ # A list of the packages in the package.
+        'urluckynum.app',
+    ],
+    package_dir={ # A dictionary that maps package names to directories.
+        'urluckynum': 'urluckynum',
+    },
+    install_requires=dependencies, # List of the dependencies that the package requires.
+) 
