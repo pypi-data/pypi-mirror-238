deebf1a3dc0aa4ca86545fca6d0f9e9776d7e6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:06:35 +0000

    Add package bbook-maker/0.4.0/bbook_maker-0.4.0.tar.gz

---
+++ packages/bbook-maker/bbook_maker-0.4.0.tar.gz/bbook_maker-0.4.0/book_maker/translator/__init__.py
@@ -0,0 +1,16 @@
+from book_maker.translator.caiyun_translator import Caiyun
+from book_maker.translator.chatgptapi_translator import ChatGPTAPI
+from book_maker.translator.deepl_translator import DeepL
+from book_maker.translator.google_translator import Google
+from book_maker.translator.gpt3_translator import GPT3
+from book_maker.translator.claude_translator import Claude
+
+MODEL_DICT = {
+    "chatgptapi": ChatGPTAPI,
+    "gpt3": GPT3,
+    "google": Google,
+    "caiyun": Caiyun,
+    "deepl": DeepL,
+    "claude": Claude,
+    # add more here
+}
