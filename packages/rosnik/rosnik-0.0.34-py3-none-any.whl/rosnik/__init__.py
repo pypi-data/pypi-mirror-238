fa8eda7fb997907d46008caa4a3a70279606841d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:04:42 +0000

    Add package rosnik/0.0.34/rosnik-0.0.34-py3-none-any.whl

---
+++ packages/rosnik/rosnik-0.0.34-py3-none-any.whl/rosnik/__init__.py
@@ -0,0 +1,5 @@
+from .client import init
+from .frameworks import flask_rosnik, django
+from .events.user import track_user_feedback, track_user_interaction
+
+__all__ = ["init", "flask_rosnik", "track_user_interaction", "track_user_feedback", "django"]
