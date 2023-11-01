64684519adbddcbb806a5bd33ac21fcb97bffb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:05:48 +0000

    Add package foursight/4.0.3/foursight-4.0.3.tar.gz

---
+++ packages/foursight/foursight-4.0.3.tar.gz/foursight-4.0.3/setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chalicelib_fourfront',
+ 'chalicelib_fourfront.checks',
+ 'chalicelib_fourfront.checks.helpers',
+ 'chalicelib_fourfront.scripts']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=3.1.2,<4.0.0',
+ 'MarkupSafe>=2.1.3,<3.0.0',
+ 'PyJWT>=2.5.0,<3.0.0',
+ 'benchmark-4dn>=0.5.17,<0.6.0',
+ 'click>=7.1.2,<8.0.0',
+ 'cron-descriptor>=1.4.0,<2.0.0',
+ 'dcicutils>=8.0.0,<9.0.0',
+ 'elasticsearch-dsl>=7.0.0,<8.0.0',
+ 'elasticsearch==7.13.4',
+ 'foursight-core>=5.0.0,<6.0.0',
+ 'geocoder==1.38.1',
+ 'gitpython>=3.1.2,<4.0.0',
+ 'google-analytics-data>=0.17.1,<0.18.0',
+ 'gspread>=3.6.0',
+ 'oauth2client>=4.1.3',
+ 'pytest-redis>=3.0.2,<4.0.0',
+ 'pytest>=7.4.2,<8.0.0',
+ 'pytz>=2020.1,<2021.0',
+ 'tibanna-ff>=3.1.1,<4.0.0',
+ 'tzlocal>=5.1,<6.0']
+
+entry_points = \
+{'console_scripts': ['local-check-execution = '
+                     'chalicelib_fourfront.scripts.local_check_execution:main',
+                     'publish-to-pypi = '
+                     'dcicutils.scripts.publish_to_pypi:main']}
+
+setup_kwargs = {
+    'name': 'foursight',
+    'version': '4.0.3',
+    'description': 'Serverless Chalice Application for Monitoring',
+    'long_description': 'None',
+    'author': '4DN-DCIC Team',
+    'author_email': 'support@4dnucleome.org',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<3.12',
+}
+
+
+setup(**setup_kwargs)
