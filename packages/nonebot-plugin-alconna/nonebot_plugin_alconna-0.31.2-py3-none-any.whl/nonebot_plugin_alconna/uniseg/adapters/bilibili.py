216db1429eeecc17154bbc0eb6eb90902c28d12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:07:34 +0000

    Add package nonebot-plugin-alconna/0.31.2/nonebot_plugin_alconna-0.31.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.31.2-py3-none-any.whl/nonebot_plugin_alconna/uniseg/adapters/bilibili.py
@@ -0,0 +1,30 @@
+from typing import cast
+
+from nonebot.adapters import Bot, Message, MessageSegment
+
+from ..segment import Text
+from ..export import Target, MessageExporter, export
+
+
+class BilibiliMessageExporter(MessageExporter):
+    def get_message_type(self):
+        from nonebot.adapters.bilibili.message import Message
+
+        return Message
+
+    @classmethod
+    def get_adapter(cls) -> str:
+        return "BilibiliLive"
+
+    @export
+    async def text(self, seg: Text, bot: Bot) -> MessageSegment:
+        msg = self.get_message_type()
+        ms = msg.get_segment_class()  # type: ignore
+
+        return ms.danmu(seg.text)
+
+    async def send_to(self, target: Target, bot: Bot, message: Message):
+        from nonebot.adapters.bilibili.adapter import Adapter
+
+        adapter: Adapter = cast(Adapter, bot.adapter)
+        return await adapter.bili.send(str(message), bot.self_id)
