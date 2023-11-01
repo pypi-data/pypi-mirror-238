8e710cc63ca1895e770477a516f17b278cc2c5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 15:06:21 +0000

    Add package mwtp/3.0.0/mwtp-3.0.0-py3-none-any.whl

---
+++ packages/mwtp/mwtp-3.0.0-py3-none-any.whl/mwtp/__init__.py
@@ -0,0 +1,12 @@
+'''Parser for MediaWiki titles'''
+
+from .namespace import Namespace
+from .parser import Parser as TitleParser
+from .title import Title
+
+
+__all__ = [
+	'Namespace',
+	'TitleParser',
+	'Title'
+]
