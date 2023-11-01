f5bf36050ed5d3f64e2b5ec98c0536be92c1d997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 06:07:38 +0000

    Add package pidgan/0.0.7/pidgan-0.0.7-py3-none-any.whl

---
+++ packages/pidgan/pidgan-0.0.7-py3-none-any.whl/pidgan/optimization/scores/ChiSquare.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from pidgan.optimization.scores.BaseScore import BaseScore
+
+
+class ChiSquare(BaseScore):
+    def __init__(self, name="chi2_score", dtype=None) -> None:
+        super().__init__(name, dtype)
