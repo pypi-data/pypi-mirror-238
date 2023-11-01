1c7bb1884d75f375831bd0b45d91809ef4b90b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:08:10 +0000

    Add package zcbot-celery-sdk/0.0.49/zcbot-celery-sdk-0.0.49.tar.gz

---
+++ packages/zcbot-celery-sdk/zcbot-celery-sdk-0.0.49.tar.gz/zcbot-celery-sdk-0.0.49/zcbot_celery_sdk/service/oss.py
@@ -0,0 +1,12 @@
+from celery.utils.log import get_task_logger
+from .base import BaseService
+from ..model.callback import Callback
+from ..model.param import OssZipDirParam
+
+LOGGER = get_task_logger(__name__)
+
+
+class OssService(BaseService):
+
+    def zip_dir(self, task_params: OssZipDirParam = None, callback: Callback = None, **kwargs):
+        return self.get_client().apply(task_name='oss.zip_dir', task_params=task_params.dict(), callback=callback, **kwargs)
