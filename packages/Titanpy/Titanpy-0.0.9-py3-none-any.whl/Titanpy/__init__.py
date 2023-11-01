4abf1201536c78fe75a54f4fb8b5b4b0924b289a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:07:32 +0000

    Add package Titanpy/0.0.9/Titanpy-0.0.9-py3-none-any.whl

---
+++ packages/Titanpy/Titanpy-0.0.9-py3-none-any.whl/Titanpy/__init__.py
@@ -0,0 +1,15 @@
+# visit my page! happy to help: https://github.com/pabboat 
+# I recommend using "from titanpy import Titanpy as tp"
+
+# imports
+from Titanpy.titanpy import Titanpy
+# from Titanpy.atlas import Atlas
+
+# test scripts
+
+if __name__ == "__main__":
+    b = Titanpy()
+
+    b.Connect(cred_path="./credentials/servicetitan_credentials.json")
+
+    print(b.Get(endpoint = 'receipts'))
