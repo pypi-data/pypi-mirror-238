02d88fb9856a8cbc923f4e3015c34b41322982b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 15:06:14 +0000

    Add package tradingview-indicators/0.0.3.4/tradingview_indicators-0.0.3.4.tar.gz

---
+++ packages/tradingview-indicators/tradingview_indicators-0.0.3.4.tar.gz/tradingview_indicators-0.0.3.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3.4'
+DESCRIPTION = 'An accurate calculation of technical analysis indicators with values aligning with those in TradingView.'
+
+setup(
+    name="tradingview_indicators",
+    version=VERSION,
+    author="m-marqx (Mateus Marques)",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas'],
+    keywords=['python', 'tradingview', 'technical analysis', 'indicators'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
