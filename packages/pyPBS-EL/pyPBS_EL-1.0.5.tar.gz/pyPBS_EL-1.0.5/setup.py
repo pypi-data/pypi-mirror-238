196bdf30af215cfb4d29d1df90b3ab5b1bee260b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:07:23 +0000

    Add package pyPBS-EL/1.0.5/pyPBS_EL-1.0.5.tar.gz

---
+++ packages/pyPBS-EL/pyPBS_EL-1.0.5.tar.gz/pyPBS_EL-1.0.5/setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages, Command
+
+from distutils.command.build_py import build_py
+
+setup(
+    name             = 'pyPBS_EL',
+    version          = '1.0.5',
+    description      = 'Package for distribution',
+    author           = 'msjeon27',
+    author_email     = 'msjeon27@cau.ac.kr',
+    url              = '',
+    download_url     = '',
+    install_requires = ['argparse'],
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['PYPBSEL', 'pypbsel'],
+    cmdclass         = {'build_py': build_py},
+	scripts          = ['scripts/pyPBS_EL'],
+    python_requires  = '>=3.6',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+) 
