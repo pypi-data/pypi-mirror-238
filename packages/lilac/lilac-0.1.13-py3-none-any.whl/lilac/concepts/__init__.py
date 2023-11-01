a3f8f651c0b65fae5e904b908504ae0aaeff0274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 06:07:45 +0000

    Add package lilac/0.1.13/lilac-0.1.13-py3-none-any.whl

---
+++ packages/lilac/lilac-0.1.13-py3-none-any.whl/lilac/concepts/__init__.py
@@ -0,0 +1,12 @@
+"""Concepts are customizable signals that help enrich documents."""
+
+from .concept import Example, ExampleIn
+from .db_concept import ConceptUpdate, DiskConceptDB, DiskConceptModelDB
+
+__all__ = [
+  'DiskConceptDB',
+  'DiskConceptModelDB',
+  'Example',
+  'ExampleIn',
+  'ConceptUpdate',
+]
