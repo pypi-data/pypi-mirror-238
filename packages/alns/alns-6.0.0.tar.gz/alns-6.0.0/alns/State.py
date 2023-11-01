3fc102f62f871b0af1d0309a111e3732cca506e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:06:00 +0000

    Add package alns/6.0.0/alns-6.0.0.tar.gz

---
+++ packages/alns/alns-6.0.0.tar.gz/alns-6.0.0/alns/State.py
@@ -0,0 +1,27 @@
+from typing import Protocol
+
+import numpy as np
+
+
+class State(Protocol):
+    """
+    Protocol for a solution state. Solutions should define an ``objective()``
+    member function for evaluation.
+    """
+
+    def objective(self) -> float:
+        """
+        Computes the state's associated objective value.
+        """
+
+
+class ContextualState(State, Protocol):
+    """
+    Protocol for a solution state that also provides context. Solutions should
+    define ``objective()`` and ``get_context()`` methods.
+    """
+
+    def get_context(self) -> np.ndarray:
+        """
+        Computes a context vector for the current state.
+        """
