b9ada637a9759d53de4ea23c6b9c170f068753fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 06:07:31 +0000

    Add package cvxportfolio/1.0.2/cvxportfolio-1.0.2.tar.gz

---
+++ packages/cvxportfolio/cvxportfolio-1.0.2.tar.gz/cvxportfolio-1.0.2/README.md
@@ -0,0 +1,166 @@
+# [Cvxportfolio](https://www.cvxportfolio.com)
+
+[![CVXportfolio on PyPI](https://img.shields.io/pypi/v/cvxportfolio.svg)](https://pypi.org/project/cvxportfolio/)
+[![linting: pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/pylint-dev/pylint)
+[![Coverage Status](https://coveralls.io/repos/github/cvxgrp/cvxportfolio/badge.svg?branch=master)](https://coveralls.io/github/cvxgrp/cvxportfolio?branch=master)
+[![Documentation Status](https://readthedocs.org/projects/cvxportfolio/badge/?version=latest)](https://cvxportfolio.readthedocs.io/en/latest/?badge=latest)
+
+
+
+`cvxportfolio` is a python library for portfolio optimization and simulation
+based on the book [Multi-Period Trading via Convex Optimization](https://web.stanford.edu/~boyd/papers/pdf/cvx_portfolio.pdf)
+(also [available in print](https://www.amazon.com/Multi-Period-Trading-Convex-Optimization-Foundations/dp/1680833286/)).
+
+The documentation of the package is kindly hosted by [Read the Docs](https://readthedocs.org) at [www.cvxportfolio.com](https://www.cvxportfolio.com). We also show some of our tutorials and examples on our
+[youtube channel](https://www.youtube.com/@Cvxportfolio).
+
+
+Installation
+------------
+All our source code and releases are kindly hosted by the [Python Package Index](https://pypi.org). You can install the latest one with
+
+```
+pip install -U cvxportfolio
+```
+You can see how this works on our [Installation and Hello World](https://youtu.be/1ThOKEu371M) youtube video.
+
+Testing locally
+------------
+We ship our unit test suite with the pip package. After installing you can test in you local environment by running
+
+```
+python -m cvxportfolio.tests
+```
+
+
+Simplest Example
+----------------
+In the following example market data is downloaded by a public source
+(Yahoo finance) and the forecasts are computed iteratively, at each point in the backtest, from past data. 
+That is, at each point in the backtest,
+the policy object only operates on **past data**, and thus the result you get is a realistic simulation of what the strategy would have performed in the market.
+The simulator by default includes holding and transaction costs, using the models described in the book, and default parameters that are typical for the US stock market.
+The logic used
+matches what is described in Chapter 7 of the book. For example, returns are forecasted as the historical mean returns 
+and covariances as historical covariances (both ignoring `np.nan`'s). The logic used is detailed in the `forecast` module. Many optimizations
+are applied to make sure the system works well with real data. 
+
+
+```python
+import cvxportfolio as cvx
+
+gamma = 3       # risk aversion parameter (Chapter 4.2)
+kappa = 0.05    # covariance forecast error risk parameter (Chapter 4.3)
+objective = cvx.ReturnsForecast() - gamma * (
+	cvx.FullCovariance() + kappa * cvx.RiskForecastError()
+) - cvx.StocksTransactionCost()
+constraints = [cvx.LeverageLimit(3)]
+
+policy = cvx.MultiPeriodOptimization(objective, constraints, planning_horizon=2)
+
+simulator = cvx.StockMarketSimulator(['AAPL', 'AMZN', 'TSLA', 'GM', 'CVX', 'NKE'])
+
+result = simulator.backtest(policy, start_time='2020-01-01')
+
+# print backtest result statistics
+print(result)
+
+# plot backtest results
+result.plot()
+```
+
+Some Other Examples
+-------------------
+We show in the example on [user-provided forecasters](https://github.com/cvxgrp/cvxportfolio/blob/master/examples/user_provided_forecasters.py) how the user can define custom classes to forecast
+the expected returns and covariances. These provide callbacks that are
+executed at each point in time during the backtest. The system enforces 
+causality and safety against numerical errors. 
+We recommend to always include 
+the default forecasters that we provide in any analysis you may do, 
+since they are very robust and well-tested. 
+
+We show in the examples on [DOW30 components](https://github.com/cvxgrp/cvxportfolio/blob/master/examples/dow30_example.py) and [wide assets-classes ETFs](https://github.com/cvxgrp/cvxportfolio/blob/master/examples/etfs_example.py) how a
+simple sweep over hyper-parameters, taking advantage of our sophisticated parallel backtest machinery, quickly provides results on the best strategy
+to apply to any given selection of assets.
+
+
+Development
+-----------
+To set up a development environment locally you should clone
+the repository (or,
+[fork on Github](https://docs.github.com/en/get-started/quickstart/fork-a-repo)
+and then clone your fork)
+
+```bash
+git clone https://github.com/cvxgrp/cvxportfolio.git
+cd cvxportfolio
+```
+
+Then, you should have a look at our
+[Makefile](https://www.gnu.org/software/make/manual/make.html#Introduction)
+and possibly change the `PYTHON` variable to match your system's python
+interpreter. Once you have done that,
+
+```bash
+make env
+make test
+```
+
+This will replicate our [development environment](https://docs.python.org/3/library/venv.html)
+and run our test suite.
+
+You activate the shell environment with one of scripts in `env/bin`
+(or `env\Scripts` on Windows), for example if you use bash on POSIX
+
+```bash
+source env/bin/activate
+```
+and from the environment you can run any of the scripts in the examples
+(the cvxportfolio package is installed in
+[editable mode](https://setuptools.pypa.io/en/latest/userguide/development_mode.html)).
+Or, if you don't want to activate the environment, you can just run scripts
+directly using `env/bin/python` (or `env\Scripts\python` on Windows)
+like we do in the Makefile.
+
+Additionally, to match our CI/CD pipeline, you may set the following
+[git hooks](https://git-scm.com/docs/githooks)
+
+```bash
+echo "make lint" > .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+echo "make test" > .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
+```
+
+Examples from the book
+----------------------
+In branch [0.0.X](https://github.com/cvxgrp/cvxportfolio/tree/0.0.X) you can find the original material used to generate plots
+and results in the book. As you may see from those
+ipython notebooks a lot of the logic that was implemented there, outside of cvxportfolio proper, is being included and made automatic
+in newer versions of cvxportfolio. 
+
+
+Academic
+------------
+
+If you use `cvxportfolio` in your academic work please cite our book:
+```
+@book{BBDKKNS:17,
+    author       = {S. Boyd and E. Busseti and S. Diamond and R. Kahn and K. Koh and P. Nystrup and J. Speth},
+    title        = {Multi-Period Trading via Convex Optimization},
+    series       = {Foundations and Trends in Optimization},
+    year         = {2017},
+    month        = {August},
+    publisher    = {Now Publishers},
+    url          = {http://stanford.edu/~boyd/papers/cvx_portfolio.html},
+}
+```
+
+
+License
+------------
+
+Cvxportfolio is licensed under the [Apache 2.0](http://www.apache.org/licenses/) permissive
+open source license.
+
+
---
+++ packages/cvxportfolio/cvxportfolio-1.0.2.tar.gz/cvxportfolio-1.0.2/cvxportfolio/returns.py
@@ -0,0 +1,195 @@
+# Copyright 2016 Enzo Busseti, Stephen Boyd, Steven Diamond, BlackRock Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module contains classes that define return models for.
+
+portfolio optimization policies, and related objects.
+"""
+
+import cvxpy as cp
+
+from .costs import Cost
+from .estimator import DataEstimator  # , ParameterEstimator
+from .forecast import HistoricalMeanError, HistoricalMeanReturn
+from .risks import BaseRiskModel
+
+__all__ = [
+    "ReturnsForecast",
+    "ReturnsForecastError",
+    "CashReturn",
+]
+
+
+class BaseReturnsModel(Cost):
+    """Base class for return models.
+
+    Use this to define any logic common to return models.
+    """
+
+
+class CashReturn(BaseReturnsModel):
+    r"""Objective term representing cash return.
+
+    By default, the forecast of cash return :math:`{\left(\hat{r}_t\right)}_n`
+    is the observed value from last period :math:`{\left({r}_{t-1}\right)}_n`.
+
+    This object is included automatically in :class:`SinglePeriodOptimization`
+    and :class:`MultiPeriodOptimization` policies. You can change
+    this behavior by setting their ``include_cash_return`` to False. If you do
+    so, you may include this cost explicitely in the objective. You need
+    to do so (only) if you provide your own cash return forecast.
+
+    :param cash_returns: if you have your forecast for the cash return, you
+        should pass it here, either as a float (if constant) or as pd.Series
+        with datetime index (if it changes in time). If you leave the default,
+        None, the cash return forecast at time t is the observed cash return
+        at time t-1. (As is suggested in the book.)
+    :type cash_returns: float or pd.Series or None
+    """
+
+    def __init__(self, cash_returns=None):
+        self.cash_returns = None if cash_returns is None else DataEstimator(
+            cash_returns, compile_parameter=True)
+        self._cash_return_parameter = None
+
+    def initialize_estimator(self, **kwargs):
+        self._cash_return_parameter = (cp.Parameter()
+            if self.cash_returns is None else self.cash_returns.parameter)
+
+    def values_in_time(self, past_returns, **kwargs):
+        """Update cash return parameter as last cash return."""
+        if self.cash_returns is None:
+            self._cash_return_parameter.value = past_returns.iloc[-1, -1]
+
+    def compile_to_cvxpy(self, w_plus, z, w_plus_minus_w_bm):
+        """Apply cash return to cash position."""
+        return w_plus[-1] * self._cash_return_parameter
+
+
+class ReturnsForecast(BaseReturnsModel):
+    r"""Returns forecast for non-cash assets, provided by the user or computed
+    from the data.
+
+    This class represents the term :math:`\hat{r}_t`,
+    the forecast of non-cash assets' returns at time :math:`t`.
+    :ref:`Optimization-based policies` use this, typically as the first
+    element of their objectives.
+    See Chapters 4 and 5 of the `book <https://web.stanford.edu/~boyd/papers/pdf/cvx_portfolio.pdf>`_
+    for more details.
+
+    It can either get return forecasts from the user, for example
+    computed with some machine learning technique, or it can estimate them
+    automatically from the data.
+
+    In the first case ``r_hat`` is specified as
+
+    * :class:`float`, if :math:`\hat{r}_t` is the same for all times and non-cash assets
+    * :class:`pandas.Series` with :class:`pandas.DatetimeIndex`, if :math:`\hat{r}_t` is the same for all assets but changes in time
+    * :class:`pandas.Series` indexed by assets' names, if :math:`\hat{r}_t` is constant in time and changes across assets
+    * :class:`pandas.DataFrame` with :class:`pandas.DatetimeIndex`, if :math:`\hat{r}_t` changes across time and assets.
+
+    The returns' forecast provided by the user must be supplied for all assets
+    excluding cash.
+
+    If instead ``r_hat`` is not speficied it defaults to None. This instructs
+    this class to compute :math:`\hat{r}_t` instead. It is
+    done, at each step of a :class:`BackTest`, by evaluating the full average of the
+    past returns (*i.e.,* the real returns :math:`{r}_{t-1}, {r}_{t-2}, \ldots`,
+    where :math:`t` is the current time),  skipping :class:`numpy.nan` values.
+    This is the default mode if no parameters are passed.
+
+    :param r_hat: constant or time varying returns estimates, provided in the form of
+        a pandas DataFrame indexed by timestamps of trading period and whose columns
+        are all non-cash assets. Alternatively it can be a pandas Series indexed by the
+        assets' names (so it is constant in time), a pandas Series indexed by time (so it is 
+        constant across assets), or a float (constant for all times and assets). 
+        If it is None, the default, the return forecasts are fitted from the data as historical means.
+    :type r_hat: pd.Series or pd.DataFrame or float or None
+    :param decay: decay factor used in :class:`MultiPeriodOptimization` policies.
+        It is as a number in :math:`[0,1]`. At step :math:`\tau` of the MPO policy, where 
+        :math:`\tau=t` is the initial one, the return predictions are multiplied by 
+        :math:`\texttt{decay}^{\tau-t}`. So, ``decay`` close to zero models a `fast` signal
+        while ``decay`` close to one a `slow` signal. The default value is 1.    
+    :type decay: float
+
+    :raises cvxportfolio.MissingTimesError: If the class accesses
+        user-provided elements of ``r_hat`` that are :class:`numpy.nan`.
+
+    :Example:
+
+    >>> import cvxportfolio as cvx
+    >>> policy = cvx.SinglePeriodOptimization(cvx.ReturnsForecast() - \
+        0.5 * cvx.FullCovariance(), [cvx.LongOnly(), cvx.LeverageLimit(1)])
+    >>> cvx.MarketSimulator(['AAPL', 'MSFT', 'GOOG']).backtest(policy).plot()
+
+    Defines a single period optimization policy where the returns' forecasts
+    :math:`\hat{r}_t` are the full average of past returns at each point in time
+    and the risk model is the full covariance, also computed from the past returns.
+    """
+
+    def __init__(self, r_hat=HistoricalMeanReturn, decay=1.):
+
+        if isinstance(r_hat, type):
+            r_hat = r_hat()
+
+        # we don't use DataEstimator's parameter
+        # because we apply the decay
+        self.r_hat = DataEstimator(r_hat)
+        self.decay = decay
+        self._r_hat_parameter = None
+
+    def initialize_estimator(self, universe, **kwargs):
+        self._r_hat_parameter = cp.Parameter(len(universe)-1)
+
+    def values_in_time(self, mpo_step=0, **kwargs):
+        self._r_hat_parameter.value = self.r_hat.current_value *\
+            self.decay**(mpo_step)
+
+    def compile_to_cvxpy(self,  w_plus, z, w_plus_minus_w_bm):
+        """Cvxpy expression acts on non-cash assets."""
+        return w_plus[:-1].T @ self._r_hat_parameter
+
+
+class ReturnsForecastError(BaseRiskModel):
+    """Simple return forecast error risk with values provided by the user.
+
+    Implements the model described in pages 31-32 of the paper. You must
+    pass the delta Series (if constant) or DataFrame (if time-varying)
+    of the forecast errors. Or, use one of the classes below to compute
+    it automatically. Multiply this object by an external multiplier
+    (which can itself be a ParameterEstimator) and calibrate for the
+    right amount of penalization.
+
+    :param deltas_errors: constant per-symbol errors on the returns
+        forecasts (if Series), or varying in time (if DataFrame), or
+        fitted from the data as the standard deviation of the historical
+        mean estimator
+    :type deltas_errors: pd.DataFrame or pd.Series or None
+    """
+
+    def __init__(self, deltas=HistoricalMeanError):
+
+        if isinstance(deltas, type):
+            deltas = deltas()
+        self.deltas = DataEstimator(deltas)
+        self._deltas_parameter = None
+
+    def initialize_estimator(self, universe, **kwargs):
+        self._deltas_parameter = cp.Parameter(len(universe)-1, nonneg=True)
+
+    def values_in_time(self, **kwargs):
+        self._deltas_parameter.value = self.deltas.current_value
+
+    def compile_to_cvxpy(self, w_plus, z, w_plus_minus_w_bm):
+        """Compile to cvxpy expression."""
+        return cp.abs(w_plus_minus_w_bm[:-1]).T @ self._deltas_parameter
