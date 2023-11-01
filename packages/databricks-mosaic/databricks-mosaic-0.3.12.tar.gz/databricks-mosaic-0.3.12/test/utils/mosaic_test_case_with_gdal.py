d1b7ef1de46fb251ef93197058d77495a65793b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:04:56 +0000

    Add package databricks-mosaic/0.3.12/databricks-mosaic-0.3.12.tar.gz

---
+++ packages/databricks-mosaic/databricks-mosaic-0.3.12.tar.gz/databricks-mosaic-0.3.12/test/utils/mosaic_test_case_with_gdal.py
@@ -0,0 +1,11 @@
+from test.context import api
+
+from .mosaic_test_case import MosaicTestCase
+
+
+class MosaicTestCaseWithGDAL(MosaicTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        api.enable_mosaic(cls.spark)
+        api.enable_gdal(cls.spark)
