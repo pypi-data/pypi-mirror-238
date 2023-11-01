378661ac7c009f5cbb1f7ddf4ebbe12e291b1b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 15:06:35 +0000

    Add package ngboost/0.4.2/ngboost-0.4.2-py3-none-any.whl

---
+++ packages/ngboost/ngboost-0.4.2-py3-none-any.whl/ngboost/distns/__init__.py
@@ -0,0 +1,12 @@
+"""NGBoost distributions"""
+from .categorical import Bernoulli, k_categorical  # NOQA
+from .cauchy import Cauchy  # NOQA
+from .distn import ClassificationDistn, Distn, RegressionDistn  # NOQA
+from .exponential import Exponential  # NOQA
+from .gamma import Gamma  # NOQA
+from .laplace import Laplace  # NOQA
+from .lognormal import LogNormal  # NOQA
+from .multivariate_normal import MultivariateNormal  # NOQA
+from .normal import Normal, NormalFixedVar  # NOQA
+from .poisson import Poisson  # NOQA
+from .t import T, TFixedDf, TFixedDfFixedVar  # NOQA
