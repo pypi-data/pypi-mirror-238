1b06e2cdc7f26ced439d66cf6b8588a80adac80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:05:49 +0000

    Add package feature-store-refract/1.0.11/feature-store-refract-1.0.11.tar.gz

---
+++ packages/feature-store-refract/feature-store-refract-1.0.11.tar.gz/feature-store-refract-1.0.11/feature_store/init_script.sh
@@ -0,0 +1,7 @@
+echo "Installing packages"
+#pip install refractio
+pip uninstall --yes pandas
+pip install -U feast==0.34.1
+#pip install jsonpickle==3.0.2
+#pip install snowflake-snowpark-python==1.6.1
+echo "Done with dependency installation!";
