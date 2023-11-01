246af880f86953e5c31a8c9f58f993867e852499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:08:34 +0000

    Add package gradio-foliumtest/0.0.1/gradio_foliumtest-0.0.1.tar.gz

---
+++ packages/gradio-foliumtest/gradio_foliumtest-0.0.1.tar.gz/gradio_foliumtest-0.0.1/demo/app.py
@@ -0,0 +1,30 @@
+
+import gradio as gr
+from gradio_foliumtest import FoliumTest
+from typing import Literal
+from folium import Map
+import random
+
+
+LAT_LONG_MAP = {
+    "New York City": (40.7128, -74.0060),
+    "London": (51.5074, -0.1278),
+    "San Francisco": (37.7749, -122.4194),
+    "Tokyo": (35.6762, 139.6503),
+    "Miami": (25.7617, -80.1918),
+}
+
+def get_city(city: Literal["New York City", "London", "San Francisco", "Tokyo", "Miami"]):
+    city = city or "Miami"
+    return Map(location=LAT_LONG_MAP[city], zoom_start=12)
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            city = gr.Radio(choices=["New York City", "London", "San Francisco", "Tokyo", "Miami"],
+                            label="City")
+        with gr.Column():
+            map_ = FoliumTest(label="Folium Map")
+        city.select(get_city, city, map_)
+
+demo.launch()
---
+++ packages/gradio-foliumtest/gradio_foliumtest-0.0.1.tar.gz/gradio_foliumtest-0.0.1/frontend/Index.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+
+	import type { Gradio } from "@gradio/utils";
+	import { Block, Empty, BlockLabel } from "@gradio/atoms";
+	import { Plot, File } from "@gradio/icons";
+	import { StatusTracker } from "@gradio/statustracker";
+	import type { LoadingStatus } from "@gradio/statustracker";
+	import type { FileData } from "@gradio/client";
+	import { normalise_file } from "@gradio/client";
+	import { tick } from "svelte";
+
+	// General Block Props
+	export let elem_id = "";
+	export let elem_classes: string[] = [];
+	export let visible = true;
+	export let value: FileData | null;
+	export let container = true;
+	export let scale: number | null = null;
+	export let min_width: number | undefined = undefined;
+	export let loading_status: LoadingStatus;
+	export let label: string | null;
+
+	// Props for handling files
+	export let root: string;
+	export let proxy_url: string;
+
+	export let height: number = 500;
+	
+	export let gradio: Gradio<{
+		change: never;
+	}>;
+
+	let new_value: FileData | null;
+
+	async function handle_change(value, new_value) {
+		await tick();
+		value = new_value;
+		gradio.dispatch("change");
+	}
+
+	$: new_value = normalise_file(value, root, proxy_url);
+	let old_value = new_value;
+
+	$: if (JSON.stringify(new_value) !== JSON.stringify(old_value)) {
+		gradio.dispatch("change");
+		old_value = new_value;
+	}
+
+</script>
+
+<Block {visible} {elem_id} {elem_classes} {container} {scale} {min_width}>
+	{#if loading_status}
+		<StatusTracker
+			autoscroll={gradio.autoscroll}
+			i18n={gradio.i18n}
+			{...loading_status}
+		/>
+	{/if}
+	<BlockLabel show_label={true} Icon={Plot} label={label || "Folium Map"} />
+	{#if value}
+		<iframe src={new_value.url} title={label ?? "Folium Map"} height="{height}px"></iframe>
+	{:else}
+		<Empty unpadded_box={true} size="large"><Plot /></Empty>
+	{/if}
+
+</Block>
+
+<style>
+	iframe	 {
+		display: flex;
+		width: var(--size-full);
+	}
+</style>
+
