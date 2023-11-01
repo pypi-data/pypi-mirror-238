154dc40493a0d3f8f44b89eaac9878741227dfc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:06:29 +0000

    Add package pyecore/0.14.0/pyecore-0.14.0.tar.gz

---
+++ packages/pyecore/pyecore-0.14.0.tar.gz/pyecore-0.14.0/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+
+
+version = tuple(sys.version_info[:2])
+
+if version < (3, 3):
+    sys.exit('Sorry, Python < 3.3 is not supported')
+
+requires = ['ordered-set>=4.0.1',
+            'restrictedpython>=5.3,>=6.1',
+            'future-fstrings']
+
+if version == (3, 4):
+    requires.append('lxml<4.4.0')
+else:
+    requires.append('lxml')
+
+packages = ['pyecore',
+            'pyecore.resources',
+            'pyecore.type']
+
+setup(
+    name='pyecore',
+    version='0.14.0',
+    description=('A Python(ic) Implementation of the Eclipse Modeling '
+                 'Framework (EMF/Ecore)'),
+    long_description=open('README.rst').read(),
+    keywords='model metamodel EMF Ecore MDE',
+    url='https://github.com/pyecore/pyecore',
+    author='Vincent Aranega',
+    author_email='vincent.aranega@gmail.com',
+
+    packages=packages,
+    package_data={'': ['README.rst', 'LICENSE', 'CHANGELOG.rst']},
+    include_package_data=True,
+    install_requires=requires,
+    tests_require=['pytest'],
+    license='BSD 3-Clause',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+    ]
+)
