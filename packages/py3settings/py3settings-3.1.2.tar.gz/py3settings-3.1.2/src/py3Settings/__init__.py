ec64b3c3740b559fd9ecd7d7f3da57bd7124b63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:08:09 +0000

    Add package py3settings/3.1.2/py3settings-3.1.2.tar.gz

---
+++ packages/py3settings/py3settings-3.1.2.tar.gz/py3settings-3.1.2/src/py3Settings/__init__.py
@@ -0,0 +1,8 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Albert Calasanz Sallen. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+from .main import InAttribute, AppSettings, Option, Attribute, getWithAttr, addFormatSupport, showFileDefs, printObjProps
+from .proxy import *
+# No hay recursividad
+__version__ = "3.0.0"
