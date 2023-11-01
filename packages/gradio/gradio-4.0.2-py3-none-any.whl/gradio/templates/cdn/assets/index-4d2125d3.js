3520d587246f1fda16eac51c52195f72ecd7a3ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:08:17 +0000

    Add package gradio/4.0.2/gradio-4.0.2-py3-none-any.whl

---
+++ packages/gradio/gradio-4.0.2-py3-none-any.whl/gradio/templates/cdn/assets/index-4d2125d3.js
@@ -0,0 +1,2 @@
+import{L as e,S as m,S as p}from"./Index-37584f50.js";import{T as i}from"./Blocks-9824d5aa.js";import"./index-0526d562.js";import"./svelte/svelte.js";import"./Button-89057c03.js";export{e as Loader,m as StatusTracker,i as Toast,p as default};
+//# sourceMappingURL=index-4d2125d3.js.map
---
+++ packages/gradio/gradio-4.0.2-py3-none-any.whl/gradio/templates/cdn/gradio.js
@@ -0,0 +1,10 @@
+
+
+function make_script(src) {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.setAttribute("crossorigin", "");
+    script.src = src;
+    document.head.appendChild(script);
+}
+make_script("https://gradio.s3-us-west-2.amazonaws.com/4.0.2/assets/index-0526d562.js");
