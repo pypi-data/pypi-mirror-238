7eafc6586d43ceda766e4f0f901f15bd87bc876a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 07:07:52 +0000

    Add package pcoetl/1.2.1/pcoetl-1.2.1.tar.gz

---
+++ packages/pcoetl/pcoetl-1.2.1.tar.gz/pcoetl-1.2.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pcoetl',
+    packages=find_packages(include=['pcoetl']),
+    version='1.2.1',
+    description='An ETL process for Planning Center',
+    author='Jacob Vandergriff',
+)
