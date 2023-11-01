c060034a6e6985fde6be91bb12bb645a482e93b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:05:55 +0000

    Add package dapla-suv-tools/0.1.1/dapla_suv_tools-0.1.1.tar.gz

---
+++ packages/dapla-suv-tools/dapla_suv_tools-0.1.1.tar.gz/dapla_suv_tools-0.1.1/src/dapla_suv_tools/_internals/integration/api_client.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+from dapla.auth import AuthClient
+
+END_USER_API_BASE_URL = os.getenv("SUV_END_USER_API_URL")
+
+
+def _get(path: str) -> str:
+
+    headers = _get_headers()
+
+    response = requests.get(f"{END_USER_API_BASE_URL}{path}")
+
+    if not _success(response.status_code):
+        raise Exception("Failed to fetch.")
+
+    return response.content.decode("UTF-8")
+
+
+def _get_headers() -> dict:
+    token: str = AuthClient.fetch_personal_token()
+
+    return {
+        "authorization": f"Bearer {token}"
+    }
+
+
+def _success(status_code: int) -> bool:
+    return str(status_code).startswith("2")
