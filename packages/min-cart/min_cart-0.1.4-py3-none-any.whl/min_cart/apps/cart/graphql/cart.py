2a2ddd5597be321bf3d0c7bae901c83e022fc92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:07:44 +0000

    Add package min-cart/0.1.4/min_cart-0.1.4-py3-none-any.whl

---
+++ packages/min-cart/min_cart-0.1.4-py3-none-any.whl/min_cart/apps/cart/graphql/cart.py
@@ -0,0 +1,20 @@
+import graphene
+from graphene import Field
+from graphene import relay
+
+
+
+class CartNode(graphene.ObjectType):
+    """Cart node for graphql api"""
+
+    total_price = graphene.Decimal()
+
+    def resolve_total_price(self, info):
+        return 0.0
+
+
+class Query(graphene.ObjectType):
+    """Query definitions"""
+    cart = Field(
+        CartNode,
+        description="Endpoint for fetching basket")
