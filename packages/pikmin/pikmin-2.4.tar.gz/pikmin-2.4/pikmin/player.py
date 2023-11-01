a38f10a13528041db8d5bd0430a203db33f565ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:06:44 +0000

    Add package pikmin/2.4/pikmin-2.4.tar.gz

---
+++ packages/pikmin/pikmin-2.4.tar.gz/pikmin-2.4/pikmin/player.py
@@ -0,0 +1,26 @@
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+import pygame
+import pkg_resources
+
+def play_audio():
+    # asset_path = pkg_resources.resource_filename('pikmin', 'assets/pikmin.wav')
+    asset_path = pkg_resources.resource_filename('pikmin', 'assets/pikmin.wav')
+
+    try:
+        pygame.init()
+        pygame.mixer.music.load(asset_path)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+    except pygame.error as e:
+        print(f"An error occurred: {e}")
+    finally:
+        pygame.quit()
+
+def main():
+    play_audio()    
+    
+if __name__ == "__main__":
+    main()
