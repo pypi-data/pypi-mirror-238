396763cabde4dbf5fea8c384b5de2bd5a4fec99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:07:19 +0000

    Add package mapeasy/0.0.1/mapeasy-0.0.1.tar.gz

---
+++ packages/mapeasy/mapeasy-0.0.1.tar.gz/mapeasy-0.0.1/README.md
@@ -0,0 +1,17 @@
+# mapeasy
+
+
+[![image](https://img.shields.io/pypi/v/mapeasy.svg)](https://pypi.python.org/pypi/mapeasy)
+[![image](https://img.shields.io/conda/vn/conda-forge/mapeasy.svg)](https://anaconda.org/conda-forge/mapeasy)
+
+
+**A python package for interactive mapping.**
+
+
+-   Free software: MIT license
+-   Documentation: https://Abubakr-Abdelrahman.github.io/mapeasy
+    
+
+## Features
+
+-   TODO
---
+++ packages/mapeasy/mapeasy-0.0.1.tar.gz/mapeasy-0.0.1/tests/test_mapeasy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `mapeasy` package."""
+
+
+import unittest
+
+from mapeasy import mapeasy
+
+
+class TestMapeasy(unittest.TestCase):
+    """Tests for `mapeasy` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
