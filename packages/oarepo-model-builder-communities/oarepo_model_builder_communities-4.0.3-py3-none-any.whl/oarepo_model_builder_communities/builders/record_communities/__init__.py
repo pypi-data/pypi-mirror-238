d2bdfc2802c6b632ddf324329116c5aa9881afaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 06:06:58 +0000

    Add package oarepo-model-builder-communities/4.0.3/oarepo_model_builder_communities-4.0.3-py3-none-any.whl

---
+++ packages/oarepo-model-builder-communities/oarepo_model_builder_communities-4.0.3-py3-none-any.whl/oarepo_model_builder_communities/builders/record_communities/__init__.py
@@ -0,0 +1,4 @@
+TEMPLATES = {
+    "service-config-additional": "templates/service_config_additional.py.jinja2",
+    "record-metadata-extra-fields": "templates/record_metadata_extra_fields.py.jinja2",
+}
