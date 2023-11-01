d7fbd927621b917c06e1921d21dd2fb5ca95a4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:08:12 +0000

    Add package deltatorch/0.0.3/deltatorch-0.0.3-py3-none-any.whl

---
+++ packages/deltatorch/deltatorch-0.0.3-py3-none-any.whl/deltatorch/pytorch.py
@@ -0,0 +1,83 @@
+from typing import List, Optional, Tuple, Any, Dict
+
+from torch.utils.data import DataLoader
+
+from .deltadataset import FieldSpec
+from .id_based_deltadataset import IDBasedDeltaDataset
+
+
+def create_pytorch_dataloader(
+    path: str,
+    id_field: str,
+    fields: List[FieldSpec],
+    partition_filter: Optional[List[Tuple[str, str, Any]]] = None,
+    version: Optional[int] = None,
+    batch_size: int = 32,
+    use_fixed_rank: bool = False,
+    rank: int = None,
+    num_ranks: int = None,
+    num_workers: int = 2,
+    shuffle: bool = False,
+    drop_last: bool = False,
+    storage_options: Optional[Dict[str, str]] = None,
+    **pytorch_dataloader_kwargs
+):
+    """Create a PyTorch DataLoader.
+
+    This method will do the following two steps:
+      1) Create an iterable dataset out of delta tables.
+      2) Create a PyTorch DataLoader based on the itearable dataset created in (1)
+
+    :param path: Path to the DeltaLake table
+    :param id_field: Autoincrement ID field.delta table must have an autoincrement ID
+    field.This field is used by deltatorch for sharding and parallel data loading
+    :param fields: List of  `detatorch.FieldSpec` used for training
+    :param version: Delta Table Version to load
+    :param partition_filter: A list of partition filters
+    :param batch_size: The number of items to return per batch. Default ``32``.
+    :param rank: Rank of the current process within the current distributed
+            group. This value is set by ``torch.distributed.get_rank()``
+            when ``use_fixed_rank`` is set to `False`.
+    :param num_ranks: number of processes in the current distributed process group.
+           This value is set by ``torch.distributed.get_world_size()``
+           when ``use_fixed_rank`` is set to `False`.
+    :param use_fixed_rank: If this values is set to `True`, `rank` and `num_ranks`
+    should be set explicitly by user
+    :param num_workers: An int for the number of workers to use in dataloader
+    :param shuffle: set to ``True`` to have the data reshuffled
+            inside the record batches (default: ``False``).
+    :param drop_last: set to ``True`` to drop the last incomplete batch,
+            if the dataset size is not divisible by the batch size. If ``False`` and
+            the size of dataset is not divisible by the batch size, then the last batch
+            will be smaller. (default: ``False``)
+    :param storage_options: a dictionary of the options to use for the storage backend
+    :param pytorch_dataloader_kwargs: arguments for `torch.utils.data.DataLoader`,
+        exclude these arguments: ``batch_size``, ``num_workers``, ``shuffle``,
+        ``drop_last``.
+
+    :return: `torch.utils.data.DataLoader` object.
+    """
+    dataset = IDBasedDeltaDataset(
+        path,
+        id_field,
+        fields,
+        version,
+        partition_filter,
+        use_fixed_rank,
+        rank,
+        num_ranks,
+        num_workers,
+        shuffle,
+        batch_size,
+        drop_last,
+        storage_options,
+    )
+
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        drop_last=drop_last,
+        **pytorch_dataloader_kwargs
+    )
---
+++ packages/deltatorch/deltatorch-0.0.3-py3-none-any.whl/deltatorch/skip_batch_deltadataset.py
@@ -0,0 +1,140 @@
+import logging
+import queue
+import threading
+from queue import Queue
+from threading import Thread
+import random
+from typing import List
+
+from deltalake import DeltaTable
+
+from deltatorch import DeltaIterableDataset
+from deltatorch.deltadataset import FieldSpec
+
+logger = logging.getLogger(__name__)
+
+
+class SkipReadDeltaDataset(DeltaIterableDataset):
+    def __init__(
+        self,
+        path: str,
+        fields: List[FieldSpec],
+        use_fixed_rank: bool = False,
+        rank: int = None,
+        num_ranks: int = None,
+        num_workers: int = 2,
+        shuffle: bool = False,
+    ):
+        super().__init__(
+            path,
+            fields,
+            use_fixed_rank,
+            rank,
+            num_ranks,
+            num_workers,
+            shuffle,
+        )
+
+    def init_loading(self, path):
+        self.delta_table = None
+        self.scanner = None
+        self.queue = Queue(maxsize=self.queue_size)
+        self.stop_event = threading.Event()
+        self.init_boundaries(self.path)
+
+        self.num_chunks = self.num_workers * self.num_ranks
+
+        self.workers = [
+            Thread(
+                target=self.worker_fn,
+                args=(
+                    self.path,
+                    self.queue,
+                    self.stop_event,
+                    self.rank * i,
+                    self.num_chunks,
+                    self.shuffle,
+                    self.field_specs,
+                    self.arrow_fields,
+                    self.timeout,
+                ),
+                daemon=True,
+            )
+            for i in range(self.num_workers)
+        ]
+        for w in self.workers:
+            w.start()
+        self.loaded = True
+
+    @staticmethod
+    def worker_fn(
+        path: str,
+        q: Queue,
+        event: threading.Event,
+        chunk: int,
+        num_chunks: int,
+        shuffle: bool,
+        fields_specs,
+        arrow_fields,
+        timeout,
+    ):
+        try:
+            delta_table = DeltaTable(path)
+            scanner = delta_table.to_pyarrow_dataset().scanner(
+                columns=arrow_fields,
+            )
+            while not event.is_set():
+                batch_id = 0
+                batch_iter = iter(scanner.to_reader())
+                while True:
+                    rb = next(batch_iter, None)
+                    if rb is None:
+                        return
+                    batch_id += 1
+                    if batch_id % num_chunks != chunk:
+                        continue
+                    logger.debug(
+                        f"Worker {chunk} is processing batch {batch_id} "
+                        f"with {rb.num_rows} rows."
+                    )
+                    num_rows = rb.num_rows
+                    indexes = list(range(num_rows))
+                    if shuffle:
+                        random.shuffle(indexes)
+
+                    for i in indexes:
+                        item = rb.slice(offset=i, length=1).to_pylist()[0]
+                        item = DeltaIterableDataset.decode_and_transform_record(
+                            item, fields_specs
+                        )
+
+                        try:
+                            q.put(
+                                item,
+                                block=True,
+                                timeout=timeout,
+                            )
+                            if event.is_set():
+                                return
+
+                        except queue.Full:
+                            logger.debug("full")
+                        except Exception as e:
+                            logger.error(str(e), e)
+
+        except Exception as e:
+            print(e)
+
+    def stop(self):
+        self.stop_event.set()
+        self.loaded = False
+        self.delta_table = None
+        self.scanner = None
+        self.queue = None
+        if self.workers:
+            for w in self.workers:
+                w.join(self.timeout)
+        self.workers = []
+
+    def __del__(self):
+        self.stop()
