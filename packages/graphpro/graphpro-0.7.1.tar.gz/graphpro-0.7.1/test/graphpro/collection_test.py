9e524381541769e202c22f0db7033c206ca91c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 14:06:22 +0000

    Add package graphpro/0.7.1/graphpro-0.7.1.tar.gz

---
+++ packages/graphpro/graphpro-0.7.1.tar.gz/graphpro-0.7.1/test/graphpro/collection_test.py
@@ -0,0 +1,37 @@
+# pylint: disable=C
+
+import numpy as np
+import tempfile
+
+from graphpro.graph import Graph
+from graphpro.collection import GraphCollection
+
+DISTANCES = np.array([[4, 2], [2, 4]])
+POSITIONS_3D = np.array([[1, 2, 3], [-1, -2, -3]])
+RESIDUES = [{"resid": 1, "resname": "GLY"}, {"resid": 2, "resname": "ALA"},]
+SIMPLE_G = Graph("test", DISTANCES, POSITIONS_3D, RESIDUES)
+
+
+def test_graph_collection():
+    col = GraphCollection([SIMPLE_G])
+    assert len(col) == 1
+
+
+def test_load_and_save():
+    col = GraphCollection([SIMPLE_G])
+    f = tempfile.NamedTemporaryFile()
+    col.save(f.name)
+    col_res = GraphCollection.load(f.name)
+
+    assert col == col_res
+
+
+def test_compare_collections():
+    col = GraphCollection([SIMPLE_G])
+    assert col == col
+
+
+def test_graphs_are_iterable():
+    col = GraphCollection([SIMPLE_G, SIMPLE_G])
+    for graph in col:
+        assert graph is not None
