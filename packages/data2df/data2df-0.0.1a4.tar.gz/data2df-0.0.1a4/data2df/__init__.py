68ede7b34d470486a1c8534713f71bcb754b997b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:06:39 +0000

    Add package data2df/0.0.1a4/data2df-0.0.1a4.tar.gz

---
+++ packages/data2df/data2df-0.0.1a4.tar.gz/data2df-0.0.1a4/data2df/__init__.py
@@ -0,0 +1,6 @@
+from .csv import CSV
+from .parquet import Parquet
+from .db import DBTable, PostgresTable, VerticaTable, SQLiteTable
+
+# from .geospatial import GeoJSON
+# from .parquet import ParquetDir
