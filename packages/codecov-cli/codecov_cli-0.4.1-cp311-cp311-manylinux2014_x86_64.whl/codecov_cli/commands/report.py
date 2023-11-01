bf78ce18ef98f3508260269f204afd8f0a8fc427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:04:50 +0000

    Add package codecov-cli/0.4.1/codecov_cli-0.4.1-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/codecov-cli/codecov_cli-0.4.1-cp311-cp311-manylinux2014_x86_64.whl/codecov_cli/commands/report.py
@@ -0,0 +1,50 @@
+import logging
+import uuid
+
+import click
+
+from codecov_cli.fallbacks import CodecovOption, FallbackFieldEnum
+from codecov_cli.helpers.git import GitService
+from codecov_cli.helpers.options import global_options
+from codecov_cli.services.report import create_report_logic
+
+logger = logging.getLogger("codecovcli")
+
+
+@click.command()
+@click.option(
+    "--code", help="The code of the report. If unsure, leave default", default="default"
+)
+@global_options
+@click.pass_context
+def create_report(
+    ctx,
+    commit_sha: str,
+    code: str,
+    slug: str,
+    git_service: str,
+    token: uuid.UUID,
+    fail_on_error: bool,
+):
+    enterprise_url = ctx.obj.get("enterprise_url")
+    logger.debug(
+        "Starting create report process",
+        extra=dict(
+            extra_log_attributes=dict(
+                commit_sha=commit_sha,
+                code=code,
+                slug=slug,
+                service=git_service,
+                enterprise_url=enterprise_url,
+                token=token,
+            )
+        ),
+    )
+    res = create_report_logic(
+        commit_sha, code, slug, git_service, token, enterprise_url, fail_on_error
+    )
+    if not res.error:
+        logger.info(
+            "Finished creating report successfully",
+            extra=dict(extra_log_attributes=dict(response=res.text)),
+        )
