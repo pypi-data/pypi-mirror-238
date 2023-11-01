59415e3b89b2537c14e6c105d8dc645268bb0c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:06:40 +0000

    Add package gradio/4.0.0/gradio-4.0.0.tar.gz

---
+++ packages/gradio/gradio-4.0.0.tar.gz/gradio-4.0.0/gradio/templates/cdn/assets/color-504c8810.js
@@ -0,0 +1,2 @@
+import{o}from"./Button-457a32dd.js";const t=r=>o[r%o.length];export{t as g};
+//# sourceMappingURL=color-504c8810.js.map
---
+++ packages/gradio/gradio-4.0.0.tar.gz/gradio-4.0.0/gradio/templates/frontend/assets/color-97459cca.js
@@ -0,0 +1,2 @@
+import{o}from"./Button-a5a40815.js";const t=r=>o[r%o.length];export{t as g};
+//# sourceMappingURL=color-97459cca.js.map
