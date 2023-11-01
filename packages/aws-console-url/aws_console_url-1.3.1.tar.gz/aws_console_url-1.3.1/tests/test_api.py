312f7cdfae3b7bf8298360484533ff2513e4f0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 15:06:16 +0000

    Add package aws-console-url/1.3.1/aws_console_url-1.3.1.tar.gz

---
+++ packages/aws-console-url/aws_console_url-1.3.1.tar.gz/aws_console_url-1.3.1/tests/test_api.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+
+def test():
+    import aws_console_url.api as aws_console_url
+
+    aws = aws_console_url.AWSConsole()
+
+    
+    _ = aws.vpc
+    _ = aws.rds
+    _ = aws.dynamodb
+    _ = aws.codecommit
+    _ = aws.iam
+    _ = aws.sqs
+    _ = aws.secretmanager
+    _ = aws.cloudformation
+    _ = aws.kinesis_firehose
+    _ = aws.batch
+    _ = aws.ground_truth
+    _ = aws.kinesis
+    _ = aws.apigateway
+    _ = aws.codebuild
+    _ = aws.eventbridge
+    _ = aws.glue
+    _ = aws.step_function
+    _ = aws.a2i
+    _ = aws.sagemaker
+    _ = aws.cloudwatch
+    _ = aws.ec2
+    _ = aws.ecr
+    _ = aws.kms
+    _ = aws.athena
+    _ = aws.codepipeline
+    _ = aws.s3
+    _ = aws.awslambda
+    _ = aws.kinesis_video
+    _ = aws.ssm
+    _ = aws.ecs
+    _ = aws.sns
+
+
+if __name__ == "__main__":
+    import os
+
+    basename = os.path.basename(__file__)
+    pytest.main([basename, "-s", "--tb=native"])
