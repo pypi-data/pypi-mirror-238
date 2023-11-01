2dd8c43a237c6ddc4618454c3d4efa288b8696b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 17:07:13 +0000

    Add package opengsl/0.0.6/opengsl-0.0.6-py3-none-any.whl

---
+++ packages/opengsl/opengsl-0.0.6-py3-none-any.whl/opengsl/method/__init__.py
@@ -0,0 +1,4 @@
+'''
+method_name = ['gcn', 'sgc', 'gat', 'jknet', 'appnp', 'gprgnn',
+'prognn', 'idgl', 'grcn', 'gaug', 'slaps', 'gen', 'gt', 'nodeformer', 'cogsl', 'sublime', 'stable', 'segsl']
+'''
