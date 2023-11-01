8bd774d89215945564b44abb63ab1162c3c24e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:08:23 +0000

    Add package piri3/3.0.4/piri3-3.0.4.tar.gz

---
+++ packages/piri3/piri3-3.0.4.tar.gz/piri3-3.0.4/piri3/handlers.py
@@ -0,0 +1,144 @@
+from typing import Any, Dict, List, Union
+
+from returns.curry import partial
+from returns.pipeline import flow, is_successful
+from returns.pointfree import bind, map_, lash
+from returns.result import ResultE, Success
+
+from piri3.collection_handlers import fetch_data_by_keys
+from piri3.constants import (
+    CASTING,
+    DEFAULT,
+    IF_STATEMENTS,
+    MAPPINGS,
+    PATH,
+    REGEXP,
+    SEPARATOR,
+    SLICING,
+)
+from piri3.functions import (
+    apply_casting,
+    apply_default,
+    apply_if_statements,
+    apply_regexp,
+    apply_separator,
+    apply_slicing,
+)
+from piri3.valuetypes import MapValue
+
+
+def handle_mapping(
+    collection: Union[Dict[str, Any], List[Any]],
+    cfg: Dict[str, Any],
+) -> ResultE[MapValue]:
+    """Find data in path and apply if statements or default value.
+
+    .. versionadded:: 0.0.1
+
+    :param configuration: :term:`configuration` data to use when mapping
+    :type configuration: Dict[str, Any]
+
+    :param collection: The collection of data to find data in
+    :type collection: Union[Dict[str, Any], List[Any]]
+
+    :return: Success/Failure containers
+    :rtype: GoResult
+
+    configuration expected to look like this:
+
+    .. code-block:: json
+        {
+            "path": [],
+            "if_statementss": [{}, {}],
+            "default": 'val'
+        }
+
+    Flow description:
+
+    find data from path or None ->
+    apply if statements ->
+    return default value if Failure else mapped value
+    """
+    return flow(
+        collection,
+        partial(fetch_data_by_keys, path=cfg.get(PATH, [])),
+        lash(lambda _: Success(None)),  # type: ignore
+        bind(
+            partial(
+                apply_regexp, regexp=cfg.get(REGEXP, {}),
+            ),
+        ),
+        lash(lambda _: Success(None)),  # type: ignore
+        map_(partial(
+            apply_slicing, slicing=cfg.get(SLICING, {}),
+        )),
+        bind(partial(
+            apply_if_statements, if_objects=cfg.get(IF_STATEMENTS, []),
+        )),
+        lash(  # type: ignore
+            lambda _: apply_default(cfg.get(DEFAULT)),
+        ),
+    )
+
+
+def handle_attribute(
+    collection: Union[Dict[str, Any], List[Any]],
+    cfg: dict,
+) -> ResultE[MapValue]:
+    """Handle one attribute with mappings, ifs, casting and default value.
+
+    :param collection: The collection of data to find data in
+    :type collection: Union[Dict[str, Any], List[Any]]
+
+    :param configuration: :term:`configuration` data to use when mapping
+    :type configuration: Dict[str, Any]
+
+    :return: Success/Failure containers
+    :rtype: MapValue
+
+    configuration expected to look like this:
+
+    .. code-block:: json
+
+        {
+            "mappings": [],  # array of mapping objects
+            "separator": None,
+            "if_statements": [],  # array of if statement objects
+            "casting": {}  # casting object, for casting types
+            "default": "default value"
+        }
+
+    flow description:
+
+    Map all objects in cfg[MAPPINGS] ->
+    Apply separator to values if there are more than 1
+    Failure -> fix to Success(None)
+    Apply if statements
+    Success -> Cast Value
+    Failure -> apply default value
+
+    Return Result
+    """
+    mapped_values = [
+        mapped.unwrap()
+        for mapped in
+        [
+            handle_mapping(collection, mapping)
+            for mapping in cfg.get(MAPPINGS, [])
+        ]
+        if is_successful(mapped)
+    ]
+
+    # partially declare if statement and casting functions
+    ifs = partial(apply_if_statements, if_objects=cfg.get(IF_STATEMENTS, []))
+    cast = partial(apply_casting, casting=cfg.get(CASTING, {}))
+
+    return flow(
+        apply_separator(mapped_values, separator=cfg.get(SEPARATOR, '')),
+        lash(lambda _: Success(None)),  # type: ignore
+        bind(ifs),
+        bind(cast),
+        lash(
+            lambda _: apply_default(default=cfg.get(DEFAULT)),
+        ),
+    )
