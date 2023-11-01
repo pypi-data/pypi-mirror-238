e3f72e81c45fe3d4fb75a6b7ec1f65913df53ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:08:27 +0000

    Add package MagneticReadoutProcessing/1.0.0/MagneticReadoutProcessing-1.0.0.tar.gz

---
+++ packages/MagneticReadoutProcessing/MagneticReadoutProcessing-1.0.0.tar.gz/MagneticReadoutProcessing-1.0.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import pathlib
+import pkg_resources
+import setuptools
+import os
+
+req_path =  os.path.join(os.path.dirname(__file__), "MRP/requirements.txt")
+with pathlib.Path(req_path).open() as requirements_txt:
+    install_requires = [str(requirement) for requirement in pkg_resources.parse_requirements(requirements_txt)]
+
+setup(name='MagneticReadoutProcessing',
+      version='1.0.0',
+      description='Process raw data from magnetometers',
+      author='Marcel Ochsendorf',
+      author_email='info@marcelochsendorf.com',
+      url='https://github.com/LFB-MRI/MagnetCharacterization/',
+      packages= ['MRP', 'MRPcli', 'MRPudpp', 'tests'],#setuptools.find_packages('src', exclude=['test'], include=['MRPcli']),
+      install_requires=install_requires,
+      entry_points={
+          'console_scripts': [
+            'MRPCli = MRPcli.cli:run', # FOR python -m MRPcli.cli --help
+            'MRPUdpp = MRPudpp.uddp:run'
+          ]
+      }
+     )
