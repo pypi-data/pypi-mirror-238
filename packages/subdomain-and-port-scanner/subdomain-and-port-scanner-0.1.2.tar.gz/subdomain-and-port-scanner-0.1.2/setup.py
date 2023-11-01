852b77992bd79ead6affe925de500121672986a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:05:47 +0000

    Add package subdomain-and-port-scanner/0.1.2/subdomain-and-port-scanner-0.1.2.tar.gz

---
+++ packages/subdomain-and-port-scanner/subdomain-and-port-scanner-0.1.2.tar.gz/subdomain-and-port-scanner-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='subdomain-and-port-scanner',
+    version='0.1.2',
+    author='scannerx',
+    author_email='dastscannerx@gmail.com',
+    description='Subdomain Scan X2',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
