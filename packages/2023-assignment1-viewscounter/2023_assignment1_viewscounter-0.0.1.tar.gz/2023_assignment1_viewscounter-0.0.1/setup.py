6579949e295157b5246bb223dc3330a625bb1b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:09:19 +0000

    Add package 2023-assignment1-viewscounter/0.0.1/2023_assignment1_viewscounter-0.0.1.tar.gz

---
+++ packages/2023-assignment1-viewscounter/2023_assignment1_viewscounter-0.0.1.tar.gz/2023_assignment1_viewscounter-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='2023_assignment1_viewscounter',
+    version="0.0.1",
+    description='Questo assignment si concentra sulla creazione di un\'applicazione che conta e monitora le visualizzazioni degli utenti',
+    author='Team CED',
+    author_email='damianoficara@gmail.com,c.ricci19@campus.unimib.it,emiliotoli21@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  
+    license='MIT',
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False
+)
