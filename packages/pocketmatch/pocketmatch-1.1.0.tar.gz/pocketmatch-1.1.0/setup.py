99c20b94d56f6b8ec46fc2babcab1aaf96e30ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:07:38 +0000

    Add package pocketmatch/1.1.0/pocketmatch-1.1.0.tar.gz

---
+++ packages/pocketmatch/pocketmatch-1.1.0.tar.gz/pocketmatch-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pocketmatch',
+      version='1.1.0',
+      description='pocketmatching',
+      author='not me',
+      author_email='adelie.louet@biophys.mpg.de',
+      url='https://github.com/louetadelie1/benchmark_phys.git/',
+      packages=[],
+     )
