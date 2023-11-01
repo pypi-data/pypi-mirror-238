7f342b9639b157d405dff4e997427a2094d7a26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 15:06:29 +0000

    Add package skymap-eofactory-data/0.0.8/skymap_eofactory_data-0.0.8.tar.gz

---
+++ packages/skymap-eofactory-data/skymap_eofactory_data-0.0.8.tar.gz/skymap_eofactory_data-0.0.8/skymap_eofactory_data/data.py
@@ -0,0 +1,62 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+EOF_URL = 'https://apitestphp.eofactory.ai/api'
+
+token_fake = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ3MCwibmFtZSI6IlR14bqlbiBOZ3V54buFbiBWxINuIiwiZW1haWwiOiJudnQxMjA3MjAxQGdtYWlsLmNvbSIsImNvdW50cnkiOm51bGwsInBpY3R1cmUiOm51bGwsImlhdCI6MTY5ODYzMzg0MiwiZXhwIjoxNzAxMjI1ODQyfQ.FBD894xE8pwEZ605zcOBn8jbdWGwIm409uYxYR5MIgAuqIhm05S2xNA1C-rA3q8-BQUw8YNLivDVbOyGNuMyFg'
+workspace_id_fake = '86b53b08-cd11-4bef-80ef-7e5e9a0c14a9'
+
+class Data:
+    def __init__(
+        self, 
+        url='', 
+        # header={'Authorization': f'Bearer {os.getenv("EOF_TOKEN")}'}, 
+        headers=None, 
+        parameters=None, 
+        **kwargs
+    ):
+        self.url = url if ('http' in url) else EOF_URL
+        self.headers = headers
+        self.parameters = parameters
+        self.kwargs = kwargs
+
+    @classmethod
+    def open(
+        cls,
+        url='',
+        headers={'Authorization': f'Bearer {token_fake}', "Content-Type":"application/json"},
+        parameters={'workspace_id': workspace_id_fake, 'region': 'test'},
+        # headers={'Authorization': f'Bearer {os.getenv("EOF_TOKEN")}', "Content-Type":"application/json"},
+        # parameters={'workspace_id': {os.getenv("WORKSPACE_ID")}, 'region': {os.getenv("REGION")}},
+    ) -> "Data":
+        client: Data = cls(
+            url=url, 
+            headers=headers, 
+            parameters=parameters
+        )
+
+        return client
+    
+    def _get(self, url, params={}, config={}, type='json'):
+        api_url = url if ('http' in url) else f'{self.url}{url}'
+        print('self', api_url, self.url, self.headers, self.parameters)
+        response = requests.get(api_url, headers=self.headers, params=params)
+
+        return response.json() if type == 'json' else response
+    
+    def _post(self, url, params, datas={}, config={}, type='json'):
+        api_url = url if ('http' in url) else f'{self.url}{url}'
+        response = requests.post(api_url, headers=self.headers, params=params, json=datas)
+
+        return response.json() if type == 'json' else response
+
+    def workspaces(self, region=None):
+        params = {
+            'region': region if region is not None else self.parameters['region']
+        }
+        result = self._get('/workspaces', params)
+        return result['data']
+    
