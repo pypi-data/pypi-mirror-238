0539273543bbf5830f33a439de5cf336e481c3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 07:07:56 +0000

    Add package SigProfilerAssignment/0.0.33/SigProfilerAssignment-0.0.33.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.33.tar.gz/SigProfilerAssignment-0.0.33/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.33'
+version = '0.0.33'
+Update = 'v0.0.33: Resolve with SBS384 plots showing up as custom plots.'
+
+    
+    
