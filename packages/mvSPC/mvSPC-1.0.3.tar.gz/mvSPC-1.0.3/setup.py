83b01587eefc192b22db4450f843a41d2b93f2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:07:26 +0000

    Add package mvSPC/1.0.3/mvSPC-1.0.3.tar.gz

---
+++ packages/mvSPC/mvSPC-1.0.3.tar.gz/mvSPC-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="mvSPC",
+    version="1.0.3",
+    author="Henrik Hviid Hansen, Sebastian Olivier Nymann Topalian, Davide Cacciarelli",
+    author_email="hehha@orsted.com, sebtop@kt.dtu.dk, dcac@dtu.dk",
+    description="Plug and play statistical process control functions for Python",
+    long_description="Python package based on work from 3 PhD projects at the Technical University of Denmark covering multivariate statistical process control (MSPC), autoencoders for MSPC and determination of delays for dynamic principal component analysis",
+    long_description_content_type="markdown",
+    url="https://github.com/hviidhenrik/pySPC",
+    packages=find_packages(exclude=["tests*"]),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scikit_learn",
+        "scipy",
+        "statsmodels",
+        "torch",
+        "openpyxl",
+        "seaborn"
+    ],
+)
