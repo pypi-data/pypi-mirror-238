fcd0b72aed3521c7633f5320ab884d990c4c46ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 15:06:23 +0000

    Add package dhicn-domain-paas-sdk-python/1.0.9/dhicn_domain_paas_sdk_python-1.0.9-py3-none-any.whl

---
+++ packages/dhicn-domain-paas-sdk-python/dhicn_domain_paas_sdk_python-1.0.9-py3-none-any.whl/dhicn_iot_service/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.asset_api import AssetApi
+from openapi_client.api.auth_api import AuthApi
+from openapi_client.api.data_api import DataApi
+from openapi_client.api.device_api import DeviceApi
+from openapi_client.api.device_maintenance_api import DeviceMaintenanceApi
+from openapi_client.api.historical_data_search_api import HistoricalDataSearchApi
+from openapi_client.api.integration_api import IntegrationApi
+from openapi_client.api.iot_group_api import IotGroupApi
+from openapi_client.api.net_request_api import NetRequestApi
+from openapi_client.api.notify_api import NotifyApi
+from openapi_client.api.opcua_api import OpcuaApi
+from openapi_client.api.telemetry_api import TelemetryApi
+from openapi_client.api.telemetry_mock_data_api import TelemetryMockDataApi
