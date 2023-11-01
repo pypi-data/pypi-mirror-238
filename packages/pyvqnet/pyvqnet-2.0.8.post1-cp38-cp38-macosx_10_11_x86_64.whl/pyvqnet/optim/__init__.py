58d74533a3788bf77e7ad6fecfe999e61bc3ff6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:07:12 +0000

    Add package pyvqnet/2.0.8.post1/pyvqnet-2.0.8.post1-cp38-cp38-macosx_10_11_x86_64.whl

---
+++ packages/pyvqnet/pyvqnet-2.0.8.post1-cp38-cp38-macosx_10_11_x86_64.whl/pyvqnet/optim/__init__.py
@@ -0,0 +1,10 @@
+"""
+Init for optim
+"""
+from .adam import Adam
+from .adagrad import Adagrad
+from .adadelta import Adadelta
+from .adamax import Adamax
+from .rmsprop import RMSProp
+from .rotosolve import Rotosolve
+from .sgd import SGD
