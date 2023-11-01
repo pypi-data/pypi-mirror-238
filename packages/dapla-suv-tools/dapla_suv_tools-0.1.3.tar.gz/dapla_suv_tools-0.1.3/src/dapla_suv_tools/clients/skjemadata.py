e24a2020e2847a96f7926aa9dd5e5aa442d88f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:06:40 +0000

    Add package dapla-suv-tools/0.1.3/dapla_suv_tools-0.1.3.tar.gz

---
+++ packages/dapla-suv-tools/dapla_suv_tools-0.1.3.tar.gz/dapla_suv_tools-0.1.3/src/dapla_suv_tools/clients/skjemadata.py
@@ -0,0 +1,16 @@
+import json
+from typing import Union
+
+import dapla_suv_tools._internals.integration.api_client as api_client
+
+
+def get_skjema_by_id(skjema_id: int) -> Union[dict, str]:
+
+    if not isinstance(skjema_id, int):
+        return "Field 'skjema_id' must be a valid integer"
+
+    try:
+        content: str = api_client._get(path=f"/skjemadata/skjema/{skjema_id}")
+        return json.loads(content)
+    except Exception as e:
+        return f"Failed to fetch for id: {e}"
