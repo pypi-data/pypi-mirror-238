c5345f4e48f6953fe9086b644a193f2b94a5c1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:05:22 +0000

    Add package patent_client/3.2.9/patent_client-3.2.9-py3-none-any.whl

---
+++ packages/patent_client/patent_client-3.2.9-py3-none-any.whl/patent_client/uspto/peds/session.py
@@ -0,0 +1,13 @@
+import hishel
+import httpx
+
+session = hishel.AsyncCacheClient(
+    transport=httpx.AsyncHTTPTransport(
+        retries=3,
+        verify=False,
+    ),
+    timeout=60 * 5,
+    controller=hishel.Controller(
+        allow_heuristics=True,
+    ),
+)
