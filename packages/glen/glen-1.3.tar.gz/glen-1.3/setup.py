cb1a1a39ef1c793b4b5c3d6b28bbe0325f89f079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:06:30 +0000

    Add package glen/1.3/glen-1.3.tar.gz

---
+++ packages/glen/glen-1.3.tar.gz/glen-1.3/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='glen',
+    version='1.3',
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.13.1",
+        "transformers>=4.30.2",
+        "pytorch-transformers>=1.2.0"
+    ],
+)
