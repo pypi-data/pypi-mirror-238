7917a2f575ade385643d11b55f90fb3821b9f3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 06:06:59 +0000

    Add package openmc-data/0.2.4/openmc_data-0.2.4-py3-none-any.whl

---
+++ packages/openmc-data/openmc_data-0.2.4-py3-none-any.whl/openmc_data/urls_xml.py
@@ -0,0 +1,14 @@
+all_chain_release_details = {
+    "endf": {
+        "b7.1": {
+            "chain": {
+                "url": "https://github.com/openmc-data-storage/openmc_data/raw/main/src/openmc_data/depletion/chain_endf_b7.1.xml",
+            }
+        },
+        "b8.0": {
+            "chain": {
+                "url": "https://github.com/openmc-data-storage/openmc_data/raw/main/src/openmc_data/depletion/chain_endf_b8.0.xml",
+            }
+        },
+    }
+}
