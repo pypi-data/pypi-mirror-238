5a4bbe4e148ad97cc8b5383c5abfc1643e8ce633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:08:18 +0000

    Add package pyepsilla/0.1.21/pyepsilla-0.1.21.tar.gz

---
+++ packages/pyepsilla/pyepsilla-0.1.21.tar.gz/pyepsilla-0.1.21/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+import sys
+if sys.version_info < (3,10):
+  print('Suggest to use Python >= 3.10')
+
+try:
+  with open("./pyepsilla/vectordb/version.py") as f:
+    version = f.read().split("'")[-2]
+except Exception as e:
+  print("Error when read version: ", e)
+
+setup(
+    name='pyepsilla',
+    version=version,
+    keywords='epsilla',
+    author= 'Epsilla Team',
+    description='Epsilla Python SDK',
+    long_description='Epsilla Python SDK',
+    license='Apache License',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'requests',
+        'sentry_sdk',
+        'posthog'
+    ],
+    url='https://github.com/epsilla-cloud/pyepsilla',
+    project_urls={
+        'Source': 'https://github.com/epsilla-cloud/pyepsilla',
+    },
+)
