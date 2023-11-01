331321c58f6409ab0605b29fe841908ae2651db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:04:47 +0000

    Add package isapilib/1.3.7/isapilib-1.3.7.tar.gz

---
+++ packages/isapilib/isapilib-1.3.7.tar.gz/isapilib-1.3.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='isapilib',
+    version='1.3.7',
+    packages=find_packages(),
+    install_requires=[
+        'django>=4.2',
+        'djangorestframework>=3.0.0',
+        'djangorestframework-simplejwt>=5.0.0',
+    ],
+)
