21b59f0575398acf96b50d7c44f719c10a7fa27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:09:13 +0000

    Add package bsb-core/4.0.0a60/bsb_core-4.0.0a60-py2.py3-none-any.whl

---
+++ packages/bsb-core/bsb_core-4.0.0a60-py2.py3-none-any.whl/bsb/simulation/results.py
@@ -0,0 +1,51 @@
+import typing
+
+from ..reporting import warn
+import traceback
+
+if typing.TYPE_CHECKING:
+    import neo
+
+
+class SimulationResult:
+    def __init__(self, simulation):
+        from neo import Block
+
+        tree = simulation.__tree__()
+        try:
+            del tree["post_prepare"]
+        except KeyError:
+            pass
+        self.block = Block(name=simulation.name, config=tree)
+        self.recorders = []
+
+    def add(self, recorder):
+        self.recorders.append(recorder)
+
+    def create_recorder(self, flush: typing.Callable[["neo.Segment"], None]):
+        recorder = SimulationRecorder()
+        recorder.flush = flush
+        self.add(recorder)
+        return recorder
+
+    def flush(self):
+        from neo import Segment
+
+        segment = Segment()
+        self.block.segments.append(segment)
+        for recorder in self.recorders:
+            try:
+                recorder.flush(segment)
+            except Exception as e:
+                traceback.print_exc()
+                warn("Recorder errored out!")
+
+    def write(self, filename, mode):
+        from neo import io
+
+        io.NixIO(filename, mode=mode).write(self.block)
+
+
+class SimulationRecorder:
+    def flush(self, segment: "neo.Segment"):
+        raise NotImplementedError("Recorders need to implement the `flush` function.")
