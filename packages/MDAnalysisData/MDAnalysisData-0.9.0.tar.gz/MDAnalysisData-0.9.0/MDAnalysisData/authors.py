ff655c4de67e475755e6c19cb100d4d585233ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:06:51 +0000

    Add package MDAnalysisData/0.9.0/MDAnalysisData-0.9.0.tar.gz

---
+++ packages/MDAnalysisData/MDAnalysisData-0.9.0.tar.gz/MDAnalysisData-0.9.0/MDAnalysisData/authors.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+
+# This file is generated from the AUTHORS file during the installation process.
+# Do not edit it as your changes will be overwritten.
+
+__authors__ = [
+    u"Irfan Alibay",
+    u"Oliver Beckstein",
+    u"Shujie Fan",
+    u"Richard J. Gowers",
+    u"Micaela Matta",
+    u"Lily Wang"
+]
