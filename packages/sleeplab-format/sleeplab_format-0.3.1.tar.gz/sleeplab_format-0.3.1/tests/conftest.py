caca13b6eda453ebdbd9c1d76d4eeb8612db4c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 03:05:36 +0000

    Add package sleeplab-format/0.3.1/sleeplab_format-0.3.1.tar.gz

---
+++ packages/sleeplab-format/sleeplab_format-0.3.1.tar.gz/sleeplab_format-0.3.1/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import shutil
+
+from sleeplab_format.models import *
+from pathlib import Path
+
+from .create_datasets import create_datasets
+from sleeplab_format.test_utils.fixtures import *
+
+
+@pytest.fixture(scope='session', autouse=True)
+def recreate_test_files() -> None:
+    """Recreate the separate test data files at the beginning of tests."""
+    # First remove the old files if existing
+    ds_dir = Path(__file__).parent / 'datasets'
+    shutil.rmtree(ds_dir, ignore_errors=True)
+
+    # Then recreate them
+    create_datasets(ds_dir)
