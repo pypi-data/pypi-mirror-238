60545132fa7863633d65bb419d31323bb0b502b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:07:16 +0000

    Add package pyvqnet/2.0.8.post1/pyvqnet-2.0.8.post1-cp38-none-manylinux1_x86_64.whl

---
+++ packages/pyvqnet/pyvqnet-2.0.8.post1-cp38-none-manylinux1_x86_64.whl/pyvqnet/qnn/vqc/__init__.py
@@ -0,0 +1,5 @@
+from .qmachine import QMachine
+from .qop import Operation, Observable, Operator, DiagonalOperation
+from .qcircuit import *
+from .qmeasure import Probability, MeasureAll, VQC_DensityMatrixFromQstate,\
+VQC_Purity,VQC_VarMeasure,VQC_VN_Entropy,VQC_DensityMatrix,VQC_Mutal_Info
