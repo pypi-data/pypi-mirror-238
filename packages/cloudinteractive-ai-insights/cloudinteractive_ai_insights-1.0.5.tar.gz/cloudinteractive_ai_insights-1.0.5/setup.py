9646b3f2b52713ff0903aa4e886f53c903f2f4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:06:50 +0000

    Add package cloudinteractive-ai-insights/1.0.5/cloudinteractive_ai_insights-1.0.5.tar.gz

---
+++ packages/cloudinteractive-ai-insights/cloudinteractive_ai_insights-1.0.5.tar.gz/cloudinteractive_ai_insights-1.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+try:
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ""
+
+setup(
+    name="cloudinteractive_ai_insights",
+    version="1.0.5",
+    description="Collection of AI tools designed to assist with your assignments and projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="CoppermineSP",
+    license="MIT",
+    author_email="copperminesp@cloudinteractive.net",
+    packages=["cloudinteractive_ai_insights"],
+    url="https://github.com/Coppermine-SP/cloudinteractive-ai-insights",
+    install_requires=["azure-cognitiveservices-vision-computervision", "openai", "pdf2image", "tiktoken"],
+    python_requires=">=3.9",
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ai-insights=cloudinteractive_ai_insights.main:main',
+        ]
+    }
+)
