113f19fa9bfafa4822572f24f8eb0754a37fd413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 06:08:43 +0000

    Add package inngest/0.1.0a3/inngest-0.1.0a3.tar.gz

---
+++ packages/inngest/inngest-0.1.0a3.tar.gz/inngest-0.1.0a3/inngest/_internal/errors.py
@@ -0,0 +1,181 @@
+from __future__ import annotations
+
+import http
+
+import pydantic
+
+from . import const
+
+
+class InternalError(Exception):
+    code: str
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(
+        self, *, code: const.ErrorCode, message: str | None = None
+    ) -> None:
+        super().__init__(message)
+        self.code = code.value
+
+
+class DevServerRegistrationNotAllowed(InternalError):
+    status_code: int = http.HTTPStatus.BAD_REQUEST
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.DISALLOWED_REGISTRATION_INITIATOR,
+            message=message,
+        )
+
+
+class InvalidBaseURL(InternalError):
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.INVALID_BASE_URL,
+            message=message,
+        )
+
+
+class InvalidConfig(InternalError):
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.INVALID_FUNCTION_CONFIG,
+            message=message,
+        )
+
+    @classmethod
+    def from_validation_error(
+        cls,
+        err: pydantic.ValidationError,
+    ) -> InvalidConfig:
+        """
+        Extract info from Pydantic's ValidationError and return our internal
+        InvalidFunctionConfig error.
+        """
+
+        default = cls(str(err))
+
+        errors = err.errors()
+        if len(errors) == 0:
+            return default
+        loc = errors[0].get("loc")
+        if loc is None or len(loc) == 0:
+            return default
+
+        field = ""
+        for part in loc:
+            if isinstance(part, int):
+                return default
+            if len(field) > 0:
+                field += "."
+            field += part
+
+        msg = errors[0].get("msg")
+        if msg is None:
+            return default
+
+        return cls(f"{loc[0]}: {msg}")
+
+
+class MismatchedSync(InternalError):
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.MISMATCHED_SYNC,
+            message=message,
+        )
+
+
+class InvalidRequestSignature(InternalError):
+    status_code: int = http.HTTPStatus.UNAUTHORIZED
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.INVALID_REQUEST_SIGNATURE,
+            message=message,
+        )
+
+
+class InvalidBody(InternalError):
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.INVALID_BODY,
+            message=message,
+        )
+
+
+class MissingEventKey(InternalError):
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.MISSING_EVENT_KEY,
+            message=message,
+        )
+
+
+class MissingFunction(InternalError):
+    status_code: int = http.HTTPStatus.BAD_REQUEST
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.MISSING_FUNCTION,
+            message=message,
+        )
+
+
+class MissingHeader(InternalError):
+    status_code: int = http.HTTPStatus.BAD_REQUEST
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.MISSING_HEADER,
+            message=message,
+        )
+
+
+class MissingParam(InternalError):
+    status_code: int = http.HTTPStatus.BAD_REQUEST
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.MISSING_HEADER,
+            message=message,
+        )
+
+
+class MissingSigningKey(InternalError):
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.MISSING_SIGNING_KEY,
+            message=message,
+        )
+
+
+class UnserializableOutput(InternalError):
+    status_code: int = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(
+            code=const.ErrorCode.UNSERIALIZABLE_OUTPUT,
+            message=message,
+        )
+
+
+class ExternalError(Exception):
+    pass
+
+
+class NonRetriableError(ExternalError):
+    """
+    End users can raise this error to prevent retries.
+    """
---
+++ packages/inngest/inngest-0.1.0a3.tar.gz/inngest-0.1.0a3/tests/test_flask.py
@@ -0,0 +1,124 @@
+import json
+import unittest
+
+import flask
+import flask.testing
+
+import inngest
+import inngest.flask
+from inngest._internal import const
+
+from . import base, cases, dev_server, http_proxy, net
+
+_client = inngest.Inngest(
+    app_id="flask",
+    base_url=f"http://{net.HOST}:{dev_server.PORT}",
+)
+
+_cases = cases.create_cases_sync(_client, "flask")
+
+
+class TestFlask(unittest.TestCase):
+    app: flask.testing.FlaskClient
+    dev_server_port: int
+    proxy: http_proxy.Proxy
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+
+        app = flask.Flask(__name__)
+        app.logger.disabled = True
+        inngest.flask.serve(
+            app,
+            _client,
+            [
+                case.fn
+                for case in _cases
+                # Should always be true but mypy doesn't know that
+                if isinstance(case.fn, inngest.FunctionSync)
+            ],
+        )
+        cls.app = app.test_client()
+        cls.proxy = http_proxy.Proxy(cls.on_proxy_request).start()
+        base.register(cls.proxy.port)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+        cls.proxy.stop()
+
+    @classmethod
+    def on_proxy_request(
+        cls,
+        *,
+        body: bytes | None,
+        headers: dict[str, list[str]],
+        method: str,
+        path: str,
+    ) -> http_proxy.Response:
+        res = cls.app.open(
+            method=method,
+            path=path,
+            headers=headers,
+            data=body,
+        )
+
+        return http_proxy.Response(
+            body=res.data,
+            headers=dict(res.headers),
+            status_code=res.status_code,
+        )
+
+
+for case in _cases:
+    test_name = f"test_{case.name}"
+    setattr(TestFlask, test_name, case.run_test)
+
+
+class TestFastAPIRegistration(unittest.TestCase):
+    def test_dev_server_to_prod(self) -> None:
+        """
+        Ensure that Dev Server cannot initiate a registration request when in
+        production mode.
+        """
+
+        client = inngest.Inngest(
+            app_id="flask_registration",
+            event_key="test",
+            is_production=True,
+        )
+
+        @inngest.create_function_sync(
+            fn_id="foo",
+            retries=0,
+            trigger=inngest.TriggerEvent(event="app/foo"),
+        )
+        def fn(**_kwargs: object) -> None:
+            pass
+
+        app = flask.Flask(__name__)
+        inngest.flask.serve(
+            app,
+            client,
+            [fn],
+            signing_key="signkey-prod-0486c9",
+        )
+        flask_client = app.test_client()
+        res = flask_client.put(
+            "/api/inngest",
+            headers={
+                const.HeaderKey.SERVER_KIND.value.lower(): const.ServerKind.DEV_SERVER.value,
+            },
+        )
+        assert res.status_code == 400
+        body: object = json.loads(res.data)
+        assert (
+            isinstance(body, dict)
+            and body["code"]
+            == const.ErrorCode.DISALLOWED_REGISTRATION_INITIATOR.value
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
