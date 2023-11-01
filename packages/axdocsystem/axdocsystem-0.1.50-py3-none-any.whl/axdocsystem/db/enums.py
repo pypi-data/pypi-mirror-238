be5e870e1efd53bcc13e7f9b19e2b127be7bc58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:07:30 +0000

    Add package axdocsystem/0.1.50/axdocsystem-0.1.50-py3-none-any.whl

---
+++ packages/axdocsystem/axdocsystem-0.1.50-py3-none-any.whl/axdocsystem/db/enums.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class DocumentStatusEnum(Enum):
+    NEW = 'new'
+    PROCESS = 'process'
+    COMPLETED = 'completed'
+    SUCCESS = 'success'
+
+
+class UsersPositionEnum(Enum):
+    STUDENT = 1
+    PROFESSOR = 2
+    ADMINISTRATOR = 3
+    LIBRARIAN = 4
+    RESEARCHER = 5
+    DEAN = 6
+    CHAIRPERSON = 7
+    JANITOR = 8
+
