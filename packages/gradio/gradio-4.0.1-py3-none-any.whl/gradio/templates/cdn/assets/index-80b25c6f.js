e0fb814af1efe16a71c98a65a9eba342ef8806cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 04:07:54 +0000

    Add package gradio/4.0.1/gradio-4.0.1-py3-none-any.whl

---
+++ packages/gradio/gradio-4.0.1-py3-none-any.whl/gradio/templates/cdn/assets/index-80b25c6f.js
@@ -0,0 +1,2 @@
+import{L as e,S as m,S as p}from"./Index-3b616ac8.js";import{T as i}from"./Blocks-5697b2ea.js";import"./index-f8f499a4.js";import"./svelte/svelte.js";import"./Button-6cfc982c.js";export{e as Loader,m as StatusTracker,i as Toast,p as default};
+//# sourceMappingURL=index-80b25c6f.js.map
---
+++ packages/gradio/gradio-4.0.1-py3-none-any.whl/gradio/templates/frontend/assets/color-83ad35b9.js
@@ -0,0 +1,2 @@
+import{o}from"./Button-ad3eff0f.js";const t=r=>o[r%o.length];export{t as g};
+//# sourceMappingURL=color-83ad35b9.js.map
