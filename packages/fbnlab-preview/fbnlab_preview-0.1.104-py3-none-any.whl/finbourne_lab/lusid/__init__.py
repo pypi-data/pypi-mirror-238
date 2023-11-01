36f6327140ead8e8378ee6b08ef8b90784ae8155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:06:29 +0000

    Add package fbnlab-preview/0.1.104/fbnlab_preview-0.1.104-py3-none-any.whl

---
+++ packages/fbnlab-preview/fbnlab_preview-0.1.104-py3-none-any.whl/finbourne_lab/lusid/__init__.py
@@ -0,0 +1,4 @@
+from finbourne_lab.lusid.client import LusidClient
+from finbourne_lab.lusid.experiment import LusidExperiment
+from finbourne_lab.lusid.instrument import LusidInstrumentLab
+from finbourne_lab.lusid.transaction import LusidTransactionLab
