2820b421ab8086ec385f04bfd02cefd09f0c98ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 05:07:31 +0000

    Add package genaikit/0.4.0/genaikit-0.4.0-py3-none-any.whl

---
+++ packages/genaikit/genaikit-0.4.0-py3-none-any.whl/genaikit/data.py
@@ -0,0 +1,10 @@
+"""
+    Package "aissistant"
+
+    This module provides package's data operations
+"""
+from .base import BaseHandleData
+
+class HandleData(BaseHandleData):
+    def __init__(self,):
+        pass
