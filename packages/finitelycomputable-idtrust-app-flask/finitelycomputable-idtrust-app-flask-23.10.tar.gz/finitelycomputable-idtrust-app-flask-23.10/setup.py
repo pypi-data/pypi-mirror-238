24e360d8363f3c638a3a9c551d66a87960c652b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-01 16:06:12 +0000

    Add package finitelycomputable-idtrust-app-flask/23.10/finitelycomputable-idtrust-app-flask-23.10.tar.gz

---
+++ packages/finitelycomputable-idtrust-app-flask/finitelycomputable-idtrust-app-flask-23.10.tar.gz/finitelycomputable-idtrust-app-flask-23.10/setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import os
+from setuptools import find_namespace_packages, setup
+from finitelycomputable_microsites_setup import (
+        version, base_setup, wsgi_extras_require, flask_version,
+)
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='finitelycomputable-idtrust-app-flask',
+    version=version,
+    packages=find_namespace_packages(),
+    entry_points={
+        'console_scripts': [
+            'finitelycomputable-idtrust-app-flask = finitelycomputable.idtrust_flask:run']
+        },
+    description='The Flask-dependent code for a microsite to explore identifying game-theory strategies',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    scripts=['finitelycomputable_microsites_setup.py'],
+    install_requires=[
+        flask_version,
+        'finitelycomputable-idtrust-common~=' + version,
+    ],
+    extras_require=wsgi_extras_require,
+    url='http://www.finitelycomputable.net/identification_of_trust',
+    **base_setup
+)
