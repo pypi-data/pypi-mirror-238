9226e92fe5622aa412d9459d3244985d88790ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 06:07:40 +0000

    Add package Balethon/0.3.5/Balethon-0.3.5-py3-none-any.whl

---
+++ packages/Balethon/Balethon-0.3.5-py3-none-any.whl/balethon/objects/chat_member.py
@@ -0,0 +1,43 @@
+from . import Object
+from balethon import objects
+
+
+class ChatMember(Object):
+
+    def __init__(
+            self,
+            user: "objects.User" = None,
+            status: str = None,
+            until_date: "objects.Date" = None,
+            can_be_edited: bool = None,
+            can_change_info: bool = None,
+            can_post_messages: bool = None,
+            can_edit_messages: bool = None,
+            can_delete_messages: bool = None,
+            can_invite_users: bool = None,
+            can_restrict_members: bool = None,
+            can_pin_messages: bool = None,
+            can_promote_members: bool = None,
+            can_send_messages: bool = None,
+            can_send_media_messages: bool = None,
+            can_send_other_messages: bool = None,
+            can_add_web_page_previews: bool = None,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.user: "objects.User" = user
+        self.status: str = status
+        self.until_date: "objects.Date" = until_date
+        self.can_be_edited: bool = can_be_edited
+        self.can_change_info: bool = can_change_info
+        self.can_post_messages: bool = can_post_messages
+        self.can_edit_messages: bool = can_edit_messages
+        self.can_delete_messages: bool = can_delete_messages
+        self.can_invite_users: bool = can_invite_users
+        self.can_restrict_members: bool = can_restrict_members
+        self.can_pin_messages: bool = can_pin_messages
+        self.can_promote_members: bool = can_promote_members
+        self.can_send_messages: bool = can_send_messages
+        self.can_send_media_messages: bool = can_send_media_messages
+        self.can_send_other_messages: bool = can_send_other_messages
+        self.can_add_web_page_previews: bool = can_add_web_page_previews
---
+++ packages/Balethon/Balethon-0.3.5-py3-none-any.whl/balethon/objects/user.py
@@ -0,0 +1,30 @@
+from . import Object
+
+
+class User(Object):
+
+    def __init__(
+            self,
+            id: int = None,
+            username: str = None,
+            first_name: str = None,
+            last_name: str = None,
+            language_code: str = None,
+            is_bot: bool = None,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.id: int = id
+        self.username: str = username
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+        self.language_code: str = language_code
+        self.is_bot: bool = is_bot
+
+    @property
+    def full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        if self.first_name:
+            return self.first_name
+        return ""
