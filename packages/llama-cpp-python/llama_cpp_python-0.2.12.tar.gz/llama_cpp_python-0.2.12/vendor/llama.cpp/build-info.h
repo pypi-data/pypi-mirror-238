4f2ca7f31fed556d8ef7053b395a0d683939a87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:08:57 +0000

    Add package llama-cpp-python/0.2.12/llama_cpp_python-0.2.12.tar.gz

---
+++ packages/llama-cpp-python/llama_cpp_python-0.2.12.tar.gz/llama_cpp_python-0.2.12/vendor/llama.cpp/build-info.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define BUILD_NUMBER 1
+#define BUILD_COMMIT "5033796"
+#define BUILD_COMPILER "cc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+#define BUILD_TARGET "x86_64-linux-gnu"
+
+#endif // BUILD_INFO_H
