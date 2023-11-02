b87ef46134882be36316b889bfe2361d51a729f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:07:53 +0000

    Add package cislunar/0.0.1/cislunar-0.0.1.tar.gz

---
+++ packages/cislunar/cislunar-0.0.1.tar.gz/cislunar-0.0.1/cislunar/core.py
@@ -0,0 +1,6 @@
+class CisLunar:
+    def __init__(self):
+        return None
+    
+    def addition(self, a, b):
+        return a+b
---
+++ packages/cislunar/cislunar-0.0.1.tar.gz/cislunar-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cislunar',
+    version='0.0.1',
+    description='Cislunar Utility Toolkit',
+    author='Erin Lee Ryan',  
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "fastapi",
+        "uvicorn",
+        "joblib",
+        "python-multipart",
+        "pydantic"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
