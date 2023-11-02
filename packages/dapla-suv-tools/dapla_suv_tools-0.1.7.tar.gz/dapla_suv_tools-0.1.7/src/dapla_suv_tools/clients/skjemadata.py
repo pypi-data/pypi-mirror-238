487482f5e164fa39e9f52accff1a592d6b25e60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:08:55 +0000

    Add package dapla-suv-tools/0.1.7/dapla_suv_tools-0.1.7.tar.gz

---
+++ packages/dapla-suv-tools/dapla_suv_tools-0.1.7.tar.gz/dapla_suv_tools-0.1.7/src/dapla_suv_tools/clients/skjemadata.py
@@ -0,0 +1,48 @@
+import json
+from typing import Union
+
+import dapla_suv_tools._internals.integration.api_client as api_client
+
+
+def get_skjema_by_id(skjema_id: int) -> dict:
+
+    if not isinstance(skjema_id, int):
+        raise ValueError("Field 'skjema_id' must be a valid integer")
+
+    try:
+        content: str = api_client._get(path=f"/skjemadata/skjema/{skjema_id}")
+        return json.loads(content)
+    except Exception as e:
+        raise Exception(f"Failed to fetch for id {skjema_id}", e)
+
+
+def get_skjema_by_ra_nummer(ra_nummer: str, latest_only: bool = False) -> dict:
+    try:
+        filters = {"ra_nummer": ra_nummer}
+        content: str = api_client._post(path=f"/skjemadata/skjema_paged?order_by=versjon&asc=false", body=filters)
+
+        result: dict = json.loads(content)
+
+        if latest_only:
+            return result["results"][0]
+
+        return result["results"]
+
+    except Exception as e:
+        raise Exception(f"Failed to fetch for ra_nummer '{ra_nummer}'.", e)
+
+
+def create_skjema() -> int:
+    pass
+
+
+def delete_skjema(skjema_id: int) -> bool:
+    pass
+
+
+def get_periode_by_id(periode_id: int) -> dict:
+    pass
+
+
+def get_perioder_by_skjema_id(skjema_id: int) -> dict:
+    pass
