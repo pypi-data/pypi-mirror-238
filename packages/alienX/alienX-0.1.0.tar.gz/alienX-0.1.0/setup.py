d6a767271d380cac3735bd4760c6a13eadbd9022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:04:35 +0000

    Add package alienX/0.1.0/alienX-0.1.0.tar.gz

---
+++ packages/alienX/alienX-0.1.0.tar.gz/alienX-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alienX',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/ishanoshada/alinex',  
+    license='MIT',
+    author='Ishan Oshada',
+    author_email='ic31908@gamil.com',
+    description='soon',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
