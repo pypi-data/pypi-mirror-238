5ab86724f9506453d47be59ee0388dcae112592f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:08:05 +0000

    Add package flaskz/1.6.4rc3/flaskz-1.6.4rc3-py3-none-any.whl

---
+++ packages/flaskz/flaskz-1.6.4rc3-py3-none-any.whl/flaskz/ext/ssh.py
@@ -0,0 +1,384 @@
+"""
+pip install paramiko
+"""
+import errno
+import os
+import socket
+import stat
+import time
+from contextlib import contextmanager
+
+import paramiko
+from paramiko.ssh_exception import SSHException
+
+
+@contextmanager
+def ssh_session(hostname, username, password=None, port=22, **kwargs):
+    """
+    SSH contextmanager.
+
+    Example:
+        with ssh_session(host, username, password, timeout=20) as ssh:
+            ssh.run_command('ls -l')
+
+        with ssh_session(host, username, password, timeout=20) as ssh:
+            ssh.run_command_list(['show version', 'show running-config'])
+
+        with ssh_session(host, username, password, timeout=20, secondary_password=enable_pwd, recv_endswith=['# ', '$ ', ': ', '? ', '#']) as ssh:
+            ssh.run_command_list(['enable', 'show run'])
+
+    """
+    ssh_client = SSH(hostname=hostname, username=username, password=password, port=port, **kwargs)
+    yield ssh_client
+    ssh_client.close()
+
+
+class SSH(object):
+    def __init__(self, hostname, username, password=None, port=22, **kwargs):
+        """
+        Create a SSH instance.
+
+        .. versionupdated:: 1.6 - add secondary_password and recv_endswith kwargs
+
+        Example:
+            ssh = SSH(host, username, password, timeout=20)
+            ssh.run_command('ls -l')
+
+            ssh = SSH(host, username, password, timeout=20)
+            ssh.run_command_list(['enable', enable_pwd, 'show run'])
+
+            ssh = SSH(host, username, password, timeout=20, secondary_password=enable_pwd, recv_endswith=['# ', '$ ', ': ', '? ', '#'])
+            ssh.run_command_list(['enable', 'show run'])
+            ssh.run_command_list(['enable', 'show run'], True) # return last command output
+            ssh.run_command_list(['show version', 'show clock', 'show running-config'], prompt=ssh.get_prompt()) # reuse prompt
+
+
+        :param hostname: the host(address) to ssh
+        :param username: the username of the host
+        :param password: the password of the host
+        :param port: the ssh port, default is 22
+        :param kwargs:
+                - secondary_password: use for enable/sudo action, if None, the password needs to be sent through the command
+                - recv_endswith: use for stop receiving, default is ['# ', '$ ', ': ', '? ']
+                - timeout: timeout on blocking read/write operations & socket timer, default is 0
+                - connect_kwargs: kwargs for Transport.connect()
+        """
+        self._username = username
+        self._password = password
+        self._secondary_password = kwargs.pop('secondary_password', None)  # for enable/sudo
+        # recv_endswith = kwargs.pop('recv_endswith', None)  # for stop receiving
+        self.recv_endswith = tuple(kwargs.pop('recv_endswith', None) or ['# ', '$ ', ': ', '? '])  # for stop receiving
+        self._timeout = kwargs.pop('timeout', 0)
+
+        # self.transport = paramiko.Transport((hostname, port))
+        self.transport = paramiko.Transport(_create_socket(hostname=hostname, port=port, timeout=self._timeout))
+        _connect_kwargs = kwargs.pop('connect_kwargs', None) or {}  # kwargs for Transport.connect()
+        self.transport.connect(username=username, password=password, **_connect_kwargs)
+
+        self.ssh = paramiko.SSHClient()
+        self.ssh._transport = self.transport
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    @property
+    def channel(self):
+        if not hasattr(self, '_channel'):
+            self._channel = self.ssh.invoke_shell(width=1000, height=100000)  # @2023-10-31 add width
+            if self._timeout > 0:
+                self._channel.settimeout(self._timeout)
+        return self._channel
+
+    def run_command(self, command, recv=True, clean=True, prompt=None):
+        """
+        Run the command.
+        If recv is False, just run the command and return immediately, without waiting for the result
+
+        .. versionupdated::
+            1.6.1   - @2023-06-26 add recv parameter
+            1.6.3   - @2023-07-10 add clean parameter
+            1.6.4   - @2023-10-30 add prompt parameter and logic
+
+        Example:
+            ssh.run_command('ls -l')
+            ssh.run_command('show run')
+            ssh.run_command({
+                'command': ' ',
+                'clean': False,
+            })
+
+         :param command: the command to run.
+         :param recv: wait for the result or not.
+         :param clean: clean output info or not, default True(clean)
+         :param prompt: the prompt of the command line
+
+         :return: the output of the command
+        """
+        command, recv, clean = _get_command_arg(command, recv, clean)
+
+        # --------------------------------------------------------------
+        if prompt is None:
+            prompt = self.get_prompt()  # 1.get prompt 2.remove welcome
+
+        output = self._run_command(command, recv, prompt)
+        if output is None:
+            return None
+
+        if self._is_secondary_login(command, output):
+            if self._secondary_login(output) is False:
+                return output
+            else:
+                prompt = self.get_prompt()
+                output = self._run_command(command, recv)
+
+        if output is None:
+            return output
+
+        if clean is not False:
+            output = _clean_output_info(output, command, prompt)
+
+        return output
+
+        # --------------------------------------------------------------
+
+    def run_command_list(self, command_list, last_result=False, prompt=None):
+        """
+        Run a command list.
+        By default, returns the list of results , if last_result==True returns the result of the last command
+
+        Example:
+            ssh.run_command_list(['show version', 'show running-config'])
+            ssh.run_command_list(['show version', 'show clock', 'show running-config'], prompt=ssh.get_prompt()) # reuse prompt
+            ssh.run_command_list(['cd /usr/projects/git/srte',
+                      'pwd',
+                      'git pull origin master',
+                      'wiui@hotmail.com',
+                      '11111111'
+                      ], True)
+
+         :param command_list: the command list to run.
+         :param last_result: if True, return the result of the last command, otherwise, return the result list
+         :param prompt: the prompt of the command line(for reuse)
+
+         :return: command output result(list/last)
+        """
+        re_list = []
+
+        for command in command_list:
+            re_list.append(self.run_command(command, prompt=prompt))
+        if last_result is True:
+            return re_list[-1]
+        return re_list
+
+    def get_prompt(self):
+        """
+        Return the prompt of the command line
+
+        .. versionadded:: 1.6.4
+
+        Example:
+            ssh.get_prompt()
+
+        :return:
+        """
+        output = self._run_command('', recv=True)
+        newline_pos = output.rfind('\n')
+
+        return output[newline_pos + 1:].lstrip('\r\n') if newline_pos != -1 else output
+
+    def close(self):
+        """Close the connect"""
+        self.channel.close()
+        self.ssh.close()
+
+    @property
+    def sftp(self):
+        if not hasattr(self, '_sftp'):
+            self._sftp = paramiko.SFTPClient.from_transport(self.transport)
+        return self._sftp
+
+    def sftp_get_dir(self, remote_dir, local_dir):
+        """
+        Download remote direction to local.
+        Return the local files list.
+
+        Example:
+            ssh.sftp_get_dir('/usr/projects/git/srte/src/', '/Users/taozh/Work/Codes/ssh_test/sftp')
+        """
+        if not self._is_path_exists(remote_dir):
+            return False
+        remote_dir = _remove_end_slash(remote_dir)
+        local_dir = _remove_end_slash(local_dir)
+        if not os.path.exists(local_dir):
+            os.mkdir(local_dir)
+        all_files = self.listdir(remote_dir, True)
+        local_files = []
+        for f in all_files:
+            local_filename = f.replace(remote_dir, local_dir)
+            local_filepath = os.path.dirname(local_filename)
+            local_files.append(local_filepath)
+            if not os.path.exists(local_filepath):
+                os.makedirs(local_filepath)
+            self.sftp.get(f, local_filename)
+        return local_files
+
+    def listdir(self, path, recursion=False):
+        """
+        List all files in the given path.
+
+        Example:
+            ssh.listdir('/usr/projects/git/srte/src/')
+        """
+        all_files = []
+        path = _remove_end_slash(path)
+        if path[-1] == '/':
+            path = path[0:-1]
+        files = self.sftp.listdir_attr(path)
+        for f in files:
+            filename = path + '/' + f.filename
+            if stat.S_ISDIR(f.st_mode):  # 如果是文件夹的话递归处理
+                if recursion is True:
+                    all_files.extend(self.listdir(filename, recursion))
+            else:
+                all_files.append(filename)
+        return all_files
+
+    def _run_command(self, command, recv=True, prompt=None):
+        _command = command.strip()
+        self.channel.send(_command + '\n')
+        if recv is False:
+            return None
+        output = self._recv_data(prompt)
+        return output
+
+    def _is_secondary_login(self, command, output):
+        """
+        Determine whether a secondary login is required by checking output text
+        """
+        if 'assword' not in output:  # input password
+            return False
+        secondary_password = self._secondary_password  # or self._password
+        if secondary_password is None:
+            return False
+        enable_commands = ('sudo', 'enable')
+        return command.lower().startswith(enable_commands)
+
+    def _secondary_login(self, output):
+        """
+        Secondary login
+        return
+            - True, if success
+            - False, if fail
+        """
+        secondary_password = self._secondary_password
+        pwd_output = self._run_command(secondary_password)
+        if pwd_output == output:
+            return False
+        return True
+
+    def _is_path_exists(self, path):
+        """Return whether the path exists"""
+        try:
+            self.sftp.stat(path)
+        except IOError as e:
+            if e.errno == errno.ENOENT:
+                return False
+            raise
+        else:
+            return True
+
+    def _recv_data(self, prompt=None):
+        """Receive the command output"""
+        while not self.channel.recv_ready():
+            time.sleep(0.01)
+        res_list = []
+        time.sleep(0.2)  # Solve the problem of incomplete data
+        # cmd_pattern = re.compile('.*[#$] ' + command) # Does not work with password entry
+
+        while True:
+            data = self.channel.recv(1024)
+            info = data.decode()
+            res = info.replace(' \r', '')
+            res_list.append(res)
+            if len(info) < 1024:  # read speed > write speed
+                if prompt is not None and info.endswith(prompt):
+                    break
+                if info.endswith(self.recv_endswith):  # Cisco C9300:# /
+                    break
+
+        return ''.join(res_list)
+
+
+def _get_command_arg(command, recv, clean):
+    if type(command) is dict:
+        _command = command.get('command').strip()
+        _recv = command.get('recv') is not False if 'recv' in command else recv
+        _clean = command.get('clean') is not False if 'clean' in command else clean
+    else:
+        _command = command.strip()
+        _recv = recv
+        _clean = clean
+    return _command, _recv, _clean
+
+
+def _create_socket(hostname, port, timeout=0):
+    """
+    Create socket instance.
+    If timeout>0, set the timeout of the instance, otherwise use the default timeout(maybe 75s).
+
+    :param hostname:
+    :param port:
+    :param timeout:
+    :return:
+    """
+
+    reason = 'No suitable address family'
+    addrinfos = socket.getaddrinfo(hostname, port, socket.AF_UNSPEC, socket.SOCK_STREAM)
+    for family, socktype, proto, canonname, sockaddr in addrinfos:
+        if socktype == socket.SOCK_STREAM:
+            sock = socket.socket(family, socket.SOCK_STREAM)
+            if timeout > 0:
+                sock.settimeout(timeout)
+            try:
+                # retry_on_signal(lambda: sock.connect((hostname, port))) # @2023-06-09 change, remove paramiko.util.retry_on_signal(Paramiko3.0.0 2023-01-20)
+                sock.connect((hostname, port))
+            except socket.error as e:
+                reason = str(e)
+            else:
+                break
+    else:
+        raise SSHException(
+            'Unable to connect to {}: {}'.format(hostname, reason)
+        )
+    return sock
+
+
+def _clean_output_info(txt, command, prompt):
+    """
+    Clear the redundant information.
+    - Welcome info      ex)Welcome to Ubuntu...
+    - Last login info   ex)Last login...
+    - Path info         ex)[root@localhost ~]...
+    - Command info      ex)ls -l
+
+    """
+    # remove the start command  ex) ls -l
+    if txt.startswith(command):
+        txt = txt[len(command):].lstrip()
+
+    # remove text before command ex) [root@localhost ~]# ls -l
+    cmd_prompt = prompt + command
+
+    cmd_start = txt.rfind(cmd_prompt)
+    if cmd_start > -1:
+        txt = txt[cmd_start + len(cmd_prompt):]
+
+    prompt_start = txt.rfind(prompt)
+    if prompt_start > -1:
+        txt = txt[:prompt_start]
+    return txt.strip()
+
+
+def _remove_end_slash(path):
+    """Remove ending slash """
+    if path[-1] == '/':
+        return path[0:-1]
+    return path
---
+++ packages/flaskz/flaskz-1.6.4rc3-py3-none-any.whl/flaskz/utils/_common.py
@@ -0,0 +1,423 @@
+from collections.abc import Mapping
+
+__all__ = [
+    'filter_list', 'find_list', 'merge_list', 'each_list', 'get_list',
+    'get_dict', 'del_dict_keys', 'get_deep', 'set_deep', 'merge_dict', 'get_ins_mapping', 'get_dict_mapping', 'get_dict_value_by_type',
+    'is_list', 'is_dict', 'slice_str',
+    'get_wrap_str', 'is_str', 'str_replace',
+    'bulk_append_child',
+    'parse_version'
+]
+
+
+# -------------------------------------------list-------------------------------------------
+def filter_list(item_list, func, with_index=False):
+    """
+    Filter the list and return a new list that matches the conditions
+    :param with_index:
+    :param item_list:
+    :param func:
+    :return:
+    """
+    if with_index is True:
+        return _filter_list_with_index(item_list, func)
+
+    return list(filter(func, item_list))
+
+
+def _filter_list_with_index(item_list, func):
+    result = []
+    for index, item in enumerate(item_list):
+        if func(item, index) is True:
+            result.append(item)
+    return result
+
+
+def find_list(item_list, func, with_index=False):
+    """
+    Find an object from the list that matches the conditions
+    :param with_index:
+    :param item_list:
+    :param func:
+    :return:
+    """
+    if with_index is True:
+        return _find_list_with_index(item_list, func)
+
+    for item in item_list:
+        if func(item) is True:
+            return item
+    return None
+
+
+def _find_list_with_index(item_list, func):
+    for index, item in enumerate(item_list):
+        if func(item, index) is True:
+            return item
+    return None
+
+
+def merge_list(lst, *to_merged_list):
+    """
+    Merge multiple lists into the first list
+    :param lst:
+    :param to_merged_list:
+    :return:
+    """
+    for item in to_merged_list:
+        lst.extend(item)
+    return lst
+
+
+def each_list(item_list, func, with_index=True):
+    """
+    Iterate each object in the list and execute the callback function
+    :param with_index:
+    :param item_list:
+    :param func:
+    :return:
+    """
+    if with_index is True:
+        return _each_list_with_index(item_list, func)
+
+    for item in item_list:
+        if func(item) is False:
+            return
+
+
+def _each_list_with_index(item_list, func):
+    for index, item in enumerate(item_list):
+        if func(item, index) is False:
+            return
+
+
+def get_list(obj, default=None):
+    """
+    Get the list instance
+    -- If obj is a list, just return
+    -- If obj is not a list, return default or []
+    :param obj:
+    :param default:
+    :return:
+    """
+    return obj if is_list(obj) else (default or [])
+
+
+# -------------------------------------------dict-------------------------------------------
+def get_dict(obj, default=None):
+    """
+     Get the dict instance
+    -- If obj is a dict, just return
+    -- If obj is not a dict, return default or {}
+
+    :param obj:
+    :param default:
+    :return:
+    """
+    return obj if is_dict(obj) else (default or {})
+
+
+def del_dict_keys(obj, keys):
+    """
+    Delete the specified keys from the dict object
+    :param obj:
+    :param keys:
+    :return:
+    """
+    for key in keys:
+        obj.pop(key, None)
+
+
+def get_deep(obj, key, key_split='.', default=None, raising=False):
+    """
+    Get the deep value of the specified key from the dict object
+    :param obj:
+    :param key:
+    :param key_split:
+    :param default:
+    :param raising:
+    :return:
+    """
+    value = obj
+    try:
+        for key in key.split(key_split):
+            if isinstance(value, dict):
+                value = value[key]
+                continue
+            else:
+                if raising:
+                    raise KeyError
+                return default
+    except KeyError:
+        if raising:
+            raise
+        return default
+    else:
+        return value
+
+
+def set_deep(obj, key, value, key_split='.'):
+    """
+    Set the deep value of the specified key from the dict object
+    :param obj:
+    :param key:
+    :param value:
+    :param key_split:
+    :return:
+    """
+    dd = obj
+    keys = key.split(key_split)
+    latest = keys.pop()
+    for k in keys:
+        dd = dd.setdefault(k, {})
+    dd[latest] = value
+
+
+def get_ins_mapping(ins_list, attr, deep=False):
+    """
+    Create a dict map of the specified object list with the specified attribute
+    :param ins_list:
+    :param attr:
+    :param deep:
+    :return:
+    """
+    map_dict = {}
+    if deep is True:
+        keys = attr.split('.')
+        for item in ins_list:
+            k_value = item
+            for k in keys:
+                k_value = getattr(k_value, k)
+            map_dict[k_value] = item
+    else:
+        for item in ins_list:
+            map_dict[getattr(item, attr, None)] = item
+
+    return map_dict
+
+
+def get_dict_mapping(dict_list, key='id', key_join="+"):
+    """
+    Create a dict map of the specified dict list withe the specified key
+    :param dict_list:
+    :param key:
+    :param key_join:
+    :return:
+    """
+    map_dict = {}
+    for item in dict_list:
+        if isinstance(key, list):
+            kv = []
+            for k_item in key:
+                kv.append(str(get_deep(item, k_item)))
+            map_dict[key_join.join(kv)] = item
+        else:
+            map_dict[get_deep(item, key)] = item
+
+    return map_dict
+
+
+def merge_dict(dct, *merged_dict_list):
+    """
+    Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+    updating only top-level keys, dict_merge recurses down into dicts nested
+    to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+    ``dct``.
+
+    .. versionupdated:: 1.5 - return dct
+
+    :param dct: dict onto which the merge is executed
+    :param merged_dict_list: dct merged into dct
+    :return: dct
+    """
+    for dict_item in merged_dict_list:
+        for k, v in dict_item.items():  # 2022-04-22 dict_item.iteritems-->dict_item.items
+            if (k in dct and isinstance(dct[k], dict)
+                    and isinstance(dict_item[k], Mapping)):
+                merge_dict(dct[k], dict_item[k])
+            else:
+                dct[k] = dict_item[k]
+    return dct  # @2023-04-12 add result
+
+
+def get_dict_value_by_type(obj, key, value_type, default=None, by_instance=False):
+    """
+    Get the dict value of the specified key, if value is not the instance of the specified type, return default value.
+
+    .. versionadded:: 1.2
+
+    :param obj:
+    :param key:
+    :param value_type:
+    :param default:
+    :param by_instance:
+    :return:
+    """
+    if key not in obj:
+        return default
+    value = obj.get(key)
+    if by_instance is True:
+        if value_type is int and (value is True or value is False):  # instance(True,input) == True
+            return default
+        if isinstance(value, value_type):
+            return value
+    else:
+        if type(value) is value_type:
+            return value
+    return default
+
+
+# -------------------------------------------type-------------------------------------------
+def is_str(value, by_instance=False):
+    """
+    Check whether the value is string.
+
+    :param value:
+    :param by_instance:
+    :return:
+    """
+    if by_instance is True:
+        return isinstance(value, str)
+    return type(value) == str
+
+
+def is_list(value, by_instance=False):
+    """
+    Check whether the value is list object.
+
+    :param value:
+    :param by_instance:
+    :return:
+    """
+    if by_instance is True:
+        return isinstance(value, list)
+    return type(value) == list
+
+
+def is_dict(value, by_instance=False):
+    """
+    Check whether the value is dict object.
+
+    :param value:
+    :param by_instance:
+    :return:
+    """
+    if by_instance is True:
+        return isinstance(value, dict)
+    return type(value) == dict
+
+
+# -------------------------------------------str-------------------------------------------
+def get_wrap_str(*items):
+    """
+    Use '\n' to join multiple strings.
+
+    :param items:
+    :return:
+    """
+    result = []
+    for item in items:
+        if item is not None:
+            result.append(str(item))
+    return '\n'.join(result)
+
+
+def slice_str(value, start_len=6, end_len=0, ellipsis_str='......'):
+    if end_len != 0:
+        end_len = -abs(end_len)
+
+    value = str(value)
+    if len(value) <= (start_len - end_len):
+        return value
+    result = value[0:start_len]
+    if end_len != 0:
+        result = result + ellipsis_str + value[end_len:]
+    return result
+
+
+def str_replace(txt, old, new=''):
+    """
+    Return a copy with all occurrences of substring old replaced by new.
+
+    .. versionadded:: 1.5
+
+    Example:
+        str_replace('abca','a','A')             # AbcA
+        str_replace('abca',{'a':'A','b':'B'})   # ABcA
+        str_replace('abca',['a','b'],"*")       # **c*
+
+    :param txt:
+    :param old:
+    :param new:
+    :return:
+    """
+    v_type = type(old)
+    if v_type is dict:
+        for key in old:
+            txt = txt.replace(key, old[key])
+    elif v_type is list:
+        for item in old:
+            txt = txt.replace(item, new)
+    else:
+        txt = txt.replace(old, new)
+    return txt
+
+
+# -------------------------------------------other-------------------------------------------
+def bulk_append_child(items, parent_map, item_parent_key='parent_id', children_key="children", parent_map_key='id'):
+    """
+    Append the items to the child list of the matched parent object.
+
+    Example:
+        bulk_append_child(interface_items,device_list, 'device_id', 'interface_list')
+
+    :param items:
+    :param item_parent_key:
+    :param parent_map:
+    :param children_key:
+    :param parent_map_key:
+    :return:
+    """
+    if not isinstance(items, list):
+        items = [items]
+
+    if isinstance(parent_map, list):
+        parent_map = get_dict_mapping(parent_map, parent_map_key)
+
+    for item in items:
+        parent_attr = item.get(item_parent_key)
+        parent_item = parent_map.get(parent_attr)
+        if isinstance(parent_item, dict):
+            p_children = parent_item[children_key] = parent_item.get(children_key) or []
+            p_children.append(item)
+
+
+def parse_version(version):
+    """
+    Parse version string to int list, if version is integer, it will be converted to int
+
+    Example:
+        parse_version(sqlalchemy.__version__)   # --> [2, 0, 20] / [2, 0, '0rc1']
+
+    :param version: the version to be parsed, str/int/float/package
+    """
+    version_type = type(version)
+
+    if version_type is int:  # 1-->1
+        return [version]
+
+    if version_type is float:  # 1.2-->[1,2]
+        version = str(version)
+    elif version_type is not str:  # sqlalchemy,sys
+        version = getattr(version, '__version__', None) or getattr(version, 'version', None)
+
+    if type(version) is not str:
+        return []
+
+    versions = []
+    for v in version.split('.'):
+        if v.isdigit():
+            versions.append(int(v))
+        else:
+            versions.append(v)
+    return versions
