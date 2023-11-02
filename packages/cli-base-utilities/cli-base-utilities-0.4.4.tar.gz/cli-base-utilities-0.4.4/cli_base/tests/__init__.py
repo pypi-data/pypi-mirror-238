617639324d20a3be3ef73ed7baf97eb911952d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:05:54 +0000

    Add package cli-base-utilities/0.4.4/cli-base-utilities-0.4.4.tar.gz

---
+++ packages/cli-base-utilities/cli-base-utilities-0.4.4.tar.gz/cli-base-utilities-0.4.4/cli_base/tests/__init__.py
@@ -0,0 +1,12 @@
+import os
+import unittest.util
+
+from cli_base.cli_tools.verbosity import MAX_LOG_LEVEL, setup_logging
+
+
+# Hacky way to expand the failed test output:
+unittest.util._MAX_LENGTH = os.environ.get('UNITTEST_MAX_LENGTH', 300)
+
+
+# Display DEBUG logs in tests:
+setup_logging(verbosity=MAX_LOG_LEVEL)
