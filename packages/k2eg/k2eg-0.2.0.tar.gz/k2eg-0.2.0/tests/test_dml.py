2cfbb2c931fadbbe5d3633bc7d69bb3fe3c0f123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:09:29 +0000

    Add package k2eg/0.2.0/k2eg-0.2.0.tar.gz

---
+++ packages/k2eg/k2eg-0.2.0.tar.gz/k2eg-0.2.0/tests/test_dml.py
@@ -0,0 +1,125 @@
+import k2eg
+from k2eg.dml import _filter_pv_uri
+import time
+import pytest
+
+k: k2eg.dml = None
+
+@pytest.fixture(scope="session", autouse=True)
+def my_setup(request):
+    print('Enable k2eg')
+    global k
+    k = k2eg.dml('test', 'app-test')
+    def fin():
+        global k
+        if k is not None:
+              k.close()
+    request.addfinalizer(fin)
+
+def test_uri_protocol_ioc_attribute():
+    protocol, resource = _filter_pv_uri('pva://ioa:a.attribute')
+    assert protocol == 'pva', "protocol should not be None"
+    assert resource == 'ioa:a.attribute', "protocol should not be None"
+
+def test_uri_protocol_ioc_multilevel_attribute():
+    protocol, resource = _filter_pv_uri('pva://ioa:a.attribute.sub-attribute')
+    assert protocol == 'pva', "protocol should not be None"
+    assert resource == 'ioa:a.attribute.sub-attribute', "protocol should not be None"
+
+def test_uri_protocol_ioc():
+    protocol, resource = _filter_pv_uri('pva://ioa:a')
+    assert protocol == 'pva', "protocol should not be None"
+    assert resource == 'ioa:a', "protocol should not be None"
+
+def test_uri_protocol_ioc_uppercase_and_dash():
+    protocol, resource = _filter_pv_uri('ca://VGXX-L3B-1602-PLOG')
+    assert protocol == 'ca', "protocol should not be None"
+    assert resource == 'VGXX-L3B-1602-PLOG', "protocol should not be None"
+
+
+def test_exception_on_get_without_protocol():
+    with pytest.raises(ValueError, 
+                    match=r".*url.*not.*formed"):
+                    k.get('//bad:pv:name', timeout=0.5)
+
+
+def test_exception_on_get_with_bad_pv_name():
+    with pytest.raises(ValueError, 
+                    match=r".*url.*not.*formed"):
+                    k.get('pva://', timeout=0.5)
+
+def test_exception_on_get_with_bad_protocol():
+    with pytest.raises(ValueError, 
+                    match=r".*url.*not.*formed"):
+                    k.get('unkonwn://', timeout=0.5)
+
+def test_k2eg_get():
+    get_value = k.get('pva://channel:ramp:ramp')
+    assert get_value is not None, "value should not be None"
+
+def test_k2eg_get_timeout():
+    with pytest.raises(k2eg.OperationTimeout, 
+                match=r"Timeout.*"):
+                k.get('pva://bad:pv:name', timeout=0.5)
+
+def test_k2eg_get_bad_pv():
+    with pytest.raises(k2eg.OperationError):
+                    k.get('pva://bad:pv:name')
+
+def test_k2eg_monitor():
+    retry = 0
+    received_message = None
+
+    def monitor_handler(pv_name, new_value):
+        nonlocal received_message
+        received_message = new_value
+
+    k.monitor('pva://channel:ramp:ramp', monitor_handler)
+    while received_message is None or retry > 5:
+        retry = retry+1
+        time.sleep(2)
+    
+    assert received_message is not None, "value should not be None"
+
+def test_k2eg_monitor_many():
+    retry = 0
+    received_message_a = False
+    received_message_b = False
+    def monitor_handler(pv_name, new_value):
+        nonlocal received_message_a
+        nonlocal received_message_b
+        if pv_name=='channel:ramp:rampa':
+            received_message_a = True
+        if pv_name=='channel:ramp:rampb':
+            received_message_b = True
+
+    k.monitor_many(['pva://channel:ramp:rampa', 'pva://channel:ramp:rampb'], monitor_handler)
+    while received_message_a is False or received_message_b is False or retry > 5:
+        retry = retry+1
+        time.sleep(2)
+    
+    assert received_message_a is not False, "value should not be None"
+    assert received_message_b is not False, "value should not be None"
+
+def test_put():
+    k.put("pva://variable:a", 0)
+    k.put("pva://variable:b", 0)
+    time.sleep(1)
+    res_get = k.get("pva://variable:sum")
+    assert res_get['value'] == 0, "value should not be 0"
+    k.put("pva://variable:a", 2)
+    k.put("pva://variable:b", 2)
+    #give some time to ioc to update
+    time.sleep(1)
+    res_get = k.get("pva://variable:sum")
+    assert res_get['value'] == 4, "value should not be 0"
+
+def test_put_timeout():
+    with pytest.raises(k2eg.OperationTimeout, 
+                    match=r"Timeout.*"):
+                    k.put("pva://bad:pv:name", 0, timeout=0.5)
+
+
+def test_put_wrong_device_timeout():
+    with pytest.raises(k2eg.OperationError):
+                    k.put("pva://bad:pv:name", 0)
