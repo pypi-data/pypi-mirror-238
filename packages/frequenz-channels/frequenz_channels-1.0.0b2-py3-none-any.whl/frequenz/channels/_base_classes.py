82e5c260269184ce88490969e09eef80bc0e6e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:06:08 +0000

    Add package frequenz-channels/1.0.0b2/frequenz_channels-1.0.0b2-py3-none-any.whl

---
+++ packages/frequenz-channels/frequenz_channels-1.0.0b2-py3-none-any.whl/frequenz/channels/_base_classes.py
@@ -0,0 +1,206 @@
+# License: MIT
+# Copyright © 2022 Frequenz Energy-as-a-Service GmbH
+
+"""Base classes for Channel Sender and Receiver."""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from collections.abc import Callable
+from typing import Generic, TypeVar
+
+from ._exceptions import ReceiverStoppedError
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+
+class Sender(ABC, Generic[T]):
+    """A channel Sender."""
+
+    @abstractmethod
+    async def send(self, msg: T) -> None:
+        """Send a message to the channel.
+
+        Args:
+            msg: The message to be sent.
+
+        Raises:
+            SenderError: if there was an error sending the message.
+        """
+
+
+class Receiver(ABC, Generic[T]):
+    """A channel Receiver."""
+
+    async def __anext__(self) -> T:
+        """Await the next value in the async iteration over received values.
+
+        Returns:
+            The next value received.
+
+        Raises:
+            StopAsyncIteration: if the receiver stopped producing messages.
+            ReceiverError: if there is some problem with the receiver.
+        """
+        try:
+            await self.ready()
+            return self.consume()
+        except ReceiverStoppedError as exc:
+            raise StopAsyncIteration() from exc
+
+    @abstractmethod
+    async def ready(self) -> bool:
+        """Wait until the receiver is ready with a value or an error.
+
+        Once a call to `ready()` has finished, the value should be read with
+        a call to `consume()` (`receive()` or iterated over). The receiver will
+        remain ready (this method will return immediately) until it is
+        consumed.
+
+        Returns:
+            Whether the receiver is still active.
+        """
+
+    @abstractmethod
+    def consume(self) -> T:
+        """Return the latest value once `ready()` is complete.
+
+        `ready()` must be called before each call to `consume()`.
+
+        Returns:
+            The next value received.
+
+        Raises:
+            ReceiverStoppedError: if the receiver stopped producing messages.
+            ReceiverError: if there is some problem with the receiver.
+        """
+
+    def __aiter__(self) -> Receiver[T]:
+        """Initialize the async iterator over received values.
+
+        Returns:
+            `self`, since no extra setup is needed for the iterator.
+        """
+        return self
+
+    async def receive(self) -> T:
+        """Receive a message from the channel.
+
+        Returns:
+            The received message.
+
+        Raises:
+            ReceiverStoppedError: if there is some problem with the receiver.
+            ReceiverError: if there is some problem with the receiver.
+        """
+        try:
+            received = await self.__anext__()  # pylint: disable=unnecessary-dunder-call
+        except StopAsyncIteration as exc:
+            # If we already had a cause and it was the receiver was stopped,
+            # then reuse that error, as StopAsyncIteration is just an artifact
+            # introduced by __anext__.
+            if (
+                isinstance(exc.__cause__, ReceiverStoppedError)
+                # pylint is not smart enough to figure out we checked above
+                # this is a ReceiverStoppedError and thus it does have
+                # a receiver member
+                and exc.__cause__.receiver is self  # pylint: disable=no-member
+            ):
+                raise exc.__cause__
+            raise ReceiverStoppedError(self) from exc
+        return received
+
+    def map(self, call: Callable[[T], U]) -> Receiver[U]:
+        """Return a receiver with `call` applied on incoming messages.
+
+        Args:
+            call: function to apply on incoming messages.
+
+        Returns:
+            A `Receiver` to read results of the given function from.
+        """
+        return _Map(self, call)
+
+    def into_peekable(self) -> Peekable[T]:
+        """Convert the `Receiver` implementation into a `Peekable`.
+
+        Once this function has been called, the receiver will no longer be
+        usable, and calling `receive` on the receiver will raise an exception.
+
+        Returns:
+            A `Peekable` that can be used to peek at the latest value in the
+                channel.
+
+        Raises:
+            NotImplementedError: when a `Receiver` implementation doesn't have
+                a custom `into_peekable` implementation.
+        """
+        raise NotImplementedError("This receiver does not implement `into_peekable`")
+
+
+class Peekable(ABC, Generic[T]):
+    """A channel peekable.
+
+    A Peekable provides a [peek()][frequenz.channels.Peekable] method that
+    allows the user to get a peek at the latest value in the channel, without
+    consuming anything.
+    """
+
+    @abstractmethod
+    def peek(self) -> T | None:
+        """Return the latest value that was sent to the channel.
+
+        Returns:
+            The latest value received by the channel, and `None`, if nothing
+                has been sent to the channel yet.
+        """
+
+
+class _Map(Receiver[U], Generic[T, U]):
+    """Apply a transform function on a channel receiver.
+
+    Has two generic types:
+
+    - The input type: value type in the input receiver.
+    - The output type: return type of the transform method.
+    """
+
+    def __init__(self, recv: Receiver[T], transform: Callable[[T], U]) -> None:
+        """Create a `Transform` instance.
+
+        Args:
+            recv: The input receiver.
+            transform: The function to run on the input data.
+        """
+        self._recv = recv
+        """The input receiver."""
+
+        self._transform = transform
+        """The function to run on the input data."""
+
+    async def ready(self) -> bool:
+        """Wait until the receiver is ready with a value or an error.
+
+        Once a call to `ready()` has finished, the value should be read with
+        a call to `consume()` (`receive()` or iterated over). The receiver will
+        remain ready (this method will return immediately) until it is
+        consumed.
+
+        Returns:
+            Whether the receiver is still active.
+        """
+        return await self._recv.ready()  # pylint: disable=protected-access
+
+    # We need a noqa here because the docs have a Raises section but the code doesn't
+    # explicitly raise anything.
+    def consume(self) -> U:  # noqa: DOC502
+        """Return a transformed value once `ready()` is complete.
+
+        Returns:
+            The next value that was received.
+
+        Raises:
+            ChannelClosedError: if the underlying channel is closed.
+        """
+        return self._transform(self._recv.consume())  # pylint: disable=protected-access
