45bc8ef2683eeba90be169f37e2ac1472e523452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:04:29 +0000

    Add package mlflow-cratedb/2.7.1/mlflow-cratedb-2.7.1.tar.gz

---
+++ packages/mlflow-cratedb/mlflow-cratedb-2.7.1.tar.gz/mlflow-cratedb-2.7.1/CHANGES.md
@@ -0,0 +1,35 @@
+# Changelog
+
+
+## in progress
+
+## 2023-11-01 v2.7.1
+- Fix uniqueness constraint with `SqlRegisteredModel.name`. Thanks, @andnig.
+- Downgrade to Python 3.10. A few packages like PyCaret are not ready for
+  Python 3.11 yet. Thanks, @andnig.
+
+## 2023-10-11 v0.2.0
+- Update to [MLflow 2.7](https://github.com/mlflow/mlflow/releases/tag/v2.7.0)
+- Improve `table_exists()` in `example_merlion.py`
+- SQLAlchemy: Use server-side `now()` function for "autoincrement" columns
+- Use SQLAlchemy patches and polyfills from `cratedb-toolkit`
+- Update and improve documentation
+
+## 2023-09-12 v0.1.1
+- Documentation: Improve "Container Usage" page
+- Documentation: Update README with real `pip install` command
+
+## 2023-09-12 v0.1.0
+- Initial thing, proof-of-concept
+- Add software tests
+- CLI: Add `mlflow-cratedb cratedb --version` command
+- Project: Add `versioningit`, for effortless versioning
+- Add patch for SQLAlchemy Inspector's `get_table_names`
+- Reorder CrateDB SQLAlchemy Dialect polyfills
+- Add example experiment program `tracking_merlion.py`, and corresponding tests
+- Add example program `tracking_dummy.py`, and improve test infrastructure
+- Documentation: Add information about how to connect to CrateDB Cloud
+- CI: Add GHA workflows to build and publish OCI container images to GHCR
+- Tests: Enable code coverage tracking
+- Fix SQL DDL files, and add missing columns to make the Models tab load in the UI,
+  see GH-17. Thanks, @hammerhead.
---
+++ packages/mlflow-cratedb/mlflow-cratedb-2.7.1.tar.gz/mlflow-cratedb-2.7.1/mlflow_cratedb/patch/mlflow/model.py
@@ -0,0 +1,47 @@
+import sqlalchemy as sa
+from cratedb_toolkit.sqlalchemy import check_uniqueness_factory
+from sqlalchemy.event import listen
+
+
+def polyfill_uniqueness_constraints():
+    """
+    Establish a manual uniqueness check on the `SqlExperiment.name` column.
+
+    TODO: Submit patch to `crate-python`, to be enabled by a
+          dialect parameter `crate_polyfill_unique` or such.
+    """
+    from mlflow.store.model_registry.dbmodels.models import SqlRegisteredModel
+    from mlflow.store.tracking.dbmodels.models import SqlExperiment
+
+    listen(SqlExperiment, "before_insert", check_uniqueness_factory(SqlExperiment, "name"))
+    listen(SqlRegisteredModel, "before_insert", check_uniqueness_factory(SqlRegisteredModel, "name"))
+
+
+def polyfill_refresh_after_dml():
+    """
+    Run `REFRESH TABLE <tablename>` after each INSERT, UPDATE, and DELETE operation.
+
+    CrateDB is eventually consistent, i.e. write operations are not flushed to
+    disk immediately, so readers may see stale data. In a traditional OLTP-like
+    application, this is not applicable.
+
+    This SQLAlchemy extension makes sure that data is synchronized after each
+    operation manipulating data.
+
+    TODO: Submit patch to `crate-python`, to be enabled by a
+          dialect parameter `crate_dml_refresh` or such.
+    """
+    from mlflow.store.db.base_sql_model import Base
+
+    for mapper in Base.registry.mappers:
+        listen(mapper.class_, "after_insert", do_refresh)
+        listen(mapper.class_, "after_update", do_refresh)
+        listen(mapper.class_, "after_delete", do_refresh)
+
+
+def do_refresh(mapper, connection, target):
+    """
+    SQLAlchemy event handler for `after_{insert,update,delete}` events, invoking `REFRESH TABLE`.
+    """
+    sql = f"REFRESH TABLE {target.__tablename__}"
+    connection.execute(sa.text(sql))
