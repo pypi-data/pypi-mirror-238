d776ce944ca20085d33799c34283d02b36782f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:08:11 +0000

    Add package test-signing-aagrawal3/0.1/test-signing-aagrawal3-0.1.tar.gz

---
+++ packages/test-signing-aagrawal3/test-signing-aagrawal3-0.1.tar.gz/test-signing-aagrawal3-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test-signing-aagrawal3",
+    version="0.1",
+    author="VMware, Inc.",
+    description="Signed test package",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
