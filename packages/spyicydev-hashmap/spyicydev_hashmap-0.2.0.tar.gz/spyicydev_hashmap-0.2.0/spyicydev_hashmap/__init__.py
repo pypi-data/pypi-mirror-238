bc0e8358a2c8bb40d42801e908dfbb06e784efdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:08:50 +0000

    Add package spyicydev-hashmap/0.2.0/spyicydev_hashmap-0.2.0.tar.gz

---
+++ packages/spyicydev-hashmap/spyicydev_hashmap-0.2.0.tar.gz/spyicydev_hashmap-0.2.0/spyicydev_hashmap/__init__.py
@@ -0,0 +1,65 @@
+from typing import Optional
+
+
+class HashMap:
+    def __init__(self, _size: int = 100):
+        self.size: int = _size
+        self.map_list: list[Optional[list[str]]] = [None] * self.size
+
+    def _get_hash(self, key: str) -> int:
+        return hash(key) % self.size
+
+    def add(self, key: str, value: str) -> None:
+        index = self._get_hash(key)
+        key_value = [key, value]
+
+        if self.map_list[index] is None:
+            self.map_list[index] = [key_value]
+        else:
+            for pair in self.map_list[index]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return None
+                self.map_list[index].append(key_value)
+
+    def get(self, key: str) -> Optional[str]:
+        index = self._get_hash(key)
+
+        if self.map_list[index] is not None:
+            for pair in self.map_list[index]:
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+    def remove(self, key: str) -> None:
+        index = self._get_hash(key)
+
+        if self.map_list[index] is not None:
+            for i, pairs in enumerate(self.map_list[index]):
+                if pairs[0] == key:
+                    self.map_list[index].pop(i)
+                    return None
+
+    def count(self) -> int:
+        count: int = 0
+        for i in self.map_list:
+            if i is not None:
+                for j in i:
+                    count += 1
+        return count
+
+    def get_keys(self) -> list[str]:
+        keys = []
+        for i in self.map_list:
+            if i is not None:
+                for j in i:
+                    keys.append(j[0])
+        return keys
+
+    def get_pairs(self) -> list[str]:
+        pairs = []
+        for i in self.map_list:
+            if i is not None:
+                for j in i:
+                    pairs.append(j)
+        return pairs
