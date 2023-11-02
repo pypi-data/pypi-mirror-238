c056d1b2f433da6fba482843ea798dc1b09bbadd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:05:56 +0000

    Add package cortex-sdk/2.1.post2/cortex_sdk-2.1.post2.tar.gz

---
+++ packages/cortex-sdk/cortex_sdk-2.1.post2.tar.gz/cortex_sdk-2.1.post2/tests/test_Models.py
@@ -0,0 +1,10 @@
+import cortex_sdk.Models as Models
+from cortex_sdk import Authenticator as auth
+from cortex_sdk.types.CortexConfig import CortexConfig
+
+
+#model_repo = 'https://...'
+#model = Models.CortexModel()
+
+def test_init():
+    assert True == True
