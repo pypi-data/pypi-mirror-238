9550397a86f2dc3b3614feb68d0bb83fa1fb7ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 06:06:20 +0000

    Add package ChatTDD/0.2.5/ChatTDD-0.2.5.tar.gz

---
+++ packages/ChatTDD/ChatTDD-0.2.5.tar.gz/ChatTDD-0.2.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ChatTDD',
+    version='0.2.5',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'openai>=0.27.0',
+        'pytest',
+        'python-dotenv',
+        'langchain',
+        'keyring'
+    ],
+    entry_points={
+        'console_scripts': [
+            'chattdd = chattdd.cli:cli',
+        ],
+    },
+    author='Simen Huuse',
+    author_email='s.huuse+chattdd@gmail.com',
+    description='A utility to assist in building Test-Driven Python code using Langchain and OpenAI.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shuuse/ChatTDD',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
