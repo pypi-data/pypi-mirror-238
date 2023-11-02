fd0a7ddcb22ad7093d28d3de81ec52284a7d4736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 04:05:58 +0000

    Add package canokey-manager/5.2.0rc3/canokey_manager-5.2.0rc3.tar.gz

---
+++ packages/canokey-manager/canokey_manager-5.2.0rc3.tar.gz/canokey_manager-5.2.0rc3/tests/device/cli/piv/test_fips.py
@@ -0,0 +1,23 @@
+from canokit.management import CAPABILITY
+from canokit.core import NotSupportedError
+from ....util import open_file
+from ... import condition
+import pytest
+
+
+@pytest.fixture(autouse=True)
+@condition.yk4_fips(True)
+@condition.capability(CAPABILITY.PIV)
+def ensure_piv(ckman_cli):
+    ckman_cli("piv", "reset", "-f")
+
+
+class TestFIPS:
+    def test_rsa1024_generate_blocked(self, ckman_cli):
+        with pytest.raises(NotSupportedError):
+            ckman_cli("piv", "keys", "generate", "9a", "-a", "RSA1024", "-")
+
+    def test_rsa1024_import_blocked(self, ckman_cli):
+        with pytest.raises(NotSupportedError):
+            with open_file("rsa_1024_key.pem") as f:
+                ckman_cli("piv", "keys", "import", "9a", f.name)
---
+++ packages/canokey-manager/canokey_manager-5.2.0rc3.tar.gz/canokey_manager-5.2.0rc3/tests/device/cli/piv/test_pin_puk.py
@@ -0,0 +1,65 @@
+from .util import (
+    old_new_new,
+    DEFAULT_PIN,
+    NON_DEFAULT_PIN,
+    DEFAULT_PUK,
+    NON_DEFAULT_PUK,
+)
+
+import pytest
+
+
+class TestPin:
+    def test_change_pin(self, ckman_cli):
+        ckman_cli(
+            "piv", "access", "change-pin", "-P", DEFAULT_PIN, "-n", NON_DEFAULT_PIN
+        )
+        ckman_cli(
+            "piv", "access", "change-pin", "-P", NON_DEFAULT_PIN, "-n", DEFAULT_PIN
+        )
+
+    def test_change_pin_prompt(self, ckman_cli):
+        ckman_cli(
+            "piv",
+            "access",
+            "change-pin",
+            input=old_new_new(DEFAULT_PIN, NON_DEFAULT_PIN),
+        )
+        ckman_cli(
+            "piv",
+            "access",
+            "change-pin",
+            input=old_new_new(NON_DEFAULT_PIN, DEFAULT_PIN),
+        )
+
+
+class TestPuk:
+    def test_change_puk(self, ckman_cli):
+        o1 = ckman_cli(
+            "piv", "access", "change-puk", "-p", DEFAULT_PUK, "-n", NON_DEFAULT_PUK
+        ).output
+        assert "New PUK set." in o1
+
+        o2 = ckman_cli(
+            "piv", "access", "change-puk", "-p", NON_DEFAULT_PUK, "-n", DEFAULT_PUK
+        ).output
+        assert "New PUK set." in o2
+
+        with pytest.raises(SystemExit):
+            ckman_cli(
+                "piv", "access", "change-puk", "-p", NON_DEFAULT_PUK, "-n", DEFAULT_PUK
+            )
+
+    def test_change_puk_prompt(self, ckman_cli):
+        ckman_cli(
+            "piv",
+            "access",
+            "change-puk",
+            input=old_new_new(DEFAULT_PUK, NON_DEFAULT_PUK),
+        )
+        ckman_cli(
+            "piv",
+            "access",
+            "change-puk",
+            input=old_new_new(NON_DEFAULT_PUK, DEFAULT_PUK),
+        )
