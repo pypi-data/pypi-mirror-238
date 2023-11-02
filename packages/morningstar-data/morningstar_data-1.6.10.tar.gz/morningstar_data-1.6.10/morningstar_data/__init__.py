d1f2557022df1b4b2a3ee0d27a25070efc1f8046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:07:09 +0000

    Add package morningstar-data/1.6.10/morningstar_data-1.6.10.tar.gz

---
+++ packages/morningstar-data/morningstar_data-1.6.10.tar.gz/morningstar_data-1.6.10/morningstar_data/__init__.py
@@ -0,0 +1,7 @@
+from . import direct
+from . import lookup
+from . import datalake
+from . import utils
+from .datalake.base import query
+from .datalake._data_objects import CSVFile, TempTable
+from ._version import __version__
---
+++ packages/morningstar-data/morningstar_data-1.6.10.tar.gz/morningstar_data-1.6.10/morningstar_data/direct/_data_objects/__init__.py
@@ -0,0 +1,3 @@
+from ._data_points_object import DataPoints, DataPointsType
+from ._investments_object import Investments, InvestmentType
+from ._custom_data_points_types import ValidCustomDataPointTypes, TypeValue
