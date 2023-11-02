f7cced4bf5014e5ce9e4a2ad96589c062970c127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:04:30 +0000

    Add package snowddl/0.21.0/snowddl-0.21.0-py3-none-any.whl

---
+++ packages/snowddl/snowddl-0.21.0-py3-none-any.whl/snowddl/blueprint/__init__.py
@@ -0,0 +1,74 @@
+from .blueprint import (
+    AbstractBlueprint,
+    SchemaObjectBlueprint,
+    DependsOnMixin,
+    RoleBlueprint,
+    AccountParameterBlueprint,
+    AlertBlueprint,
+    BusinessRoleBlueprint,
+    DatabaseBlueprint,
+    DatabaseShareBlueprint,
+    DynamicTableBlueprint,
+    EventTableBlueprint,
+    ExternalFunctionBlueprint,
+    ExternalTableBlueprint,
+    FileFormatBlueprint,
+    ForeignKeyBlueprint,
+    FunctionBlueprint,
+    MaterializedViewBlueprint,
+    MaskingPolicyBlueprint,
+    NetworkPolicyBlueprint,
+    OutboundShareBlueprint,
+    PipeBlueprint,
+    PrimaryKeyBlueprint,
+    ProcedureBlueprint,
+    ResourceMonitorBlueprint,
+    RowAccessPolicyBlueprint,
+    SchemaBlueprint,
+    SchemaRoleBlueprint,
+    StageBlueprint,
+    StageFileBlueprint,
+    SequenceBlueprint,
+    StreamBlueprint,
+    TableBlueprint,
+    TagBlueprint,
+    TaskBlueprint,
+    TechRoleBlueprint,
+    UniqueKeyBlueprint,
+    UserBlueprint,
+    ViewBlueprint,
+    WarehouseBlueprint,
+    T_Blueprint,
+)
+
+from .column import ExternalTableColumn, TableColumn, ViewColumn, NameWithType, SearchOptimizationItem
+from .data_type import BaseDataType, DataType
+from .edition import Edition
+from .grant import Grant, FutureGrant
+
+from .ident import (
+    AbstractIdent,
+    AbstractIdentWithPrefix,
+    Ident,
+    AccountIdent,
+    AccountObjectIdent,
+    DatabaseIdent,
+    InboundShareIdent,
+    OutboundShareIdent,
+    SchemaIdent,
+    SchemaObjectIdent,
+    SchemaObjectIdentWithArgs,
+    StageFileIdent,
+    TableConstraintIdent,
+)
+
+from .ident_builder import (
+    build_schema_object_ident,
+    build_role_ident,
+    build_grant_name_ident_snowflake,
+    build_default_namespace_ident,
+)
+
+from .object_type import ObjectType
+from .reference import MaskingPolicyReference, RowAccessPolicyReference, TagReference
+from .stage import StageWithPath, StageUploadFile
