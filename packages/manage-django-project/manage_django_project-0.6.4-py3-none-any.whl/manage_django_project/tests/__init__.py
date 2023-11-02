47ab396c9a3697e43bd1fa728653d6448660976a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:05:13 +0000

    Add package manage-django-project/0.6.4/manage_django_project-0.6.4-py3-none-any.whl

---
+++ packages/manage-django-project/manage_django_project-0.6.4-py3-none-any.whl/manage_django_project/tests/__init__.py
@@ -0,0 +1,12 @@
+import os
+import unittest.util
+from pathlib import Path
+
+import manage_django_project
+
+
+PROJECT_ROOT = Path(manage_django_project.__file__).parent.parent
+
+
+# Hacky way to expand the failed test output:
+unittest.util._MAX_LENGTH = os.environ.get('UNITTEST_MAX_LENGTH', 300)
