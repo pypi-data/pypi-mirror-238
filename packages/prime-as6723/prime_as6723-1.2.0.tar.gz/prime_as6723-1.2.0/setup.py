e3a37f0852824bee3c2551d213c55dd5de548f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:06:54 +0000

    Add package prime-as6723/1.2.0/prime_as6723-1.2.0.tar.gz

---
+++ packages/prime-as6723/prime_as6723-1.2.0.tar.gz/prime_as6723-1.2.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="prime_as6723",
+    version="1.2.0",
+    packages=find_packages(),
+)
+
