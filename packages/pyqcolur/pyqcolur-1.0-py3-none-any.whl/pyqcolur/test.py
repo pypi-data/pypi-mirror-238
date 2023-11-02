19182d071bc6fe57a5f071e5486efb2fb7ced36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:06:32 +0000

    Add package pyqcolur/1.0/pyqcolur-1.0-py3-none-any.whl

---
+++ packages/pyqcolur/pyqcolur-1.0-py3-none-any.whl/pyqcolur/test.py
@@ -0,0 +1,110 @@
+#https://github.com/smithshawo/pyqcolur
+#from pyqcolur import *
+#u can use it like this
+
+#red,blue,orange,etc
+
+#from pyqcolur import Colors, Colorate
+
+#print(Colorate.Horizontal(Colors.yellow_to_red, "Hello", 1))
+
+#from pyqcolur import Colors, Colorate
+
+#text = "Hello world!"
+
+#print(Colors.blue + text)
+
+# or
+
+#print(Colorate.Color(Colors.blue, text, True))
+
+#print(Colors.red+"Hello world")
+
+#print(Colors.blue+"Hello world")
+
+#print(Colors.yellow+"Hello world")
+
+#print(Colors.green+"Hello world")
+
+#print(Colors.red+"Hello world")
+
+
+
+#text: the text to be written to the terminal
+#color: the color you want for the text
+#interval: the interval of the writing effect
+#hide_cursor: whether you want the cursor to be hidden or not
+#end: the end color, the default is white
+
+
+
+
+#from pyqcolur import Write, Colors
+
+#name = Write.Input("Enter your name -> ", Colors.red_to_purple, interval=0.0025)
+#Write.Print(f"Nice to meet you, {name}!", Colors.blue_to_green, interval=0.05)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
