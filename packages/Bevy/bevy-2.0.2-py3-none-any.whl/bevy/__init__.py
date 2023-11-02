49eec112aebc241382cfe280a55f24dd005c1995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 06:06:19 +0000

    Add package Bevy/2.0.2/bevy-2.0.2-py3-none-any.whl

---
+++ packages/Bevy/bevy-2.0.2-py3-none-any.whl/bevy/__init__.py
@@ -0,0 +1,5 @@
+from bevy.injectors.classes import dependency
+from bevy.injectors.functions import inject
+from bevy.repository import Repository, get_repository
+
+__all__ = ("dependency", "inject", "Repository", "get_repository")
