30155aecdfd9fabd94e3f250c636895f21221cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:08:06 +0000

    Add package imface/0.0.0.2.2/imface-0.0.0.2.2.tar.gz

---
+++ packages/imface/imface-0.0.0.2.2.tar.gz/imface-0.0.0.2.2/imface/utils/deepface_util.py
@@ -0,0 +1,41 @@
+import os
+from deepface import DeepFace
+from deepface.commons import functions, realtime, distance as dst
+
+
+models = [
+  "VGG-Face", 
+  "Facenet", 
+  "Facenet512", 
+  "OpenFace", 
+  "DeepFace", 
+  "DeepID", 
+  "ArcFace", 
+  "Dlib", 
+  "SFace",
+]
+
+backends = [
+    "retinaface",
+]
+
+
+def getEmbeddingVector(path: str):
+    embed = []
+    data = DeepFace.represent(path, model_name=models[2], enforce_detection=True, detector_backend=backends[0])
+    for imgdata in data:
+        embed.append(imgdata['embedding'])
+    
+    return embed
+
+def extractFace(path: str):
+    data = DeepFace.represent(path, model_name=models[2], enforce_detection=True, detector_backend=backends[0])
+    return data
+
+def getDistance(source, target):
+    dist = dst.findCosineDistance(source, target)
+    return dist
+
+def getTreshold():
+    threshold = dst.findThreshold(models[2], "cosine")
+    return threshold
