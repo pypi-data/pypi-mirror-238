12ce5d7aed3660249f6e31051a9ff021b7ecaae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:09:44 +0000

    Add package aligned/0.0.34/aligned-0.0.34-py3-none-any.whl

---
+++ packages/aligned/aligned-0.0.34-py3-none-any.whl/aligned/data_file.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import polars as pl
+
+
+class DataFileReference:
+    """
+    A reference to a data file.
+
+    It can therefore be loaded in and writen to.
+    Either as a pandas data frame, or a dask data frame.
+    """
+
+    async def read_pandas(self) -> pd.DataFrame:
+        raise NotImplementedError()
+
+    async def to_pandas(self) -> pd.DataFrame:
+        return await self.read_pandas()
+
+    async def to_polars(self) -> pl.LazyFrame:
+        raise NotImplementedError()
+
+    async def write_polars(self, df: pl.LazyFrame) -> None:
+        raise NotImplementedError()
+
+    async def write_pandas(self, df: pd.DataFrame) -> None:
+        raise NotImplementedError()
