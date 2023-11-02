680a1bf2c4a7b7cffe65fb0f5b551a1b493ba780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:08:10 +0000

    Add package pypatcher/0.8/pypatcher-0.8.tar.gz

---
+++ packages/pypatcher/pypatcher-0.8.tar.gz/pypatcher-0.8/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+setup(
+    name = 'pypatcher',
+    packages = ['pypatcher'],
+    version = '0.8',
+    license='GPL-3.0',
+    description = 'Utility module for handling incremental git patches',
+    author = 'David Angulo',
+    author_email = 'david.angulo.arias@gmail.com',
+    url = 'https://github.com/sgtpepper9907/pypatcher',
+    download_url = 'https://github.com/sgtpepper9907/pypatcher/archive/refs/tags/v0.1.tar.gz',
+    keywords = ['git', 'patches', 'incremental'],
+    install_requires=install_requires
+)
