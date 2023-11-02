f2c217aec9cee242a5724700ae57b79f256f2830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 04:06:10 +0000

    Add package projectaria-tools/1.2.1a1/projectaria_tools-1.2.1a1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/projectaria-tools/projectaria_tools-1.2.1a1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/projectaria_tools/core/__init__.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from . import calibration
+from . import data_provider
+from . import image
+from . import mps
+from . import sensor_data
+from . import sophus
+from . import stream_id
+from . import vrs
+__all__ = ['calibration', 'data_provider', 'image', 'mps', 'sensor_data', 'sophus', 'stream_id', 'vrs']
