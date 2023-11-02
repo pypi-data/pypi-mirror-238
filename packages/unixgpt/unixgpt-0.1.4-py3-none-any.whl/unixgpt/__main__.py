7f57f752e77deabb7e2a42d6ce469483c9eaa4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:09:36 +0000

    Add package unixgpt/0.1.4/unixgpt-0.1.4-py3-none-any.whl

---
+++ packages/unixgpt/unixgpt-0.1.4-py3-none-any.whl/unixgpt/__main__.py
@@ -0,0 +1,19 @@
+import os
+
+from .cli import cli
+
+
+OPENAI_API_KEY: str = os.environ.get("OPENAI_API_KEY", None)
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY environment variable is required.")
+
+
+def main():
+    cli(OPENAI_API_KEY)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (EOFError, KeyboardInterrupt):
+        exit()
