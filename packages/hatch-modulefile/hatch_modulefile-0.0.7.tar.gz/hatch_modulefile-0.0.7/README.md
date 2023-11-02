60528229ea3ad4a9893fde76e495c7da88864ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:05:44 +0000

    Add package hatch-modulefile/0.0.7/hatch_modulefile-0.0.7.tar.gz

---
+++ packages/hatch-modulefile/hatch_modulefile-0.0.7.tar.gz/hatch_modulefile-0.0.7/README.md
@@ -0,0 +1,21 @@
+# hatch_modulefile
+
+[![PyPI - Version](https://img.shields.io/pypi/v/hatch-modulefile.svg)](https://pypi.org/project/hatch-modulefile)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/hatch-modulefile.svg)](https://pypi.org/project/hatch-modulefile)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install hatch-modulefile
+```
+
+## License
+
+`hatch-modulefile` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/hatch-modulefile/hatch_modulefile-0.0.7.tar.gz/hatch_modulefile-0.0.7/tests/test_build.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+from .utils import build_project, install_project_venv
+
+
+@pytest.mark.slow
+def test_modulefile(new_project: Path):
+    build_project()
+    install_directory = install_project_venv()
+
+    site_customize = install_directory.joinpath("lib", "python3.7", "site-packages", "sitecustomize.py") # TODO: hard coded for python/3.7
+    assert site_customize.exists()
+
+    modulefile = install_directory.joinpath("lib", "python3.7", "site-packages", "modulefiles", "my-app") # TODO: hard coded for python/3.7
+    assert modulefile.exists()
+
+    text = modulefile.read_text()
+
+    requirements = [s.strip() for s in text.split("necessary       {\n")[1].split("}", 1)[0].splitlines()]
+    assert requirements == ["python/3.7", "my_module"]
+    assert get_setting(text, "setenv") == [["QT_XCB_GL_INTEGRATION", "none"]]
+    assert get_setting(text, "prepend-path") == [["PATH", "$venv/bin"], ["PATH", "/my/custom/path"]]
+    assert get_setting(text, "append-path") == [
+        ["PYTHONPATH", "$venv/lib/python3.7/site-packages"],
+        ["PYTHON_SITE_PACKAGES", "$venv/lib/python3.7/site-packages"],
+        ["OTHER_VARIABLE", "/my/custom/path2"]
+    ]
+
+    assert requirements == ["python/3.7", "my_module"]
+
+
+@pytest.mark.slow
+def test_modulefile_no_site_customize(new_project_no_site_customize: Path):
+    build_project()
+    install_directory = install_project_venv()
+
+    site_customize = install_directory.joinpath("lib", "python3.7", "site-packages", "sitecustomize.py") # TODO: hard coded for python/3.7
+    assert not site_customize.exists()
+
+    modulefile = install_directory.joinpath("lib", "python3.7", "site-packages", "modulefiles", "my-app") # TODO: hard coded for python/3.7
+    assert modulefile.exists()
+
+    text = modulefile.read_text()
+
+    requirements = [s.strip() for s in text.split("necessary       {\n")[1].split("}", 1)[0].splitlines()]
+    assert requirements == ["python/3.7", "my_module"]
+    assert get_setting(text, "setenv") == [["QT_XCB_GL_INTEGRATION", "none"]]
+    assert get_setting(text, "prepend-path") == [["PATH", "$venv/bin"], ["PATH", "/my/custom/path"]]
+    assert get_setting(text, "append-path") == [
+        ["PYTHONPATH", "$venv/lib/python3.7/site-packages"],
+        ["OTHER_VARIABLE", "/my/custom/path2"]
+    ]
+
+    assert requirements == ["python/3.7", "my_module"]
+
+
+def get_setting(text: str, key: str) -> list[tuple[str, str]]:
+    environments = []
+    for line in text.splitlines():
+        if line.startswith(key):
+            environments.append(line.split()[1:])
+
+    return environments
