2607056415064e83cc6046b6566739d036b9a9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:07:05 +0000

    Add package find-similar/1.6.1/find-similar-1.6.1.tar.gz

---
+++ packages/find-similar/find-similar-1.6.1.tar.gz/find-similar-1.6.1/find_similar/calc_models.py
@@ -0,0 +1,37 @@
+"""
+Models for calculation and compare
+"""
+from typing import Optional
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
+
+
+class Item(BaseModel):
+    """
+    Item
+    """
+    id: Optional[int]  # pylint: disable=invalid-name
+    label: str
+    part_number: str
+    id_shop: int
+    id_base_item: Optional[int]
+    cos: Optional[float] = 0
+    token_set: Optional[set] = {}
+
+    def __eq__(self, other):
+        return self.id == other.id_base_item
+
+
+class TokenizeException(Exception):
+    """
+    Base Exception class for Tokenize exceptions
+    """
+
+
+class LanguageNotFoundException(TokenizeException):
+    """
+    Language not found error
+    """
+
+    def __init__(self, language):
+        self.language = language
+        super().__init__(f"Language '{language}' is not supported by nltk module")
