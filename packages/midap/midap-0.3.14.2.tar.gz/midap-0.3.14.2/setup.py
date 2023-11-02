5e925e4a123721e4b86ba874dad82fe1ea4de2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:05:06 +0000

    Add package midap/0.3.14.2/midap-0.3.14.2.tar.gz

---
+++ packages/midap/midap-0.3.14.2.tar.gz/midap-0.3.14.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="midap",
+    version="0.3.14.2",
+    description='A package for cell segmentation and tracking.',
+    author='Oschmann  Franziska, Fluri Janis',
+    author_email='franziska.oschmann@id.ethz.ch, janis.fluri@id.ethz.ch',
+    python_requires='>=3.9, <4',
+    download_url = 'https://github.com/Microbial-Systems-Ecology/midap/archive/refs/tags/0.3.14.2.tar.gz',
+    keywords='Segmentation, Tracking, Biology',
+    install_requires=['btrack==0.4.6',
+                        'coverage',
+                        'gitpython',
+                        'matplotlib>=3.3.0,<=3.6.3',
+                        'napari',
+                        'omnipose==0.4.4',
+                        'opencv-python',
+                        'pandas==2.0.2',
+                        'psutil==5.9.5',
+                        'PySimpleGUI>=4.60.4',
+                        'pytest',
+                        'scikit-image==0.19.3',
+                        'stardist==0.8.5',
+                        'tensorflow==2.13.0',
+                        'tqdm==4.65.0'],
+    packages=find_packages(include=["midap.*"]),
+    project_urls={'Midap': 'https://gitlab.ethz.ch/oschmanf/ackermann-bacteria-segmentation/'},
+    entry_points={
+        'console_scripts': [
+            'midap = midap.main:run_module',
+            'correct_segmentation = midap.apps.correct_segmentation:main',
+            'midap_download = midap.apps.download_files:main',
+        ],
+    },
+)
