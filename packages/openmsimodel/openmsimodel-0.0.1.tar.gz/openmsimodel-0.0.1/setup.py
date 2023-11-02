7bd9382fa710f90761fde7e241344b2fd80abed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:08:57 +0000

    Add package openmsimodel/0.0.1/openmsimodel-0.0.1.tar.gz

---
+++ packages/openmsimodel/openmsimodel-0.0.1.tar.gz/openmsimodel-0.0.1/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+version = "0.0.1"
+
+setupkwargs = dict(
+    name="openmsimodel",
+    version=version,
+    description="data modelling package",
+    url="https://github.com/openmsi/openmsimodel",
+    author="Ali Rachidi",
+    author_email="arachid1@jhu.edu",
+    license="MIT",
+    packages=setuptools.find_packages(include=["openmsimodel*"]),
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "open_db=openmsimodel.db.open_db:main",
+            "open_graph=openmsimodel.graph.open_graph:main",
+        ],
+    },
+    python_requires=">=3.7,<3.10",
+    install_requires=[
+        "gemd==1.13.0",
+        "networkx",
+        "pymssql==2.2.8",
+        "SQLAlchemy==2.0.17",
+        "PyInquirer",
+        "prompt_toolkit",
+        "numpy",
+        "pandas",
+        "methodtools",
+        "matplotlib",
+        "yfiles_jupyter_graphs",
+        "ipython",
+        "ipykernel",
+        "openpyxl",
+        "graphviz",
+        "questionary",
+    ],
+    extras_require={},
+)
+
+setupkwargs["extras_require"]["all"] = sum(setupkwargs["extras_require"].values(), [])
+
+setuptools.setup(**setupkwargs)
