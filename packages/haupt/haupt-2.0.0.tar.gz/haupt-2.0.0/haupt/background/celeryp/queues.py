71f92c0738ec3ba593dc8862114107d49084e90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:09:11 +0000

    Add package haupt/2.0.0/haupt-2.0.0.tar.gz

---
+++ packages/haupt/haupt-2.0.0.tar.gz/haupt-2.0.0/haupt/background/celeryp/queues.py
@@ -0,0 +1,11 @@
+class CeleryCoreQueues:
+    """Celery Core Queues.
+
+    N.B. make sure that the queue name is not < 128.
+    """
+
+    SCHEDULER_HEALTH = "queues.scheduler.health"
+    SCHEDULER_RUNS = "queues.scheduler.runs"
+    SCHEDULER_COMPILER = "queues.scheduler.compiler"
+    SCHEDULER_ARTIFACTS = "queues.scheduler.artifacts"
+    SCHEDULER_CLEAN = "queues.scheduler.clean"
