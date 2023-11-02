597ee2b45ae98eb57dece1d456928a7676f6e9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:07:43 +0000

    Add package falkor/0.0.1/falkor-0.0.1.tar.gz

---
+++ packages/falkor/falkor-0.0.1.tar.gz/falkor-0.0.1/README.md
@@ -0,0 +1,8 @@
+# Falkor Kit
+
+## Description
+A python library to deploy the Falkor Large Language Model.
+
+## 🚀 Quick Install
+
+```pip install falkor```
---
+++ packages/falkor/falkor-0.0.1.tar.gz/falkor-0.0.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='falkor',
+    version='0.0.1',
+    description='Deploys the Falkor Large Language Model',
+    author='Collin Paran',  
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "transformers",
+        "sentencepiece",
+        "bitsandbytes",
+        "accelerate",
+        "torch",
+        "fastapi",
+        "uvicorn",
+        "joblib",
+        "python-multipart",
+        "langchain",
+        "pydantic"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    license='Apache-2.0', 
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'serve=vectorlite.server:main',
+        ],
+    },
+)
