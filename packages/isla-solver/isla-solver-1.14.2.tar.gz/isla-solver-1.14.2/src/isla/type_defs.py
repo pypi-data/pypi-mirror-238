de1217ff33de74a8f6e853ace2952a2020e04a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 04:06:02 +0000

    Add package isla-solver/1.14.2/isla-solver-1.14.2.tar.gz

---
+++ packages/isla-solver/isla-solver-1.14.2.tar.gz/isla-solver-1.14.2/src/isla/type_defs.py
@@ -0,0 +1,30 @@
+# Copyright © 2022 CISPA Helmholtz Center for Information Security.
+# Author: Dominic Steinhöfel.
+#
+# This file is part of ISLa.
+#
+# ISLa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ISLa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ISLa.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Tuple, Optional, List, Dict, TypeVar, TypeAlias
+
+S = TypeVar("S")
+T = TypeVar("T")
+
+ParseTree = Tuple[str, Optional[List["ParseTree"]]]
+Path = Tuple[int, ...]
+Grammar = Dict[str, List[str]]
+ImmutableGrammar = Tuple[Tuple[str, Tuple[str, ...]], ...]
+CanonicalGrammar = Dict[str, List[List[str]]]
+ImmutableList: TypeAlias = Tuple[T, ...]
+Pair: TypeAlias = Tuple[S, T]
