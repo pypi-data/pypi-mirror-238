f8d570f806a71b0b720e318426561bc8a8492827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:08:05 +0000

    Add package mfio/1.2.3/mfio-1.2.3.tar.gz

---
+++ packages/mfio/mfio-1.2.3.tar.gz/mfio-1.2.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mfio',
+    version='1.2.3',
+    description='自用文件读写检索匹配工具',
+    url='',
+    author='yunhgu',
+    author_email='1508777473@qq.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'loguru',
+        'xlrd==1.2.0',
+        'xlwt',
+        'numpy',
+        'opencv_python',
+        'xmltodict',
+        "pyyaml",
+        "requests",
+        "openpyxl"
+    ],
+    python_requires='>=3.7'
+)
