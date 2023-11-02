5b6141dd8f59fbc88c3bbb5569dffb23adc88416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:09:35 +0000

    Add package sagemaker-jupyterlab-emr-extension/0.1.5/sagemaker-jupyterlab-emr-extension-0.1.5.tar.gz

---
+++ packages/sagemaker-jupyterlab-emr-extension/sagemaker-jupyterlab-emr-extension-0.1.5.tar.gz/sagemaker-jupyterlab-emr-extension-0.1.5/src/utils/CommonUtils.ts
@@ -0,0 +1,9 @@
+import isDate from 'lodash/isDate';
+
+const getISOStringFromDate = (date?: Date): string | undefined => (isDate(date) ? date.toISOString() : undefined);
+
+const strHasLength = (str: unknown): str is string => typeof str === 'string' && str.length > 0;
+
+const arrayHasLength = <T>(arr: unknown): arr is T[] => Array.isArray(arr) && arr.length > 0;
+
+export { getISOStringFromDate, strHasLength, arrayHasLength };
