48073c1027c66afc3cb678a283ecba6dd45554e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:07:28 +0000

    Add package integrationsdk/0.0.1/integrationsdk-0.0.1.tar.gz

---
+++ packages/integrationsdk/integrationsdk-0.0.1.tar.gz/integrationsdk-0.0.1/integrationsdk/__main__.py
@@ -0,0 +1,6 @@
+from cookiecutter.cli import main as ccmain
+if __name__ == "__main__":  # pragma: no cover
+    ccmain("integrationsdk", "integrationsdk")
+
+def main():
+    ccmain("integrationsdk", "integrationsdk")
