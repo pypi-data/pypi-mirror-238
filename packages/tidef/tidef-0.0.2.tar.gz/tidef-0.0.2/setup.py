dbc034aafc941134832d8de2a706086e6e5ee2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 04:06:48 +0000

    Add package tidef/0.0.2/tidef-0.0.2.tar.gz

---
+++ packages/tidef/tidef-0.0.2.tar.gz/tidef-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import os, glob
+# from tidef.__version__ import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setuptools.setup(
+    name="tidef", # Replace with your own username
+    version='0.0.2',
+    author="Babatunde Akinsanmi",
+    author_email="tunde.akinsanmi@unige.ch",
+    description="Package to model the phase curve of tidally deformed planets",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tundeakins/tidef",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "scipy", "matplotlib", "batman-package","ellc"],
+    extras_require = { "dev": [ "pytest >=3.7 "],},
+)
---
+++ packages/tidef/tidef-0.0.2.tar.gz/tidef-0.0.2/tidef/__init__.py
@@ -0,0 +1,2 @@
+from .phase_curve import deformed_PC
+from tidef.__version__ import __version__ 
