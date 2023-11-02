e6de09366e9c076aed4922693400bc3ed17c58ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:08:19 +0000

    Add package tinyprofiler/0.1.6/tinyprofiler-0.1.6.tar.gz

---
+++ packages/tinyprofiler/tinyprofiler-0.1.6.tar.gz/tinyprofiler-0.1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tinyprofiler",
+    version="0.1.6",
+    description="A Profiling Library for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Carter Fulcher",
+    author_email="fulcher.carter@gmail.com",
+    packages=find_packages(),
+)
