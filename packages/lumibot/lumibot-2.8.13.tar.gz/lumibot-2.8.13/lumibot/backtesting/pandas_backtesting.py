cf02912f6af893e83ab4c0e64d42ebf943e8f4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 15:07:51 +0000

    Add package lumibot/2.8.13/lumibot-2.8.13.tar.gz

---
+++ packages/lumibot/lumibot-2.8.13.tar.gz/lumibot-2.8.13/lumibot/backtesting/pandas_backtesting.py
@@ -0,0 +1,19 @@
+from lumibot.data_sources import PandasData
+
+from .data_source_backtesting import DataSourceBacktesting
+
+
+class PandasDataBacktesting(DataSourceBacktesting, PandasData):
+    """
+    Backtesting implementation of the PandasData class.
+
+    Parameters
+    ----------
+    data_source : PandasData
+        The data source to use for backtesting.
+    """ 
+
+    def __init__(self, datetime_start, datetime_end, pandas_data=None, **kwargs):
+        self.LIVE_DATA_SOURCE = PandasData
+        PandasData.__init__(self, pandas_data, **kwargs)
+        DataSourceBacktesting.__init__(self, datetime_start, datetime_end)
