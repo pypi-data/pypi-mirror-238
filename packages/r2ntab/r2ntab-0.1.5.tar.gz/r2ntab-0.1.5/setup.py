db262fb7a140833ad3b09f17e4ed630a9b4079fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:04:30 +0000

    Add package r2ntab/0.1.5/r2ntab-0.1.5.tar.gz

---
+++ packages/r2ntab/r2ntab-0.1.5.tar.gz/r2ntab-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='r2ntab',
+    version='0.1.5',
+    author='M.J. van der Zwart',
+    author_email='mvdzwart01@hotmail.nl',
+    description='Model for binary classification combining deep learning and rule learning',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    keywords=['python', 'rule learning', 'neural networks', 'deep learning', 'classification'],
+    install_requires=['torch', 'torchvision', 'numpy'],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
