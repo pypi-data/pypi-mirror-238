199de05f7ce18f014d74d20cc465d8e43ca7e20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:05:25 +0000

    Add package aws-org-client/0.0.9/aws_org_client-0.0.9-py3-none-any.whl

---
+++ packages/aws-org-client/aws_org_client-0.0.9-py3-none-any.whl/aws_org_client/modules/identity_store.py
@@ -0,0 +1,63 @@
+import boto3
+from aws_org_client.modules.logger import Logger
+
+
+logger = Logger(__name__)
+
+
+class IdentityStore:
+    def __init__(self, identity_store_id):
+        """Initialise client for identity store.
+
+        Args:
+            identity_store_id (string): The globally unique identifier for the identity store.
+        """
+        logger.info("Init idc client...")
+        self.client = boto3.client("identitystore")
+        self.identity_store_id = identity_store_id
+
+    def list_users(self):
+        """List all users in identity store.
+
+        Returns:
+            list: Collection of user objects.
+        """
+        logger.info("Listing users...")
+        response = self.client.list_users(IdentityStoreId=self.identity_store_id)
+
+        return response.get("Users", [])
+
+    def describe_user(self, user_id):
+        logger.info(f"Describing user: {user_id}...")
+        response = self.client.describe_user(
+            IdentityStoreId=self.identity_store_id, UserId=user_id
+        )
+
+        return response
+
+    def list_groups(self):
+        logger.info("Listing groups...")
+        paginator = self.client.get_paginator("list_groups")
+        operation_parameters = {"IdentityStoreId": self.identity_store_id}
+
+        page_iterator = paginator.paginate(**operation_parameters)
+
+        groups = [group for page in page_iterator for group in page["Groups"]]
+
+        return groups
+
+    def describe_group(self, group_id):
+        logger.info(f"Describing group: {group_id}...")
+        response = self.client.describe_group(
+            IdentityStoreId=self.identity_store_id, GroupId=group_id
+        )
+
+        return response
+
+    def list_group_memberships(self, group_id):
+        logger.info(f"Listing group memberships: {group_id}...")
+        response = self.client.list_group_memberships(
+            IdentityStoreId=self.identity_store_id, GroupId=group_id
+        )
+
+        return response.get("GroupMemberships", [])
