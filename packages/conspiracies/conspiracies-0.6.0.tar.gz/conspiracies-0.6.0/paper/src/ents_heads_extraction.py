a30e6fdb559decb474753022260ad024c13af290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:08:45 +0000

    Add package conspiracies/0.6.0/conspiracies-0.6.0.tar.gz

---
+++ packages/conspiracies/conspiracies-0.6.0.tar.gz/conspiracies-0.6.0/paper/src/ents_heads_extraction.py
@@ -0,0 +1,47 @@
+"""Pipeline for headwords/entities extractions and frequency count."""
+import multiprocessing
+import os
+
+import spacy
+import torch
+from tqdm import tqdm
+
+from conspiracies.docprocessing.headwordextraction import contains_ents
+
+
+def main():
+    nlp = spacy.load("en_core_web_lg")
+
+    test_sents = ["Mette Frederiksen is the Danish politician."] * 1000
+
+    config = {"confidence_threshold": 2.7, "model_args": {"batch_size": 10}}
+    nlp.add_pipe("relation_extractor", config=config)
+    nlp.add_pipe("heads_extraction")
+
+    # multiprocessing and torch with multiple threads result in a deadlock
+    torch.set_num_threads(1)
+
+    pipe = nlp.pipe(test_sents, n_process=os.cpu_count(), batch_size=25)
+
+    heads_spans = []
+    ents_spans = []
+
+    for d in tqdm(pipe):
+        for span in d._.relation_head:
+            heads_spans.append(span._.most_common_ancestor)
+            if span.ents:
+                ents_spans.append(list(span.ents))
+        for span in d._.relation_tail:
+            heads_spans.append(span._.most_common_ancestor)
+            if span.ents:
+                ents_spans.append(list(span.ents))
+
+    # Filter out headwords without an entity type
+    filtered_heads = list(filter(contains_ents, heads_spans))
+
+    # Flatten the list with ents
+    flat_ents_list = [ent for sublist in ents_spans for ent in sublist]
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/conspiracies/conspiracies-0.6.0.tar.gz/conspiracies-0.6.0/src/conspiracies/docprocessing/doc_utils.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+from typing import List, Union
+
+import jsonlines
+from spacy.language import Language
+from spacy.tokens import Doc
+
+from conspiracies.docprocessing.relationextraction.data_classes import (
+    install_extensions,
+)
+from conspiracies.docprocessing.relationextraction.gptprompting import (
+    DocTriplets,
+    SpanTriplet,
+)
+
+
+def _doc_to_json(doc: Doc):
+    if Doc.has_extension("relation_triplets"):
+        triplets = doc._.relation_triplets
+    else:
+        triplets = []
+    json = doc.to_json()
+    json["semantic_triplets"] = [
+        triplet.to_dict(include_doc=False) for triplet in triplets
+    ]
+    return json
+
+
+def _doc_from_json(json: dict, nlp: Language) -> Doc:
+    doc = Doc(nlp.vocab).from_json(json)
+    triplets = [
+        SpanTriplet.from_dict(triplet_json, nlp=nlp, doc=doc)
+        for triplet_json in json["semantic_triplets"]
+    ]
+    if not Doc.has_extension("relation_triplets"):
+        install_extensions()
+    doc._.relation_triplets = DocTriplets(span_triplets=triplets, doc=doc)
+    return doc
+
+
+def docs_to_jsonl(
+    docs: List[Doc],
+    path: Union[Path, str],
+) -> None:
+    """Write docs and triplets to a jsonl file.
+
+    Args:
+        docs: a list of docs. If the docs have the extension
+            "relation_triplets", the triplets will written to the jsonl file.
+        path: path to the jsonl file.
+    """
+    jsonl = [_doc_to_json(doc) for doc in docs]
+    with jsonlines.open(path, "w") as writer:
+        writer.write_all(jsonl)
+
+
+def docs_from_jsonl(
+    path: Union[Path, str],
+    nlp: Language,
+) -> List[Doc]:
+    """Read docs and triplets from a jsonl file.
+
+    Args:
+        path: path to the jsonl file.
+        nlp: a spacy language model.
+
+    Returns:
+        A list of docs with the extension `doc._.relation_triplets` set.
+    """
+    if not Doc.has_extension("relation_triplets"):
+        install_extensions(force=True)
+    docs = []
+    with jsonlines.open(path, "r") as reader:
+        for json in reader:
+            doc = _doc_from_json(json, nlp)
+            docs.append(doc)
+    return docs
