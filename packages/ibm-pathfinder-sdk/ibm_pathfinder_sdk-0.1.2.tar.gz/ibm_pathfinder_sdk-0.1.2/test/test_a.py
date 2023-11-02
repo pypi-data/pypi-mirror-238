22e8399ccf9e66a3b13f9ad74f4fcc13d101fedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:07:26 +0000

    Add package ibm-pathfinder-sdk/0.1.2/ibm_pathfinder_sdk-0.1.2.tar.gz

---
+++ packages/ibm-pathfinder-sdk/ibm_pathfinder_sdk-0.1.2.tar.gz/ibm_pathfinder_sdk-0.1.2/test/test_a.py
@@ -0,0 +1,15 @@
+"""
+// SPDX-License-Identifier: Apache-2.0
+// Copyright IBM Corp. 2023
+"""
+
+from ibm_pathfinder_sdk import pfmodelclasses
+
+def test_sample():
+    assert 1 == 1
+
+def test_sample2():
+    assert 1 == 1
+    
+    s = pfmodelclasses.Object
+    assert 2 == 2
