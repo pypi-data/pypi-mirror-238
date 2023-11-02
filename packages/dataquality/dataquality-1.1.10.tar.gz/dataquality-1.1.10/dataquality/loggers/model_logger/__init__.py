1a83f66ade835767df2ba319c4a47520b0b8addd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:05:31 +0000

    Add package dataquality/1.1.10/dataquality-1.1.10.tar.gz

---
+++ packages/dataquality/dataquality-1.1.10.tar.gz/dataquality-1.1.10/dataquality/loggers/model_logger/__init__.py
@@ -0,0 +1,19 @@
+from dataquality.loggers.model_logger import (
+    image_classification,
+    seq2seq,
+    tabular_classification,
+    text_classification,
+    text_multi_label,
+    text_ner,
+)
+from dataquality.loggers.model_logger.base_model_logger import BaseGalileoModelLogger
+
+__all__ = [
+    "image_classification",
+    "tabular_classification",
+    "text_classification",
+    "text_multi_label",
+    "text_ner",
+    "BaseGalileoModelLogger",
+    "seq2seq",
+]
