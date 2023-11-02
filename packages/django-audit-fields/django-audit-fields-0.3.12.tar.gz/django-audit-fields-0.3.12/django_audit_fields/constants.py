8abd15d6b1c39548f76e3d3b2bdbd79efc27f529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:04:44 +0000

    Add package django-audit-fields/0.3.12/django-audit-fields-0.3.12.tar.gz

---
+++ packages/django-audit-fields/django-audit-fields-0.3.12.tar.gz/django-audit-fields-0.3.12/django_audit_fields/constants.py
@@ -0,0 +1,14 @@
+from typing import List
+
+AUDIT_MODEL_UPDATE_FIELDS: List[str] = ["modified", "user_modified", "hostname_modified"]
+
+AUDIT_MODEL_FIELDS: List[str] = [
+    "created",
+    "modified",
+    "user_created",
+    "user_modified",
+    "hostname_created",
+    "hostname_modified",
+    "device_created",
+    "device_modified",
+]
