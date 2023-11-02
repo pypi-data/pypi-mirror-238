6f26757e531b0b4e614f636089e53c4f284ea135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:09:20 +0000

    Add package hscan/3.0.3/hscan-3.0.3.tar.gz

---
+++ packages/hscan/hscan-3.0.3.tar.gz/hscan-3.0.3/scan/database/__init__.py
@@ -0,0 +1,7 @@
+from scan.database.rabbitmq import RabbitMQ
+from scan.database.redis import Redis
+from scan.database.mongodb import MongoDB
+from scan.database.oss import OSS
+from scan.database.pg import Postgresql
+from scan.database.kafka import Kafka
+from scan.database.r2 import CFR2
