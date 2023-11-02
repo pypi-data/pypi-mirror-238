007abeae11763034f9c626d348bd1cfa9d15e601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:09:18 +0000

    Add package pympipool/0.7.2/pympipool-0.7.2.tar.gz

---
+++ packages/pympipool/pympipool-0.7.2.tar.gz/pympipool-0.7.2/tests/test_future.py
@@ -0,0 +1,29 @@
+import numpy as np
+import unittest
+from time import sleep
+from pympipool.mpi.executor import PyMPISingleTaskExecutor
+from concurrent.futures import Future
+
+
+def calc(i):
+    return np.array(i**2)
+
+
+class TestFuture(unittest.TestCase):
+    def test_pool_serial(self):
+        with PyMPISingleTaskExecutor(cores=1) as p:
+            output = p.submit(calc, i=2)
+            self.assertTrue(isinstance(output, Future))
+            self.assertFalse(output.done())
+            sleep(1)
+        self.assertTrue(output.done())
+        self.assertEqual(output.result(), np.array(4))
+
+    def test_pool_serial_multi_core(self):
+        with PyMPISingleTaskExecutor(cores=2) as p:
+            output = p.submit(calc, i=2)
+            self.assertTrue(isinstance(output, Future))
+            self.assertFalse(output.done())
+            sleep(1)
+        self.assertTrue(output.done())
+        self.assertEqual(output.result(), [np.array(4), np.array(4)])
