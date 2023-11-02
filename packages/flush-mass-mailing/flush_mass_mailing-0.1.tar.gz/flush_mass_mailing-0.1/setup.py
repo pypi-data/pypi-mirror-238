0dccb470a255efa0dc25b9a71c054e8129f97780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:05:42 +0000

    Add package flush-mass-mailing/0.1/flush_mass_mailing-0.1.tar.gz

---
+++ packages/flush-mass-mailing/flush_mass_mailing-0.1.tar.gz/flush_mass_mailing-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flush_mass_mailing',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'rich',
+        'smtplib',
+        'csv',
+        'getpass',
+        'time',
+    ],
+)
