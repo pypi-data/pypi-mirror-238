91cab1dc0ba27252909303217ed787006ded0c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 16:08:52 +0000

    Add package LensToZotero/0.1.2/LensToZotero-0.1.2.tar.gz

---
+++ packages/LensToZotero/LensToZotero-0.1.2.tar.gz/LensToZotero-0.1.2/LensToZotero/LensToZotero.py
@@ -0,0 +1,28 @@
+from pyzotero import zotero
+
+from utils import *
+
+
+def transfer(settingsPath):
+
+    settigs = get_settings(settingsPath)
+
+    zot = zotero.Zotero(settigs["library_id"],
+                        settigs["library_type"], settigs["api_key"])
+
+    jsonfile = 'lens_json.json'
+
+    csv_to_json(settigs["filePath"], jsonfile)
+
+    doc = json.loads(get_doc(jsonfile, cwd=os.getcwd()))
+
+    delete_doc(jsonfile, cwd=os.getcwd())
+
+    for item in doc:
+        template = zot.item_template(itemType[item["Publication.Type"]])
+        for key in template:
+            if key in itemTemplate:
+                template[key] = itemTemplate[key](item, key)
+        list_template = zot.check_items([template])
+        resp = zot.create_items(list_template)
+    print("Transer: "+settigs["filePath"]+" to zotero")
---
+++ packages/LensToZotero/LensToZotero-0.1.2.tar.gz/LensToZotero-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pip==23.3.1", "wheel==0.41.3",
+                "twine==4.0.2", "pyzotero==1.5.18"]
+
+setup(
+    name="LensToZotero",
+    version="0.1.2",
+    author="Kostuk Danill",
+    author_email="kostukml@gmail.com",
+    description="A package to transfer Lens publications in csv format to Zotero",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RIEPPfunction/LensToZotero",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
