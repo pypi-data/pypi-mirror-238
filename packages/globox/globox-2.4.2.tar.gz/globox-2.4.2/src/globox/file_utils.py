e0050e7864b087ecd2a6ddb1fbb99bba58701921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:05:52 +0000

    Add package globox/2.4.2/globox-2.4.2.tar.gz

---
+++ packages/globox/globox-2.4.2.tar.gz/globox-2.4.2/src/globox/file_utils.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Union, Iterable
+
+
+PathLike = Union[str, Path]
+
+
+def glob(
+    folder: PathLike, extensions: Union[str, Iterable[str]], recursive: bool = False
+) -> Iterable[Path]:
+    """Glob files by providing extensions to match."""
+    if isinstance(extensions, str):
+        extensions = {extensions}
+    else:
+        extensions = set(extensions)
+
+    assert all(
+        e.startswith(".") for e in extensions
+    ), f"Parameter `extensions' ({extensions}) should all start with a dot."
+
+    path = Path(folder).expanduser().resolve()
+
+    files = path.glob("**/*") if recursive else path.glob("*")
+
+    return (f for f in files if f.suffix in extensions and not f.name.startswith("."))
