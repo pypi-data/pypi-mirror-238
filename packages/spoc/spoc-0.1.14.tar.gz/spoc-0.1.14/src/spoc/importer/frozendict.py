555d377e1a66314d576bfcf0300ce39bcdab5a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 14:08:22 +0000

    Add package spoc/0.1.14/spoc-0.1.14.tar.gz

---
+++ packages/spoc/spoc-0.1.14.tar.gz/spoc-0.1.14/src/spoc/importer/frozendict.py
@@ -0,0 +1,33 @@
+"""
+    Exception & Error Messages
+"""
+
+
+class FrozenDict(dict):
+    """Immutable Dictionary"""
+
+    def fromkeys(self, S, v):
+        """From-Keys"""
+        return type(self)(dict(self).fromkeys(S, v))
+
+    def __getattribute__(self, attribute):
+        """Get Attribute"""
+        if attribute in ("clear", "update", "pop", "popitem", "setdefault"):
+            raise AttributeError(
+                f"{type(self).__name__} object has no attribute {attribute}"
+            )
+        return dict.__getattribute__(self, attribute)
+
+    def __setitem__(self, key, value):
+        """Set"""
+        raise TypeError(
+            f"{type(self).__name__} object does not support item assignment"
+        )
+
+    def __delitem__(self, key):
+        """Delete"""
+        raise TypeError(f"{type(self).__name__} object does not support item deletion")
+
+    def __hash__(self):
+        """Hash"""
+        return hash(tuple(sorted(self.iteritems())))
---
+++ packages/spoc/spoc-0.1.14.tar.gz/spoc-0.1.14/tests/config/settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+""" [Settings]
+    Project Settings { app.settings }.
+"""
+import pathlib
+
+# Base Directory
+BASE_DIR = pathlib.Path(__file__).parents[1]
+
+# Installed Apps
+INSTALLED_APPS = ["demo"]
