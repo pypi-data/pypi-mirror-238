7f4170d4c588a1cfe304d10c9fddd1f41b7451da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 06:07:42 +0000

    Add package yival/0.1.0a9/yival-0.1.0a9-py3-none-any.whl

---
+++ packages/yival/yival-0.1.0a9-py3-none-any.whl/yival/cli/__init__.py
@@ -0,0 +1,23 @@
+"""The CLI subcommands."""
+
+from argparse import ArgumentParser
+
+from .bot import add_arguments_to as BOT
+from .demo import add_arguments_to as DEMO
+from .init import add_arguments_to as INIT
+from .run import add_arguments_to as RUN
+from .validate import add_arguments_to as VALIDATE
+
+
+def add_subcommands_to(parser: ArgumentParser):
+    """Add subcommands to the main parser."""
+    subparser = parser.add_subparsers(title="actions")
+
+    for func in (
+        INIT,
+        VALIDATE,
+        RUN,
+        DEMO,
+        BOT,
+    ):
+        func(subparser)
