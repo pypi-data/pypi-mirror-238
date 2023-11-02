ae5c295276c0af55a4a7d51fe3f38c86e5aa2ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:05:38 +0000

    Add package e2D/1.2.6/e2D-1.2.6-py3-none-any.whl

---
+++ packages/e2D/e2D-1.2.6-py3-none-any.whl/e2D/envs.py
@@ -0,0 +1,276 @@
+from .utils import *
+import pygame as pg
+
+"""  CODE EXAMPLE FOR RootEnv
+from e2D.envs import *
+
+class Env:
+    def __init__(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
+
+(rootEnv:=RootEnv()).init(Env())
+while not rootEnv.quit:
+    rootEnv.frame()
+"""
+
+pg.init()
+pg.font.init()
+font_arial_16 = pg.font.SysFont("Arial", 16)
+font_arial_32 = pg.font.SysFont("Arial", 32)
+font_arial_64 = pg.font.SysFont("Arial", 64)
+my_arial_font_size = lambda size: pg.font.SysFont("Arial", size)
+
+TEXT_FIXED_SIDES_TOP_LEFT = 0
+TEXT_FIXED_SIDES_TOP_MIDDLE = 1
+TEXT_FIXED_SIDES_TOP_RIGHT = 2
+TEXT_FIXED_SIDES_MIDDLE_LEFT = 3
+TEXT_FIXED_SIDES_MIDDLE_MIDDLE = 4
+TEXT_FIXED_SIDES_MIDDLE_RIGHT = 5
+TEXT_FIXED_SIDES_BOTTOM_LEFT = 6
+TEXT_FIXED_SIDES_BOTTOM_MIDDLE = 7
+TEXT_FIXED_SIDES_BOTTOM_RIGHT = 8
+
+class RootEnv:
+    __fixed_sides_multiplier = [V2(x,y) for y in [0, .5, 1] for x in [0, .5, 1]]
+    def __init__(self, screen_size:V2|Vector2D=V2(1920, 1080), vsync:bool=True, target_fps:int=60, show_fps=True, quit_on_key_pressed:None|int=pg.K_x, window_flag:int=0, clear_screen_each_frame:bool=True) -> None:
+        self.quit = False
+        self.screen_size :V2|Vector2D= screen_size
+        self.screen = pg.display.set_mode(self.screen_size(), vsync=vsync, flags=window_flag)
+        self.target_fps = target_fps
+        self.show_fps = show_fps
+        self.clock = pg.time.Clock()
+        self.keyboard = Keyboard(self)
+        self.mouse = Mouse(self)
+        self.events :list= []
+        self.background_color = rgb(0,0,0)
+        self._quit_on_key_pressed = quit_on_key_pressed
+        self.clear_screen_each_frame = clear_screen_each_frame
+    
+    def init(self, sub_env) -> None:
+        self.env = sub_env
+    
+    def clear(self) -> None:
+        self.screen.fill(self.background_color)
+    
+    def clear_rect(self, position:V2|Vector2D, size:V2|Vector2D) -> None:
+        self.screen.fill(self.background_color, position() + size())
+    
+    def print(self, text:str, position:V2|Vector2D, color:tuple[float,float,float]=(255,255,255), fixed_sides=TEXT_FIXED_SIDES_TOP_LEFT, font:pg.font.Font=font_arial_32, bg_color:None|tuple[int,int,int]|list[int]=None, personalized_surface=None) -> None:
+        text_box = font.render(text, True, color)
+        size = V2(*text_box.get_size())
+        position -= size * self.__fixed_sides_multiplier[fixed_sides]
+        if bg_color != None:
+            pg.draw.rect((self.screen if personalized_surface == None else personalized_surface), bg_color, position() + size())
+        (self.screen if personalized_surface == None else personalized_surface).blit(text_box, position())
+
+    def __draw__(self) -> None:
+        self.clock.tick(self.target_fps)
+        if self.clear_screen_each_frame: self.clear()
+        if self.show_fps: self.print(str(round(self.clock.get_fps(),2)), self.screen_size * .01, bg_color=(0,0,0))
+
+        self.env.draw()
+        pg.display.update()
+    
+    def __update__(self) -> None:
+        self.mouse.update()
+        self.keyboard.update()
+        self.env.update()
+
+    def frame(self) -> None:
+        self.__update__()
+        self.__draw__()
+
+        self.events = pg.event.get()
+        for event in self.events:
+            if event.type == pg.QUIT or ((event.type == pg.KEYDOWN and event.key == self._quit_on_key_pressed) if self._quit_on_key_pressed != None else False):
+                self.quit = True
+
+################################################################################################################################################################################################################################
+################################################################################################################################################################################################################################
+
+""" TODO
+from e2D import *
+import pygame as pg
+import easygui
+import uuid
+
+pg.init()
+pg.font.init()
+myfont = pg.font.SysFont("Arial", 32)
+
+DEFAULT_MOUSE_BUTTON_MODE = 0
+OVER_MOUSE_BUTTON_MODE = 1
+CLICKED_MOUSE, clear_screen_each_frame=True, clear_screen_each_frame=True_BUTTON_MODE = 2
+
+CALLBACK_MODE_ON_PRESSED = 0
+CALLBACK_MODE_ON_RELEASED = 1
+
+class Button:
+    def_side_border = .85
+    def __init__(self,
+                 parent,
+                 text,
+                 screen_ratio_position:V2,
+                 screen_ratio_size:V2, 
+                 text_color:tuple[int|float,int|float,int|float],
+                 def_color:tuple[int|float,int|float,int|float],
+                 over_color:tuple[int|float,int|float,int|float],
+                 click_color:tuple[int|float,int|float,int|float],
+                 callback,
+                 border_color:tuple[int|float,int|float,int|float]=(255,255,255),
+                 border_width:int|float=2,
+                 corners:list[int|float]|int|float|None=10,
+                 callback_mode=CALLBACK_MODE_ON_RELEASED,
+                 font="Algerian",
+                 font_downscale:float=1.0,
+                 **callback_kwargs) -> None:
+        self.parent :Env= parent
+        self.text :str= text
+        self.screen_ratio_size = screen_ratio_size  # ratio position is bot right
+        self.screen_ratio_position = screen_ratio_position # ratio position is top left
+        self.text_color = text_color
+        self.callback = callback
+        self.callback_kwargs = callback_kwargs
+        self.def_color = def_color
+        self.over_color = over_color
+        self.click_color = click_color
+        self.mouse_mode = DEFAULT_MOUSE_BUTTON_MODE
+        self.font = font
+        self.font_downscale = font_downscale
+        self.callback_mode = callback_mode
+        self.border_color = border_color
+        self.border_width = border_width
+        self.corners = corners
+        self.update_screen_ratio_position()
+    
+    def update_screen_ratio_position(self) -> None:
+        self.position = self.parent.screen_size * self.screen_ratio_position
+        self.size = self.parent.screen_size * self.screen_ratio_size - self.parent.screen_size * self.screen_ratio_position
+        self.set_box_and_text_size()
+    
+    def set_box_and_text_size(self) -> None:
+        text_box = pg.font.SysFont(self.font, int(self.size.y)).render(self.text, True, self.text_color) #type: ignore
+        text_box_size = V2(*text_box.get_rect()[2:])
+        ratio = text_box_size.x / text_box_size.y
+        self.font_size = min(self.size.x * self.def_side_border / ratio, self.size.y) * self.font_downscale
+        self.text_box = pg.font.SysFont(self.font, int(self.font_size)).render(self.text, True, self.text_color) #type: ignore
+        self.text_box_size = V2(*self.text_box.get_rect()[2:])
+
+    def draw(self) -> None:
+        color = self.def_color
+        if self.mouse_mode == DEFAULT_MOUSE_BUTTON_MODE:
+            color = self.def_color
+        elif self.mouse_mode == OVER_MOUSE_BUTTON_MODE:
+            color = self.over_color
+        elif self.mouse_mode == CLICKED_MOUSE_BUTTON_MODE:
+            color = self.click_color
+        
+        if self.corners == None:
+            pg.draw.rect(self.parent.screen, color, self.position() + self.size()) #type: ignore
+        elif isinstance(self.corners, int|float):
+            pg.draw.rect(self.parent.screen, color, self.position() + self.size(), border_radius=self.corners) #type: ignore
+        elif type(self.corners) == list:
+            pg.draw.rect(self.parent.screen, color, self.position() + self.size(), border_top_left_radius=self.corners[0], border_top_right_radius=self.corners[1], border_bottom_left_radius=self.corners[2], border_bottom_right_radius=self.corners[3]) #type: ignore
+        if self.border_width:
+            if self.corners == None:
+                pg.draw.rect(self.parent.screen, self.border_color, self.position() + self.size(), self.border_width) #type: ignore
+            elif isinstance(self.corners, int|float):
+                pg.draw.rect(self.parent.screen, self.border_color, self.position() + self.size(), self.border_width, border_radius=self.corners) #type: ignore
+            elif type(self.corners) == list:
+                pg.draw.rect(self.parent.screen, self.border_color, self.position() + self.size(), self.border_width, border_top_left_radius=self.corners[0], border_top_right_radius=self.corners[1], border_bottom_left_radius=self.corners[2], border_bottom_right_radius=self.corners[3]) #type: ignore
+        
+        self.parent.screen.blit(self.text_box, (self.position + (self.size - self.text_box_size) / 2)())
+
+    def update(self) -> None:
+        if self.position.x < self.parent.mouse.position.x < self.position.x + self.size.x and self.position.y < self.parent.mouse.position.y < self.position.y + self.size.y:
+            if self.parent.mouse.just_pressed[0] and CALLBACK_MODE_ON_PRESSED or self.parent.mouse.just_released[0] and CALLBACK_MODE_ON_RELEASED:
+                self.mouse_mode = CLICKED_MOUSE_BUTTON_MODE
+                self.callback(**self.callback_kwargs)
+            elif self.parent.mouse.pressed[0]:
+                self.mouse_mode = CLICKED_MOUSE_BUTTON_MODE
+            else:
+                self.mouse_mode = OVER_MOUSE_BUTTON_MODE
+        else:
+            self.mouse_mode = DEFAULT_MOUSE_BUTTON_MODE
+
+
+class ButtonCallBacks:
+    def __init__(self, parent) -> None:
+        self.parent :Env= parent
+
+    def on_load_image_callback(self, **kwargs) -> None:
+        path = easygui.fileopenbox(filetypes=["*.png"])
+        if not path: return
+        image = pg.image.load(path) #type: ignore
+        self.parent.reference_images.append({"path": path, "image":image, "id": uuid.uuid4()})
+
+class Env:
+    def __init__(self) -> None:
+        self.quit = False
+        self.screen_size = V2(1920, 1080)
+        self.screen = pg.display.set_mode(self.screen_size(), vsync=1, flags=pg.RESIZABLE)
+        self.clock = pg.time.Clock()
+        self.keyboard = Keyboard(self)
+        self.mouse = Mouse(self)
+        self.buttons_callbacks = ButtonCallBacks(self)
+        self.buttons = [
+            Button(self,
+                   text="add photo path",
+                   screen_ratio_position=V2(.75, .1),
+                   screen_ratio_size=V2(.99, .25),
+                   text_color=rgb(255, 255, 255),
+                   def_color=rgb(194, 87, 0),
+                   over_color=rgb(107, 57, 15),
+                   click_color=rgb(32, 20, 9),
+                   callback=self.buttons_callbacks.on_load_image_callback,
+                   font_downscale=.9)
+                   ]
+        self.reference_images = []
+    
+    def print(self, text, position, color=(255,255,255)) -> None:
+        text_box = myfont.render(text, True, color)
+        self.screen.blit(text_box, position())
+
+    def clear(self) -> None:
+        self.screen.fill((0,0,0))
+    
+    def draw(self) -> None:
+        self.clock.tick(0)
+        self.clear()
+        self.print(str(self.clock.get_fps()), self.screen_size * .01)
+
+        for button in self.buttons: button.draw()
+        self.mouse.draw()
+
+        pg.display.update()
+    
+    def update(self) -> None:
+        last_screen_size = self.screen_size()
+        self.screen_size = V2(*self.screen.get_rect()[2:])
+        if last_screen_size != self.screen_size():
+            for button in self.buttons: button.update_screen_ratio_position()
+        
+        self.keyboard.update()
+        self.mouse.update()
+        for button in self.buttons: button.update()
+    
+    def frame(self) -> None:
+        self.update()
+        self.draw()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_x):
+                self.quit = True
+
+env = Env()
+
+while not env.quit:
+    env.frame()
+
+"""
---
+++ packages/e2D/e2D-1.2.6-py3-none-any.whl/e2D/plots.py
@@ -0,0 +1,162 @@
+from .envs import *
+
+def no_error_function(function, args) -> int|float|None:
+    try:
+        return res if type(res:=function(args)) != complex else None
+    except Exception as err:
+        return float("inf")
+
+def no_error_complex_function(function, args) -> V2|Vector2D:
+    res :complex= function(args)
+    return V2(res.real, res.imag)
+
+class Function:
+    def __init__(self, function, plot:"Plot", color, use_y_as_primary=False, auto_connect_treshold:float=float("inf"), points_radius=2, points_color=None) -> None:
+        self.auto_connect_treshold = auto_connect_treshold
+        self.plot = plot
+        self.color = color
+        self.points_radius = points_radius
+        self.use_y_as_primary = use_y_as_primary
+        self.points_color = self.color if points_color == None else self.points_color
+        self.update_function(function, self.use_y_as_primary)
+    
+    def update_points(self) -> None:
+        self.update_function(self.function, self.use_y_as_primary)
+
+    def update_function(self, new_function, use_y_as_primary=False) -> None:
+        self.use_y_as_primary = use_y_as_primary
+        self.function = new_function
+
+        if not self.use_y_as_primary:
+            real_step = abs(self.plot.ending_x - self.plot.starting_x) * self.plot.step
+            self.points = [V2(self.plot.starting_x + x * real_step, y) for x in range(int(1 / self.plot.step)) if (y:=no_error_function(new_function, self.plot.starting_x + x * real_step)) != None and self.plot.ending_y < y < self.plot.starting_y]
+        else:
+            real_step = abs(self.plot.ending_y - self.plot.starting_y) * self.plot.step
+            self.points = [V2(x, self.plot.starting_y + y * real_step) for y in range(int(1 / self.plot.step)) if (x:=no_error_function(new_function, self.plot.starting_y + y * real_step)) != None and self.plot.starting_x < x < self.plot.ending_x]
+        self.full_auto_connect = not any(abs(point.y - self.points[i].y) > self.auto_connect_treshold for i,point in enumerate(self.points[1:]))
+    
+    def draw(self) -> None:
+        if self.points_radius:
+            for point in self.points:
+                pg.draw.circle(self.plot.canvas, self.points_color, self.plot.plot2real(point)(), self.points_radius)
+
+        if len(self.points) < 2: return
+        self.draw_line = [self.plot.plot2real(point)() for point in self.points]
+        if self.full_auto_connect:
+            pg.draw.lines(self.plot.canvas, self.color, False, self.draw_line) #type: ignore
+        else:
+            for i,(point, real_point) in enumerate(zip(self.points[1:], self.draw_line[1:])):
+                if abs(point[1] - self.points[i][1]) < self.auto_connect_treshold:
+                    pg.draw.line(self.plot.canvas, self.color, self.draw_line[i], real_point) #type: ignore
+    
+    @classmethod
+    def load_from_list_of_points(cls, points:list[V2|Vector2D], plot:"Plot", color, auto_connect_treshold:float=float("inf"), points_radius=2, points_color=None):
+        smoothed_function = lambda t: points[0].y if t <= 0  else (points[-1].y if t >= 1 else (points[rt] if (isinstance(rt:=t*len(points), int) or rt.is_integer()) else ((p0:=points[rt.__floor__()]).y-(p1:=points[rt.__ceil__()]).y)/(p0.x-p1.x)*(points[t*len(points)]-p0.x)+p0.y))
+        return cls(smoothed_function, plot, color, auto_connect_treshold=auto_connect_treshold, points_radius=points_radius, points_color=points_color)
+
+class ComplexFunction:
+    def __init__(self, function, plot:"Plot", starting_t:float=-10, ending_t:float=10, step=.01, color=(255,255,255), auto_connect_treshold=float("inf"), points_radius=2, points_color=None) -> None:
+        self.auto_connect_treshold = auto_connect_treshold
+        self.plot = plot
+        self.starting_t = starting_t
+        self.ending_t = ending_t
+        self.color = color
+        self.step = step
+        self.points_color = points_color
+        self.points_radius = points_radius
+        self.update_function(function)
+    
+    def update_points(self) -> None:
+        self.update_function(self.function)
+    
+    def update_function(self, new_function) -> None:
+        self.function = new_function
+        self.points :list[V2|Vector2D]= [point for t in range(int(self.starting_t / self.step), int(self.ending_t / self.step)) if (self.plot.ending_y < (point:=no_error_complex_function(new_function, t * self.step)).y < self.plot.starting_y) and (self.plot.starting_x < point.x < self.plot.ending_x)]
+        self.full_auto_connect = not any(point.distance_to(self.points[i]) > self.auto_connect_treshold for i,point in enumerate(self.points[1:]))
+
+    def draw(self) -> None:
+        if self.points_radius:
+            for point in self.points:
+                pg.draw.circle(self.plot.canvas, self.color if self.points_color == None else self.points_color, self.plot.plot2real(point)(), self.points_radius)
+
+        if len(self.points) < 2: return
+        if self.full_auto_connect:
+            pg.draw.lines(self.plot.canvas, self.color, False, [self.plot.plot2real(point)() for point in self.points])
+        else:
+            real_points = [self.plot.plot2real(point)() for point in self.points]
+            for i,(point, real_point) in enumerate(zip(self.points[1:], real_points[1:])):
+                if point.distance_to(self.points[i]) < self.auto_connect_treshold:
+                    pg.draw.line(self.plot.canvas, self.color, real_points[i], real_point) #type: ignore
+
+class Plot:
+    def __init__(self, rootEnv:"RootEnv", top_left_coord:V2|Vector2D, bottom_right_coord: V2|Vector2D, draw_position:V2|Vector2D, draw_size:V2|Vector2D, step:float=.01) -> None:
+        self.rootEnv = rootEnv
+
+        self.top_left_coord = top_left_coord
+        self.bottom_right_coord = bottom_right_coord
+        self.update_zoom(0)
+
+        self.position = draw_position
+        self.size = draw_size
+        self.step = step
+        self.functions :list[Function|ComplexFunction]= []
+
+        self.canvas = pg.Surface(self.size())
+        self.dragging = None
+
+    def update_zoom(self, zoom_input:int|float) -> None:
+        self.top_left_coord += V2(-1, 1) * -zoom_input
+        self.bottom_right_coord += V2(1, -1) * -zoom_input
+        self.starting_x, self.starting_y = self.top_left_coord
+        self.ending_x, self.ending_y = self.bottom_right_coord
+
+    def load_function(self, function:Function|ComplexFunction) -> None:
+        self.functions.append(function)
+
+    def plot2real(self, plot_position:V2|Vector2D) -> V2|Vector2D:
+        return (plot_position + self.top_left_coord * -1) * self.size / (self.bottom_right_coord - self.top_left_coord)
+
+    def update_canvas(self) -> None:
+        self.canvas.fill((0,0,0))
+        if self.starting_x < 0 < self.ending_x: pg.draw.line(self.canvas, (100,100,100), self.plot2real(V2(self.starting_x, 0))(), self.plot2real(V2(self.ending_x, 0))())
+        if self.ending_y < 0 < self.starting_y: pg.draw.line(self.canvas, (100,100,100), self.plot2real(V2(0, self.starting_y))(), self.plot2real(V2(0, self.ending_y))())
+        
+        for function in self.functions: function.draw()
+        pg.draw.rect(self.canvas, (255,255,255), V2z() + self.size(), 5) #type: ignore
+
+        self.rootEnv.print(str(self.top_left_coord), V2z.copy(), bg_color=(0,0,0), personalized_surface=self.canvas)
+        self.rootEnv.print(str(V2(self.top_left_coord.x, self.bottom_right_coord.y)), self.size * V2(0, 1), fixed_sides=TEXT_FIXED_SIDES_BOTTOM_LEFT, bg_color=(0,0,0), personalized_surface=self.canvas)
+        self.rootEnv.print(str(self.bottom_right_coord), self.size.copy(), fixed_sides=TEXT_FIXED_SIDES_BOTTOM_RIGHT, bg_color=(0,0,0), personalized_surface=self.canvas)
+        self.rootEnv.print(str(V2(self.bottom_right_coord.x, self.top_left_coord.y)), self.size * V2(1, 0), fixed_sides=TEXT_FIXED_SIDES_TOP_RIGHT, bg_color=(0,0,0), personalized_surface=self.canvas)
+    
+    def update(self) -> None:
+        if self.rootEnv.mouse.just_released[0] and self.dragging != None:
+            self.dragging = None
+            for function in self.functions:
+                function.update_points()
+            self.update_canvas()
+
+        if self.position.x < self.rootEnv.mouse.position.x < self.position.x + self.size.x and \
+           self.position.y < self.rootEnv.mouse.position.y < self.position.y + self.size.y:
+            for event in self.rootEnv.events:
+                if event.type == pg.MOUSEWHEEL:
+                    self.update_zoom(event.y)
+                    for function in self.functions:
+                        function.update_points()
+                    self.update_canvas()
+            
+            if self.rootEnv.mouse.just_pressed[0] and self.dragging == None:
+                self.dragging = self.rootEnv.mouse.position.copy()
+            
+            if self.dragging:
+                offset = (self.dragging - self.rootEnv.mouse.position)* V2(1, -1) * (abs(self.bottom_right_coord - self.top_left_coord) / self.size)
+                self.dragging = self.rootEnv.mouse.position.copy()
+                self.top_left_coord += offset
+                self.bottom_right_coord += offset
+                self.top_left_coord = self.top_left_coord.__round__(.01)
+                self.bottom_right_coord = self.bottom_right_coord.__round__(.01)
+                self.update_zoom(0)
+                self.update_canvas()
+
+    def draw(self) -> None:
+        self.rootEnv.screen.blit(self.canvas, self.position())
