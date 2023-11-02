628b488cda41624ace4cc2fff0c651c3415a6435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 03:07:21 +0000

    Add package docker-remote-sync/0.3.0/docker_remote_sync-0.3.0-py3-none-any.whl

---
+++ packages/docker-remote-sync/docker_remote_sync-0.3.0-py3-none-any.whl/drsync/subprocess_utils.py
@@ -0,0 +1,21 @@
+import subprocess
+
+from drsync.io_util import print_error
+
+
+def check_subprocess_errors(error_lines) -> list[str] | None:
+    error_lines = [el for e in error_lines if len(el := e.rstrip()) > 0]
+    if len(error_lines) == 0:
+        return None
+    error_msg = "\n".join(error_lines)
+    print_error(error_msg)
+    return error_lines
+
+
+def start_subprocess(*args, text=True) -> subprocess.Popen:
+    return subprocess.Popen(
+        args=args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=text,
+    )
