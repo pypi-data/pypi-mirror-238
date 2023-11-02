77dde0b1c334560151571d645f66193f49c4c3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-02 05:08:02 +0000

    Add package akello/0.0.2.2/akello-0.0.2.2.tar.gz

---
+++ packages/akello/akello-0.0.2.2.tar.gz/akello-0.0.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2.2'
+DESCRIPTION = 'FastAPI server akello.io'
+LONG_DESCRIPTION = 'FastAPI server for running Measurement Based Care (MBC) programs'
+
+setup(
+    name="akello",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    include_package_data=True,
+    test_suite="akello",
+    package_data={'akello': ['*.yaml']},
+    author="Vijay Selvaraj",
+    author_email="vijay@akellohealth.com",
+    license="Apache License 2.0",
+    packages=find_packages(include=['akello', 'akello.*']),
+    tests_require=['PyYAML', 'responses'],
+    install_requires=['PyYAML', 'requests', 'responses'],
+    keywords='healthcare, chatgpt, screening',
+    classifiers= [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        "Programming Language :: Python :: 3",
+    ]
+)
