b37c2517a23dc859063de730cc2faca55ca82af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:56 +0000

    Add package for-adp/0.0.7/for_adp-0.0.7-py3-none-any.whl

---
+++ packages/for-adp/for_adp-0.0.7-py3-none-any.whl/for_adp/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.0.7'
+from .time import time
+from .m25 import m25
+from .m30 import m30
+from .m29 import m29
+from .m28 import m28
+from .m27 import m27
+from .m26 import m26
+from .m24 import m24
+from .m23 import m23
+from .m22 import m22
+from .m21 import m21
+from .m20 import m20
+from .m19 import m19
+from .m18 import m18
