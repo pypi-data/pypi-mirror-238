ba52d39193981b01fb9fa1bc53c7a14990cd6aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:04:49 +0000

    Add package raga-testing-platform/1.0.95/raga_testing_platform-1.0.95-py3-none-any.whl

---
+++ packages/raga-testing-platform/raga_testing_platform-1.0.95-py3-none-any.whl/raga/examples/lm_model_upload.py
@@ -0,0 +1,6 @@
+from raga import *
+
+# # create test_session object of TestSession instance
+test_session = TestSession(project_name="testingProject", run_name= "LM-Model-Upload-Run-Test-v1", access_key="LGXJjQFD899MtVSrNHGH", secret_key="TC466Qu9PhpOjTuLu5aGkXyGbM7SSBeAzYH6HpcP", host="http://3.111.106.226:8080")
+
+
