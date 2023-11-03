37cea1048095eae8661f830041e2ea7653fc8828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:57 +0000

    Add package for-adp/0.0.8/for_adp-0.0.8-py3-none-any.whl

---
+++ packages/for-adp/for_adp-0.0.8-py3-none-any.whl/for_adp/__init__.py
@@ -0,0 +1,42 @@
+"""
+help(for_adp.m25)
+help(for_adp.m24)
+help(for_adp.m23)
+help(for_adp.m22)
+help(for_adp.m21)
+help(for_adp.m20)
+help(for_adp.m19)
+help(for_adp.m18)
+"""
+
+__version__ = '0.0.8'
+from .time import time
+from .m25 import m25
+from .m30 import m30
+from .m29 import m29
+from .m28 import m28
+from .m27 import m27
+from .m26 import m26
+from .m24 import m24
+from .m23 import m23
+from .m22 import m22
+from .m21 import m21
+from .m20 import m20
+from .m19 import m19
+from .m18 import m18
+
+
+from .m2 import m2
+from .m3 import m3
+from .m4 import m4
+from .m5 import m5
+from .m6 import m6
+from .m7 import m7
+from .m8 import m8
+from .m9 import m9
+from .m10 import m10
+from .m11 import m11
+from .m12 import m12
+from .m13 import m13
+from .m14 import m14
+
