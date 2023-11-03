333e8ae6feb6447e880c5fb8234de307234887c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:43 +0000

    Add package ab-biga/1.0/ab_biga-1.0.tar.gz

---
+++ packages/ab-biga/ab_biga-1.0.tar.gz/ab_biga-1.0/ab_biga/__init__.py
@@ -0,0 +1,2 @@
+from .GaussianDistribution import Gaussian
+from .Binomialdistribution import Binomial
---
+++ packages/ab-biga/ab_biga-1.0.tar.gz/ab_biga-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ab_biga',
+      version='1.0',
+      description='Gaussian distrbutions',
+      packages=['ab_biga'],
+      author='Abrham Gebregiorgis',
+      author_email="abrhamgg@gmail.com",
+      zip_safe=False)
