d9cfdc0d05faf23d5f69807451d46e41b5416b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:08:53 +0000

    Add package flake8-pep3101/2.1.0/flake8_pep3101-2.1.0.tar.gz

---
+++ packages/flake8-pep3101/flake8_pep3101-2.1.0.tar.gz/flake8_pep3101-2.1.0/CHANGES.rst
@@ -0,0 +1,140 @@
+.. -*- coding: utf-8 -*-
+
+Changelog
+=========
+
+2.1.0 (2023-11-03)
+------------------
+
+- Drop python 3.7 support.
+  [gforcada]
+
+- Test against python 3.12.
+  [gforcada]
+
+- Use `pyproject.toml` rather than `setup.py`.
+  [gforcada]
+
+- Switch from `setuptools` to `hatchling`.
+  [gforcada]
+
+- Switch to `main` branch.
+  [gforcada]
+
+- Use `tox` and `pre-commit` to ease project maintenance.
+  [gforcada]
+
+2.0.0 (2022-10-08)
+------------------
+
+- Update dependencies. [gforcada]
+
+- Test the code on GitHub actions. [gforcada]
+
+- Drop python 2.7 and only support python 3.7+. [gforcada]
+
+- Use linters and formatters to keep code sane and beautiful. [gforcada]
+
+- Simplify the amount of code to write a test. [gforcada]
+
+1.3.0 (2019-12-15)
+------------------
+
+- Warn about `"string %i" % 33` which was previously ignored.
+  Reported by @unformatt.
+  [gforcada]
+
+
+1.2.1 (2018-04-05)
+------------------
+
+- Fix text on the error message.
+  [icemac]
+
+1.2 (2017-12-12)
+----------------
+
+- Handle when there are numbers in either side of the modulo operator.
+  [gforcada]
+
+1.1 (2017-05-12)
+----------------
+
+- Handle variable formatting (i.e. ``my_string_variable % ('one', 'two', )``).
+  [gforcada]
+
+- Test againsta Python 3.6.
+  [gforcada]
+
+- Release universal wheels as well.
+  [gforcada]
+
+1.0 (2016-11-27)
+----------------
+- Remove tox and use plain travis matrix to test.
+  [gforcada]
+
+- Test on python 2.7, python 3.5, pypy and pypy3.
+  [gforcada]
+
+- Require flake8 3.0.
+  [gforcada]
+
+- Use ast to analyze the code.
+  The regular expression used so far was mostly a hit or miss,
+  with each corner case making it even more difficult to read.
+  The new checker with ast is simple, elegant and so much easy to read.
+  [gforcada]
+
+0.6 (2016-10-29)
+----------------
+- Handle edge case when a single or double quoted string
+  has another pair of quotes inside ('lala "lu" la') with
+  some % symbol inside as well.
+  [gforcada]
+
+0.5 (2016-10-26)
+----------------
+- Add all possible string formatters.
+  [gforcada]
+
+- Fix extension to work with flake8 > 3.0.
+  [gforcada]
+
+- Fix crash when the % symbol was the last character of a line.
+  [gforcada]
+
+0.4 (2016-07-03)
+----------------
+- Rename pep8 to pycodestyle.
+  [alefteris]
+
+- Add support for python 3.5.
+  [alefteris]
+
+- Add flake8 pypi classifier.
+  [alefteris]
+
+- Drop python 3.3 and 3.4 support (only testing it probably works just fine).
+  [gforcada]
+
+- Fix travis and coveralls to work properly with python 3.5.
+  [gforcada]
+
+0.3 (2016-03-05)
+----------------
+- Allow stdin processing, this way text editor can pass input to flake8.
+  [mjacksonw]
+
+0.2 (2015-09-16)
+----------------
+- 0.1 was a brown bag release.
+  [gforcada]
+
+0.1 (2015-09-16)
+----------------
+- Initial release
+  [gforcada]
+
+- Create the flake8 plugin per se.
+  [gforcada]
---
+++ packages/flake8-pep3101/flake8_pep3101-2.1.0.tar.gz/flake8_pep3101-2.1.0/run_tests.py
@@ -0,0 +1,176 @@
+from flake8_pep3101 import Flake8Pep3101
+from unittest import mock
+
+import ast
+import textwrap
+import unittest
+
+
+class NewTestFlake8Pep3101(unittest.TestCase):
+    def check_code(self, source, expected_codes=None):
+        """Check if the given source code generates the given flake8 errors
+
+        If `expected_codes` is a string is converted to a list,
+        if it is not given, then it is expected to **not** generate any error.
+        """
+        if isinstance(expected_codes, str):
+            expected_codes = [expected_codes]
+        elif expected_codes is None:
+            expected_codes = []
+        tree = ast.parse(textwrap.dedent(source))
+        checker = Flake8Pep3101(tree, '/home/script.py')
+        return_statements = list(checker.run())
+
+        self.assertEqual(
+            len(return_statements), len(expected_codes), f'Got {return_statements}'
+        )
+
+        for item, code in zip(return_statements, expected_codes):
+            self.assertTrue(
+                item[2].startswith(f'{code} '),
+                f'Actually got {item[2]} rather than {code}',
+            )
+
+    @mock.patch('flake8.utils.stdin_get_value')
+    def test_stdin(self, stdin_get_value):
+        source = 'a = "hello %s" % ("world")'
+        stdin_get_value.return_value = source
+        checker = Flake8Pep3101('', 'stdin')
+        ret = list(checker.run())
+        self.assertEqual(
+            len(ret),
+            1,
+        )
+
+    def test_no_old_formatter(self):
+        source = 'b = 3'
+        self.check_code(source)
+
+    def test_new_formatter(self):
+        source = 'print("hello {0:s}".format("world"))'
+        self.check_code(source)
+
+    def test_error(self):
+        source = 'print("hello %s" % ("world"))'
+        self.check_code(source, 'S001')
+
+    def test_line_number(self):
+        source = """
+        a = 2
+        open = 4
+        msg = "hello %s" % ("world")
+        """
+        tree = ast.parse(textwrap.dedent(source))
+        checker = Flake8Pep3101(tree, '/home/script.py')
+        ret = list(checker.run())
+        self.assertEqual(ret[0][0], 4)
+
+    def test_offset(self):
+        source = """
+        def bla():
+            msg = "hello %s" % ("world")
+        """
+        tree = ast.parse(textwrap.dedent(source))
+        checker = Flake8Pep3101(tree, '/home/script.py')
+        ret = list(checker.run())
+        self.assertEqual(ret[0][1], 10)
+
+    def test_s_formatter(self):
+        source = 'print("hello %s" % (\'lo\'))'
+        self.check_code(source, 'S001')
+
+    def test_multiline_formatter(self):
+        source = """
+        print("hello %s"
+        % ('world')
+        )
+        """
+        self.check_code(source, 'S001')
+
+    def test_multiline_aligned_formatter(self):
+        source = """
+        print("hello %s"
+                     % ('world')
+        )
+        """
+        self.check_code(source, 'S001')
+
+    def test_logging_module(self):
+        source = """
+        import logging
+        logger = logging.getLogger()
+        logger.info("%s world", "hello")
+        """
+        self.check_code(source)
+
+    def test_multiple_strings(self):
+        source = '"""""""1" if "%" else "2"'
+        self.check_code(source)
+
+    def test_multiple_single_quotes_strings(self):
+        source = "'''''''1' if '%' else '2'"
+        self.check_code(source)
+
+    def test_multiple_strings_with_old_formatting(self):
+        """Check that multiple quoting is handled properly.
+
+        In this case correctly detecting it.
+        """
+        source = '"""""""1" if "%" else "2%s" % "x"'
+        self.check_code(source, 'S001')
+
+    def test_percent_on_string(self):
+        """Check that multiple quoting is handled properly.
+
+        In this case no string substitution is happening.
+
+        Found in plone.app.drafts.tests
+        """
+
+        source = 'a = \'"%2B%2Badd%2B%2BMyDocument"\''
+        self.check_code(source)
+
+    def test_percent_as_last_character_on_line(self):
+        """Check that a percent symbol as the last character on a line is
+        handled properly.
+        """
+        source = """
+        a = 'my string %s %s' \
+            % \
+            ('3', '4', )
+        """
+        self.check_code(source, 'S001')
+
+    def test_variable(self):
+        source = """
+        a = 'my string %s %s'
+        a % ('3', '4', )
+        """
+        self.check_code(source, 'S001')
+
+    def test_right_hand_number_modulo(self):
+        source = """
+        var = 40
+        if var % 50 == 0:
+            print(var)
+        """
+        self.check_code(source)
+
+    def test_left_hand_number_modulo(self):
+        source = """
+        var = 40
+        if 50 % var == 0:
+            print(var)
+        """
+        self.check_code(source)
+
+    def test_right_hand_string_left_hand_number(self):
+        source = 'print("asd %s" % 1)'
+        self.check_code(source, 'S001')
+
+    def test_right_hand_string_left_hand_variable(self):
+        source = """
+        a = 44
+        print("asd %s" % a)
+        """
+        self.check_code(source, 'S001')
