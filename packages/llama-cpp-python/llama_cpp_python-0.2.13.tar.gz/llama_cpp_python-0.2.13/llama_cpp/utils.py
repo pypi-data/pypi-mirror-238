e0c3457254a1013b0d87595c7931d34fe545a64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:07:52 +0000

    Add package llama-cpp-python/0.2.13/llama_cpp_python-0.2.13.tar.gz

---
+++ packages/llama-cpp-python/llama_cpp_python-0.2.13.tar.gz/llama_cpp_python-0.2.13/llama_cpp/utils.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+
+class suppress_stdout_stderr(object):
+    # NOTE: these must be "saved" here to avoid exceptions when using
+    #       this context manager inside of a __del__ method
+    open = open
+    sys = sys
+    os = os
+
+    # Oddly enough this works better than the contextlib version
+    def __enter__(self):
+        self.outnull_file = self.open(self.os.devnull, "w")
+        self.errnull_file = self.open(self.os.devnull, "w")
+
+        self.old_stdout_fileno_undup = self.sys.stdout.fileno()
+        self.old_stderr_fileno_undup = self.sys.stderr.fileno()
+
+        self.old_stdout_fileno = self.os.dup(self.sys.stdout.fileno())
+        self.old_stderr_fileno = self.os.dup(self.sys.stderr.fileno())
+
+        self.old_stdout = self.sys.stdout
+        self.old_stderr = self.sys.stderr
+
+        self.os.dup2(self.outnull_file.fileno(), self.old_stdout_fileno_undup)
+        self.os.dup2(self.errnull_file.fileno(), self.old_stderr_fileno_undup)
+
+        self.sys.stdout = self.outnull_file
+        self.sys.stderr = self.errnull_file
+        return self
+
+    def __exit__(self, *_):
+        self.sys.stdout = self.old_stdout
+        self.sys.stderr = self.old_stderr
+
+        self.os.dup2(self.old_stdout_fileno, self.old_stdout_fileno_undup)
+        self.os.dup2(self.old_stderr_fileno, self.old_stderr_fileno_undup)
+
+        self.os.close(self.old_stdout_fileno)
+        self.os.close(self.old_stderr_fileno)
+
+        self.outnull_file.close()
+        self.errnull_file.close()
