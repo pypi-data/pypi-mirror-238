0482034b71a115df1ef1436890ee66f5ac040351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:07:35 +0000

    Add package kanbanize-sdk/0.2.3/kanbanize_sdk-0.2.3-py3-none-any.whl

---
+++ packages/kanbanize-sdk/kanbanize_sdk-0.2.3-py3-none-any.whl/kanbanize_sdk/__init__.py
@@ -0,0 +1,18 @@
+from .client import Kanbanize
+from .dataclasses import (
+    UsersListParams,
+    UsersInsertBody,
+    UsersUpdateBody,
+    TeamsInsertBody,
+    TeamsUpdateBody,
+    TeamsListParams,
+    WorkspacesListParams,
+    WorkspacesInsertBody,
+    WorkspacesUpdateBody,
+    WorkspaceHistoryListParams,
+    BoardsInsertBody,
+    BoardsUpdateBody,
+    BoardsListParams,
+    BoardSettingsUpdateBody,
+    BoardHistoryListParams,
+)
