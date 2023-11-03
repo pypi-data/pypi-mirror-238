11c06203e32aaa6eceabde775d1091e944a0abd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:05:43 +0000

    Add package MorseCodePy/2.1.6/MorseCodePy-2.1.6.tar.gz

---
+++ packages/MorseCodePy/MorseCodePy-2.1.6.tar.gz/MorseCodePy-2.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md', 'r') as readme:
+    documentation = readme.read()
+
+setup(name='MorseCodePy',
+      packages=['MorseCodePy'],
+      requires=['pygame'],
+      package_data={'MorseCodePy': ['sounds/*.wav']},
+      include_package_data=True,
+      version='2.1.6',
+      author='CrazyFlyKite',
+      author_email='karpenkoartem2846@gmail.com',
+      url='https://github.com/CrazyFlyKite/MorseCodePy/',
+      description='Easily and correctly encode, decode and play the Morse code',
+      long_description=documentation,
+      long_description_content_type='text/markdown'
+      )
