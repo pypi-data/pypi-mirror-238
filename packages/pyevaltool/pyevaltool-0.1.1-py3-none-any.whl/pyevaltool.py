61a717ee79598b3db607c28ca079790d1b6de28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:05:09 +0000

    Add package pyevaltool/0.1.1/pyevaltool-0.1.1-py3-none-any.whl

---
+++ packages/pyevaltool/pyevaltool-0.1.1-py3-none-any.whl/pyevaltool.py
@@ -0,0 +1,58 @@
+import requests
+import pandas as pd
+
+def gpt_test(frontend_url,token,chat_completion_format, input_data,output_file = 'answered_prompt.csv', ):
+#UAT
+    url = frontend_url
+    token =   token
+    try :
+        try:
+            df  =  pd.read_excel(input_data)
+        except:
+            df  =  pd.read_csv(input_data)
+    except:
+        print("Input data file format is not correct. Please make sure that data is in excel or csv format.")
+
+    try:
+        questions = list(df['Prompt'])
+    except:
+        print("Please make sure that questions/user input attribute/collumn name is Prompt")
+
+    output_df = pd.DataFrame()
+    Question, Answer = [], []
+    n = 1
+    try:
+        for question in questions:
+            url = url
+            data = chat_completion_format
+            data['messages'][0]["content"] = question
+            token = token
+            try:
+                try:
+                    headers = {     
+                        "Content-Type": "application/json",     
+                        "Ocp-Apim-Subscription-Key": token}
+                except:
+                    headers = {
+                        'Authorization': f'Bearer {token}',
+                        'Content-Type': 'application/json'}
+            except:
+                print("It is appearing you are using other than Apim-Subscription-Key or Bearer token. Please use any of these")
+            try:
+                response = requests.post(url, json=data, headers=headers)
+                if response.status_code ==401:
+                    print("update the token")
+                    break
+                if response.status_code == 200:
+                    Question.append(question)
+                    Answer.append(response.json()['choices'][0]['message']['content'])
+                    print(f'Done {n}/{len(df)}', end="\r")
+                    n += 1
+            except:
+                print("chat_completion_format should have question at chat_completion_format['messages'][0]['content']. Please check documentation for more information.")
+        output_df['Question'] = Question
+        output_df['Answer']= Answer
+        output_df.to_csv(output_file)
+        print("Job Completed !")
+    except:
+        print("Saving file failed")
