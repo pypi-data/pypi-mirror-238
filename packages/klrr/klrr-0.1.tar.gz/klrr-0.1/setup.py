26ea7026053986ec5dc360f06719ac271958e726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:07:30 +0000

    Add package klrr/0.1/klrr-0.1.tar.gz

---
+++ packages/klrr/klrr-0.1.tar.gz/klrr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='klrr',
+version='0.1',
+description='The Goal Of This Library Is To Help Students to learn programming',
+packages=['klrr'],
+zip_safe=False)
