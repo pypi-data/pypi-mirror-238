4f6e3502aa38440c6399a21aeb0d48fb99a721ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:07:30 +0000

    Add package dicognito/0.16.0/dicognito-0.16.0.tar.gz

---
+++ packages/dicognito/dicognito-0.16.0.tar.gz/dicognito-0.16.0/tests/test_readme.py
@@ -0,0 +1,28 @@
+from itertools import dropwhile, islice, takewhile
+from os.path import abspath, join
+from typing import Iterator
+
+from dicognito.anonymizer import Anonymizer
+
+
+def test_anonymized_attributes_described():
+    def before_description(line: str) -> bool:
+        return not line.startswith("## Exactly what does dicognito do?")
+
+    def still_description(line: str) -> bool:
+        return not line.startswith("#") and not line.startswith("----")
+
+    def drop_header(iterable: Iterator[str]) -> Iterator[str]:
+        return islice(iterable, 1, None)
+
+    data_root = abspath(join(__file__, "..", ".."))
+    with open(join(data_root, "README.md")) as readme:
+        anonymizer = Anonymizer()
+
+        readme_description = "".join(
+            takewhile(still_description, drop_header(dropwhile(before_description, readme))),
+        ).strip()
+        anonymizer_description = anonymizer.describe_actions()
+
+    print(anonymizer_description)
+    assert readme_description == anonymizer_description
