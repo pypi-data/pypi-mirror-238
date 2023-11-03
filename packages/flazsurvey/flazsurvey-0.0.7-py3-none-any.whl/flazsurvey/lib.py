03bad182a0af8c28554b5d44d1262d65071a6a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:06:30 +0000

    Add package flazsurvey/0.0.7/flazsurvey-0.0.7-py3-none-any.whl

---
+++ packages/flazsurvey/flazsurvey-0.0.7-py3-none-any.whl/flazsurvey/lib.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+import requests
+from io import StringIO
+import json
+
+@dataclass
+class ABQuestion:
+  id: int
+  question: str
+  a: str = "a"
+  b: str = "b"
+
+@dataclass
+class TextInputQuestion:
+  id: int
+  question: str
+  pattern: str = ""
+
+@dataclass
+class Form:
+  id: str
+  token: str
+  server: str = "http://localhost:8080"
+
+  def csv(self) -> str:
+    resp = requests.get(f"{self.server}/data?id={self.id}&csv", headers={"Authorization": f"Bearer {self.token}"})
+    csv = resp.content.decode("utf-8")
+    return StringIO(csv)
+  
+  def questions(self) -> list[ABQuestion]:
+    resp = requests.get(f"{self.server}/form?id={self.id}", headers={"Authorization": f"Bearer {self.token}"})
+    raw = resp.content.decode("utf-8")
+    data = json.loads(raw)
+
+    questions = []
+
+    for q in data["questions"]:
+      match q["type"]:
+        case "ab-question":
+          config = q["config"]
+          questions.append(ABQuestion(q["id"], config["question"], config["a"], config["b"]))
+        case "text":
+          config = q["config"]
+          questions.append(TextInputQuestion(q["id"], config["question"], config["pattern"]))
+        case _:
+          pass
+    return questions
