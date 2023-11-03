4a135a6ec4236c86d514972613f827aa7f0191dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:06:51 +0000

    Add package scs-core/3.2.4/scs-core-3.2.4.tar.gz

---
+++ packages/scs-core/scs-core-3.2.4.tar.gz/scs-core-3.2.4/src/scs_core/sys/command.py
@@ -0,0 +1,103 @@
+"""
+Created on 15 Sep 2021
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+
+https://pymotw.com/2/subprocess/
+"""
+
+import sys
+
+from subprocess import Popen, PIPE
+
+from scs_core.sys.logging import Logging
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class Command(object):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, verbose, on_abort=None):
+        """
+        Constructor
+        """
+        self.__verbose = verbose                            # bool
+        self.__on_abort = on_abort                          # function
+
+        self.__logger = Logging.getLogger()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def s(self, cmd_args, wait=True, no_verbose=False, abort_on_fail=True):
+        tokens = self.__cmd(cmd_args, no_verbose=no_verbose)
+        p = Popen(' '.join(tokens), shell=True)
+
+        return self.__process(p, wait=wait, abort_on_fail=abort_on_fail)
+
+
+    def o(self, cmd_args, wait=False, abort_on_fail=True):
+        tokens = self.__cmd(cmd_args)
+        p = Popen(tokens, stdout=PIPE)
+
+        return self.__process(p, wait=wait, abort_on_fail=abort_on_fail)
+
+
+    def io(self, p, cmd_args, wait=False, abort_on_fail=True):
+        tokens = self.__cmd(cmd_args)
+        p = Popen(tokens, stdin=p.stdout, stdout=PIPE)
+
+        return self.__process(p, wait=wait, abort_on_fail=abort_on_fail)
+
+
+    def i(self, p, cmd_args, wait=True, abort_on_fail=True):
+        tokens = self.__cmd(cmd_args)
+        p = Popen(tokens, stdin=p.stdout, stdout=sys.stderr)
+
+        return self.__process(p, wait=wait, abort_on_fail=abort_on_fail)
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __cmd(self, cmd_args, no_verbose=False):
+        tokens = [str(cmd_arg) for cmd_arg in cmd_args if cmd_arg is not None]
+
+        if self.__verbose and not no_verbose:
+            tokens = tokens[:1] + ['-v'] + tokens[1:]
+
+        self.__logger.info(' '.join(tokens))
+
+        return tokens
+
+
+    def __process(self, p, wait=False, abort_on_fail=True):
+        if wait:
+            p.wait()
+
+        if abort_on_fail and (p.returncode is not None and p.returncode != 0):
+            self.__logger.error('ABORTED.')
+
+            if self.__on_abort:
+                self.__on_abort()
+
+            exit(p.returncode)
+
+        return p
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def verbose(self):
+        return self.__verbose
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        return "Command:{verbose:%s, on_abort:%s}" % (self.verbose, self.__on_abort)
