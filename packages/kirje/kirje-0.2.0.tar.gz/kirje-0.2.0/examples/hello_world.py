cb70413568b151866720daa9e3fbcc65ce10968d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 06:07:13 +0000

    Add package kirje/0.2.0/kirje-0.2.0.tar.gz

---
+++ packages/kirje/kirje-0.2.0.tar.gz/kirje-0.2.0/examples/hello_world.py
@@ -0,0 +1,28 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+from src.kirje import Kirje, KirjeDetails
+
+details = KirjeDetails(
+    headers={
+        'ID': 2,
+        'Title': 'Hello world',
+        'Created': '2023-10-27 14:18:14',
+        'Modified': '2023-10-27 14:18:25'
+    },
+    content="Hello\nworld."
+)
+
+message = Kirje(details)
+
+styles = ["Default", "Rounded", "Doubled", "Streamlined"]
+
+for style in styles:
+    print(f"{style} style letter:")
+    message.display(style.lower())
+    print("")
+
+print("Headers separated with dash:")
+details.header_separation = ' - '
+message.display("streamlined")
