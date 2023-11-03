96ff2d048e4c3f978d10998594e2a32d442aa47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:45 +0000

    Add package ellipsis/3.1.29/ellipsis-3.1.29-py3-none-any.whl

---
+++ packages/ellipsis/ellipsis-3.1.29-py3-none-any.whl/ellipsis/__init__.py
@@ -0,0 +1,11 @@
+#python3 setup.py sdist bdist_wheel
+#twine upload --repository pypi dist/*
+
+from ellipsis import account
+from ellipsis import user
+from ellipsis import path
+from ellipsis import view
+from ellipsis import util
+
+__version__ = '3.1.29'
+pypi-AgEIcHlwaS5vcmcCJGZjNTJmZWZiLTM1YTgtNDQwMC05MjQxLWQ0YTQ3MDMyN2VhZQACKlszLCI0Y2Y3MWVmNi04M2RkLTQ3ZTctYTRhNi1lNjAyZWU0MTQzMGQiXQAABiCaf0h22vH6GHl7MGxOBcQH4Ol9RN9mavWVUm5qS4UwfQ
