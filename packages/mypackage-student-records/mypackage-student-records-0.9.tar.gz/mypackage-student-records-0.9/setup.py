4f8a9ad3051558638680d46d6a33b8cddd5351c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:06:53 +0000

    Add package mypackage-student-records/0.9/mypackage-student-records-0.9.tar.gz

---
+++ packages/mypackage-student-records/mypackage-student-records-0.9.tar.gz/mypackage-student-records-0.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="mypackage-student-records",
+    version="0.9",
+    description="Progetto di Produzione del Software",
+    author="SMJ",
+    author_email="s.madaghiele@campus.unimib.it",
+    url="https://gitlab.com/MarcecaManu/2023_assignment1_student_records",
+    packages=["src"],
+    install_requires=[
+        "requirements.txt",
+    ],
+)
