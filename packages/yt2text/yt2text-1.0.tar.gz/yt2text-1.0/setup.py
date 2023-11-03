c79c2c0f50058505c918528345b0c5127e6de173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:06:29 +0000

    Add package yt2text/1.0/yt2text-1.0.tar.gz

---
+++ packages/yt2text/yt2text-1.0.tar.gz/yt2text-1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yt2text',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy','ffmpeg','whisper','ffmpeg'
+    ],
+    author='Atahan Uz',
+    author_email='atahanuz23@gmail.com',
+    description="Extract text from a YouTube video in a single command, using OpenAi's Whisper speech recognition model",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/atahanuz/yt2text',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
