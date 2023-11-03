a61dd84752a33c8df6a3318defe41a868fe76eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:56 +0000

    Add package interpax/0.2.1/interpax-0.2.1.tar.gz

---
+++ packages/interpax/interpax-0.2.1.tar.gz/interpax-0.2.1/interpax/__init__.py
@@ -0,0 +1,14 @@
+"""interpax: interpolation and function approximation with JAX."""
+
+from . import _version
+from ._fourier import fft_interp1d, fft_interp2d
+from ._spline import (
+    Interpolator1D,
+    Interpolator2D,
+    Interpolator3D,
+    interp1d,
+    interp2d,
+    interp3d,
+)
+
+__version__ = _version.get_versions()["version"]
