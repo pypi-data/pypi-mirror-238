7090c34d27c1bc6913ea59eea43e18aee51d6832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:05:09 +0000

    Add package django-jsnlog/0.3.0/django-jsnlog-0.3.0.tar.gz

---
+++ packages/django-jsnlog/django-jsnlog-0.3.0.tar.gz/django-jsnlog-0.3.0/HISTORY.rst
@@ -0,0 +1,21 @@
+.. :changelog:
+
+History
+-------
+
+0.3.0 (2023-11-02)
+++++++++++++++++++
+
+* change status code from 200 to 204
+
+0.2.0 (2021-04-14)
+++++++++++++++++++
+
+* Second release on PyPI.
+* clean up the code
+
+
+0.1.0 (2021-04-14)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/django-jsnlog/django-jsnlog-0.3.0.tar.gz/django-jsnlog-0.3.0/jsnlog/apps.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8
+import logging
+
+from django.apps import AppConfig
+
+
+class DefaultJSNLogConfig(AppConfig):
+    logger = None
+    name = 'jsnlog'
+    verbose_name = 'JSNLog'
+
+
+class LoggerJSNLogConfig(DefaultJSNLogConfig):
+    def ready(self):
+        self.logger = logging.getLogger(self.name)
