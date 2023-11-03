f407b20c6ec20010e0be0a187083323744a84a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:56 +0000

    Add package qwak-core/0.1.61/qwak_core-0.1.61-py3-none-any.whl

---
+++ packages/qwak-core/qwak_core-0.1.61-py3-none-any.whl/qwak/inner/build_logic/interface/build_logger_interface.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class BuildLogger(ABC):
+    verbose: int
+    json_logs: bool
+
+    @abstractmethod
+    def error(self, line: str) -> None:
+        pass
+
+    @abstractmethod
+    def exception(self, line: str, e: BaseException) -> None:
+        pass
+
+    @abstractmethod
+    def warning(self, line: str) -> None:
+        pass
+
+    @abstractmethod
+    def info(self, line: str) -> None:
+        pass
+
+    @abstractmethod
+    def debug(self, line: str) -> None:
+        pass
+
+    @abstractmethod
+    def spinner_text(self, line: str) -> None:
+        pass
+
+    @abstractmethod
+    def set_spinner(self, *args, **kwargs) -> None:
+        pass
