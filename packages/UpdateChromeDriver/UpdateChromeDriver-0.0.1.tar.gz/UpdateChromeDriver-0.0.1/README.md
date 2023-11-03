30693489037e649363f8624546eda79490783f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:06:48 +0000

    Add package UpdateChromeDriver/0.0.1/UpdateChromeDriver-0.0.1.tar.gz

---
+++ packages/UpdateChromeDriver/UpdateChromeDriver-0.0.1.tar.gz/UpdateChromeDriver-0.0.1/README.md
@@ -0,0 +1,14 @@
+用法示例：
+
+```python
+from UpdateChromeDriver.UpdateChromeDriver import *
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option(
+    'excludeSwitches', ['enable-logging', 'enable-automation'])
+browser = getChromeBrowser(options)
+browser.get("https://www.baidu.com/")
+time.sleep(2)
+```
+驱动会从下载到~\.cache\selenium
+下载来源：https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json
---
+++ packages/UpdateChromeDriver/UpdateChromeDriver-0.0.1.tar.gz/UpdateChromeDriver-0.0.1/setup.py
@@ -0,0 +1,32 @@
+"""
+CSDN主页：https://blog.csdn.net/as604049322
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="u8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UpdateChromeDriver",
+    version="0.0.1",
+    author="小小明",
+    author_email="604049322@qq.com",
+    description="针对115以上版本的谷歌游览器自动升级selenium驱动。",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="GPLv3",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'requests', 'tqdm', "selenium > 4.0.0"
+    ],
+    platforms='any',
+    zip_safe=True,
+    python_requires=">=3.6"
+)
