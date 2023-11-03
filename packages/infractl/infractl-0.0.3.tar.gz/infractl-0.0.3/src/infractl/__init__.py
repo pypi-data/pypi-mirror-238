0521c89dff21f31cd542035932cd73277918db72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 06:07:27 +0000

    Add package infractl/0.0.3/infractl-0.0.3.tar.gz

---
+++ packages/infractl/infractl-0.0.3.tar.gz/infractl-0.0.3/src/infractl/__init__.py
@@ -0,0 +1,16 @@
+"""ICL Python API."""
+
+
+from infractl.api.deploy import deploy
+from infractl.api.infrastructure import infrastructure
+from infractl.api.program import program
+from infractl.api.run import run
+from infractl.api.runtime import runtime
+
+__all__ = [
+    'infrastructure',
+    'runtime',
+    'program',
+    'deploy',
+    'run',
+]
