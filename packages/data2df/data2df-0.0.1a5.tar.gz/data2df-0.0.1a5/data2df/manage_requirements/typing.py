7657c85cec1aa4e9281674f1c138d6f7b528f73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:06:34 +0000

    Add package data2df/0.0.1a5/data2df-0.0.1a5.tar.gz

---
+++ packages/data2df/data2df-0.0.1a5.tar.gz/data2df-0.0.1a5/data2df/manage_requirements/typing.py
@@ -0,0 +1,8 @@
+from typing import TypeVar
+
+PandasDataFrame = TypeVar("pd.DataFrame")
+PySparkDataFrame = TypeVar("pyspark.sql.DataFrame")
+DaskDataFrame = TypeVar("dask.dataframe.DataFrame")
+PolarsDataFrame = TypeVar("polars.DataFrame")
+
+SparkSession = TypeVar("pyspark.sql.SparkSession")
