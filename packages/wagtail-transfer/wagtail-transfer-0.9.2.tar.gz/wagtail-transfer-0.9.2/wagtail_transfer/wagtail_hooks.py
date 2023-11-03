3c02b26361d2678fbddb2669e4362abcfb80dbf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:59 +0000

    Add package wagtail-transfer/0.9.2/wagtail-transfer-0.9.2.tar.gz

---
+++ packages/wagtail-transfer/wagtail-transfer-0.9.2.tar.gz/wagtail-transfer-0.9.2/wagtail_transfer/wagtail_hooks.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.contrib.auth.models import Permission
+from django.templatetags.static import static
+from django.urls import include, re_path, reverse
+from wagtail import hooks
+from wagtail.admin.menu import MenuItem
+
+from . import admin_urls
+
+
+@hooks.register('register_admin_urls')
+def register_admin_urls():
+    return [
+        re_path(r'^wagtail-transfer/', include(admin_urls, namespace='wagtail_transfer_admin')),
+    ]
+
+
+class WagtailTransferMenuItem(MenuItem):
+    def is_shown(self, request):
+        return all(
+            [
+                bool(getattr(settings, "WAGTAILTRANSFER_SOURCES", None)),
+                request.user.has_perm("wagtail_transfer.wagtailtransfer_can_import"),
+            ]
+        )
+
+
+@hooks.register('register_admin_menu_item')
+def register_admin_menu_item():
+    return WagtailTransferMenuItem('Import', reverse('wagtail_transfer_admin:choose_page'), classnames='icon icon-doc-empty-inverse', order=10000)
+
+
+@hooks.register("register_permissions")
+def register_wagtail_transfer_permission():
+    return Permission.objects.filter(
+        content_type__app_label="wagtail_transfer",
+        codename="wagtailtransfer_can_import",
+    )
