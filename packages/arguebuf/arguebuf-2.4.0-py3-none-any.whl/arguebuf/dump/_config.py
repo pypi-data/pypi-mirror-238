c77b94d16696e42155d24324a87cd441d099e5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:07:00 +0000

    Add package arguebuf/2.4.0/arguebuf-2.4.0-py3-none-any.whl

---
+++ packages/arguebuf/arguebuf-2.4.0-py3-none-any.whl/arguebuf/dump/_config.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+
+__all__ = ("Format", "Config")
+
+
+class Format(str, Enum):
+    ARGUEBUF = "arguebuf"
+    AIF = "aif"
+    XAIF = "xaif"
+
+
+@dataclass
+class Config:
+    format: Format = Format.ARGUEBUF
+    prettify: bool = True
+
+
+DefaultConfig = Config()
