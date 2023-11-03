931267ecd486eaadd9b91936ec768fb475950a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:06:41 +0000

    Add package animl/1.1.4/animl-1.1.4.tar.gz

---
+++ packages/animl/animl-1.1.4.tar.gz/animl-1.1.4/README.md
@@ -0,0 +1,119 @@
+# animl-py
+AniML comprises a variety of machine learning tools for analyzing ecological data. This Python package includes a set of functions to classify subjects within camera trap field data and can handle both images and videos. 
+This package is also available in R: [animl](https://github.com/conservationtechlab/animl)
+
+Table of Contents
+1. Installation
+2. [Usage](#usage)
+
+## Installation Instructions
+
+It is recommended that you set up a conda environment for using animl.
+See **Dependencies** below for more detail. You will have to activate the conda environment first each
+time you want to run AniML from a new terminal.
+
+### From GitHub
+```
+git clone https://github.com/conservationtechlab/animl-py.git
+cd animl-py
+conda env create --file environment.yml
+conda activate animl-gpu
+pip install -e .
+```
+
+### From PyPi
+With NVIDIA GPU
+```
+conda create -n animl-gpu python=3.7
+conda activate animl-gpu
+conda install cudatoolkit=11.3.1 cudnn=8.2.1
+pip install animl
+```
+CPU only
+```
+conda create -n animl-cpu python=3.7
+conda activate animl
+pip install animl
+```
+
+### Dependencies
+We recommend running AniML on GPU-enabled hardware. **If using an NVIDIA GPU, ensure driviers, cuda-toolkit and cudnn are installed.
+The /models/ and /utils/ modules are from the YOLOv5 repository.  https://github.com/ultralytics/yolov5
+
+Python Package Dependencies
+- pandas = 1.3.5
+- tensorflow = 2.6
+- torch = 1.13.1
+- torchvision = 0.14.1
+- numpy = 1.19.5
+- cudatoolkit = 11.3.1 **
+- cudnn = 8.2.1 **
+
+A full list of dependencies can be found in environment.yml
+
+### Verify Install 
+We recommend you download the [examples](https://github.com/conservationtechlab/animl-py/blob/main/examples/Southwest.zip) folder within this repository.
+Download and unarchive the zip folder. Then with the conda environment active:
+```
+python3 -m animl /path/to/example/folder
+```
+This should create an Animl-Directory subfolder within
+the example folder.
+
+## Usage
+
+### Inference
+The functionality of animl can be parcelated into its individual functions to suit your data and scripting needs.
+The sandbox.ipynb notebook has all of these steps available for further exploration.
+
+1. It is recommended that you use the animl working directory for storing intermediate steps.
+```python
+from animl import file_management
+workingdir = file_management.WorkingDirectory(imagedir)
+```
+
+2. Build the file manifest of your given directory. This will find both images and videos.
+```python
+files = file_management.build_file_manifest('/path/to/images', out_file = workingdir.filemanifest)
+```
+
+3. If there are videos, extract individual frames for processing.
+   Select either the number of frames or fps using the argumments.
+   The other option can be set to None or removed.
+```python
+from animl import video_processing
+allframes = video_processing.images_from_videos(files, out_dir=workingdir.vidfdir,
+                                                out_file=workingdir.imageframes,
+                                                parallel=True, frames=3, fps=None)
+```
+4. Pass all images into MegaDetector. We recommend [MDv5a](https://github.com/agentmorris/MegaDetector/releases/download/v5.0/md_v5a.0.0.pt)
+   parseMD will merge detections with the original file manifest, if provided.
+
+```python
+from animl import detectMD, megadetector, parse_results
+detector = megadetector.MegaDetector('/path/to/mdmodel.pt')
+mdresults = detectMD.detect_MD_batch(detector, allframes["Frame"], quiet=True)
+mdres = parse_results.from_MD(mdresults, manifest=allframes, out_file = workingdir.mdresults)
+```
+5. For speed and efficiency, extract the empty/human/vehicle detections before classification.
+```python
+from animl import split
+animals = split.getAnimals(mdres)
+empty = split.getEmpty(mdres)
+```
+6. Classify using the appropriate species model. Merge the output with the rest of the detections
+   if desired.
+```python
+from animl import classify, parse_results
+classifier = classify.load_classifier('/path/to/classifier/')
+predresults = classify.predict_species(animals, classifier, batch = 4)
+animals = parse_results.from_classifier(animals, predresults, '/path/to/classlist.txt',
+                                         out_file=workingdir.predictions)
+manifest = pd.concat([animals,empty])
+```
+
+
+---
+### Training
+
+Training workflows are available in the repo but still under development.
---
+++ packages/animl/animl-1.1.4.tar.gz/animl-1.1.4/src/animl/detectMD.py
@@ -0,0 +1,150 @@
+# modified from CameraTraps/MegaDetector
+from tqdm import tqdm
+import json
+import os
+import pandas as pd
+from shutil import copyfile
+from PIL import Image
+from .file_management import build_file_manifest
+
+
+def process_image(im_file, detector, confidence_threshold, image=None,
+                  quiet=False, image_size=None, skip_image_resizing=False):
+    """
+    From AgentMorris/MegaDetector
+    Runs MegaDetector on a single image file.
+
+    Args
+    - im_file: str, path to image file
+    - detector: loaded model
+    - confidence_threshold: float, only detections above this threshold are returned
+    - image: previously-loaded image, if available
+    - skip_image_resizing: whether to skip internal image resizing and rely on external resizing
+
+    Returns:
+    - result: dict representing detections on one image
+        see the 'images' key in
+        https://github.com/agentmorris/MegaDetector/tree/master/api/batch_processing#batch-processing-api-output-format
+    """
+    if not quiet:
+        print('Processing image {}'.format(im_file))
+
+    if image is None:
+        try:
+            image = Image.open(im_file).convert(mode='RGB')
+            image.load()
+        except Exception as e:
+            if not quiet:
+                print('Image {} cannot be loaded. Exception: {}'.format(im_file, e))
+            result = {
+                'file': im_file,
+                'failure': 'Failure image access'
+            }
+            return result
+
+    try:
+        result = detector.generate_detections_one_image(image, im_file,
+                                                        confidence_threshold=confidence_threshold,
+                                                        image_size=image_size,
+                                                        skip_image_resizing=skip_image_resizing)
+    except Exception as e:
+        if not quiet:
+            print('Image {} cannot be processed. Exception: {}'.format(im_file, e))
+        result = {
+            'file': im_file,
+            'failure': 'Failure inference'
+        }
+        return result
+
+    return result
+
+
+def detect_MD_batch(detector, image_file_names, checkpoint_path=None, checkpoint_frequency=-1,
+                    confidence_threshold=0.005, results=None, quiet=False, image_size=None):
+    """
+    Args
+        - detector: preloaded md model
+        - image_file_names: list of strings (image filenames), a single image filename,
+                            a folder to recursively search for images in, or a .json file
+                            containing a list of images.
+        - checkpoint_path: str, path to JSON checkpoint file
+        - confidence_threshold: float, only detections above this threshold are returned
+        - checkpoint_frequency: int, write results to JSON checkpoint file every N images
+        - results: list of dict, existing results loaded from checkpoint
+
+    Returns
+        - results: list of dict, each dict represents detections on one image
+    """
+    if confidence_threshold is None:
+        confidence_threshold = 0.005  # Defult from MegaDetector
+
+    if checkpoint_frequency is None:
+        checkpoint_frequency = -1
+
+    # Handle the case where image_file_names is not yet actually a list
+    if isinstance(image_file_names, str):
+        # Find the images to score; images can be a directory, may need to recurse
+        if os.path.isdir(image_file_names):
+            image_dir = image_file_names
+            image_file_names = build_file_manifest(image_dir, True)
+            print('{} image files found in folder {}'.format(len(image_file_names), image_dir))
+
+        # A json list of image paths
+        elif os.path.isfile(image_file_names) and image_file_names.endswith('.json'):
+            list_file = image_file_names
+            with open(list_file) as f:
+                image_file_names = json.load(f)
+            print('Loaded {} image filenames from list file {}'.format(len(image_file_names), list_file))
+
+        # Dataframe, expected input
+        elif isinstance(image_file_names, pd.Series):
+            pass
+
+        # A single image file
+        elif os.path.isfile(image_file_names):
+            image_file_names = [image_file_names]
+        else:
+            raise ValueError('image_file_names is a string, but is not a directory, a json ' +
+                             'list (.json), or an image file (png/jpg/jpeg/gif)')
+
+    if results is None:
+        results = []
+
+    already_processed = set([i['Frame'] for i in results])
+
+    count = 0
+    for im_file in tqdm(image_file_names):
+        # Will not add additional entries not in the starter checkpoint
+        if im_file in already_processed:
+            if not quiet:
+                print('Bypassing image {}'.format(im_file))
+            continue
+
+        count += 1
+
+        result = process_image(im_file, detector,
+                               confidence_threshold, quiet=quiet,
+                               image_size=image_size)
+        results.append(result)
+
+        # Write a checkpoint if necessary
+        if checkpoint_frequency != -1 and count % checkpoint_frequency == 0:
+            print('Writing a new checkpoint after having processed {} images since last restart'.format(count))
+
+            assert checkpoint_path is not None
+            # Back up any previous checkpoints, to protect against crashes while we're writing
+            # the checkpoint file.
+            checkpoint_tmp_path = None
+            if os.path.isfile(checkpoint_path):
+                checkpoint_tmp_path = checkpoint_path + '_tmp'
+                copyfile(checkpoint_path, checkpoint_tmp_path)
+
+            # Write the new checkpoint
+            with open(checkpoint_path, 'w') as f:
+                json.dump({'images': results}, f, indent=1)
+
+            # Remove the backup checkpoint if it exists
+            if checkpoint_tmp_path is not None:
+                os.remove(checkpoint_tmp_path)
+
+    return results
