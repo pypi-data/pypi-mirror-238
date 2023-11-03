0b58bc987a735050db4dae37f8caec20b5e9c860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 06:08:16 +0000

    Add package perftracker/1.0.0/perftracker-1.0.0.tar.gz

---
+++ packages/perftracker/perftracker-1.0.0.tar.gz/perftracker-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="perftracker",
+    version="1.0.0",
+    url="https://github.com/vertyco/perftracker",
+    author="Alex Goble",
+    author_email="alex.c.goble@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="A Python performance tracking package",
+    packages=find_packages(),
+    install_requires=["pydantic"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
