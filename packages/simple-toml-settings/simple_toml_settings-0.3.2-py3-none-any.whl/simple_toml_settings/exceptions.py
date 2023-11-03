598bb85981116822a7be58a2bc24bc7c4d8c3d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 06:07:11 +0000

    Add package simple-toml-settings/0.3.2/simple_toml_settings-0.3.2-py3-none-any.whl

---
+++ packages/simple-toml-settings/simple_toml_settings-0.3.2-py3-none-any.whl/simple_toml_settings/exceptions.py
@@ -0,0 +1,17 @@
+"""Define exceptions for the simple_toml_settings package."""
+
+
+class SettingsError(Exception):
+    """Base exception for settings errors."""
+
+
+class SettingsNotFoundError(SettingsError):
+    """The Settings file has not been found.
+
+    This will be raised if the settings file is not found and auto_create is
+    False.
+    """
+
+
+# temporary alias for backwards compatibility
+SettingsNotFound = SettingsNotFoundError
