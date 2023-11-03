a9f67c71cec636d930611a4286242600941dea6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:05:43 +0000

    Add package vladvlad/0.1/vladvlad-0.1.tar.gz

---
+++ packages/vladvlad/vladvlad-0.1.tar.gz/vladvlad-0.1/README.md
@@ -0,0 +1,9 @@
+# Age
+
+Age is a Python package for get Vlad's age
+
+## Installation
+
+You can install Age using pip:
+
+### pip install vlad
---
+++ packages/vladvlad/vladvlad-0.1.tar.gz/vladvlad-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vladvlad',
+    version='0.1',
+    description='My age info',
+    author='Vladyslav Khimich',
+    author_email='khimich.vladyslav@gmail.com',
+    packages=find_packages(),
+)
