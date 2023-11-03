a6e812b623161027ee918aa26e079a6c6da1f201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:06:56 +0000

    Add package kanbanize-sdk/0.2.2/kanbanize_sdk-0.2.2-py3-none-any.whl

---
+++ packages/kanbanize-sdk/kanbanize_sdk-0.2.2-py3-none-any.whl/kanbanize_sdk/__init__.py
@@ -0,0 +1,14 @@
+from .client import Kanbanize
+from .dataclasses import (
+    UsersListParams,
+    UsersInsertBody,
+    UsersUpdateBody,
+    TeamsInsertBody,
+    TeamsUpdateBody,
+    TeamsListParams,
+    BoardsInsertBody,
+    BoardsUpdateBody,
+    BoardsListParams,
+    BoardSettingsUpdateBody,
+    BoardHistoryListParams,
+)
---
+++ packages/kanbanize-sdk/kanbanize_sdk-0.2.2-py3-none-any.whl/kanbanize_sdk/client.py
@@ -0,0 +1,34 @@
+from .wrapper import KanbanizeSession
+from .users import Users
+from .teams import Teams
+from .boards import Boards
+from .board_settings import BoardSettings
+from .board_structure import BoardStructure
+from .board_structure_revisions import BoardStructureRevisions
+from .board_history import BoardHistory
+
+
+class Kanbanize:
+    def __init__(self, options):
+        self.service = KanbanizeSession(options)
+
+    def users(self):
+        return Users(self.service)
+
+    def teams(self):
+        return Teams(self.service)
+
+    def boards(self):
+        return Boards(self.service)
+
+    def board_settings(self):
+        return BoardSettings(self.service)
+
+    def board_structure(self):
+        return BoardStructure(self.service)
+
+    def board_structure_revisions(self):
+        return BoardStructureRevisions(self.service)
+
+    def board_history(self):
+        return BoardHistory(self.service)
