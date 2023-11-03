7e00d36752e9b97474ee875a27747a25e91587c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:07:06 +0000

    Add package synapse-s3-storage-provider-delete-local/1.2.1/synapse-s3-storage-provider-delete-local-1.2.1.tar.gz

---
+++ packages/synapse-s3-storage-provider-delete-local/synapse-s3-storage-provider-delete-local-1.2.1.tar.gz/synapse-s3-storage-provider-delete-local-1.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+__version__ = "1.2.1"
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="synapse-s3-storage-provider-delete-local",
+    version=__version__,
+    zip_safe=False,
+    author="daniel mazurkin",
+    description="A storage provider which can fetch and store media in Amazon S3 with local delete after upload",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matrix-org/synapse-s3-storage-provider",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    py_modules=["s3_storage_provider"],
+    scripts=["scripts/s3_media_upload"],
+    install_requires=[
+        "boto3>=1.9.23,<2.0",
+        "botocore>=1.12.23,<2.0",
+        "humanize>=0.5.1,<0.6",
+        "psycopg2>=2.7.5,<3.0",
+        "PyYAML>=5.4,<7.0",
+        "tqdm>=4.26.0,<5.0",
+        "Twisted",
+    ],
+)
