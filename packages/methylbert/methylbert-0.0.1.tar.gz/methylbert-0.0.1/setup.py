3951864142c4ebca017bc8c244ec8e53062a43b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:07:40 +0000

    Add package methylbert/0.0.1/methylbert-0.0.1.tar.gz

---
+++ packages/methylbert/methylbert-0.0.1.tar.gz/methylbert-0.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import os, glob
+
+setup(name='methylbert',
+	version='0.0.1',
+
+	packages=find_packages("src"),
+	package_dir = {"": "src"},
+
+	install_requires=[
+		'Bio', 
+		'matplotlib',
+		'numpy',
+		'pandas',
+		'pysam',
+		'scikit_learn',
+		'scipy',
+		'torch>=1.10.0',
+		'tqdm',
+		'transformers>=2,<3'
+	],
+
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/*.py")]+ \
+    		   [os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/methylbert/*.py")],
+
+	test_suite="test",
+
+	author='Yunhee Jeong',
+	author_email='y.jeong@dkfz-heidelberg.de',
+
+	# cli
+	entry_points={
+		"console_scripts": [
+			"methylbert = cli:main"
+		]
+	}
+
+)
