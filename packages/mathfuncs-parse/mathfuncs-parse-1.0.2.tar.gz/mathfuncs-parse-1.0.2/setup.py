f2abf385b47c1e15f346b9114b833be912568a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:07:21 +0000

    Add package mathfuncs-parse/1.0.2/mathfuncs-parse-1.0.2.tar.gz

---
+++ packages/mathfuncs-parse/mathfuncs-parse-1.0.2.tar.gz/mathfuncs-parse-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+
+# Define the extension module
+extension_module = Extension(
+    name='mathfuncs_parse',
+    sources=['extern/PyParser.cpp'],  # Add other source files if necessary
+    include_dirs=['extern/pybind11/include', 'include'],
+    extra_compile_args=['-std=c++20'],  # Adjust the C++ standard flag as needed
+)
+
+setup(
+    name='mathfuncs-parse',
+    version='1.0.2',
+    description='A small module for parsing and evaluating expressions of any number of variables',
+    ext_modules=[extension_module],
+)
