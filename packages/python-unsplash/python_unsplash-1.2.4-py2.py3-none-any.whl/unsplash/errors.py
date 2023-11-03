7a60c83301ee8853e4e4cf6828a58d0af02980bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:05:09 +0000

    Add package python-unsplash/1.2.4/python_unsplash-1.2.4-py2.py3-none-any.whl

---
+++ packages/python-unsplash/python_unsplash-1.2.4-py2.py3-none-any.whl/unsplash/errors.py
@@ -0,0 +1,17 @@
+class UnsplashError(Exception):
+    """Unsplash exception"""
+
+    def __init__(self, message, **kwargs):
+        self.message = str(message) if message else "Unknown error"
+        super(UnsplashError, self).__init__(message, **kwargs)
+
+    def __str__(self):
+        return self.message
+
+
+class UnsplashAuthError(UnsplashError):
+
+    def __init__(self, reason, **kwargs):
+        self.reason = reason
+        self.message = "Unsplash Authentication Error: %s " % self.reason
+        super(UnsplashAuthError, self).__init__(message=self.message, **kwargs)
