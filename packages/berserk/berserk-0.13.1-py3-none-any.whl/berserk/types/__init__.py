f96fcb84e3447c476d61911229469bc7dfe76c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 06:09:38 +0000

    Add package berserk/0.13.1/berserk-0.13.1-py3-none-any.whl

---
+++ packages/berserk/berserk-0.13.1-py3-none-any.whl/berserk/types/__init__.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from .account import AccountInformation, Perf, Preferences, Profile, StreamerInfo
+from .bulk_pairings import BulkPairing, BulkPairingGame
+from .challenges import Challenge
+from .common import ClockConfig, ExternalEngine, LightUser, OnlineLightUser, Variant
+from .puzzles import PuzzleRace
+from .opening_explorer import (
+    OpeningExplorerRating,
+    OpeningStatistic,
+    Speed,
+)
+from .team import PaginatedTeams, Team
+from .tournaments import ArenaResult, CurrentTournaments, SwissResult, SwissInfo
+
+__all__ = [
+    "AccountInformation",
+    "ArenaResult",
+    "BulkPairing",
+    "BulkPairingGame",
+    "Challenge",
+    "ClockConfig",
+    "CurrentTournaments",
+    "ExternalEngine",
+    "LightUser",
+    "OnlineLightUser",
+    "OpeningExplorerRating",
+    "OpeningStatistic",
+    "PaginatedTeams",
+    "Perf",
+    "Preferences",
+    "Profile",
+    "PuzzleRace",
+    "Speed",
+    "StreamerInfo",
+    "SwissInfo",
+    "SwissResult",
+    "Team",
+    "Variant",
+]
