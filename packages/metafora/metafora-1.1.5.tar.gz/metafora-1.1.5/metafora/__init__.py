88c7bf1f9371f0dda966bc128e283db019d36207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:05:39 +0000

    Add package metafora/1.1.5/metafora-1.1.5.tar.gz

---
+++ packages/metafora/metafora-1.1.5.tar.gz/metafora-1.1.5/metafora/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.1.5"
+__author__ = 'Ramon Dalmau-Codina'
+
+from metafora.metar import Metar
+from metafora.taf import Taf, Forecast, propagate_forecasts
+from metafora.engineering import simplify_report
+
