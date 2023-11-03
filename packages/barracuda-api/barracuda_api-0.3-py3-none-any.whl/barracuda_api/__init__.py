e29f02e7cfefff253db4840d89212740721c6c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:04:35 +0000

    Add package barracuda-api/0.3/barracuda_api-0.3-py3-none-any.whl

---
+++ packages/barracuda-api/barracuda_api-0.3-py3-none-any.whl/barracuda_api/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__author__ = "Peter Gastinger"
+__copyright__ = "Copyright 2022, OMV"
+__credits__ = ["Peter Gastinger"]
+__license__ = "GPL"
+__version__ = "0.2"
+__maintainer__ = "Peter Gastinger"
+__email__ = "peter.gastinger@omv.at"
+__status__ = "Production"
+
+name = "barracuda_api"
---
+++ packages/barracuda-api/barracuda_api-0.3-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,1 @@
+name = "barracuda_api"
