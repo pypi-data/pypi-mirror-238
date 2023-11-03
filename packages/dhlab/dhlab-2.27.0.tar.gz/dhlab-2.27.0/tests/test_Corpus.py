fddd47c4b024f16e210792781c1391e3636b665d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:06:59 +0000

    Add package dhlab/2.27.0/dhlab-2.27.0.tar.gz

---
+++ packages/dhlab/dhlab-2.27.0.tar.gz/dhlab-2.27.0/tests/test_Corpus.py
@@ -0,0 +1,24 @@
+import dhlab as dh
+
+
+class TestCorpus():
+
+    def test_book_corpus(self):
+        c = dh.Corpus(doctype="digibok")
+        assert len(c.frame) == 10
+        assert "urn" in c.frame.columns
+        
+    def test_news_corpus(self):
+        c = dh.Corpus(doctype="digavis")
+        assert len(c.frame) == 10
+        assert "urn" in c.frame.columns
+        
+    def test_corpus_freq(self):
+        c = dh.Corpus(doctype="digavis", limit=2)
+        freq = c.freq()
+        assert len(freq.frame) > 0 
+        assert len(freq.frame.columns) == 2
+        
+    def test_empty_corpus(self):
+        pass
+    
