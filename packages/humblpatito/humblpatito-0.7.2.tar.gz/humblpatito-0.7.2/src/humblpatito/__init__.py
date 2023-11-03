68edb8c5204310f1dd63324fda76acddb81a2257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 05:07:04 +0000

    Add package humblpatito/0.7.2/humblpatito-0.7.2.tar.gz

---
+++ packages/humblpatito/humblpatito-0.7.2.tar.gz/humblpatito-0.7.2/src/humblpatito/__init__.py
@@ -0,0 +1,53 @@
+"""Patito, a data-modelling library built on top of polars and pydantic."""
+from polars import Expr, Series, col
+
+from patito import exceptions, sql
+from patito.polars import DataFrame, LazyFrame
+from patito.pydantic import Field, Model
+from patito.exceptions import DataFrameValidationError
+
+_CACHING_AVAILABLE = False
+_DUCKDB_AVAILABLE = False
+field = col("_")
+__all__ = [
+    "DataFrame",
+    "DataFrameValidationError",
+    "Expr",
+    "Field",
+    "LazyFrame",
+    "Model",
+    "Series",
+    "_CACHING_AVAILABLE",
+    "_DUCKDB_AVAILABLE",
+    "col",
+    "exceptions",
+    "field",
+    "sql",
+]
+
+try:
+    from patito import duckdb
+
+    _DUCKDB_AVAILABLE = True
+    __all__ += ["duckdb"]
+except ImportError:  # pragma: no cover
+    pass
+
+try:
+    from patito.database import Database
+
+    _CACHING_AVAILABLE = True
+    __all__ += ["Database"]
+except ImportError:
+    pass
+
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
