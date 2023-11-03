89c60614b89cb503a65c1d8ebe0eaac78b8f8b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 06:10:48 +0000

    Add package Python-Algorithm-pyal/1.0.4/Python_Algorithm_pyal-1.0.4-py3-none-any.whl

---
+++ packages/Python-Algorithm-pyal/Python_Algorithm_pyal-1.0.4-py3-none-any.whl/pyal/example/linked_list.py
@@ -0,0 +1,40 @@
+import pyal
+
+
+def display_list(list: pyal.LinkedList):
+  iter = list.begin()
+  while iter is not list.end():
+    print(iter(), end=" ")
+    iter = iter.next()
+  print()
+
+
+def main():
+  list = pyal.LinkedList()
+
+  list.push_back(1)
+  list.push_back(2)
+  list.push_back(3)
+  list.push_back(4)
+  display_list(list)
+
+  list.insert_element(list.begin(), 100)
+  iter = list.begin()
+  print(f"After inserting 100 to the first position, "
+        f"the first element becomes {iter()}")
+
+  last_element = list.pop_back()
+  print(f"The last element '{last_element}' is removed")  # 4
+
+  list.insert_element(list.rbegin(), 0)
+  print(f"Insert 0 before the last element")
+  display_list(list)
+
+  iter = list.rbegin()
+  print(f"The last element is {iter()}")
+  iter = iter.prev()
+  print(f"The element before the last element is {iter()}")
+
+
+if __name__ == "__main__":
+  main()
