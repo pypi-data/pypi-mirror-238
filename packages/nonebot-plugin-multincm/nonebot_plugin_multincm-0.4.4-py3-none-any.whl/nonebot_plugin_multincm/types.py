cf4e995e116070b95a35fabd19d5c5d57b26c4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:06:48 +0000

    Add package nonebot-plugin-multincm/0.4.4/nonebot_plugin_multincm-0.4.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-multincm/nonebot_plugin_multincm-0.4.4-py3-none-any.whl/nonebot_plugin_multincm/types.py
@@ -0,0 +1,140 @@
+# ruff: noqa: A003 N815
+
+from typing import List, Literal, Optional, Union
+
+from pydantic import BaseModel
+
+BrLevelType = Literal["hires", "lossless", "exhigh", "higher", "standard", "none"]
+
+SearchRespModelType = Union["SongSearchResult", "VoiceSearchResult"]
+SongInfoModelType = Union["Song", "VoiceBaseInfo"]
+
+
+class Artist(BaseModel):
+    id: int
+    name: str
+    tns: List[str]
+    alias: List[str]
+
+
+class Album(BaseModel):
+    id: int
+    name: str
+    picUrl: str
+    tns: List[str]
+
+
+class Privilege(BaseModel):
+    id: int
+    pl: int
+    # plLevel: BrLevel
+
+
+class Song(BaseModel):
+    name: str
+    id: int
+    ar: List[Artist]
+    alia: List[str]
+    pop: float
+    al: Album
+    dt: int
+    """歌曲时长，单位 ms"""
+    privilege: Privilege
+
+
+class QcReminder(BaseModel):
+    qcReminderPart: str
+    """纠正内容部分"""
+    highLight: bool
+    """部分是否高亮（可点击纠正）"""
+
+
+class SearchQcReminder(BaseModel):
+    qcReminders: List[QcReminder]
+    qcReminderChannel: str
+
+
+class SongSearchResult(BaseModel):
+    searchQcReminder: Optional[SearchQcReminder]
+    """搜索纠正"""
+    songCount: int
+    songs: Optional[List[Song]]
+
+
+class TrackAudio(BaseModel):
+    id: int
+    url: str
+    br: int
+    size: int
+    md5: str
+    level: Optional[str]
+    encodeType: Optional[str]
+    time: int
+
+
+class User(BaseModel):
+    id: int
+    userid: int
+    nickname: str
+
+
+class Lyric(BaseModel):
+    version: int
+    lyric: str
+
+
+class LyricData(BaseModel):
+    transUser: Optional[User]
+    lyricUser: Optional[User]
+    lrc: Optional[Lyric]
+    tlyric: Optional[Lyric]
+    romalrc: Optional[Lyric]
+
+
+class DJ(BaseModel):
+    userId: int
+    nickname: str
+    avatarUrl: str
+    gender: int
+    signature: str
+    backgroundUrl: str
+
+
+class Radio(BaseModel):
+    id: int
+    name: str
+    picUrl: str
+    desc: str
+    subCount: int
+    programCount: int
+    categoryId: int
+    secondCategoryId: Optional[int]
+    category: str
+    secondCategory: Optional[str]
+    lastProgramId: int
+
+
+class VoiceBaseInfo(BaseModel):
+    id: int
+    mainTrackId: int
+    name: str
+    coverUrl: str
+    description: str
+    dj: DJ
+    radio: Radio
+    duration: int
+    listenerCount: int
+    shareCount: int
+    likedCount: int
+    commentCount: int
+    commentThreadId: str
+
+
+class VoiceResource(BaseModel):
+    baseInfo: VoiceBaseInfo
+
+
+class VoiceSearchResult(BaseModel):
+    resources: Optional[List[VoiceResource]]
+    totalCount: int
+    searchQcReminder: Optional[SearchQcReminder]
