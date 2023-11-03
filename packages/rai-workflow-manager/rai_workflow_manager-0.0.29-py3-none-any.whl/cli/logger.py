331f4126b57d3a71867f374f541d32dabffc5284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 04:07:07 +0000

    Add package rai-workflow-manager/0.0.29/rai_workflow_manager-0.0.29-py3-none-any.whl

---
+++ packages/rai-workflow-manager/rai_workflow_manager-0.0.29-py3-none-any.whl/cli/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+
+def configure(level=logging.INFO) -> logging.Logger:
+    # override default logging level for azure
+    logger = logging.getLogger('azure.core')
+    logger.setLevel(logging.ERROR)
+    logger = logging.getLogger('snowflake')
+    logger.setLevel(logging.ERROR)
+
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s][%(levelname)s][%(name)s] %(message)s', "%H:%M:%S")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger.getChild("cli")
