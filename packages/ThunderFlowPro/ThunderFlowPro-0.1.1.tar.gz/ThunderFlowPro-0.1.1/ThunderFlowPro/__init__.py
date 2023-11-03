8f424e55a68225a8fac118676dd31e2df0e9026e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 03:05:37 +0000

    Add package ThunderFlowPro/0.1.1/ThunderFlowPro-0.1.1.tar.gz

---
+++ packages/ThunderFlowPro/ThunderFlowPro-0.1.1.tar.gz/ThunderFlowPro-0.1.1/ThunderFlowPro/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author: deiro
+"""
+from .ConFunctions import consumption
+from .ConFunctions import create_trajectories
+from .ConFunctions import distance_difference
+from .ConFunctions import MapMatching_Valhalla_traj
+from .ConFunctions import MapMatching_seq_osrm
+from .ConFunctions import MapMatching_Valhalla
+from .ConFunctions import MapMatching_OSRM
