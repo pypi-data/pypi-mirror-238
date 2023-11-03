ae2fd46e7af57782835824858a192a00a1387810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-03 14:08:54 +0000

    Add package jrvc/0.0.1.dev14/jrvc-0.0.1.dev14.tar.gz

---
+++ packages/jrvc/jrvc-0.0.1.dev14.tar.gz/jrvc-0.0.1.dev14/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+def parse_requirements(requirements):
+    with open(requirements) as f:
+        return [l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')]
+
+reqs = parse_requirements("jrvc/requirements.txt")
+
+setup(
+    name='jrvc',
+    description="Libraries for RVC inference",
+    author_email="hugo.gonzalezdev@gmail.com",
+    version='0.0.1dev014',
+    packages=find_packages(),
+    include_package_data=True,
+    author="RVC Community",
+    license="MIT",
+    install_requires=reqs,
+    keywords=["RVC", "Retrieval","Voice", "AI", "Conversion"],
+    python_requires=">=3.8"
+)
+
+#python setup.py sdist bdist_wheel
+#twine upload
